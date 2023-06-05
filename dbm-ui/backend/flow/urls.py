e9eb0f63325e3196c843b6c5dc8589a7c6c2e64a
fanfanyangyang <yushang88@qq.com>
2023-06-05 15:30:28 +0800

    feat(backend): riak_apply_cluster_flow #51

--- dbm-ui/backend/flow/urls.py
+++ dbm-ui/backend/flow/urls.py
@@ -108,6 +108,7 @@ from backend.flow.views.redis_scene import (
     RedisClusterSlaveCutOffSceneApiView,
     RedisInstallDbmonSceneApiView,
 )
+from backend.flow.views.riak_apply import RiakApplySceneApiView
 from backend.flow.views.rollback_pipeline import PipelineTreeApiView, RollbackPipelineApiView
 from backend.flow.views.spider_add_nodes import AddSpiderNodesSceneApiView
 from backend.flow.views.spider_add_tmp_node import AddTmpSpiderSceneApiView
@@ -261,4 +262,6 @@ urlpatterns = [
     # spider 添加
     url(r"^scene/add_spider_nodes$", AddSpiderNodesSceneApiView.as_view()),
     url(r"^scene/tendbcluster_full_backup$", TenDBClusterFullBackupView.as_view()),
+    # riak
+    url(r"^scene/riak_cluster_apply$", RiakApplySceneApiView.as_view()),
 ]
