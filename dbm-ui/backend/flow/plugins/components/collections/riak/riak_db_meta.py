f576ce0f006d815255e55966da863aedc476993b
fanfanyangyang <yushang88@qq.com>
2023-06-05 15:30:28 +0800

    feat(backend): riak_apply_cluster_flow #51

---
+++ dbm-ui/backend/flow/plugins/components/collections/riak/riak_db_meta.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+import copy
+import logging
+from dataclasses import asdict
+
+from django.utils.translation import ugettext as _
+from pipeline.component_framework.component import Component
+
+from backend.flow.plugins.components.collections.common.base_service import BaseService
+from backend.flow.utils.riak.riak_db_meta import RiakDBMeta
+
+logger = logging.getLogger("flow")
+
+
+class RiakDBMetaService(BaseService):
+    """
+    根据Riak单据类型来更新cmdb
+    """
+
+    def _execute(self, data, parent_data) -> bool:
+        kwargs = data.get_one_of_inputs("kwargs")
+        global_data = data.get_one_of_inputs("global_data")
+        trans_data = data.get_one_of_inputs("trans_data")
+
+        riak_meta = RiakDBMeta(ticket_data=global_data, cluster=trans_data)
+
+        result = getattr(riak_meta, kwargs.get("db_meta_class_func"))()
+
+        self.log_info("DBMata re successfully")
+        data.outputs.ext_result = result
+        return result
+
+
+class RiakDBMetaComponent(Component):
+    name = __name__
+    code = "riak_db_meta"
+    bound_service = RiakDBMetaService
