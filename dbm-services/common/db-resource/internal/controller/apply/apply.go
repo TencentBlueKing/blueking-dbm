4365b2ac83276968b91e205221e8139980155505
ymakedaq <996156275@qq.com>
2023-06-13 10:42:19 +0800

    fix: 确认获取资源后,触发归档资源

--- dbm-services/common/db-resource/internal/controller/apply/apply.go
+++ dbm-services/common/db-resource/internal/controller/apply/apply.go
@@ -87,7 +87,21 @@ func (c *ApplyHandler) ConfirmApply(r *gin.Context) {
 		c.SendResponse(r, err, err.Error(), requestId)
 		return
 	}
+	archive(hostIds)
 	c.SendResponse(r, nil, "successful", requestId)
+}
+
+func archive(bkHostIds []int) {
+	var rs []model.TbRpDetail
+	err := model.DB.Self.Table(model.TbRpDetailName()).Where(" bk_host_id in (?) and status =  ", bkHostIds,
+		model.Used).Find(&rs).Error
+	if err != nil {
+		logger.Error("query used resource failed %s", err.Error())
+		return
+	}
+	for _, v := range rs {
+		task.ArchiverResourceChan <- v.ID
+	}
 }
 
 // ApplyResource TODO
