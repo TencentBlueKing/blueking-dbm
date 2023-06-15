d7bbcc5dbd91618e9c397aeadb3ae07ce3e80b14
hlinx <327159425@qq.com>
2023-06-15 20:31:07 +0800

    feat(frontend): 资源池 #71

--- dbm-ui/frontend/src/views/resource-pool/list/components/search-box/components/field-config.ts
+++ dbm-ui/frontend/src/views/resource-pool/list/components/search-box/components/field-config.ts
@@ -113,7 +113,7 @@ export default {
         return true;
       }
       const [min, max] = value;
-      if (min >= max) {
+      if (min && max && min >= max) {
         return '请输入合理的范围值';
       }
       return true;
@@ -128,7 +128,7 @@ export default {
         return true;
       }
       const [min, max] = value;
-      if (min >= max) {
+      if (min && max && min >= max) {
         return '请输入合理的范围值';
       }
       return true;
@@ -143,7 +143,7 @@ export default {
         return true;
       }
       const [min, max] = value;
-      if (min >= max) {
+      if (min && max && min >= max) {
         return '请输入合理的范围值';
       }
       return true;
