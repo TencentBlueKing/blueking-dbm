e44659e48eaf94943017b89bc42315acc22b6c48
yksitu <1297650644@qq.com>
2023-06-15 11:54:00 +0800

    feat: MySQL集群适配is_standby特性；同时删除无用代码 #184
    调整切换类单据，传入slave的is_standby为False跳出异常
    
    fix

--- dbm-ui/backend/flow/engine/bamboo/scene/mysql/common/exceptions.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/mysql/common/exceptions.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
 Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
@@ -8,20 +7,17 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
-from backend.bk_web.models import AuditedModel
-from backend.db_meta.models import StorageInstance
+from backend.exceptions import AppBaseException, ErrorCode
 
 
-class MySQLStorageInstanceExt(AuditedModel):
-    """
-    TenDBHA 和 TenDBCluster 多 slave 用
-    一个 master 实例正常情况下, 有且只有一个 is_stand_by = True 的 slave 同步实例
-    用于 dbha, 迁移等场景的备选目标
-    master 默认为 True
-    """
+class TenDBFlowBaseException(AppBaseException):
+    MODULE_CODE = ErrorCode.FLOW_CODE
+    MESSAGE = _("Flow模块TenDB 异常")
 
-    instance = models.OneToOneField(StorageInstance, on_delete=models.PROTECT)
-    is_stand_by = models.BooleanField(default=True, help_text=_("多 slave 的备选标志"))
+
+class NormalTenDBFlowException(TenDBFlowBaseException):
+    ERROR_CODE = "001"
+    MESSAGE = _("通用异常")
+    MESSAGE_TPL = _("{message}")
