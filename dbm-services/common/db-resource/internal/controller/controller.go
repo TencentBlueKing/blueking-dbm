24a1f88cb5cea4f6d9ecf3b63978cbb061094b82
ymakedaq <996156275@qq.com>
2023-06-30 10:28:35 +0800

    fix(dbm-services): 补充资源池归档表缺少字段 close #348

--- dbm-services/common/db-resource/internal/controller/controller.go
+++ dbm-services/common/db-resource/internal/controller/controller.go
@@ -1,3 +1,13 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 // Package controller TODO
 package controller
 
@@ -41,7 +51,6 @@ func (c *BaseHandler) Prepare(r *gin.Context, schema interface{}) error {
 }
 
 // SendResponse TODO
-// SendResponseT TODO
 func (c *BaseHandler) SendResponse(r *gin.Context, err error, data interface{}, requestId string) {
 	code, message := errno.DecodeErr(err)
 	r.JSON(http.StatusOK, Response{
--- dbm-services/common/db-resource/internal/lock/lock_test.go
+++ dbm-services/common/db-resource/internal/lock/lock_test.go
@@ -8,22 +8,35 @@
  * specific language governing permissions and limitations under the License.
  */
 
-// Package routers TODO
-package routers
+package lock_test
 
 import (
-	"dbm-services/common/db-resource/internal/controller/apply"
-	"dbm-services/common/db-resource/internal/controller/manage"
+	"testing"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"dbm-services/common/db-resource/internal/lock"
+	"dbm-services/common/go-pubpkg/cmutil"
 )
 
-// RegisterRoutes TODO
-func RegisterRoutes(engine *gin.Engine) {
-	// 注册路由
-	apply := apply.ApplyHandler{}
-	apply.RegisterRouter(engine)
-	// 机器资源管理
-	manage := manage.MachineResourceHandler{}
-	manage.RegisterRouter(engine)
+func TestRedisLock(t *testing.T) {
+	t.Log("start testing...")
+	// lock.InitRedisDb()
+	l := lock.NewSpinLock(&lock.RedisLock{
+		Name:    "Tendb",
+		RandKey: cmutil.RandStr(16),
+		Expiry:  10 * time.Second,
+	}, 30, 1*time.Second)
+	for i := 0; i < 20; i++ {
+		go func(j int) {
+			if err := l.Lock(); err != nil {
+				t.Log(j, "lock failed")
+				return
+			}
+			t.Log(j, "lock success")
+			time.Sleep(100 * time.Millisecond)
+			l.Unlock()
+		}(i)
+	}
+
+	time.Sleep(20 * time.Second)
 }
