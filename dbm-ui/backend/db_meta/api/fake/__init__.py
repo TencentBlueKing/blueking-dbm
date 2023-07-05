3b67f017d13d0609fc4cbc0dcb4e1479220c4830
xfwduke <xfwduke@gmail.com>
2023-07-05 12:02:11 +0800

    feat(mysql): dbha api 增加 tendbcluster 支持 close #391

--- dbm-ui/backend/db_meta/api/fake/__init__.py
+++ dbm-ui/backend/db_meta/api/fake/__init__.py
@@ -8,5 +8,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from .fake_tendbcluster import fake_reset_tendbcluster_cluster
 from .fake_tendbha import fake_create_tendbha_cluster, fake_reset_tendbha_cluster
 from .fake_tendbsingle import fake_create_tendbsingle
