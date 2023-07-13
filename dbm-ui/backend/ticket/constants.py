4c30cb42f31242686de55b64cff1bab9639ac336
gaohongsong <ghs@qq.com>
2023-07-13 12:38:58 +0800

    feat: redis主从故障切换

--- dbm-ui/backend/ticket/constants.py
+++ dbm-ui/backend/ticket/constants.py
@@ -194,6 +194,7 @@ class TicketType(str, StructuredEnum):
 
     REDIS_SCALE_UPDOWN = EnumField("REDIS_SCALE_UPDOWN", _("Redis 集群容量变更"))
     REDIS_CLUSTER_CUTOFF = EnumField("REDIS_CLUSTER_CUTOFF", _("Redis 整机替换"))
+    REDIS_MASTER_SLAVE_SWITCH = EnumField("REDIS_MASTER_SLAVE_SWITCH", _("Redis 主从故障切换"))
     PROXY_SCALE_UP = EnumField("PROXY_SCALE_UP", _("Proxy 扩容"))
     PROXY_SCALE_DOWN = EnumField("PROXY_SCALE_DOWN", _("Proxy 缩容"))
     REDIS_NEW_DTS_JOB = EnumField("REDIS_NEW_DTS_JOB", _("Redis 新建DTS任务"))
