3d870744878032c9c9104eda6836b2aca56a911d
fanfanyangyang <yushang88@qq.com>
2023-07-21 15:41:12 +0800

    fix: 修复分区定时任务spider日志表名称 #565

--- dbm-services/mysql/db-partition/service/manage_config.go
+++ dbm-services/mysql/db-partition/service/manage_config.go
@@ -28,7 +28,7 @@ import (
 // GetPartitionsConfig TODO
 func (m *QueryParititionsInput) GetPartitionsConfig() ([]*PartitionConfigWithLog, int64, error) {
 	allResults := make([]*PartitionConfigWithLog, 0)
-	var tbName string
+	var configTb, logTb string
 	// Cnt 用于返回匹配到的行数
 	type Cnt struct {
 		Count int64 `gorm:"column:cnt"`
@@ -36,9 +36,11 @@ func (m *QueryParititionsInput) GetPartitionsConfig() ([]*PartitionConfigWithLog
 	// 判断是mysql集群还是spider集群
 	switch strings.ToLower(m.ClusterType) {
 	case Tendbha, Tendbsingle:
-		tbName = MysqlPartitionConfig
+		configTb = MysqlPartitionConfig
+		logTb = MysqlPartitionCronLogTable
 	case Tendbcluster:
-		tbName = SpiderPartitionConfig
+		configTb = SpiderPartitionConfig
+		logTb = SpiderPartitionCronLogTable
 	default:
 		return nil, 0, errors.New("不支持的db类型")
 	}
@@ -59,8 +61,8 @@ func (m *QueryParititionsInput) GetPartitionsConfig() ([]*PartitionConfigWithLog
 		where = where + tblike
 	}
 	cnt := Cnt{}
-	vsql := fmt.Sprintf("select count(*) as cnt from `%s`", tbName)
-	err := model.DB.Self.Debug().Table(tbName).Raw(vsql).Scan(&cnt).Error
+	vsql := fmt.Sprintf("select count(*) as cnt from `%s`", configTb)
+	err := model.DB.Self.Debug().Table(configTb).Raw(vsql).Scan(&cnt).Error
 	if err != nil {
 		slog.Error(vsql, "execute error", err)
 		return nil, 0, err
@@ -72,15 +74,15 @@ func (m *QueryParititionsInput) GetPartitionsConfig() ([]*PartitionConfigWithLog
 		"d.ticket_id as ticket_id, d.ticket_status as ticket_status, d.check_info as check_info, "+
 		"d.status as status FROM "+
 		"%s AS config LEFT JOIN (SELECT c.*, ticket.status as ticket_status  FROM "+
-		"(SELECT a.* FROM partition_cron_log AS a, "+
+		"(SELECT a.* FROM %s AS a, "+
 		"(SELECT inner_config.id AS config_id, MAX(inner_log.id) AS log_id FROM "+
 		"%s AS inner_config LEFT JOIN "+
-		"partition_cron_log AS inner_log ON inner_config.id = inner_log.config_id where "+
+		"%s AS inner_log ON inner_config.id = inner_log.config_id where "+
 		"inner_log.create_time > DATE_SUB(now(),interval 100 day) GROUP BY inner_config.id) "+
 		"AS b WHERE a.id = b.log_id) AS c LEFT JOIN `%s`.ticket_ticket AS ticket "+
-		"ON ticket.id = c.ticket_id) AS d ON config.id = d.config_id where %s;", tbName, tbName,
+		"ON ticket.id = c.ticket_id) AS d ON config.id = d.config_id where %s;", configTb, logTb, configTb, logTb,
 		viper.GetString("dbm_db_name"), condition)
-	err = model.DB.Self.Debug().Table(tbName).Raw(vsql).Scan(&allResults).Error
+	err = model.DB.Self.Debug().Table(configTb).Raw(vsql).Scan(&allResults).Error
 	if err != nil {
 		slog.Error(vsql, "execute error", err)
 		return nil, 0, err
@@ -91,12 +93,14 @@ func (m *QueryParititionsInput) GetPartitionsConfig() ([]*PartitionConfigWithLog
 // GetPartitionLog TODO
 func (m *QueryLogInput) GetPartitionLog() ([]*PartitionLog, int64, error) {
 	allResults := make([]*PartitionLog, 0)
-	var tbName string
+	var configTb, logTb string
 	switch strings.ToLower(m.ClusterType) {
 	case Tendbha, Tendbsingle:
-		tbName = MysqlPartitionConfig
+		configTb = MysqlPartitionConfig
+		logTb = MysqlPartitionCronLogTable
 	case Tendbcluster:
-		tbName = SpiderPartitionConfig
+		configTb = SpiderPartitionConfig
+		logTb = SpiderPartitionCronLogTable
 	default:
 		return nil, 0, errors.New("不支持的db类型")
 	}
@@ -107,19 +111,19 @@ func (m *QueryLogInput) GetPartitionLog() ([]*PartitionLog, int64, error) {
 	vsql := fmt.Sprintf("select logs.*,ticket.status as ticket_status from "+
 		"(select config.id as id, log.ticket_id as ticket_id, log.create_time as execute_time, "+
 		"log.check_info as check_info, log.status as status "+
-		"from %s as config join partition_cron_log as log "+
+		"from %s as config join %s as log "+
 		"where log.config_id=config.id and config.id=%d and log.create_time>"+
 		"DATE_SUB(now(),interval 100 day)) as logs left join "+
 		"`%s`.ticket_ticket as ticket on ticket.id=logs.ticket_id where "+
 		"ticket.create_at > DATE_SUB(now(),interval 100 day)) "+
-		"order by execute_time desc ", tbName, m.ConfigId, viper.GetString("dbm_db_name"))
-	err := model.DB.Self.Debug().Table(tbName).Raw(vsql).Scan(&allResults).Error
+		"order by execute_time desc ", configTb, logTb, m.ConfigId, viper.GetString("dbm_db_name"))
+	err := model.DB.Self.Debug().Table(configTb).Raw(vsql).Scan(&allResults).Error
 	if err != nil {
 		return nil, 0, err
 	}
 	cnt := Cnt{}
 	countSQL := fmt.Sprintf("select count(*) as cnt from (%s) c", vsql)
-	err = model.DB.Self.Debug().Table(tbName).Raw(countSQL).Scan(&cnt).Error
+	err = model.DB.Self.Debug().Table(configTb).Raw(countSQL).Scan(&cnt).Error
 	if err != nil {
 		return nil, 0, err
 	}
