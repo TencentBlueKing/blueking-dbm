b63632989854467c42f88afdae39012096aeef7b
ymakedaq <996156275@qq.com>
2023-08-02 17:25:24 +0800

    refactor(dbm-services): 优化记录模拟执行请求日志 close #686

--- dbm-services/common/db-resource/internal/middleware/middleware.go
+++ dbm-services/common/db-resource/internal/middleware/middleware.go
@@ -1,10 +1,20 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 // Package middleware TODO
 package middleware
 
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"time"
 
@@ -64,7 +74,7 @@ func ApiLogger(c *gin.Context) {
 		}
 		var bodyBytes []byte
 		// read from the original request body
-		bodyBytes, err := ioutil.ReadAll(c.Request.Body)
+		bodyBytes, err := io.ReadAll(c.Request.Body)
 		if err != nil {
 			return
 		}
@@ -72,7 +82,7 @@ func ApiLogger(c *gin.Context) {
 			bodyBytes = []byte("{}")
 		}
 		// create a new buffer and replace the original request body
-		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 		if err := model.CreateTbRequestLog(model.TbRequestLog{
 			RequestID:   rid,
 			RequestUser: "",
