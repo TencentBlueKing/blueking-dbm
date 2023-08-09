6c4d7509c546d5094a56bffba0cf97ad7de04685
ymakedaq <996156275@qq.com>
2023-08-09 10:08:55 +0800

    feat(dbm-services): 语法分析时，提前记录TendbCluster DDL的库表名 close #715

--- dbm-services/common/go-pubpkg/bkrepo/bkrepo.go
+++ dbm-services/common/go-pubpkg/bkrepo/bkrepo.go
@@ -1,3 +1,13 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 // Package bkrepo TODO
 package bkrepo
 
@@ -63,6 +73,11 @@ func (b *BkRepoClient) getBaseUrl() string {
 	uri := u.ResolveReference(r).String()
 	logger.Info("uri:%s", uri)
 	return uri
+}
+
+// Upload TODO
+func (b *BkRepoClient) Upload(filepath, filname, repopath string) (*BkRepoRespone, error) {
+	return UploadDirectToBkRepo(filepath, b.getBaseUrl()+path.Join("/", repopath, filname), b.BkRepoUser, b.BkRepoPwd)
 }
 
 // Download 从制品库下载文件
@@ -265,7 +280,11 @@ func UploadDirectToBkRepo(filepath string, targetURL string, username string, pa
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("返回码非200 %d", resp.StatusCode)
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("返回码非200 http code: %d,body:%s\n", resp.StatusCode, string(b))
 	}
 	var baseResp BkRepoRespone
 	if err = json.NewDecoder(resp.Body).Decode(&baseResp); err != nil {
--- dbm-services/mysql/db-simulation/router/router.go
+++ dbm-services/mysql/db-simulation/router/router.go
@@ -1,3 +1,13 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 // Package router TODO
 package router
 
@@ -20,6 +30,7 @@ func RegisterRouter(engine *gin.Engine) {
 	s := engine.Group("/syntax")
 	s.POST("/check/file", handler.SyntaxCheckFile)
 	s.POST("/check/sql", handler.SyntaxCheckSQL)
+	s.POST("/upload/ddl/tbls", handler.CreateAndUploadDDLTblListFile)
 	// rule
 	r := engine.Group("/rule")
 	r.POST("/manage", handler.ManageRule)
@@ -29,7 +40,6 @@ func RegisterRouter(engine *gin.Engine) {
 	sp := engine.Group("/spider")
 	sp.POST("/simulation", handler.SpiderClusterSimulation)
 	sp.POST("/create", handler.CreateTmpSpiderPodCluster)
-
 }
 
 // TurnOnDebug TODO
