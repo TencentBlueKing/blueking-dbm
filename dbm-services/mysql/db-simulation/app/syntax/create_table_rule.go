dad60ddccafb3e454e8712ccc3f21fdfcaf744c8
ymakedaq <996156275@qq.com>
2023-08-29 16:44:29 +0800

    refactor(dbm-services): 优化库表名称合法检查 close #917

--- dbm-services/mysql/db-simulation/app/syntax/create_table_rule.go
+++ dbm-services/mysql/db-simulation/app/syntax/create_table_rule.go
@@ -12,7 +12,6 @@ package syntax
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
 	"dbm-services/common/go-pubpkg/cmutil"
@@ -23,6 +22,16 @@ import (
 // SpiderChecker TODO
 func (c CreateTableResult) SpiderChecker(spiderVersion string) (r *CheckerResult) {
 	r = &CheckerResult{}
+	if R.BuiltInRule.TableNameSpecification.KeyWord {
+		r.ParseBultinBan(func() (bool, string) {
+			return KeyWordValidator(spiderVersion, c.TableName)
+		})
+	}
+	if R.BuiltInRule.TableNameSpecification.SpeicalChar {
+		r.ParseBultinBan(func() (bool, string) {
+			return SpecialCharValidator(c.TableName)
+		})
+	}
 	r.Parse(SR.SpiderCreateTableRule.CreateTbLike, !c.IsCreateTableLike, "")
 	r.Parse(SR.SpiderCreateTableRule.CreateWithSelect, !c.IsCreateTableSelect, "")
 	r.Parse(SR.SpiderCreateTableRule.ColChasetNotEqTbChaset, c.ColCharsetEqTbCharset(), "")
@@ -44,11 +53,15 @@ func (c CreateTableResult) Checker(mysqlVersion string) (r *CheckerResult) {
 	r = &CheckerResult{}
 	r.Parse(R.CreateTableRule.SuggestEngine, c.GetEngine(), "")
 	r.Parse(R.CreateTableRule.SuggestBlobColumCount, c.BlobColumCount(), "")
-	// 检查表名规范
-	// R.CreateTableRule.NormalizedName指明yaml文件中的键，根据键获得item 进而和 val比较
-	etypesli, charsli := NameCheck(c.TableName, mysqlVersion)
-	for i, etype := range etypesli {
-		r.Parse(R.CreateTableRule.NormalizedName, etype, charsli[i])
+	if R.BuiltInRule.TableNameSpecification.KeyWord {
+		r.ParseBultinBan(func() (bool, string) {
+			return KeyWordValidator(mysqlVersion, c.TableName)
+		})
+	}
+	if R.BuiltInRule.TableNameSpecification.SpeicalChar {
+		r.ParseBultinBan(func() (bool, string) {
+			return SpecialCharValidator(c.TableName)
+		})
 	}
 	return
 }
@@ -194,44 +207,4 @@ func (c CreateTableResult) ColCharsetEqTbCharset() bool {
 		return true
 	}
 	return false
-}
-
-// NameCheck TODO
-func NameCheck(name string, mysqlVersion string) (etypesli, charsli []string) {
-	reservesmap := getKewords(mysqlVersion)
-	etypesli = []string{}
-	charsli = []string{}
-	if _, ok := reservesmap[name]; ok {
-		etypesli = append(etypesli, "Keyword_exception")
-		charsli = append(charsli, fmt.Sprintf("。库表名中包含了MySQL关键字: %s。请避免使用这些关键字！", name))
-	}
-	if regexp.MustCompile(`[￥$!@#%^&*()+={}\[\];:'"<>,.?/\\| ]`).MatchString(name) {
-		chars := regexp.MustCompile(`[￥$!@#%^&*()+={}\[\];:'"<>,.?/\\| ]`).FindAllString(name, -1)
-		etypesli = append(etypesli, "special_char")
-		charsli = append(charsli, fmt.Sprintf("。库表名中包含以下特殊字符: %s。请避免在库表名称中使用这些特殊字符！", chars))
-	}
-	if regexp.MustCompile(`^[_]`).MatchString(name) {
-		etypesli = append(etypesli, "first_char_exception")
-		charsli = append(charsli, "。首字符不规范，请使用尽量字母或数字作为首字母")
-	}
-	return etypesli, charsli
-}
-
-func getKewords(mysqlVersion string) (keywordsmap map[string]string) {
-	var keysli []string
-	switch mysqlVersion {
-	case "mysql5.6":
-		keysli = MySQL56_KEYWORD
-	case "mysql5.7":
-		keysli = MySQL57_KEYWORD
-	case "mysql8.0":
-		keysli = MySQL80_KEYWORD
-	default:
-		keysli = ALL_KEYWORD
-	}
-	keywordsmap = map[string]string{}
-	for _, key := range keysli {
-		keywordsmap[key] = ""
-	}
-	return keywordsmap
 }
