fa54b61d9cda0c864125337771f3a01cc36998cb
austinqli <1344583166@qq.com>
2023-08-29 20:15:21 +0800

    feat(frontend): 监控告警功能 #926

---
+++ dbm-ui/frontend/src/views/monitor-alarm-db/routes.ts
@@ -0,0 +1,35 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+*/
+
+import type { RouteRecordRaw } from 'vue-router';
+
+import { MainViewRouteNames } from '@views/main-views/common/const';
+
+import { t } from '@locales/index';
+
+const routes: RouteRecordRaw[] = [
+  {
+    name: 'DBMonitorAlarmGroup',
+    path: 'alarm-group',
+    meta: {
+      routeParentName: MainViewRouteNames.Database,
+      navName: t('告警组'),
+      isMenu: true,
+    },
+    component: () => import('@views/monitor-alarm-db/alarm-group/Index.vue'),
+  },
+];
+
+export default function getRoutes() {
+  return routes;
+}
