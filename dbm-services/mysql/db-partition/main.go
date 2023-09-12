842714b8bf393d02052fc6125fcfa47fe5392620
fanfanyangyang <yushang88@qq.com>
2023-09-12 16:20:53 +0800

    feat: 新增调整分区单据名称_调整监控配置获取方式的功能 #1053

--- dbm-services/mysql/db-partition/main.go
+++ dbm-services/mysql/db-partition/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"os"
-
 	"dbm-services/mysql/db-partition/assests"
 	"dbm-services/mysql/db-partition/cron"
 	"dbm-services/mysql/db-partition/model"
+	"dbm-services/mysql/db-partition/monitor"
 	"dbm-services/mysql/db-partition/router"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-migrate/migrate/v4"
@@ -22,6 +25,31 @@ func main() {
 		if err := assests.DoMigrateFromEmbed(); err != nil && err != migrate.ErrNoChange {
 			slog.Error("migrate fail", err)
 			os.Exit(0)
+		}
+	}
+
+	// 获取监控配置，多次尝试
+	i := 1
+	for ; i <= 10; i++ {
+		setting, err := monitor.GetMonitorSetting()
+		if err != nil {
+			slog.Error(fmt.Sprintf("try %d time", i), "get monitor setting error", err)
+			if i == 10 {
+				slog.Error("try too many times")
+				os.Exit(0)
+			}
+			time.Sleep(3 * time.Second)
+		} else {
+			// for test
+			slog.Info("msg", "monitor setting", setting)
+			viper.Set("monitor.service", setting.MonitorService)
+			// 蓝鲸监控自定义事件
+			viper.Set("monitor.event.data_id", setting.MonitorEventDataID)
+			viper.Set("monitor.event.access_token", setting.MonitorEventAccessToken)
+			// 蓝鲸监控自定义指标
+			viper.Set("monitor.metric.data_id", setting.MonitorMetricDataID)
+			viper.Set("monitor.metric.access_token", setting.MonitorMetricAccessToken)
+			break
 		}
 	}
 
@@ -42,6 +70,9 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.Use(gin.Recovery())
+	r.Handle(http.MethodGet, "/ping", func(context *gin.Context) {
+		context.String(http.StatusOK, "pong")
+	})
 	router.RegisterRouter(r)
 	if err = r.Run(viper.GetString("listen_address")); err != nil {
 		slog.Error("register router fail:", err)
