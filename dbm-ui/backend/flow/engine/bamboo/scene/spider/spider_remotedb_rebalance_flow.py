e3ce3c5ec3004390dc48578697db2b262558ff7f
zfrendo <135297126+zfrendo@users.noreply.github.com>
2023-09-14 16:09:01 +0800

    feat(mysql): tendbha 从库重建/主从迁移接入新备份系统 #996

--- dbm-ui/backend/flow/engine/bamboo/scene/spider/spider_remotedb_rebalance_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/spider/spider_remotedb_rebalance_flow.py
@@ -201,13 +201,9 @@ class TenDBRemoteRebalanceFlow(object):
                 ins_cluster["slave_ip"] = node["slave"]["ip"]
                 ins_cluster["master_port"] = node["master"]["port"]
                 ins_cluster["slave_port"] = node["slave"]["port"]
-                # todo 正式环境放开file_target_path,需要备份接口支持自动创建目录
-                # ins_cluster["file_target_path"] = "/data/dbbak/{}/{}"\
-                #     .format(self.root_id, ins_cluster["new_master_port"])
-                ins_cluster["file_target_path"] = "/home/mysql/install"
+                ins_cluster["file_target_path"] = f"/data/dbbak/{self.root_id}/{ins_cluster['master_port']}"
                 ins_cluster["shard_id"] = shard_id
                 ins_cluster["change_master_force"] = False
-
                 ins_cluster["backupinfo"] = backup_info["remote_node"].get(shard_id, {})
                 # 判断 remote_node 下每个分片的备份信息是否正常
                 if (
---
+++ dbm-ui/backend/flow/views/mysql_restore_slave_remote.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+import logging
+import uuid
+
+from django.utils.translation import ugettext as _
+from rest_framework.response import Response
+
+from backend.flow.engine.controller.mysql import MySQLController
+from backend.flow.views.base import FlowTestView
+
+logger = logging.getLogger("root")
+
+
+class RestoreMysqlSlaveRemoteSceneApiView(FlowTestView):
+    """
+    {
+        tenDB slave 恢复流程编排
+                {
+        "uid":"2022051612120001",
+        "created_by":"xxxx",
+        "bk_biz_id":"152",
+        "module":1,
+        "ticket_type":"MYSQL_RESTORE_SLAVE",
+        "infos":[
+            {
+                "cluster_ids":[
+                    1,2,3,4
+                ],
+                # 表示重建后是否强制卸载旧实例
+                "force":false,
+                "old_slave_ip":"xxx",
+                "new_slave_ip":xxx",
+                "backup_source":"local"
+            }
+        ]
+        }
+    """
+
+    def post(self, request):
+        logger.info(_("开始重建slave"))
+        root_id = uuid.uuid1().hex
+        logger.info("define root_id: {}".format(root_id))
+        request.data["add_slave_only"] = False
+        mysql_controller_new = MySQLController(root_id, request.data)
+        mysql_controller_new.mysql_restore_slave_remote_scene()
+        return Response({"root_id": root_id})
