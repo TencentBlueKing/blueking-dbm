3c62b1b42915e3cacfb2cb934c60ecf70c5129ce
ymakedaq <996156275@qq.com>
2023-09-15 10:50:54 +0800

    refactor(dbm-services): 优化资源导入磁盘识别 close #1096

--- dbm-services/common/db-resource/internal/svr/bk/get_block_info.sh
+++ dbm-services/common/db-resource/internal/svr/bk/get_block_info.sh
@@ -54,69 +54,61 @@ do
     then
         continue
     fi
-    #echo  -n "${dname}: "
+
+    raiddev=false
+        ## 是否存在磁盘做了软raid
+    if [ -e /proc/mdstat ];then
+        IFS=' ' read -ra array <<< `cat  /proc/mdstat |grep md|awk -F: '{print $2}'| sed 's/\[[0-9]\]//g'`
+        for raidname in "${array[@]}"
+        do
+            if [ ${dname} == ${raidname} ];then
+                raiddev=true
+                break
+            fi
+        done
+    fi
+    if [ "$raiddev" = true ];then
+        continue
+    fi
+    # echo  -n "${dname}: "
     diskType=`getDiskType ${dname}`
-    #echo "${diskType}"
+    # echo "${diskType}"
     rotational=`cat ${SYSBLOCK_DIR}/${dname}/queue/rotational`
-    #echo ${rotational}
+    # echo ${rotational}
     if [ ! $rotational == 1 ]
     then
         diskType="SSD"
     fi
-
-    if [[ ${dname} =~ nvme && ${dname} != nvme ]]  || [[ ${dname} =~  ^md[0-9] ]] || [[ ${dname} == "fioa" ]] 
-    then
-        mp=`getMountPoint ${dname}`
-        sz=`cat ${SYSBLOCK_DIR}/${dname}/size`
-        sz=$((sz+0)) 
-        totalSize=`expr $sz \* 512 / 1024 / 1024 / 1024`   
-        sft=`getSysFileType ${dname}`
-        #echo -n "{\"MountPoint\":\"${mp}\",\"Size\":${totalSize},\"FileType\":\"${sft}\",\"DiskType\":\"${diskType}\"}"
-        if [ -f ${SYSBLOCK_DIR}/${dname}/serial ]
-        then
-            diskId=`cat  ${SYSBLOCK_DIR}/${dname}/serial`
-        fi
-        tmp_arr[${#tmp_arr[*]}]="{\"mount_point\":\"${mp}\",\"size\":${totalSize},\"file_type\":\"${sft}\",\"disk_type\":\"${diskType}\",\"disk_id\":\"${diskId}\"}"
-        continue
-   fi
-
-    pt=0
-    for pname in `ls  ${SYSBLOCK_DIR}/${dname}`
-    do
-        if [[  ! ${pname} =~ ^${dname} ]]
-        then
-            continue   
-        fi
-        pt=$((pt+1))
-        mp=`getMountPoint ${pname}`
-        sft=`getSysFileType ${pname}` 
-        sz=`cat ${SYSBLOCK_DIR}/${dname}/${pname}/size`     
-        sz=$((sz+0)) 
-        totalSize=`expr $sz \* 512 / 1024 / 1024 / 1024  `   
-        if [[ ! ${mp} =~ data ]]
-        then
-            continue
-        fi
-        if [ -f ${SYSBLOCK_DIR}/${dname}/serial ]
-        then
-            diskId=`cat  ${SYSBLOCK_DIR}/${dname}/serial`
-        fi
-        tmp_arr[${#tmp_arr[*]}]="{\"mount_point\":\"${mp}\",\"size\":${totalSize},\"file_type\":\"${sft}\",\"disk_type\":\"${diskType}\",\"disk_id\":\"${diskId}\"}"
-    done
-    if [[ $pt == 0 ]];
-    then
-        mp=`getMountPoint ${dname}`
-        sz=`cat ${SYSBLOCK_DIR}/${dname}/size`
-        sz=$((sz+0)) 
-        totalSize=`expr $sz \* 512 / 1024 / 1024 / 1024`   
-        sft=`getSysFileType ${dname}`
-        #echo -n "{\"MountPoint\":\"${mp}\",\"Size\":${totalSize},\"FileType\":\"${sft}\",\"DiskType\":\"${diskType}\"}"
-        if [ -f ${SYSBLOCK_DIR}/${dname}/serial ]
-        then
-            diskId=`cat  ${SYSBLOCK_DIR}/${dname}/serial`
-        fi
-        tmp_arr[${#tmp_arr[*]}]="{\"mount_point\":\"${mp}\",\"size\":${totalSize},\"file_type\":\"${sft}\",\"disk_type\":\"${diskType}\",\"disk_id\":\"${diskId}\"}"
+    partitioned=false
+    parttions=($(lsblk |grep $dname|grep  part|awk -F"└─|-" '{print $2}'|awk '{print $1}'))
+    if [ ${#parttions[*]} -ge 1 ];then
+        partitioned=true
+    else
+        parttions=(${dname})
     fi
+    for part in ${parttions[*]}
+    do
+                    # 不采集根目录磁盘的大小
+                    mp=`getMountPoint ${part}`
+                    if [ ${mp} == "/" ];then
+                        continue
+                    fi
+
+                    if [ "$partitioned" = true ];then
+                        sz=`cat ${SYSBLOCK_DIR}/${dname}/${part}/size`
+                    else
+                        sz=`cat ${SYSBLOCK_DIR}/${part}/size`
+                    fi
+                    sz=$((sz+0)) 
+                    totalSize=`expr $sz \* 512 / 1024 / 1024 / 1024`   
+                    sft=`getSysFileType ${part}`
+                    if [ -f ${SYSBLOCK_DIR}/${dname}/serial ]
+                    then
+                        diskId=`cat  ${SYSBLOCK_DIR}/${dname}/serial`
+                    fi
+                    tmp_arr[${#tmp_arr[*]}]="{\"mount_point\":\"${mp}\",\"size\":${totalSize},\"file_type\":\"${sft}\",\"disk_type\":\"${diskType}\",\"disk_id\":\"${diskId}\"}"
+                    continue
+    done
 done
 
 
