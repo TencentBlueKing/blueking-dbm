0babb791b65bc5348d9d05a30461905dc7aa7f43
austinqli <1344583166@qq.com>
2023-09-19 15:58:27 +0800

    fix(frontend): spider工具箱自测问题 #902

--- dbm-ui/frontend/src/views/spider-manage/add-mnt/pages/page1/components/RenderData/RenderHost.vue
+++ dbm-ui/frontend/src/views/spider-manage/add-mnt/pages/page1/components/RenderData/RenderHost.vue
@@ -48,7 +48,9 @@
       <div
         class="edit-btn"
         @click="handleShowIpSelector">
-        <DbIcon type="host-select" />
+        <div class="edit-btn-inner">
+          <DbIcon type="host-select" />
+        </div>
       </div>
     </BkPopover>
   </div>
@@ -161,15 +163,26 @@
     .edit-btn{
       display: flex;
       width: 24px;
-      height: 24px;
-      cursor: pointer;
-      border-radius: 2px;
+      height: 40px;
       align-items: center;
       justify-content: center;
+      background-color: #fafbfd;
 
-      &:hover {
-        background: #F0F1F5;
+      .edit-btn-inner {
+        display: flex;
+        width: 24px;
+        height: 24px;
+        cursor: pointer;
+        border-radius: 2px;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+          background: #F0F1F5;
+        }
       }
+
+
     }
   }
 </style>
