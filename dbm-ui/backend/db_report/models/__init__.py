aa3fdefe7380e749ce24685b953bd61a41205e0b
fanfanyangyang <yushang88@qq.com>
2023-10-12 19:06:48 +0800

    feat: 新增checksum巡检报告的功能 #1349

--- dbm-ui/backend/db_report/models/__init__.py
+++ dbm-ui/backend/db_report/models/__init__.py
@@ -8,4 +8,5 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from .checksum_check_report import ChecksumCheckReport, ChecksumInstance
 from .meta_check_report import MetaCheckReport
