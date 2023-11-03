be63105bdde60d3438f2087c547d41c8805db29b
seanlook <seanlook7@gmail.com>
2023-11-03 15:39:55 +0800

    feat(mysql): mysql binlog time parser修复 close #1656

--- dbm-services/mysql/db-tools/dbactuator/pkg/components/mysql/parse_binlog_time.go
+++ dbm-services/mysql/db-tools/dbactuator/pkg/components/mysql/parse_binlog_time.go
@@ -3,7 +3,10 @@ package mysql
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"path/filepath"
+
+	"github.com/pkg/errors"
 
 	"dbm-services/common/go-pubpkg/cmutil"
 	binlogParser "dbm-services/mysql/db-tools/mysql-rotatebinlog/pkg/binlog-parser"
@@ -45,6 +48,7 @@ func (t *BinlogTimeComp) Init() error {
 
 // Start TODO
 func (t *BinlogTimeComp) Start() error {
+	var errs []error
 	for _, f := range t.Params.BinlogFiles {
 		filename := filepath.Join(t.Params.BinlogDir, f)
 		if err := cmutil.FileExistsErr(filename); err != nil {
@@ -52,11 +56,24 @@ func (t *BinlogTimeComp) Start() error {
 			continue
 		}
 		if events, err := t.Params.parser.GetTime(filename, true, true); err != nil {
-			fmt.Printf("%s: %v\n", filename, err)
+			fmt.Fprintf(os.Stderr, "%s: %v\n", filename, err)
+			errs = append(errs, err)
 		} else {
 			b, _ := json.Marshal(events)
 			fmt.Printf("%s: %s\n", filename, b)
 		}
 	}
+	if len(errs) > 0 {
+		return errors.New("parse error")
+	}
 	return nil
+}
+
+func (t *BinlogTimeComp) ExampleOutput() interface{} {
+	return []binlogParser.BinlogEventHeaderWrapper{
+		{EventType: "FormatDescriptionEvent", EventSize: 137, EventTime: "2022-06-20 15:17:47",
+			Timestamp: 1655709467, ServerID: 111},
+		{EventType: "RotateEvent", EventSize: 49, EventTime: "2022-07-07 23:38:36",
+			Timestamp: 1657208316, ServerID: 111},
+	}
 }
