5ea9123edfcbfa8574c28928b43ef70d9ac461a9
durant <826035498@qq.com>
2023-11-08 11:27:16 +0800

    feat(other): 保证编译md5一致性 #1690

--- dbm-services/redis/db-tools/dbmon/package.sh
+++ dbm-services/redis/db-tools/dbmon/package.sh
@@ -7,9 +7,9 @@ make build
 
 cd build
 
-version=$(./bk-dbmon -v | awk '{print $2}')
+version=$(./bk-dbmon -v | awk '{print $2}') || ""
 targetDir="bk-dbmon-$version"
-tarName="$targetDir.tar.gz"
+tarName="$targetDir.tar"
 
 if [[ ! -d $targetDir ]]; then
     mkdir -p $targetDir
@@ -24,6 +24,9 @@ if [[ -e $tarName ]]; then
     rm -rf $tarName
 fi
 
-tar --remove-files -zcf $tarName $targetDir
+# 为了保证 tar 压缩得到的包的 md5 一致，这里修改文件的时间戳，同时把 tar -zcf 拆为 tar -cf && gzip
+find ${targetDir} -exec touch -t 202301010000 {} +
+tar --numeric-owner -cvf ${tarName} $targetDir
+gzip -n -f ${tarName}
 
 echo "$tarName success"
