7bca63bf206784f379137ca54b15f3bf3fad1f1f
xfwduke <xfwduke@gmail.com>
2023-11-23 16:46:37 +0800

    fix(mysql): 调整tendbha全备参数保持和tendbcluster一致 close #2056

--- dbm-ui/backend/flow/engine/bamboo/scene/mysql/mysql_ha_full_backup_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/mysql/mysql_ha_full_backup_flow.py
@@ -57,17 +57,19 @@ class MySQLHAFullBackupFlow(object):
         "infos": {
             "backup_type": enum of backend.flow.consts.MySQLBackupTypeEnum
             "file_tag": enum of backend.flow.consts.MySQLBackupFileTagEnum
-            "cluster_ids": List[int],
-            "backup_local": enum of backend.db_meta.enum.InstanceInnerRole
+            "clusters": [{"cluster_id":"", "backup_local": enum []}]
         }
         }
         """
-        cluster_ids = self.data["infos"]["cluster_ids"]
+        clusters = self.data["infos"]["clusters"]
 
         backup_pipeline = Builder(root_id=self.root_id, data=self.data)
 
         sub_pipes = []
-        for cluster_id in cluster_ids:
+        for cluster in clusters:
+            cluster_id = cluster["cluster_id"]
+            backup_local = cluster["backup_local"]
+
             try:
                 cluster_obj = Cluster.objects.get(
                     pk=cluster_id, bk_biz_id=self.data["bk_biz_id"], cluster_type=ClusterType.TenDBHA.value
@@ -77,9 +79,9 @@ class MySQLHAFullBackupFlow(object):
                     cluster_type=ClusterType.TenDBHA.value, cluster_id=cluster_id, immute_domain=""
                 )
 
-            if self.data["infos"]["backup_local"] == InstanceInnerRole.MASTER.value:
+            if backup_local == InstanceInnerRole.MASTER.value:
                 backend_obj = cluster_obj.storageinstance_set.get(instance_inner_role=InstanceInnerRole.MASTER.value)
-            elif self.data["infos"]["backup_local"] == InstanceInnerRole.SLAVE.value:
+            elif backup_local == InstanceInnerRole.SLAVE.value:
                 try:
                     backend_obj = cluster_obj.storageinstance_set.get(
                         instance_inner_role=InstanceInnerRole.SLAVE.value, is_stand_by=True
@@ -87,7 +89,7 @@ class MySQLHAFullBackupFlow(object):
                 except ObjectDoesNotExist:
                     raise DBMetaBaseException(message=_("{} standby slave 不存在".format(cluster_obj.immute_domain)))
             else:
-                raise MySQLBackupLocalException(msg=_("不支持的备份位置 {}".format(self.data["infos"]["backup_local"])))
+                raise MySQLBackupLocalException(msg=_("不支持的备份位置 {}".format(backup_local)))
 
             sub_pipe = SubBuilder(
                 root_id=self.root_id,
