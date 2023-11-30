974ab6003a277ae28cfdd5e2c569ef81a3fafca8
gaohongsong <gmaclinuxer@gmail.com>
2023-11-30 10:13:53 +0800

    fix(backend): 实例搜索 close #2204

--- dbm-ui/backend/db_services/quick_search/handlers.py
+++ dbm-ui/backend/db_services/quick_search/handlers.py
@@ -8,10 +8,10 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from django.db.models import Q
+from django.db.models import F, Q
 
 from backend.db_meta.enums import ClusterType
-from backend.db_meta.models import Cluster, Machine, StorageInstance
+from backend.db_meta.models import Cluster, Machine, ProxyInstance, StorageInstance
 from backend.db_services.quick_search.constants import FilterType, ResourceType
 from backend.flow.models import FlowTree
 
@@ -22,7 +22,7 @@ class QSearchHandler(object):
         self.db_types = db_types
         self.resource_types = resource_types
         self.filter_type = filter_type
-        self.limit = 20
+        self.limit = 5
 
         # db_type -> cluster_type
         self.cluster_types = []
@@ -40,13 +40,20 @@ class QSearchHandler(object):
 
         return result
 
-    def common_filter(self, objs):
+    def common_filter(self, objs, return_type="list", fields=None):
+        """
+        return_type: list | objects
+        """
         if self.bk_biz_ids:
             objs = objs.filter(bk_biz_id__in=self.bk_biz_ids)
         if self.db_types:
             objs = objs.filter(cluster_type__in=self.cluster_types)
 
-        return list(objs[: self.limit].values())
+        if return_type == "objects":
+            return objs
+
+        fields = fields or []
+        return list(objs[: self.limit].values(*fields))
 
     def filter_cluster_name(self, keyword):
         """过滤集群名"""
@@ -75,8 +82,43 @@ class QSearchHandler(object):
         else:
             qs = Q(machine__ip__contains=keyword) | Q(name__contains=keyword)
 
-        objs = StorageInstance.objects.filter(qs)
-        return self.common_filter(objs)
+        if self.bk_biz_ids:
+            qs = Q(bk_biz_id__in=self.bk_biz_ids) & qs
+
+        if self.db_types:
+            qs = Q(cluster_type__in=self.cluster_types) & qs
+
+        objs = (
+            StorageInstance.objects.filter(qs)
+            .annotate(
+                role=F("instance_role"),
+                cluster_id=F("cluster__id"),
+                cluster_domain=F("cluster__immute_domain"),
+                ip=F("machine__ip"),
+            )
+            .union(
+                ProxyInstance.objects.filter(qs).annotate(
+                    role=F("access_layer"),
+                    cluster_id=F("cluster__id"),
+                    cluster_domain=F("cluster__immute_domain"),
+                    ip=F("machine__ip"),
+                )
+            )
+        )
+
+        return objs[: self.limit].values(
+            "id",
+            "name",
+            "bk_biz_id",
+            "cluster_id",
+            "role",
+            "ip",
+            "port",
+            "machine_type",
+            "machine_id",
+            "status",
+            "phase",
+        )
 
     def filter_task(self, keyword):
         """过滤任务"""
@@ -92,7 +134,7 @@ class QSearchHandler(object):
             objs = objs.filter(bk_biz_id__in=self.bk_biz_ids)
 
         # TODO: db类型任务的过滤
-        return list(objs[: self.limit].values())
+        return list(objs[: self.limit].values("uid", "bk_biz_id", "ticket_type", "root_id", "status", "created_by"))
 
     def filter_machine(self, keyword):
         """过滤主机"""
@@ -103,4 +145,17 @@ class QSearchHandler(object):
             qs = Q(ip__contains=keyword)
 
         objs = Machine.objects.filter(qs)
-        return self.common_filter(objs)
+        return self.common_filter(
+            objs,
+            fields=[
+                "bk_biz_id",
+                "bk_host_id",
+                "ip",
+                "cluster_type",
+                "spec_id",
+                "db_module_id",
+                "bk_cloud_id",
+                "bk_agent_id",
+                "bk_city",
+            ],
+        )
