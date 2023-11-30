f6047d95cb9fa2833fb256d38b4c88ec53cbf80f
gaohongsong <gmaclinuxer@gmail.com>
2023-11-30 19:49:12 +0800

    refactor(backend): 优化app同步任务 close #2232

--- dbm-ui/backend/db_periodic_task/local_tasks/db_meta/update_app_cache.py
+++ dbm-ui/backend/db_periodic_task/local_tasks/db_meta/update_app_cache.py
@@ -76,3 +76,88 @@ def update_app_cache():
         len(created_bizs),
         len(updated_bizs),
     )
+
+
+@register_periodic_task(run_every=crontab(minute="*/20"))
+def bulk_update_app_cache():
+    """缓存空闲机拓扑"""
+
+    def get_db_app_abbr(biz):
+        """TODO（好绕的逻辑）: 获取db_app_abbr，为空则尝试从bk_app_abbr同步"""
+        bk_app_abbr = biz.get(env.BK_APP_ABBR, "")
+        db_app_abbr = biz.get(CC_APP_ABBR_ATTR, "").lower().replace(" ", "-").replace("_", "-")
+
+        # 目标环境中存在bk_app_abbr，则同步过来
+        if env.BK_APP_ABBR and env.BK_APP_ABBR != CC_APP_ABBR_ATTR:
+            # db_app_abbr为空才同步
+            if not db_app_abbr and db_app_abbr != bk_app_abbr:
+                CCApi.update_business(
+                    {"bk_biz_id": biz["bk_biz_id"], "data": {"db_app_abbr": bk_app_abbr}}, use_admin=True
+                )
+                db_app_abbr = bk_app_abbr
+
+        return db_app_abbr
+
+    LIMIT = 500
+    total = CCApi.search_business({"page": {"start": 0, "limit": 1}}).get("count", 0)
+    start = 0
+
+    begin_at = datetime.datetime.now()
+    logger.warning("bulk_update_app_cache: start update app cache start: total", total)
+
+    # 批量创建和更新
+    create_cnt, update_cnt = 0, 0
+    update_fields = [CC_APP_ABBR_ATTR, "bk_biz_name", "time_zone", "bk_biz_maintainer"]
+    while start < total:
+        info = CCApi.search_business({"page": {"start": start, "limit": LIMIT}}).get("info", [])
+        biz_map = {i["bk_biz_id"]: i for i in info}
+
+        bk_biz_ids = list(biz_map.keys())
+        exists = list(AppCache.objects.filter(bk_biz_id__in=bk_biz_ids).values_list("bk_biz_id", flat=True))
+        not_exists = list(set(bk_biz_ids) - set(exists))
+
+        # 整理需要批量创建的app
+        new_apps = []
+        for bk_biz_id in not_exists:
+            cc_app = biz_map[bk_biz_id]
+            db_app_abbr = get_db_app_abbr(cc_app)
+            new_apps.append(
+                AppCache(
+                    bk_biz_id=bk_biz_id,
+                    bk_biz_name=cc_app["bk_biz_name"],
+                    time_zone=cc_app["time_zone"],
+                    bk_biz_maintainer=cc_app["bk_biz_maintainer"],
+                    db_app_abbr=db_app_abbr,
+                )
+            )
+
+        # 整理需要批量更新的app
+        update_apps = []
+        for app in AppCache.objects.filter(bk_biz_id__in=exists):
+            need_update = False
+            for field in update_fields:
+                cc_app = biz_map[app.key]
+                new_value = cc_app[field]
+                old_value = getattr(app, field)
+                if new_value != old_value:
+                    logger.info("bulk_update_app_cache[%s]: %s:%s -> %s:%s", start, field, old_value, field, new_value)
+                    setattr(cc_app, field, new_value)
+                    need_update = True
+
+                if need_update:
+                    update_apps.append(AppCache)
+
+        AppCache.objects.bulk_create(new_apps)
+        AppCache.objects.bulk_update(update_apps, fields=update_fields)
+        create_cnt += len(new_apps)
+        update_cnt += len(update_apps)
+        logger.info("bulk_update_app_cache[%s]: new: %s, update: %s", start, len(new_apps), len(update_apps))
+
+        start += LIMIT
+
+    logger.warning(
+        "bulk_update_app_cache [%s] finish update app cache end: %s, create_cnt: %s, update_cnt: %s",
+        datetime.datetime.now() - begin_at,
+        create_cnt,
+        update_cnt,
+    )
