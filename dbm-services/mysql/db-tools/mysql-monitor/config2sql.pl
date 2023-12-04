7273d84a292a73dd7057120ffeb822a45fbe283b
xfwduke <xfwduke@gmail.com>
2023-12-04 14:29:17 +0800

    fix(mysql): mysql-monitor配置enable类型错误;context和scenes权限错误 close #2307

--- dbm-services/mysql/db-tools/mysql-monitor/config2sql.pl
+++ dbm-services/mysql/db-tools/mysql-monitor/config2sql.pl
@@ -6,6 +6,9 @@ use JSON qw(encode_json);
 my $items_config = LoadFile('items-config.yaml');
 
 foreach my $item (@$items_config) {
+    my $item_value = encode_json($item);
+    $item_value =~ s/"enable":"1"/"enable":true/;
+    $item_value =~ s/"enable":"0"/"enable":false/;
     my $sql = sprintf(q#REPLACE INTO 
     tb_config_name_def(
         namespace, conf_type, conf_file, conf_name, 
@@ -15,7 +18,7 @@ foreach my $item (@$items_config) {
             'tendb', 'mysql_monitor', 'items-config.yaml', '%s',
              'STRING', '%s', '', 'MAP', 
              1, 0, 0, 0, 1);#,
-        $item->{name}, encode_json($item));
+        $item->{name}, $item_value);
     # 没什么实际的意义, 只是让输入的 sql 好看些
     $sql =~ s/\n//g;
     $sql =~ s/\s+/ /g;
