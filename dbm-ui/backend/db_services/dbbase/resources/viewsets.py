41d9673ab90de731347f72b9d85341e36c43a3d2
durant <826035498@qq.com>
2023-12-23 17:26:13 +0800

    feat(backend): 全局搜索接口支持批量查询 #2781

--- dbm-ui/backend/db_services/dbbase/resources/viewsets.py
+++ dbm-ui/backend/db_services/dbbase/resources/viewsets.py
@@ -71,13 +71,13 @@ class ResourceViewSet(SystemViewSet):
         """获取拓扑图"""
         return Response(self.query_class.get_topo_graph(bk_biz_id, cluster_id))
 
-    @action(methods=["GET"], detail=False, url_path="export_cluster")
+    @action(methods=["POST"], detail=False, url_path="export_cluster")
     def export_cluster(self, request, bk_biz_id: int):
         """导出集群数据为 excel 文件"""
         cluster_ids = request.POST.get("cluster_ids")
         return self.query_class.export_cluster(bk_biz_id, cluster_ids)
 
-    @action(methods=["GET"], detail=False, url_path="export_instance")
+    @action(methods=["POST"], detail=False, url_path="export_instance")
     def export_instance(self, request, bk_biz_id: int):
         """导出实例数据为 excel 文件"""
         bk_host_ids = request.POST.get("bk_host_ids")
