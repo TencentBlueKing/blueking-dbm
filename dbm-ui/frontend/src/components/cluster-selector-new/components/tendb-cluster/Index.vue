f4dfd414078277a6cf163b7338cabfc6587f3864
austinqli <1344583166@qq.com>
2024-01-11 16:39:14 +0800

    feat(frontend): 集群选择器新增已禁用交互与mongodb类型 #3004

--- dbm-ui/frontend/src/components/cluster-selector-new/components/tendb-cluster/Index.vue
+++ dbm-ui/frontend/src/components/cluster-selector-new/components/tendb-cluster/Index.vue
@@ -90,9 +90,25 @@
         />
       ),
       render: ({ data }: { data: ResourceItem }) => {
+        if (data.phase === 'offline') {
+          return (
+            <bk-popover
+              theme="dark"
+              placement="top"
+              popoverDelay={0}>
+              {{
+                default: () => <bk-checkbox style="vertical-align: middle;" disabled />,
+                content: () => <span>{t('集群已禁用')}</span>,
+              }}
+            </bk-popover>
+          );
+        }
         if (props.disabledRowConfig && props.disabledRowConfig.handler(data)) {
           return (
-            <bk-popover theme="dark" placement="top" popoverDelay={0}>
+            <bk-popover
+              theme="dark"
+              placement="top"
+              popoverDelay={0}>
               {{
                 default: () => <bk-checkbox style="vertical-align: middle;" disabled />,
                 content: () => <span>{props.disabledRowConfig?.tip}</span>,
@@ -117,14 +133,23 @@
       render: ({ data }: { data: ResourceItem }) => (
       <div class="cluster-name-box">
           <div class="cluster-name">{data.master_domain}</div>
-          {data.operations && data.operations.length > 0 && <bk-popover
-            theme="light"
-            width="360">
-            {{
-              default: () => <bk-tag theme="info" class="tag-box">{data.operations.length}</bk-tag>,
-              content: () => <ClusterRelatedTasks data={data.operations} />,
-            }}
-          </bk-popover>}
+          {data.phase === 'offline' && (
+            <db-icon
+              svg
+              type="yijinyong"
+              class="mr-8"
+              style="width: 38px; height: 16px;" />
+          )}
+          {data.operations && data.operations.length > 0 && (
+            <bk-popover
+              theme="light"
+              width="360">
+              {{
+                default: () => <bk-tag theme="info" class="tag-box">{data.operations.length}</bk-tag>,
+                content: () => <ClusterRelatedTasks data={data.operations} />,
+              }}
+            </bk-popover>
+          )}
       </div>),
     },
     {
@@ -216,7 +241,6 @@
   watch(() => activeTab.value, (tab) => {
     if (tab) {
       searchSelectValue.value = [];
-      handleTablePageChange(1);
     }
   });
 
@@ -234,7 +258,9 @@
       return;
     }
     for (const data of tableData.value) {
-      handleSelecteRow(data, value);
+      if (data.phase !== 'offline') {
+        handleSelecteRow(data, value);
+      }
     }
   };
 
@@ -278,7 +304,7 @@
   };
 
   const handleRowClick = (row:any, data: ResourceItem) => {
-    if (props.disabledRowConfig && props.disabledRowConfig.handler(data)) {
+    if ((props.disabledRowConfig && props.disabledRowConfig.handler(data)) || data.phase === 'offline') {
       return;
     }
     const currentSelected = selectedMap.value[activeTab.value];
@@ -287,19 +313,19 @@
   };
 
 
-  function handleTablePageChange(value: number) {
+  const handleTablePageChange = (value: number) => {
     handleChangePage(value)
       .then(() => {
         checkSelectedAll();
       });
-  }
+  };
 
-  function handleTableLimitChange(value: number) {
+  const handleTableLimitChange = (value: number) => {
     handeChangeLimit(value)
       .then(() => {
         checkSelectedAll();
       });
-  }
+  };
 </script>
 
 <style lang="less" scoped>
