7cd1495127307a630706050b1cf08d7da4198a7a
yyhenryyy <henryyyang@tencent.com>
2024-01-15 11:41:47 +0800

    feat(mongodb): 增加原子任务，cluster安装flow #3018

---
+++ dbm-services/mongo/db-tools/dbactuator/pkg/atomjobs/atommongodb/mongo_process_restart.go
@@ -0,0 +1,399 @@
+package atommongodb
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"dbm-services/mongo/db-tools/dbactuator/pkg/common"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/consts"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/jobruntime"
+
+	"github.com/go-playground/validator/v10"
+	"gopkg.in/yaml.v2"
+)
+
+// RestartConfParams 重启进程参数
+type RestartConfParams struct {
+	IP                       string `json:"ip" validate:"required"`
+	Port                     int    `json:"port" validate:"required"`
+	InstanceType             string `json:"instanceType" validate:"required"` // mongos mongod
+	SingleNodeInstallRestart bool   `json:"singleNodeInstallRestart"`         // mongod替换节点安装后重启
+	Auth                     bool   `json:"auth"`                             // true->auth false->noauth
+	CacheSizeGB              int    `json:"cacheSizeGB"`                      // 可选，重启mongod的参数
+	MongoSConfDbOld          string `json:"mongoSConfDbOld"`                  // 可选，ip:port
+	MongoSConfDbNew          string `json:"MongoSConfDbNew"`                  // 可选，ip:port
+	AdminUsername            string `json:"adminUsername"`
+	AdminPassword            string `json:"adminPassword"`
+}
+
+// MongoRestart 重启mongo进程
+type MongoRestart struct {
+	runtime            *jobruntime.JobGenericRuntime
+	BinDir             string
+	DataDir            string
+	DbpathDir          string
+	Mongo              string
+	OsUser             string // MongoDB安装在哪个用户下
+	OsGroup            string
+	ConfParams         *RestartConfParams
+	AuthConfFilePath   string
+	NoAuthConfFilePath string
+}
+
+// NewMongoRestart 实例化结构体
+func NewMongoRestart() jobruntime.JobRunner {
+	return &MongoRestart{}
+}
+
+// Name 获取原子任务的名字
+func (r *MongoRestart) Name() string {
+	return "mongo_restart"
+}
+
+// Run 运行原子任务
+func (r *MongoRestart) Run() error {
+	// 修改配置文件参数
+	if err := r.changeParam(); err != nil {
+		return err
+	}
+
+	// mongod的primary进行主备切换
+	if err := r.RsStepDown(); err != nil {
+		return err
+	}
+
+	// 关闭服务
+	if err := r.shutdown(); err != nil {
+		return err
+	}
+
+	// 启动服务
+	if err := r.startup(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Retry 重试
+func (r *MongoRestart) Retry() uint {
+	return 2
+}
+
+// Rollback 回滚
+func (r *MongoRestart) Rollback() error {
+	return nil
+}
+
+// Init 初始化
+func (r *MongoRestart) Init(runtime *jobruntime.JobGenericRuntime) error {
+	// 获取安装参数
+	r.runtime = runtime
+	r.runtime.Logger.Info("start to init")
+	r.BinDir = consts.UsrLocal
+	r.DataDir = consts.GetRedisDataDir()
+	r.OsUser = consts.GetProcessUser()
+	r.OsGroup = consts.GetProcessUserGroup()
+	r.Mongo = filepath.Join(r.BinDir, "mongodb", "bin", "mongo")
+
+	// 获取MongoDB配置文件参数
+	if err := json.Unmarshal([]byte(r.runtime.PayloadDecoded), &r.ConfParams); err != nil {
+		r.runtime.Logger.Error(fmt.Sprintf(
+			"get parameters of mongo restart fail by json.Unmarshal, error:%s", err))
+		return fmt.Errorf("get parameters of mongo restart fail by json.Unmarshal, error:%s", err)
+	}
+
+	// 设置各种路径
+	strPort := strconv.Itoa(r.ConfParams.Port)
+	r.DbpathDir = filepath.Join(r.DataDir, "mongodata", strPort, "db")
+	r.AuthConfFilePath = filepath.Join(r.DataDir, "mongodata", strPort, "mongo.conf")
+	r.NoAuthConfFilePath = filepath.Join(r.DataDir, "mongodata", strPort, "noauth.conf")
+	r.runtime.Logger.Info("init successfully")
+
+	// 安装前进行校验
+	if err := r.checkParams(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// checkParams 校验参数
+func (r *MongoRestart) checkParams() error {
+	// 校验重启配置参数
+	validate := validator.New()
+	r.runtime.Logger.Info("start to validate parameters of restart")
+	if err := validate.Struct(r.ConfParams); err != nil {
+		r.runtime.Logger.Error(fmt.Sprintf("validate parameters of restart fail, error:%s", err))
+		return fmt.Errorf("validate parameters of restart fail, error:%s", err)
+	}
+	r.runtime.Logger.Info("validate parameters of restart successfully")
+	return nil
+}
+
+// changeParam 修改参数
+func (r *MongoRestart) changeParam() error {
+	if r.ConfParams.InstanceType == "mongos" &&
+		r.ConfParams.MongoSConfDbOld != "" && r.ConfParams.MongoSConfDbNew != "" {
+		if err := r.changeConfigDb(); err != nil {
+			return err
+		}
+		return nil
+	}
+	if err := r.changeCacheSizeGB(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// changeConfigDb 修改mongoS的ConfigDb参数
+func (r *MongoRestart) changeConfigDb() error {
+	r.runtime.Logger.Info("start to change configDB value of config file")
+	// 获取配置文件内容
+	readAuthConfFileContent, _ := ioutil.ReadFile(r.AuthConfFilePath)
+	readNoAuthConfFileContent, _ := ioutil.ReadFile(r.NoAuthConfFilePath)
+
+	// 修改configDB配置
+	yamlAuthMongoSConf := common.NewYamlMongoSConf()
+	yamlNoAuthMongoSConf := common.NewYamlMongoSConf()
+	_ = yaml.Unmarshal(readAuthConfFileContent, yamlAuthMongoSConf)
+	_ = yaml.Unmarshal(readNoAuthConfFileContent, yamlNoAuthMongoSConf)
+	yamlAuthMongoSConf.Sharding.ConfigDB = strings.Replace(yamlAuthMongoSConf.Sharding.ConfigDB,
+		r.ConfParams.MongoSConfDbOld, r.ConfParams.MongoSConfDbNew, -1)
+	yamlNoAuthMongoSConf.Sharding.ConfigDB = strings.Replace(yamlNoAuthMongoSConf.Sharding.ConfigDB,
+		r.ConfParams.MongoSConfDbOld, r.ConfParams.MongoSConfDbNew, -1)
+	authConfFileContent, _ := yamlAuthMongoSConf.GetConfContent()
+	noAuthConfFileContent, _ := yamlNoAuthMongoSConf.GetConfContent()
+
+	// 修改authConfFile
+	authConfFile, err := os.OpenFile(r.AuthConfFilePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, DefaultPerm)
+	defer authConfFile.Close()
+	if err != nil {
+		r.runtime.Logger.Error(
+			fmt.Sprintf("create auth config file fail, error:%s", err))
+		return fmt.Errorf("create auth config file fail, error:%s", err)
+	}
+	if _, err = authConfFile.WriteString(string(authConfFileContent)); err != nil {
+		r.runtime.Logger.Error(
+			fmt.Sprintf("change configDB value of auth config file write content fail, error:%s",
+				err))
+		return fmt.Errorf("change configDB value of auth config file write content fail, error:%s",
+			err)
+	}
+
+	// 修改noAuthConfFile
+	noAuthConfFile, err := os.OpenFile(r.NoAuthConfFilePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, DefaultPerm)
+	defer noAuthConfFile.Close()
+	if err != nil {
+		r.runtime.Logger.Error(fmt.Sprintf("create no auth config file fail, error:%s", err))
+		return fmt.Errorf("create no auth config file fail, error:%s", err)
+	}
+	if _, err = noAuthConfFile.WriteString(string(noAuthConfFileContent)); err != nil {
+		r.runtime.Logger.Error(
+			fmt.Sprintf("change configDB value of no auth config file write content fail, error:%s",
+				err))
+		return fmt.Errorf("change configDB value of no auth config file write content fail, error:%s",
+			err)
+	}
+	r.runtime.Logger.Info("change configDB value of config file successfully")
+
+	return nil
+}
+
+// changeCacheSizeGB 修改CacheSizeGB
+func (r *MongoRestart) changeCacheSizeGB() error {
+	if r.ConfParams.CacheSizeGB == 0 {
+		return nil
+	}
+
+	// 检查mongo版本
+	r.runtime.Logger.Info("start to check mongo version")
+	version, err := common.CheckMongoVersion(r.BinDir, "mongod")
+	if err != nil {
+		r.runtime.Logger.Error(fmt.Sprintf("check mongo version fail, error:%s", err))
+		return fmt.Errorf("check mongo version fail, error:%s", err)
+	}
+	mainVersion, _ := strconv.Atoi(strings.Split(version, ".")[0])
+	r.runtime.Logger.Info("check mongo version successfully")
+
+	if mainVersion >= 3 {
+		r.runtime.Logger.Info("start to change CacheSizeGB value of config file")
+		// 获取配置文件内容
+		readAuthConfFileContent, _ := ioutil.ReadFile(r.AuthConfFilePath)
+		readNoAuthConfFileContent, _ := ioutil.ReadFile(r.NoAuthConfFilePath)
+
+		// 修改CacheSizeGB大小并写入文件
+		yamlAuthConfFile := common.NewYamlMongoDBConf()
+		yamlNoAuthConfFile := common.NewYamlMongoDBConf()
+		_ = yaml.Unmarshal(readAuthConfFileContent, &yamlAuthConfFile)
+		_ = yaml.Unmarshal(readNoAuthConfFileContent, &yamlNoAuthConfFile)
+		if r.ConfParams.CacheSizeGB == 0 {
+			return nil
+		}
+		if r.ConfParams.CacheSizeGB != yamlAuthConfFile.Storage.WiredTiger.EngineConfig.CacheSizeGB {
+			yamlAuthConfFile.Storage.WiredTiger.EngineConfig.CacheSizeGB = r.ConfParams.CacheSizeGB
+			yamlNoAuthConfFile.Storage.WiredTiger.EngineConfig.CacheSizeGB = r.ConfParams.CacheSizeGB
+			authConfFileContent, _ := yamlAuthConfFile.GetConfContent()
+			noAuthConfFileContent, _ := yamlNoAuthConfFile.GetConfContent()
+
+			// 修改authConfFile
+			authConfFile, err := os.OpenFile(r.AuthConfFilePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, DefaultPerm)
+			defer authConfFile.Close()
+			if err != nil {
+				r.runtime.Logger.Error(
+					fmt.Sprintf("create auth config file fail, error:%s", err))
+				return fmt.Errorf("create auth config file fail, error:%s", err)
+			}
+			if _, err = authConfFile.WriteString(string(authConfFileContent)); err != nil {
+				r.runtime.Logger.Error(
+					fmt.Sprintf("change CacheSizeGB value of auth config file write content fail, error:%s",
+						err))
+				return fmt.Errorf("change CacheSizeGB value of auth config file write content fail, error:%s",
+					err)
+			}
+
+			// 修改noAuthConfFile
+			noAuthConfFile, err := os.OpenFile(r.NoAuthConfFilePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, DefaultPerm)
+			defer noAuthConfFile.Close()
+			if err != nil {
+				r.runtime.Logger.Error(fmt.Sprintf("create no auth config file fail, error:%s", err))
+				return fmt.Errorf("create no auth config file fail, error:%s", err)
+			}
+			if _, err = noAuthConfFile.WriteString(string(noAuthConfFileContent)); err != nil {
+				r.runtime.Logger.Error(
+					fmt.Sprintf("change CacheSizeGB value of no auth config file write content fail, error:%s",
+						err))
+				return fmt.Errorf("change CacheSizeGB value of no auth config file write content fail, error:%s",
+					err)
+			}
+		}
+		r.runtime.Logger.Info("change CacheSizeGB value of config file successfully")
+	}
+	return nil
+}
+
+// checkPrimary 检查该节点是否是primary
+func (r *MongoRestart) checkPrimary() (bool, error) {
+	r.runtime.Logger.Info("start to check if it is primary")
+	var info string
+	var err error
+	// 安装时无需密码验证。安装成功后需要密码验证
+	if r.ConfParams.AdminUsername != "" && r.ConfParams.AdminPassword != "" {
+		info, err = common.AuthGetPrimaryInfo(r.Mongo, r.ConfParams.AdminUsername,
+			r.ConfParams.AdminPassword, r.ConfParams.IP, r.ConfParams.Port)
+	} else {
+		info, err = common.NoAuthGetPrimaryInfo(r.Mongo,
+			r.ConfParams.IP, r.ConfParams.Port)
+	}
+	if err != nil {
+		r.runtime.Logger.Error("get primary info fail, error:%s", err)
+		return false, fmt.Errorf("get primary info fail, error:%s", err)
+	}
+	if info == fmt.Sprintf("%s:%d", r.ConfParams.IP, r.ConfParams.Port) {
+		return true, nil
+	}
+	r.runtime.Logger.Info("check if it is primary successfully")
+	return false, nil
+}
+
+// RsStepDown  主备切换
+func (r *MongoRestart) RsStepDown() error {
+	if r.ConfParams.InstanceType != "mongos" {
+		if r.ConfParams.SingleNodeInstallRestart == true {
+			return nil
+		}
+		r.runtime.Logger.Info("start to check mongod service before rsStepDown")
+		flag, _, err := common.CheckMongoService(r.ConfParams.Port)
+		if err != nil {
+			r.runtime.Logger.Error("check mongod service fail, error:%s", err)
+			return fmt.Errorf("check mongod service fail, error:%s", err)
+		}
+		r.runtime.Logger.Info("check mongod service before rsStepDown successfully")
+		if flag == false {
+			return nil
+		}
+
+		// 检查是否是primary
+		flag1, err := r.checkPrimary()
+		if err != nil {
+			return err
+		}
+		if flag1 == true {
+			r.runtime.Logger.Info("start to convert primary secondary db")
+			// 安装时无需密码验证。安装成功后需要密码验证
+			var flag2 bool
+			if r.ConfParams.AdminUsername != "" && r.ConfParams.AdminPassword != "" {
+				flag2, err = common.AuthRsStepDown(r.Mongo, r.ConfParams.IP, r.ConfParams.Port,
+					r.ConfParams.AdminUsername, r.ConfParams.AdminPassword)
+			} else {
+				flag2, err = common.NoAuthRsStepDown(r.Mongo, r.ConfParams.IP, r.ConfParams.Port)
+			}
+			if err != nil {
+				r.runtime.Logger.Error("convert primary secondary db fail, error:%s", err)
+				return fmt.Errorf("convert primary secondary db fail, error:%s", err)
+			}
+			if flag2 == true {
+				r.runtime.Logger.Info("convert primary secondary db successfully")
+				return nil
+			}
+		}
+	}
+
+	return nil
+}
+
+// shutdown 关闭服务
+func (r *MongoRestart) shutdown() error {
+	// 检查服务是否存在
+	r.runtime.Logger.Info("start to check %s service", r.ConfParams.InstanceType)
+	result, _, err := common.CheckMongoService(r.ConfParams.Port)
+	if err != nil {
+		r.runtime.Logger.Error("check %s service fail, error:%s", r.ConfParams.InstanceType, err)
+		return fmt.Errorf("check %s service fail, error:%s", r.ConfParams.InstanceType, err)
+	}
+	if result != true {
+		r.runtime.Logger.Info("%s service has been close", r.ConfParams.InstanceType)
+		return nil
+	}
+	r.runtime.Logger.Info("check %s service successfully", r.ConfParams.InstanceType)
+
+	// 关闭服务
+	r.runtime.Logger.Info("start to shutdown %s", r.ConfParams.InstanceType)
+	if err = common.ShutdownMongoProcess(r.OsUser, r.ConfParams.InstanceType, r.BinDir, r.DbpathDir,
+		r.ConfParams.Port); err != nil {
+		r.runtime.Logger.Error(fmt.Sprintf("shutdown %s fail, error:%s", r.ConfParams.InstanceType, err))
+		return fmt.Errorf("shutdown %s fail, error:%s", r.ConfParams.InstanceType, err)
+	}
+	r.runtime.Logger.Info("shutdown %s successfully", r.ConfParams.InstanceType)
+	return nil
+}
+
+// startup 开启服务
+func (r *MongoRestart) startup() error {
+	// 检查服务是否存在
+	r.runtime.Logger.Info("start to check %s service", r.ConfParams.InstanceType)
+	result, _, err := common.CheckMongoService(r.ConfParams.Port)
+	if err != nil {
+		r.runtime.Logger.Error("check %s service fail, error:%s", r.ConfParams.InstanceType, err)
+		return fmt.Errorf("check %s service fail, error:%s", r.ConfParams.InstanceType, err)
+	}
+	if result == true {
+		r.runtime.Logger.Info("%s service has been open", r.ConfParams.InstanceType)
+		return nil
+	}
+	r.runtime.Logger.Info("check %s service successfully", r.ConfParams.InstanceType)
+
+	// 开启服务
+	r.runtime.Logger.Info("start to startup %s", r.ConfParams.InstanceType)
+	if err = common.StartMongoProcess(r.BinDir, r.ConfParams.Port, r.OsUser, r.ConfParams.Auth); err != nil {
+		r.runtime.Logger.Error("startup %s fail, error:%s", r.ConfParams.InstanceType, err)
+		return fmt.Errorf("startup %s fail, error:%s", r.ConfParams.InstanceType, err)
+	}
+	r.runtime.Logger.Info("startup %s successfully", r.ConfParams.InstanceType)
+	return nil
+}
---
+++ dbm-services/mongo/db-tools/dbactuator/pkg/backupsys/backupsys.go
@@ -0,0 +1,231 @@
+// Package backupsys 备份系统
+package backupsys
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"dbm-services/mongo/db-tools/dbactuator/mylog"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/consts"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/util"
+)
+
+// UploadTask 操作备份系统
+type UploadTask struct {
+	Files   []string `json:"files"` // 全路径
+	TaskIDs []uint64 `json:"taskids"`
+	Tag     string   `json:"tag"`
+}
+
+// UploadFiles 上传文件
+func (task *UploadTask) UploadFiles() (err error) {
+	var taskIDStr string
+	var taskIDNum uint64
+	if len(task.Files) == 0 {
+		return
+	}
+	if task.Tag == "" {
+		err = fmt.Errorf("BackupSystem uploadFiles tag(%s) cannot be empty", task.Tag)
+		mylog.Logger.Error(err.Error())
+		return
+	}
+	for _, file := range task.Files {
+		if !util.FileExists(file) {
+			err = fmt.Errorf("BackupSystem uploadFiles %s not exists", file)
+			mylog.Logger.Error(err.Error())
+			return
+		}
+	}
+	for _, bkfile := range task.Files {
+		bkCmd := fmt.Sprintf("%s -n -f %s --with-md5 -t %s|grep 'taskid'|awk -F: '{print $2}'",
+			consts.BackupClient, bkfile, task.Tag)
+		mylog.Logger.Info(bkCmd)
+		taskIDStr, err = util.RunBashCmd(bkCmd, "", nil, 10*time.Minute)
+		if err != nil {
+			return
+		}
+		taskIDNum, err = strconv.ParseUint(taskIDStr, 10, 64)
+		if err != nil {
+			err = fmt.Errorf("%s ParseUint failed,err:%v", taskIDStr, err)
+			mylog.Logger.Error(err.Error())
+			return
+		}
+		task.TaskIDs = append(task.TaskIDs, taskIDNum)
+	}
+	return
+}
+
+// CheckTasksStatus 检查tasks状态
+func (task *UploadTask) CheckTasksStatus() (runningTaskIDs, failTaskIDs, succTaskIDs []uint64,
+	runningFiles, failedFiles, succFiles []string, failMsgs []string, err error) {
+	var status TaskStatus
+	for idx, taskID := range task.TaskIDs {
+		status, err = GetTaskStatus(taskID)
+		if err != nil {
+			return
+		}
+		if status.Status > 4 {
+			// err = fmt.Errorf("ToBackupSystem %s failed,err:%s,taskid:%d",
+			// 	status.File, status.StatusInfo, taskID)
+			// mylog.Logger.Error(err.Error())
+			failMsgs = append(failMsgs, fmt.Sprintf("taskid:%d,failMsg:%s", taskID, status.StatusInfo))
+			failedFiles = append(failedFiles, task.Files[idx])
+			failTaskIDs = append(failTaskIDs, task.TaskIDs[idx])
+		} else if status.Status == 4 {
+			succFiles = append(succFiles, task.Files[idx])
+			succTaskIDs = append(succTaskIDs, task.TaskIDs[idx])
+		} else if status.Status < 4 {
+			runningFiles = append(runningFiles, task.Files[idx])
+			runningTaskIDs = append(runningTaskIDs, task.TaskIDs[idx])
+		}
+	}
+	return
+}
+
+// WaitForUploadFinish 等待所有files上传成功
+func (task *UploadTask) WaitForUploadFinish() (err error) {
+	var times int64
+	var msg string
+	var runningFiles, failFiles, succFiles, failMsgs []string
+	for {
+		times++
+		_, _, _, runningFiles, failFiles, succFiles, failMsgs, err = task.CheckTasksStatus()
+		if err != nil {
+			return
+		}
+		// 只要有running的task,则继续等待
+		if len(runningFiles) > 0 {
+			if times%6 == 0 {
+				// 每分钟打印一次日志
+				msg = fmt.Sprintf("files[%+v] cnt:%d upload to backupSystem still running", runningFiles, len(runningFiles))
+				mylog.Logger.Info(msg)
+			}
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		if len(failMsgs) > 0 {
+			err = fmt.Errorf("failCnt:%d,failFiles:[%+v],err:%s", len(failFiles), failFiles, strings.Join(failFiles, ","))
+			mylog.Logger.Error(err.Error())
+			return
+		}
+		if len(succFiles) == len(task.Files) {
+			return nil
+		}
+		break
+	}
+	return
+}
+
+// TaskStatus backup_client  -q --taskid=xxxx 命令的结果
+type TaskStatus struct {
+	File           string    `json:"file"`
+	Host           string    `json:"host"`
+	SednupDateTime time.Time `json:"sendup_datetime"`
+	Status         int       `json:"status"`
+	StatusInfo     string    `json:"status_info"`
+	StartTime      time.Time `json:"start_time"`
+	CompleteTime   time.Time `json:"complete_time"`
+	ExpireTime     time.Time `json:"expire_time"`
+}
+
+// String 用于打印
+func (status *TaskStatus) String() string {
+	statusBytes, _ := json.Marshal(status)
+	return string(statusBytes)
+}
+
+// GetTaskStatus 执行backup_client  -q --taskid=xxxx 命令的结果并解析
+func GetTaskStatus(taskid uint64) (status TaskStatus, err error) {
+	var cmdRet string
+	bkCmd := fmt.Sprintf("%s -q --taskid=%d", consts.BackupClient, taskid)
+	cmdRet, err = util.RunBashCmd(bkCmd, "", nil, 30*time.Second)
+	if err != nil {
+		return
+	}
+	scanner := bufio.NewScanner(strings.NewReader(cmdRet))
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		l01 := strings.SplitN(line, ":", 2)
+		if len(l01) != 2 {
+			err = fmt.Errorf("len()!=2,cmd:%s,result format not correct:%s", bkCmd, cmdRet)
+			mylog.Logger.Error(err.Error())
+			return
+		}
+		first := strings.TrimSpace(l01[0])
+		second := strings.TrimSpace(l01[1])
+		switch first {
+		case "file":
+			status.File = second
+		case "host":
+			status.Host = second
+		case "sendup datetime":
+			if second == "0000-00-00 00:00:00" {
+				status.SednupDateTime = time.Time{} // "0000-01-00 00:00:00"
+				break
+			}
+			status.SednupDateTime, err = time.ParseInLocation(consts.UnixtimeLayout, second, time.Local)
+			if err != nil {
+				err = fmt.Errorf("time.Parse 'sendup datetime' failed,err:%v,value:%s,cmd:%s", err, second, bkCmd)
+				mylog.Logger.Error(err.Error())
+				return
+			}
+		case "status":
+			status.Status, err = strconv.Atoi(second)
+			if err != nil {
+				err = fmt.Errorf("strconv.Atoi failed,err:%v,value:%s,cmd:%s", err, second, bkCmd)
+				mylog.Logger.Error(err.Error())
+				return
+			}
+		case "status info":
+			status.StatusInfo = second
+		case "start_time":
+			if second == "0000-00-00 00:00:00" {
+				status.StartTime = time.Time{} // "0000-01-00 00:00:00"
+				break
+			}
+			status.StartTime, err = time.ParseInLocation(consts.UnixtimeLayout, second, time.Local)
+			if err != nil {
+				err = fmt.Errorf("time.Parse start_time failed,err:%v,value:%s,cmd:%s", err, second, bkCmd)
+				mylog.Logger.Error(err.Error())
+				return
+			}
+		case "complete_time":
+			if second == "0000-00-00 00:00:00" {
+				status.CompleteTime = time.Time{} // "0000-01-00 00:00:00"
+				break
+			}
+			status.CompleteTime, err = time.ParseInLocation(consts.UnixtimeLayout, second, time.Local)
+			if err != nil {
+				err = fmt.Errorf("time.Parse complete_time failed,err:%v,value:%s,cmd:%s", err, second, bkCmd)
+				mylog.Logger.Error(err.Error())
+				return
+			}
+		case "expire_time":
+			if second == "0000-00-00 00:00:00" {
+				status.ExpireTime = time.Time{} // "0000-01-00 00:00:00"
+				break
+			}
+			status.ExpireTime, err = time.ParseInLocation(consts.UnixtimeLayout, second, time.Local)
+			if err != nil {
+				err = fmt.Errorf("time.Parse expire_time failed,err:%v,value:%s,cmd:%s", err, second, bkCmd)
+				mylog.Logger.Error(err.Error())
+				return
+			}
+		}
+	}
+	if err = scanner.Err(); err != nil {
+		err = fmt.Errorf("scanner.Scan failed,err:%v,cmd:%s", err, cmdRet)
+		mylog.Logger.Error(err.Error())
+		return
+	}
+	return
+}
