9cd817cdd96db60d15db7ef59571678245e6f54e
yyhenryyy <henryyyang@tencent.com>
2024-01-15 11:41:47 +0800

    feat(mongodb): 增加原子任务，cluster安装flow #3018

---
+++ dbm-services/mongo/db-tools/dbactuator/pkg/atomjobs/atommongodb/cluster_balancer.go
@@ -0,0 +1,159 @@
+package atommongodb
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"dbm-services/mongo/db-tools/dbactuator/pkg/common"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/consts"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/jobruntime"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/util"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// BalancerConfParams 参数
+type BalancerConfParams struct {
+	IP            string `json:"ip" validate:"required"`
+	Port          int    `json:"port" validate:"required"`
+	Open          bool   `json:"open"` // true：打开 false：关闭
+	AdminUsername string `json:"adminUsername" validate:"required"`
+	AdminPassword string `json:"adminPassword" validate:"required"`
+}
+
+// Balancer 添加分片到集群
+type Balancer struct {
+	runtime    *jobruntime.JobGenericRuntime
+	BinDir     string
+	Mongo      string
+	OsUser     string
+	ConfParams *BalancerConfParams
+}
+
+// NewBalancer 实例化结构体
+func NewBalancer() jobruntime.JobRunner {
+	return &Balancer{}
+}
+
+// Name 获取原子任务的名字
+func (b *Balancer) Name() string {
+	return "cluster_balancer"
+}
+
+// Run 运行原子任务
+func (b *Balancer) Run() error {
+	// 执行脚本
+	if err := b.execScript(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Retry 重试
+func (b *Balancer) Retry() uint {
+	return 2
+}
+
+// Rollback 回滚
+func (b *Balancer) Rollback() error {
+	return nil
+}
+
+// Init 初始化
+func (b *Balancer) Init(runtime *jobruntime.JobGenericRuntime) error {
+	// 获取安装参数
+	b.runtime = runtime
+	b.runtime.Logger.Info("start to init")
+	b.BinDir = consts.UsrLocal
+	b.Mongo = filepath.Join(b.BinDir, "mongodb", "bin", "mongo")
+	b.OsUser = consts.GetProcessUser()
+
+	// 获取MongoDB配置文件参数
+	if err := json.Unmarshal([]byte(b.runtime.PayloadDecoded), &b.ConfParams); err != nil {
+		b.runtime.Logger.Error(
+			"get parameters of clusterBalancer fail by json.Unmarshal, error:%s", err)
+		return fmt.Errorf("get parameters of clusterBalancer fail by json.Unmarshal, error:%s", err)
+	}
+	b.runtime.Logger.Info("init successfully")
+
+	// 进行校验
+	if err := b.checkParams(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// checkParams 校验参数
+func (b *Balancer) checkParams() error {
+	// 校验配置参数
+	validate := validator.New()
+	b.runtime.Logger.Info("start to validate parameters of clusterBalancer")
+	if err := validate.Struct(b.ConfParams); err != nil {
+		b.runtime.Logger.Error(fmt.Sprintf("validate parameters of clusterBalancer fail, error:%s", err))
+		return fmt.Errorf("validate parameters of clusterBalancer fail, error:%s", err)
+	}
+	b.runtime.Logger.Info("validate parameters of clusterBalancer successfully")
+	return nil
+}
+
+// execScript 执行脚本
+func (b *Balancer) execScript() error {
+	// 检查状态
+	b.runtime.Logger.Info("start to get balancer status")
+	result, err := common.CheckBalancer(b.Mongo, b.ConfParams.IP, b.ConfParams.Port,
+		b.ConfParams.AdminUsername, b.ConfParams.AdminPassword)
+	if err != nil {
+		b.runtime.Logger.Error("get cluster balancer status fail, error:%s", err)
+		return fmt.Errorf("get cluster balancer status fail, error:%s", err)
+	}
+	flag, _ := strconv.ParseBool(result)
+	b.runtime.Logger.Info("get balancer status successfully")
+	if flag == b.ConfParams.Open {
+		b.runtime.Logger.Info("balancer status has been %t", b.ConfParams.Open)
+		os.Exit(0)
+	}
+
+	// 执行脚本
+	var cmd string
+	if b.ConfParams.Open == true {
+		cmd = fmt.Sprintf(
+			"%s -u %s -p '%s' --host %s --port %d --authenticationDatabase=admin --quiet --eval \"sh.startBalancer()\"",
+			b.Mongo, b.ConfParams.AdminUsername, b.ConfParams.AdminPassword, b.ConfParams.IP, b.ConfParams.Port)
+	} else {
+		cmd = fmt.Sprintf(
+			"%s -u %s -p '%s' --host %s --port %d --authenticationDatabase=admin --quiet --eval \"sh.stopBalancer()\"",
+			b.Mongo, b.ConfParams.AdminUsername, b.ConfParams.AdminPassword, b.ConfParams.IP, b.ConfParams.Port)
+	}
+	b.runtime.Logger.Info("start to execute script")
+	_, err = util.RunBashCmd(
+		cmd,
+		"", nil,
+		10*time.Second)
+	if err != nil {
+		b.runtime.Logger.Error("set cluster balancer status fail, error:%s", err)
+		return fmt.Errorf("set cluster balancer status fail, error:%s", err)
+	}
+	b.runtime.Logger.Info("execute script successfully")
+
+	// 检查状态
+	b.runtime.Logger.Info("start to check balancer status")
+	result, err = common.CheckBalancer(b.Mongo, b.ConfParams.IP, b.ConfParams.Port,
+		b.ConfParams.AdminUsername, b.ConfParams.AdminPassword)
+	if err != nil {
+		b.runtime.Logger.Error("get cluster balancer status fail, error:%s", err)
+		return fmt.Errorf("get cluster balancer status fail, error:%s", err)
+	}
+	flag, _ = strconv.ParseBool(result)
+	b.runtime.Logger.Info("check balancer status successfully")
+	if flag == b.ConfParams.Open {
+		b.runtime.Logger.Info("set balancer status:%t successfully", b.ConfParams.Open)
+	}
+
+	return nil
+}
---
+++ dbm-services/mongo/db-tools/dbactuator/pkg/consts/test.go
@@ -0,0 +1,94 @@
+package consts
+
+import "fmt"
+
+// test consts
+const (
+	// ----- tendisplus  master指定的端口范围 [11000,11999] ------
+	// ----- tendisplus slave指定的端口范围 [12000,12999] ------
+	// TestTendisPlusMasterStartPort master start port
+	TestTendisPlusMasterStartPort = 11000
+	// TestTendisPlusSlaveStartPort slave start port
+	TestTendisPlusSlaveStartPort = 12000
+
+	// ExpansionTestTendisPlusMasterStartPort master start port
+	ExpansionTestTendisPlusMasterStartPort = 11100
+	// ExpansionTestTendisPlusSlaveStartPort slave start port
+	ExpansionTestTendisPlusSlaveStartPort = 12100
+
+	// SlotTestTendisPlusMasterPort master start port
+	SlotTestTendisPlusMasterPort = 11200
+	// SLotTestTendisPlusSlaveStart slave start port
+	SLotTestTendisPlusSlaveStart = 12200
+	// SlotsMigrateTest 指定迁移slot
+	SlotsMigrateTest = "0-100"
+
+	// TestSyncTendisPlusMasterStartPort make sync /redo slave
+	TestSyncTendisPlusMasterStartPort = 11300
+	// TestSyncTendisPlusSlaveStartPort make sync /
+	TestSyncTendisPlusSlaveStartPort = 12300
+
+	// ----- cache redis master指定的端口范围 [13000,13999] ------
+	// ----- cache redis slave指定的端口范围 [14000,14999] ------
+
+	// TestRedisMasterStartPort master start port
+	TestRedisMasterStartPort = 13000
+	// TestRedisSlaveStartPort slave start port
+	TestRedisSlaveStartPort = 14000
+
+	// TestSyncRedisMasterStartPort make sync /redo slave
+	TestSyncRedisMasterStartPort = 13300
+	// TestSyncRedisSlaveStartPort make sync /
+	TestSyncRedisSlaveStartPort = 14300
+
+	// ----- tendisssd master指定的端口范围 [14000,14999] ------
+	// ----- tendisssd slave指定的端口范围 [15000,15999] ------
+
+	// TestTendisSSDMasterStartPort master start port
+	TestTendisSSDMasterStartPort = 15000
+	// TestTendisSSDSlaveStartPort slave start port
+	TestTendisSSDSlaveStartPort = 16000
+
+	// TestTwemproxyPort twemproxy port
+	TestTwemproxyPort = 50100
+	// TestPredixyPort predixy port
+	TestPredixyPort = 50200
+	// TestSSDClusterTwemproxyPort twemproxy port
+	TestSSDClusterTwemproxyPort = 50300
+
+	// TestRedisInstanceNum instance number
+	TestRedisInstanceNum = 4
+
+	// ExpansionTestRedisInstanceNum instance number
+	ExpansionTestRedisInstanceNum = 2
+	// SLotTestRedisInstanceNum instance number
+	SLotTestRedisInstanceNum = 1
+)
+const (
+	// RedisTestPasswd redis test password
+	RedisTestPasswd = "redisPassTest"
+	// ProxyTestPasswd proxy test password
+	ProxyTestPasswd = "proxyPassTest"
+)
+
+// test uid/rootid/nodeid
+const (
+	TestUID    = 1111
+	TestRootID = 2222
+	TestNodeID = 3333
+)
+
+var (
+	// ActuatorTestCmd actuator测试命令
+	ActuatorTestCmd = fmt.Sprintf(
+		// NOCC:tosa/linelength(设计如此)
+		"cd %s && ./dbactuator_redis  --uid=%d --root_id=%d --node_id=%d --version_id=v1 --atom-job-list=%%q  --payload=%%q --payload-format=raw",
+		PackageSavePath, TestUID, TestRootID, TestNodeID)
+)
+
+const (
+	// PayloadFormatRaw raw
+	PayloadFormatRaw = "raw"
+	// PayloadFormatBase64 base64
+	PayloadFormatBase64 = "base64"
+)
