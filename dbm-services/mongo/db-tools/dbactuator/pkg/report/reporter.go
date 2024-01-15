1c8d8283c651ea5a0447e62f237a99065a830c5d
yyhenryyy <henryyyang@tencent.com>
2024-01-15 11:41:47 +0800

    feat(mongodb): 增加原子任务，cluster安装flow #3018

---
+++ dbm-services/mongo/db-tools/dbactuator/pkg/report/reporter.go
@@ -0,0 +1,58 @@
+package report
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"dbm-services/mongo/db-tools/dbactuator/mylog"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/consts"
+	"dbm-services/mongo/db-tools/dbactuator/pkg/util"
+)
+
+// Reporter 上报接口
+type Reporter interface {
+	AddRecord(item string, flush bool) error
+	Close() error
+}
+
+// CreateReportDir 创建上报目录 /home/mysql/dbareport -> {REDIS_BACKUP_DIR}/dbbak/dbareport
+func CreateReportDir() (err error) {
+	mylog.Logger.Info("begin to create reportDir(%s)", consts.DbaReportSaveDir)
+	var realLink string
+	realReportDir := filepath.Join(consts.GetRedisBackupDir(), "dbbak", "dbareport") // 如 /data/dbbak/dbareport
+	if !util.FileExists(realReportDir) {
+		err = util.MkDirsIfNotExists([]string{realReportDir})
+		if err != nil {
+			mylog.Logger.Error(err.Error())
+			return
+		}
+	}
+	util.LocalDirChownMysql(realReportDir)
+	if util.FileExists(consts.DbaReportSaveDir) {
+		realLink, err = filepath.EvalSymlinks(consts.DbaReportSaveDir)
+		if err != nil {
+			err = fmt.Errorf("filepath.EvalSymlinks %s fail,err:%v", consts.DbaReportSaveDir, err)
+			mylog.Logger.Error(err.Error())
+			return err
+		}
+		// /home/mysql/dbareport -> /data/dbbak/dbareport ok,直接返回
+		if realLink == realReportDir {
+			return nil
+		}
+		// 如果 /home/mysql/dbareport 不是指向 /data/dbbak/dbareport,先删除
+		rmCmd := "rm -rf " + consts.DbaReportSaveDir
+		util.RunBashCmd(rmCmd, "", nil, 1*time.Minute)
+	}
+	err = os.Symlink(realReportDir, filepath.Dir(consts.DbaReportSaveDir))
+	if err != nil {
+		err = fmt.Errorf("os.Symlink %s -> %s fail,err:%s", consts.DbaReportSaveDir, realReportDir, err)
+		mylog.Logger.Error(err.Error())
+		return
+	}
+	mylog.Logger.Info("create softLink success,%s -> %s", consts.DbaReportSaveDir, realReportDir)
+	util.MkDirsIfNotExists([]string{consts.RedisReportSaveDir})
+	util.LocalDirChownMysql(consts.DbaReportSaveDir)
+	return
+}
---
+++ dbm-services/mongo/db-tools/dbactuator/tests/test_mongo.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+source /etc/profile
+repoUser=""
+repoPassword=""
+
+mongodbPkgName="mongodb-linux-x86_64-3.4.20.tar.gz"
+mongodbPkgMd5="e68d998d75df81b219e99795dec43ffb"
+
+localMongodbPkgName="/data/install/$mongodbPkgName"
+localMongodbPkgMd5=""
+
+usage() {
+    echo -e "Usage: $0 [OPTIONS]"
+    echo -e ""
+    echo -e "-H --help -h required,display help info"
+    echo -e "--repo-user required,bk repo user name,"
+    echo -e "--repo-password required,bk repo user password"
+    echo -e ""
+    exit 1
+}
+
+if [[ $# -lt 2 ]]; then
+    usage
+fi
+
+for i in "$@"; do
+    case $i in
+    --repo-user=*)
+        repoUser="${i#*=}"
+        shift
+        ;;
+    --repo-password=*)
+        repoPassword="${i#*=}"
+        shift
+        ;;
+      --repo-url=*)
+        repoUrl="${i#*=}"
+        shift
+        ;;
+    *)
+        echo -e "unknown option:$i"
+        usage
+        ;;
+    esac
+done
+
+if [[ -z $repoUser ]]; then
+    echo -e "error: --repo-user must be passed,repoUser=$repoUser"
+    usage
+fi
+
+if [[ -z $repoPassword ]]; then
+    echo -e "error: --repo-password must be passed,repoPassword=$repoPassword"
+    usage
+fi
+
+if [[ -z $repoUrl ]]; then
+    echo -e "error: --repo-url must be passed,repoUrl=$repoUrl"
+    usage
+fi
+
+
+if [[ -e $localMongodbPkgName ]]; then
+    localMongodbPkgMd5=$(md5sum $localMongodbPkgName | awk '{print $1}')
+fi
+
+
+wgetCmd="wget --user=$repoUser --password=$repoPassword $repoUrl/mongodb/install_package/$mongodbPkgName -O $localMongodbPkgName"
+if [[ ! -e $localMongodbPkgName ]]; then
+    echo $wgetCmd
+    $wgetCmd
+elif [[ -n $localMongodbPkgMd5 && $localMongodbPkgMd5 != $mongodbPkgMd5 ]]; then
+    echo "rm -f $localMongodbPkgName"
+    rm -f $localMongodbPkgName
+    echo $wgetCmd
+    $wgetCmd
+fi
+
+cd $(dirname $0)/../pkg/atomjobs/atommongodb/
+go test -v
+rm -rf logs
+
