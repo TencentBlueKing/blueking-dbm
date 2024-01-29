5f5576a3dc4b3dbd8ee141978d03d1179600f46e
durant <826035498@qq.com>
2024-01-29 17:51:32 +0800

    fix(backend): 全局搜索补充返回字段 #3073
    fix(backend): 全局搜索补充返回字段 #3073

--- dbm-ui/backend/db_services/mysql/resources/tendbsingle/query.py
+++ dbm-ui/backend/db_services/mysql/resources/tendbsingle/query.py
@@ -8,8 +8,8 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from django.utils.translation import ugettext_lazy as _
 from django.db.models import F
+from django.utils.translation import ugettext_lazy as _
 
 from backend.db_meta.api.cluster.tendbsingle.detail import scan_cluster
 from backend.db_meta.enums.cluster_type import ClusterType
