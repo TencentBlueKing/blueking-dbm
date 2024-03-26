78877f8020180d4d0990cde42104885c4b450534
cycker <cycker@gmail.com>
2024-03-26 09:47:48 +0800

    fix(mongodb): 修改Makefile适应打包脚本 #3689

--- dbm-services/mongodb/db-tools/dbmon/package.sh
+++ dbm-services/mongodb/db-tools/dbmon/package.sh
@@ -4,7 +4,7 @@ repoVersion=0.0.1
 respGitHash=$(git rev-parse --short HEAD)
 respGitDate=202301010000
 
-# 解析传入的 --version=xxx 和 --git-hash=xxx 参数
+# 解析传入的 --version=xxx 和 --git-hash=xxx --git-date="yyyymmdd hhmmss"参数
 while [ $# -gt 0 ]; do
     case "$1" in
     --version=*)
@@ -24,28 +24,25 @@ done
 DIR=$(dirname $0)
 cd $DIR
 
-make build VERSION=$repoVersion GITHASH=$respGitHash
+make build VERSION=$repoVersion GITHASH=$respGitHash BUILDSTAMP="$respGitDate"
 
 cd package
-
 targetDir="bk-dbmon"
 tarName="$targetDir-mg.tar"
 
-if [[ ! -d $targetDir ]]; then
-    mkdir -p $targetDir
+if [[  -d $targetDir ]]; then
+    rm -rf $targetDir
 fi
+
+mkdir -p $targetDir
 
 cp ../build/bk-dbmon $targetDir/
 cp start.sh stop.sh gojq conn.sh $targetDir/
 chmod +x $targetDir/*.sh $targetDir/gojq $targetDir/bk-dbmon
 
-
 if [[ -e $tarName ]]; then
     rm -rf $tarName
 fi
-
-
-
 
 # 为了保证 tar 压缩得到的包的 md5 一致，这里修改文件的时间戳，同时把 tar -zcf 拆为 tar -cf && gzip
 find ${targetDir} -exec touch -t $respGitDate {} +
@@ -56,4 +53,11 @@ if [[ -e $targetDir ]]; then
     rm -rf $targetDir
 fi
 
-echo "$tarName.gz success"
+mv ${tarName}.gz ../build/
+
+if [ $? -eq 0 ]; then
+    echo "package build/${tarName}.gz success"
+else
+    echo "package build/${tarName}.gz failed"
+fi
+
