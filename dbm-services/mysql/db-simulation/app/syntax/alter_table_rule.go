4a349b85bd8b80fdcc4f447876f63b8ae2704999
ymakedaq <996156275@qq.com>
2024-03-27 09:47:58 +0800

    refactor(dbm-services): 语法关键字检查大小写问题 close #3715

--- dbm-services/mysql/db-simulation/app/syntax/alter_table_rule.go
+++ dbm-services/mysql/db-simulation/app/syntax/alter_table_rule.go
@@ -1,6 +1,19 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package syntax
 
-import util "dbm-services/common/go-pubpkg/cmutil"
+import (
+	util "dbm-services/common/go-pubpkg/cmutil"
+	"dbm-services/common/go-pubpkg/logger"
+)
 
 // Checker TODO
 func (c AlterTableResult) Checker(mysqlVersion string) (r *CheckerResult) {
@@ -9,6 +22,13 @@ func (c AlterTableResult) Checker(mysqlVersion string) (r *CheckerResult) {
 		r.Parse(R.AlterTableRule.HighRiskType, altercmd.Type, "")
 		r.Parse(R.AlterTableRule.HighRiskPkAlterType, altercmd.GetPkAlterType(), "")
 		r.Parse(R.AlterTableRule.AlterUseAfter, altercmd.After, "")
+		// 如果是增加字段，需要判断增加的字段名称是否是关键字
+		if altercmd.Type == ALTER_TYPE_ADD_COLUMN {
+			logger.Info("col name is %s", altercmd.ColDef.ColName)
+			r.ParseBultinRisk(func() (bool, string) {
+				return KeyWordValidator(mysqlVersion, altercmd.ColDef.ColName)
+			})
+		}
 	}
 	r.Parse(R.AlterTableRule.AddColumnMixed, c.GetAllAlterType(), "")
 	return
