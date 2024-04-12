2c2d145f5086ae639a912e05fba704a6313d99cd
hlinx <327159425@qq.com>
2024-04-12 16:21:40 +0800

    feature(frontend): 前端支持 iframe 嵌入 #3958

---
+++ dbm-ui/frontend/src/SubApp.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <DbRouterView style="height: 100%" />
+  </div>
+</template>
+<script setup lang="ts">
+  import DbRouterView from '@components/db-router-view/Index.vue';
+</script>
