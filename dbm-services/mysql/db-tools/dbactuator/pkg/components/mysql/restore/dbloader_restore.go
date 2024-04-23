35492b997fa630b8cc1b77092f7cb8332a2e27b9
seanlook <seanlook7@gmail.com>
2024-04-23 15:27:42 +0800

    fix(mysql): 修复spider 备份恢复的问题 #4136

--- dbm-services/mysql/db-tools/dbactuator/pkg/components/mysql/restore/dbloader_restore.go
+++ dbm-services/mysql/db-tools/dbactuator/pkg/components/mysql/restore/dbloader_restore.go
@@ -193,11 +193,11 @@ func (m *DBLoader) getChangeMasterPos(masterInst native.Instance) (*mysqlutil.Ch
 	logger.Info("metadata: %+v", m.indexObj.BinlogInfo)
 	masterInfo := m.indexObj.BinlogInfo.ShowMasterStatus
 	slaveInfo := m.indexObj.BinlogInfo.ShowSlaveStatus
-	if masterInfo == nil || masterInfo.BinlogFile == "" {
-		return nil, errors.New("no master info found in metadata")
-	}
 	if masterInst.Host == "" || masterInst.Port == 0 { // 说明不关注备份位点信息
 		return &mysqlutil.ChangeMaster{}, nil
+	}
+	if masterInfo == nil || masterInfo.BinlogFile == "" {
+		return nil, errors.New("no master info found in metadata")
 	}
 	// 如果备份文件的源实例，就是当前恢复要change master to 的实例，直接用 MasterStatus info
 	if masterInfo.MasterHost == masterInst.Host && masterInfo.MasterPort == masterInst.Port {
