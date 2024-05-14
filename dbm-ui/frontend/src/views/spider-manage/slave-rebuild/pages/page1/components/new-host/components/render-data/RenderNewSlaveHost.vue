7c91e960ef21690dae97cf2b4d014a0ab2fd2b82
3octaves <873551943@qq.com>
2024-05-14 10:30:19 +0800

    fix(frontend): spider暂时仅支持资源池自动匹配 #4400

--- dbm-ui/frontend/src/views/spider-manage/slave-rebuild/pages/page1/components/new-host/components/render-data/RenderNewSlaveHost.vue
+++ dbm-ui/frontend/src/views/spider-manage/slave-rebuild/pages/page1/components/new-host/components/render-data/RenderNewSlaveHost.vue
@@ -19,7 +19,6 @@
     :list="optionList"
     :model-value="localValue"
     :placeholder="t('请选择')"
-    :rules="rules"
     :select-disabled="!clusterData"
     :select-display-fun="selectDisplayFun"
     @change="(value) => handleChange(value as string)">
@@ -30,7 +29,7 @@
       </div>
     </template>
   </TableEditSelect>
-  <ResourcePoolManualSelector
+  <!-- <ResourcePoolManualSelector
     v-model:is-show="isShowResourcePoolSelector"
     :disable-dialog-submit-method="disablePoolDialogSubmitMethod"
     :disable-host-method="disablePoolHostMethod"
@@ -75,38 +74,36 @@
         </span>
       </I18nT>
     </template>
-  </IpSelector>
+  </IpSelector> -->
 </template>
 
-<script lang="ts">
+<!-- <script lang="ts">
   const poolHostSelectMemo: { [key: string]: Record<string, boolean> } = {};
   const idleHostSelectMemo: { [key: string]: Record<string, boolean> } = {};
-</script>
+</script> -->
 
 <script lang="ts" setup>
   import { useI18n } from 'vue-i18n';
 
-  import DbResourceModel from '@services/model/db-resource/DbResource';
-  import { fetchList, getSpecResourceCount } from '@services/source/dbresourceResource';
+  // import DbResourceModel from '@services/model/db-resource/DbResource';
+  // import { fetchList, getSpecResourceCount } from '@services/source/dbresourceResource';
+  import { getSpecResourceCount } from '@services/source/dbresourceResource';
   import { getResourceSpecList } from '@services/source/dbresourceSpec'
-  import { checkHost, getHostTopo } from '@services/source/ipchooser'
-  import type { HostDetails } from '@services/types/ip';
 
+  // import { checkHost, getHostTopo } from '@services/source/ipchooser'
+  // import type { HostDetails } from '@services/types/ip';
   import { useGlobalBizs } from '@stores';
 
   import { ClusterTypes } from '@common/const';
 
-  import IpSelector from '@components/ip-selector/IpSelector.vue';
-
+  // import IpSelector from '@components/ip-selector/IpSelector.vue';
   import TableEditSelect from '@views/spider-manage/common/edit/SelectInput.vue';
 
-  import { random } from '@utils';
-
-  import ResourcePoolManualSelector from '../resource-pool-manual-selector/Index.vue';
-
+  // import { random } from '@utils';
+  // import ResourcePoolManualSelector from '../resource-pool-manual-selector/Index.vue';
   import type { IDataRow } from './Row.vue';
 
-  import { ipv4 } from '@/common/regex';
+  // import { ipv4 } from '@/common/regex';
 
   interface Props {
     clusterData?: IDataRow['oldSlave'];
@@ -121,9 +118,9 @@
   const { t } = useI18n();
   const { currentBizId } = useGlobalBizs();
 
-  const instanceKey = `render_host_instance_key_${random()}`;
-  poolHostSelectMemo[instanceKey] = {};
-  idleHostSelectMemo[instanceKey] = {};
+  // const instanceKey = `render_host_instance_key_${random()}`;
+  // poolHostSelectMemo[instanceKey] = {};
+  // idleHostSelectMemo[instanceKey] = {};
 
   const optionList = [
     {
@@ -131,119 +128,119 @@
       name: t('资源池自动匹配'),
       tooltips: t('当前为资源池自动匹配，切换类型将会清空并需重新选择')
     },
-    {
-      id: 'resource_pool_manual',
-      name: t('资源池手动选择'),
-      tooltips: t('当前填写的主机为资源池主机，切换类型将会清空并需重新选择')
-    },
-    {
-      id: 'manual_input',
-      name: t('业务空闲机'),
-      tooltips: t('当前填写的主机为业务空闲机，切换类型将会清空并需重新选择')
-    },
+    // {
+    //   id: 'resource_pool_manual',
+    //   name: t('资源池手动选择'),
+    //   tooltips: t('当前填写的主机为资源池主机，切换类型将会清空并需重新选择')
+    // },
+    // {
+    //   id: 'manual_input',
+    //   name: t('业务空闲机'),
+    //   tooltips: t('当前填写的主机为业务空闲机，切换类型将会清空并需重新选择')
+    // },
   ];
 
-  const rules = [
-    {
-      validator: (value: string) => {
-        if (['resource_pool_manual', 'manual_input'].includes(value)) {
-          return false
-        }
-        return Boolean(value)
-      },
-      message: t('新从库主机不能为空'),
-    },
-    {
-      validator: (value: string) => {
-        if (value !== "resource_pool") {
-          return ipv4.test(value)
-        }
-        return true
-      },
-      message: t('IP格式错误'),
-    },
-    {
-      validator: (value: string) => {
-        const currentSelectItem = editSelectRef.value!.getCurrentItem()
-        if (currentSelectItem && currentSelectItem.id === "resource_pool_manual") {
-          return fetchList({
-            bk_biz_id: currentBizId,
-            hosts: value,
-            limit: 10,
-            offset: 0
-          }).then(resourceResult => {
-            if (resourceResult.results.length === 0) {
-              return false
-            }
-            poolHostSelectMemo[instanceKey] = {};
-            localHostList.value = resourceResult.results.map(hostItem => ({
-              bk_biz_id: hostItem.bk_biz_id,
-              bk_cloud_id: hostItem.bk_cloud_id,
-              bk_host_id: hostItem.bk_host_id,
-              ip: hostItem.ip
-            }));
-            poolHostSelectMemo[instanceKey][value] = true;
-            return true
-          })
-        }
-        return true
-      },
-      message: t('该主机在资源池中不存在'),
-    },
-    {
-      validator: (value: string) => {
-        const currentSelectItem = editSelectRef.value!.getCurrentItem()
-        if (currentSelectItem && currentSelectItem.id === "manual_input") {
-          return checkHost({
-            ip_list: [value],
-            mode: "idle_only",
-            scope_list: [
-              {
-                scope_id: currentBizId,
-                scope_type: "biz",
-                bk_cloud_id: props.clusterData?.bkCloudId || 0
-              }
-            ]
-          }).then(hostList => {
-            if (hostList.length === 0) {
-              return false
-            }
-            idleHostSelectMemo[instanceKey] = {};
-            localHostList.value = hostList.map(hostItem => ({
-              bk_biz_id: hostItem.biz.id,
-              bk_cloud_id: hostItem.cloud_id,
-              bk_host_id: hostItem.host_id,
-              ip: hostItem.ip
-            }));
-            idleHostSelectMemo[instanceKey][value] = true;
-            return true
-          })
-        }
-        return true
-      },
-      message: t('该业务空闲机中不存在'),
-    },
-    {
-      validator: (value: string) => {
-        const otherAllSelectHostMap = getOtherAllSelectHostMap(poolHostSelectMemo)
-        if (otherAllSelectHostMap[value]) {
-          return false;
-        }
-        return true;
-      },
-      message: t('资源池IP重复'),
-    },
-    {
-      validator: (value: string) => {
-        const otherAllSelectHostMap = getOtherAllSelectHostMap(idleHostSelectMemo)
-        if (otherAllSelectHostMap[value]) {
-          return false;
-        }
-        return true;
-      },
-      message: t('业务空闲机IP重复'),
-    },
-  ];
+  // const rules = [
+  // {
+  //   validator: (value: string) => {
+  //     if (['resource_pool_manual', 'manual_input'].includes(value)) {
+  //       return false
+  //     }
+  //     return Boolean(value)
+  //   },
+  //   message: t('新从库主机不能为空'),
+  // },
+  // {
+  //   validator: (value: string) => {
+  //     if (value !== "resource_pool") {
+  //       return ipv4.test(value)
+  //     }
+  //     return true
+  //   },
+  //   message: t('IP格式错误'),
+  // },
+  // {
+  //   validator: (value: string) => {
+  //     const currentSelectItem = editSelectRef.value!.getCurrentItem()
+  //     if (currentSelectItem && currentSelectItem.id === "resource_pool_manual") {
+  //       return fetchList({
+  //         bk_biz_id: currentBizId,
+  //         hosts: value,
+  //         limit: 10,
+  //         offset: 0
+  //       }).then(resourceResult => {
+  //         if (resourceResult.results.length === 0) {
+  //           return false
+  //         }
+  //         poolHostSelectMemo[instanceKey] = {};
+  //         localHostList.value = resourceResult.results.map(hostItem => ({
+  //           bk_biz_id: hostItem.bk_biz_id,
+  //           bk_cloud_id: hostItem.bk_cloud_id,
+  //           bk_host_id: hostItem.bk_host_id,
+  //           ip: hostItem.ip
+  //         }));
+  //         poolHostSelectMemo[instanceKey][value] = true;
+  //         return true
+  //       })
+  //     }
+  //     return true
+  //   },
+  //   message: t('该主机在资源池中不存在'),
+  // },
+  // {
+  //   validator: (value: string) => {
+  //     const currentSelectItem = editSelectRef.value!.getCurrentItem()
+  //     if (currentSelectItem && currentSelectItem.id === "manual_input") {
+  //       return checkHost({
+  //         ip_list: [value],
+  //         mode: "idle_only",
+  //         scope_list: [
+  //           {
+  //             scope_id: currentBizId,
+  //             scope_type: "biz",
+  //             bk_cloud_id: props.clusterData?.bkCloudId || 0
+  //           }
+  //         ]
+  //       }).then(hostList => {
+  //         if (hostList.length === 0) {
+  //           return false
+  //         }
+  //         idleHostSelectMemo[instanceKey] = {};
+  //         localHostList.value = hostList.map(hostItem => ({
+  //           bk_biz_id: hostItem.biz.id,
+  //           bk_cloud_id: hostItem.cloud_id,
+  //           bk_host_id: hostItem.host_id,
+  //           ip: hostItem.ip
+  //         }));
+  //         idleHostSelectMemo[instanceKey][value] = true;
+  //         return true
+  //       })
+  //     }
+  //     return true
+  //   },
+  //   message: t('该业务空闲机中不存在'),
+  // },
+  // {
+  //   validator: (value: string) => {
+  //     const otherAllSelectHostMap = getOtherAllSelectHostMap(poolHostSelectMemo)
+  //     if (otherAllSelectHostMap[value]) {
+  //       return false;
+  //     }
+  //     return true;
+  //   },
+  //   message: t('资源池IP重复'),
+  // },
+  // {
+  //   validator: (value: string) => {
+  //     const otherAllSelectHostMap = getOtherAllSelectHostMap(idleHostSelectMemo)
+  //     if (otherAllSelectHostMap[value]) {
+  //       return false;
+  //     }
+  //     return true;
+  //   },
+  //   message: t('业务空闲机IP重复'),
+  // },
+  // ];
 
   const editSelectRef = ref<InstanceType<typeof TableEditSelect>>();
   const localValue = ref('resource_pool');
@@ -282,88 +279,88 @@
           })
         }
       })
-      fetchList({
-        bk_biz_id: currentBizId,
-        limit: 0,
-        offset: 0
-      }).then(resourceResult => {
-        const {length} = resourceResult.results
-        if (length !== 0) {
-          countMap.resource_pool_manual = length
-        }
-      })
-      getHostTopo({
-        mode: "idle_only",
-        all_scope: true,
-        scope_list: [
-          {
-            scope_id: currentBizId,
-            scope_type: "biz",
-            bk_cloud_id: newClusterData.bkCloudId
-          }
-        ]
-      }).then(hostTopoResult => {
-        countMap.manual_input = hostTopoResult[0].count
-      })
+      // fetchList({
+      //   bk_biz_id: currentBizId,
+      //   limit: 0,
+      //   offset: 0
+      // }).then(resourceResult => {
+      //   const {length} = resourceResult.results
+      //   if (length !== 0) {
+      //     countMap.resource_pool_manual = length
+      //   }
+      // })
+      // getHostTopo({
+      //   mode: "idle_only",
+      //   all_scope: true,
+      //   scope_list: [
+      //     {
+      //       scope_id: currentBizId,
+      //       scope_type: "biz",
+      //       bk_cloud_id: newClusterData.bkCloudId
+      //     }
+      //   ]
+      // }).then(hostTopoResult => {
+      //   countMap.manual_input = hostTopoResult[0].count
+      // })
     }
   }, {
     immediate: true
   })
 
-  const getOtherAllSelectHostMap = (hostSelectMemo: { [key: string]: Record<string, boolean> }) => {
-    const otherHostSelectMemo = { ...hostSelectMemo };
-    delete otherHostSelectMemo[instanceKey];
-    return Object.values(otherHostSelectMemo).reduce((result, selectItem) => ({
-      ...result,
-      ...selectItem,
-    }), {} as Record<string, boolean>);
-  }
+  // const getOtherAllSelectHostMap = (hostSelectMemo: { [key: string]: Record<string, boolean> }) => {
+  //   const otherHostSelectMemo = { ...hostSelectMemo };
+  //   delete otherHostSelectMemo[instanceKey];
+  //   return Object.values(otherHostSelectMemo).reduce((result, selectItem) => ({
+  //     ...result,
+  //     ...selectItem,
+  //   }), {} as Record<string, boolean>);
+  // }
 
-  const disablePoolDialogSubmitMethod = (list: DbResourceModel[]) => list.length >= 1 ? false : t('还差n台，请先勾选足够的IP', { n: 1 - list.length })
+  // const disablePoolDialogSubmitMethod = (list: DbResourceModel[]) => list.length >= 1 ? false : t('还差n台，请先勾选足够的IP', { n: 1 - list.length })
 
-  const disablePoolHostMethod = (data: DbResourceModel, list: DbResourceModel[]) => {
-    const otherAllSelectHostMap = getOtherAllSelectHostMap(poolHostSelectMemo)
-    if (otherAllSelectHostMap[data.ip]) {
-      return t('已被其他行选中');
-    }
-    if (list.length >= 1) {
-      return t('仅需n台', { n: 1 })
-    }
-    return false
-  }
+  // const disablePoolHostMethod = (data: DbResourceModel, list: DbResourceModel[]) => {
+  //   const otherAllSelectHostMap = getOtherAllSelectHostMap(poolHostSelectMemo)
+  //   if (otherAllSelectHostMap[data.ip]) {
+  //     return t('已被其他行选中');
+  //   }
+  //   if (list.length >= 1) {
+  //     return t('仅需n台', { n: 1 })
+  //   }
+  //   return false
+  // }
 
-  const disableIdleDialogSubmitMethod = (hostList: HostDetails[]) => hostList.length === 1 ? false : t('需n台', { n: 1 });
+  // const disableIdleDialogSubmitMethod = (hostList: HostDetails[]) => hostList.length === 1 ? false : t('需n台', { n: 1 });
 
-  const disableIdleHostMethod = (data: HostDetails, list: HostDetails[]) => {
-    const otherAllSelectHostMap = getOtherAllSelectHostMap(idleHostSelectMemo)
-    if (otherAllSelectHostMap[data.ip]) {
-      return t('已被其他行选中');
-    }
-    if (list.length >= 1) {
-      return t('仅需n台', { n: 1 })
-    }
-    return false
-  }
+  // const disableIdleHostMethod = (data: HostDetails, list: HostDetails[]) => {
+  //   const otherAllSelectHostMap = getOtherAllSelectHostMap(idleHostSelectMemo)
+  //   if (otherAllSelectHostMap[data.ip]) {
+  //     return t('已被其他行选中');
+  //   }
+  //   if (list.length >= 1) {
+  //     return t('仅需n台', { n: 1 })
+  //   }
+  //   return false
+  // }
 
-  const handleIdleHostChange = (hostList: HostDetails[]) => {
-    localHostList.value = hostList.map(hostItem => ({
-      bk_biz_id: hostItem.biz.id,
-      bk_cloud_id: hostItem.cloud_id,
-      bk_host_id: hostItem.host_id,
-      ip: hostItem.ip
-    }));
-    localValue.value = hostList.map(hostItem => hostItem.ip).join(',')
-  };
+  // const handleIdleHostChange = (hostList: HostDetails[]) => {
+  //   localHostList.value = hostList.map(hostItem => ({
+  //     bk_biz_id: hostItem.biz.id,
+  //     bk_cloud_id: hostItem.cloud_id,
+  //     bk_host_id: hostItem.host_id,
+  //     ip: hostItem.ip
+  //   }));
+  //   localValue.value = hostList.map(hostItem => hostItem.ip).join(',')
+  // };
 
-  const handlePoolHostChange = (hostList: DbResourceModel[]) => {
-    localHostList.value = hostList.map(hostItem => ({
-      bk_biz_id: hostItem.bk_biz_id,
-      bk_cloud_id: hostItem.bk_cloud_id,
-      bk_host_id: hostItem.bk_host_id,
-      ip: hostItem.ip
-    }));
-    localValue.value = hostList.map(hostItem => hostItem.ip).join(',')
-  }
+  // const handlePoolHostChange = (hostList: DbResourceModel[]) => {
+  //   localHostList.value = hostList.map(hostItem => ({
+  //     bk_biz_id: hostItem.bk_biz_id,
+  //     bk_cloud_id: hostItem.bk_cloud_id,
+  //     bk_host_id: hostItem.bk_host_id,
+  //     ip: hostItem.ip
+  //   }));
+  //   localValue.value = hostList.map(hostItem => hostItem.ip).join(',')
+  // }
 
   const handleChange = (value: string) => {
     if (value === 'manual_input') {
@@ -385,10 +382,10 @@
     }
     return value
   }
-  onBeforeUnmount(() => {
-    poolHostSelectMemo[instanceKey] = {};
-    idleHostSelectMemo[instanceKey] = {};
-  });
+  // onBeforeUnmount(() => {
+  //   poolHostSelectMemo[instanceKey] = {};
+  //   idleHostSelectMemo[instanceKey] = {};
+  // });
 
   defineExpose<Exposes>({
     getValue() {
