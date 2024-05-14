98500165505b9c31856aaf40d7b27767af1a3793
hlinx <327159425@qq.com>
2024-05-14 11:33:12 +0800

    feat(frontend): 业务选择框支持按业务缩写选择、搜索 #4370

---
+++ dbm-ui/frontend/src/components/app-select/RendeItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    ref="rootRef"
+    v-bk-tooltips="{
+      disabled: !Boolean(text),
+      content: text,
+      allowHtml: true,
+      extCls : 'db-app-select-tooltips',
+    }"
+    class="db-app-select-text">
+    <div
+      class="db-app-select-name"
+      :style="{
+        'max-width': text ? '175px': 'unset',
+        'flex': text ? '1 0 auto' : 'unset'
+      }">
+      {{ data.name }}
+    </div>
+    <div class="db-app-select-desc">
+      (#{{ data.bk_biz_id }}
+      <div
+        v-if="data.english_name"
+        class="db-app-select-en-name">
+        , {{ data.english_name }}
+      </div>)
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+
+  import { getBizs } from '@services/source/cmdb';
+
+  import { calcTextWidth } from '@utils';
+
+  defineProps<{
+    data: ServiceReturnType<typeof getBizs>[number]
+  }>();
+
+  const rootRef = ref<HTMLElement>();
+  const text = ref('');
+
+  onMounted(() => {
+    const renderText = rootRef.value!.innerText.replace(/\n/g, '');
+
+    const width = calcTextWidth(renderText, rootRef.value);
+    const {
+      width: maxWidth,
+    } = rootRef.value!.getBoundingClientRect();
+
+    console.log('rendertest = ', renderText, maxWidth, width);
+    text.value = width > maxWidth ? renderText : '';
+  });
+
+</script>
+
