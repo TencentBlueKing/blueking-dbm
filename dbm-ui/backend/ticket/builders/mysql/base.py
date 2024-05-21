45c25989e50d5b8c06154e3c056c413ffa62792e
iSecloud <869820505@qq.com>
2024-05-21 20:03:08 +0800

    fix(backend): slave异常不影响分区单据 #4515

--- dbm-ui/backend/ticket/builders/mysql/base.py
+++ dbm-ui/backend/ticket/builders/mysql/base.py
@@ -58,6 +58,7 @@ class MySQLBaseOperateDetailSerializer(SkipToRepresentationMixin, serializers.Se
         TicketType.MYSQL_RESTORE_LOCAL_SLAVE.value,
         TicketType.MYSQL_RESTORE_SLAVE.value,
         TicketType.MYSQL_HA_TRUNCATE_DATA.value,
+        TicketType.MYSQL_PARTITION.value,
     ]
     MASTER_UNAVAILABLE_WHITELIST = [
         TicketType.MYSQL_MASTER_FAIL_OVER.value,
