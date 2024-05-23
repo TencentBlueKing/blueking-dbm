30c1f0efc81fcafc5fbf06f2bf911b37665d3bd3
durant <826035498@qq.com>
2024-05-23 21:47:59 +0800

    feat(backend): 自定义监控策略限定目标范围 #4553

--- dbm-ui/backend/db_monitor/views/notice_group.py
+++ dbm-ui/backend/db_monitor/views/notice_group.py
@@ -10,7 +10,6 @@ specific language governing permissions and limitations under the License.
 """
 from collections import Counter
 
-import django_filters
 from django.db.models import OuterRef, Q, Subquery
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
@@ -28,38 +27,6 @@ from backend.db_monitor.serializers import NoticeGroupSerializer
 from backend.iam_app.handlers.drf_perm import DBManageIAMPermission
 
 SWAGGER_TAG = _("监控告警组")
-
-
-class MonitorGroupListFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains", label=_("告警组名称"))
-    bk_biz_id = django_filters.NumberFilter(field_name="bk_biz_id", label=_("业务ID"))
-    db_type = django_filters.CharFilter(field_name="db_type", label=_("DB类型"))
-
-    def filter_queryset(self, queryset):
-        # 1. 在指定业务和平台业务的告警组中过滤
-        bk_biz_id = self.form.cleaned_data.get("bk_biz_id", PLAT_BIZ_ID)
-        queryset = queryset.filter(bk_biz_id__in=(PLAT_BIZ_ID, bk_biz_id))
-
-        # 2. 指定告警组名字查询
-        name = self.form.cleaned_data.get("name", "")
-        if name:
-            subquery = queryset.filter(name__icontains=name, db_type=OuterRef("db_type")).order_by("-bk_biz_id")
-        else:
-            # 优先获取业务下的告警组，没有则有则返回平台告警组
-            subquery = queryset.filter(db_type=OuterRef("db_type")).order_by("-bk_biz_id")
-        queryset = queryset.filter(bk_biz_id=Subquery(subquery.values("bk_biz_id")[:1])).order_by("-update_at")
-
-        # 3. 获取业务下指定 db 类型的告警组，如果业务
-        db_type = self.form.cleaned_data.get("db_type")
-        if db_type:
-            db_type_group = queryset.filter(db_type=db_type).order_by("bk_biz_id").first()
-            group_id = getattr(db_type_group, "id", 0)
-            queryset = queryset.filter(Q(id=group_id) | Q(db_type=""))
-        return queryset
-
-    class Meta:
-        model = NoticeGroup
-        fields = ["bk_biz_id", "name", "db_type"]
 
 
 @method_decorator(
@@ -94,7 +61,6 @@ class MonitorNoticeGroupViewSet(viewsets.AuditedModelViewSet):
     queryset = NoticeGroup.objects.all().order_by("-update_at")
     serializer_class = NoticeGroupSerializer
     pagination_class = AuditedLimitOffsetPagination
-    filter_class = MonitorGroupListFilter
 
     def _get_custom_permissions(self):
         return [DBManageIAMPermission()]
@@ -104,6 +70,31 @@ class MonitorNoticeGroupViewSet(viewsets.AuditedModelViewSet):
         notify_groups = MonitorPolicy.objects.exclude(notify_groups=[]).values_list("notify_groups", flat=True)
         context["group_used"] = dict(Counter([item for group in notify_groups for item in group]))
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action == "list":
+            # 1. 在指定业务和平台业务的告警组中过滤
+            bk_biz_id = self.request.query_params.get("bk_biz_id", PLAT_BIZ_ID)
+            queryset = queryset.filter(bk_biz_id__in=(PLAT_BIZ_ID, bk_biz_id))
+
+            # 2. 指定告警组名字查询
+            name = self.request.query_params.get("name", "")
+            if name:
+                subquery = queryset.filter(name__icontains=name, db_type=OuterRef("db_type")).order_by("-bk_biz_id")
+            else:
+                # 优先获取业务下的告警组，没有则有则返回平台告警组
+                subquery = queryset.filter(db_type=OuterRef("db_type")).order_by("-bk_biz_id")
+            queryset = queryset.filter(bk_biz_id=Subquery(subquery.values("bk_biz_id")[:1])).order_by("-update_at")
+
+            # 3. 获取业务下指定 db 类型的告警组，如果业务
+            db_type = self.request.query_params.get("db_type")
+            if db_type:
+                db_type_group = queryset.filter(db_type=db_type).order_by("bk_biz_id").first()
+                group_id = getattr(db_type_group, "id", 0)
+                queryset = queryset.filter(Q(id=group_id) | Q(db_type=""))
+
+        return queryset
 
     @common_swagger_auto_schema(operation_summary=_("查询通知类型"), tags=[SWAGGER_TAG])
     @action(methods=["GET"], detail=False)
