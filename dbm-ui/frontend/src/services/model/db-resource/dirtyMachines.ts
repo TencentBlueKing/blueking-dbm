3b4419f38cb6fdb63f7590da78c5ab2387a2aa53
Austin <1344583166@qq.com>
2024-05-29 17:41:59 +0800

    feat(frontend): 污点主机支持标记为已处理操作 #4670

--- dbm-ui/frontend/src/services/model/db-resource/dirtyMachines.ts
+++ dbm-ui/frontend/src/services/model/db-resource/dirtyMachines.ts
@@ -4,7 +4,12 @@ export default class DirtyMachines {
   bk_cloud_id: number;
   bk_cloud_name: string;
   bk_host_id: number;
+  bk_module_infos: {
+    bk_module_id: number;
+    bk_module_name: number;
+  }[];
   ip: string;
+  is_dirty: boolean;
   task_id: string;
   ticket_id: number;
   ticket_type: string;
@@ -22,7 +27,9 @@ export default class DirtyMachines {
     this.bk_cloud_id = payload.bk_cloud_id;
     this.bk_cloud_name = payload.bk_cloud_name;
     this.bk_host_id = payload.bk_host_id;
+    this.bk_module_infos = payload.bk_module_infos ?? [];
     this.ip = payload.ip;
+    this.is_dirty = payload.is_dirty;
     this.task_id = payload.task_id;
     this.ticket_id = payload.ticket_id;
     this.ticket_type = payload.ticket_type;
