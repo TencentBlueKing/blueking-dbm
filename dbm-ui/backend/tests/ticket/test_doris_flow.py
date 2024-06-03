b5e06ed17f4a106902ce503877ebb34304dc2f85
ycggyao <yao18274936495@163.com>
2024-06-03 10:23:20 +0800

    feat(backend): doris views开发 #4612 # Reviewed, transaction id: 10456

--- dbm-ui/backend/tests/ticket/test_doris_flow.py
+++ dbm-ui/backend/tests/ticket/test_doris_flow.py
@@ -16,6 +16,7 @@ import pytest
 from django.conf import settings
 from rest_framework.test import APIClient
 
+from backend.db_meta.enums import ClusterType
 from backend.db_meta.models import AppCache, Cluster, Machine, Spec, StorageInstance
 from backend.db_meta.models.db_module import DBModule
 from backend.tests.mock_data import constant
@@ -25,8 +26,15 @@ from backend.tests.mock_data.ticket.doris_flow import (
     DORIS_DESTROY_TICKET_DATA,
     DORIS_DISABLE_TICKET_DATA,
     DORIS_ENABLE_TICKET_DATA,
+    DORIS_MACHINE_DATA,
+    DORIS_REBOOT_TICKET_DATA,
+    DORIS_REPLACE_TICKET_DATA,
+    DORIS_SHRINK_TICKET_DATA,
     DORIS_SOURCE_APPLICATION_DATA,
     DORIS_SPEC_DATA,
+    DORIS_STORAGE_INSTANCE_DATA,
+    SCALEUP_INPUT_TICKET_DATA,
+    SCALEUP_POOL_TICKET_DATA,
 )
 from backend.tests.ticket.server_base import TestFlowBase
 from backend.ticket.constants import TicketFlowStatus, TicketStatus
@@ -66,6 +74,26 @@ class TestDorisApplyFlow(TestFlowBase, TestCase):
     def test_doris_destroy_flow(self):
         self.flow_test(client, DORIS_DESTROY_TICKET_DATA)
 
+    # DORIS scale_up_pool: start --> itsm --> PAUSE --> INNER_FLOW --> end
+    def test_scaleup_pool_flow(self):
+        self.flow_test(client, SCALEUP_POOL_TICKET_DATA)
+
+    # DORIS scale_up_input: start --> itsm --> INNER_FLOW --> end
+    def test_scaleup_input_flow(self):
+        self.flow_test(client, SCALEUP_INPUT_TICKET_DATA)
+
+    # DORIS shrink: start --> itsm --> INNER_FLOW --> end
+    def test_doris_shrink_flow(self):
+        self.flow_test(client, DORIS_SHRINK_TICKET_DATA)
+
+    # DORIS reboot: start --> itsm --> PAUSE --> INNER_FLOW --> end
+    def test_doris_reboot_flow(self):
+        self.flow_test(client, DORIS_REBOOT_TICKET_DATA)
+
+    # DORIS replace: start --> itsm --> PAUSE --> INNER_FLOW --> end
+    def test_doris_replace_flow(self):
+        self.flow_test(client, DORIS_REPLACE_TICKET_DATA)
+
     def apply_patches(self):
         # 扩展基类的apply_patches方法来包括新的patch
         super().apply_patches()
@@ -75,17 +103,30 @@ class TestDorisApplyFlow(TestFlowBase, TestCase):
             "backend.ticket.flow_manager.resource.ResourceApplyFlow.apply_resource",
             lambda resource_request_id, node_infos: (1, DORIS_SOURCE_APPLICATION_DATA),
         )
+        mock_idle_pool_patch = patch(
+            "backend.ticket.builders.common.bigdata.BigDataDetailsSerializer.validate_hosts_from_idle_pool"
+        )
+        mock_db_meta_patch = patch(
+            "backend.ticket.builders.common.bigdata.BigDataDetailsSerializer.validate_hosts_not_in_db_meta"
+        )
         # 启动新的patch并添加到self.mocks列表中以便随后可以停止它
-        self.mocks.extend([mock_resource_apply_patch.start()])
+        self.mocks.extend(
+            [mock_resource_apply_patch.start(), mock_idle_pool_patch.start(), mock_db_meta_patch.start()]
+        )
 
     def setup(self):
         """
         测试的初始设置。
         """
-        AppCache.objects.create(bk_biz_id=constant.BK_BIZ_ID, db_app_abbr="DBA", bk_biz_name="dba")
         Spec.objects.all().delete()
+        AppCache.objects.get_or_create(bk_biz_id=constant.BK_BIZ_ID, db_app_abbr="DBA", bk_biz_name="dba")
         Spec.objects.bulk_create([Spec(**data) for data in DORIS_SPEC_DATA])
         Cluster.objects.bulk_create([Cluster(**data) for data in DORIS_CLUSTER_DATA])
+        Machine.objects.bulk_create([Machine(**data) for data in DORIS_MACHINE_DATA])
+        StorageInstance.objects.bulk_create([StorageInstance(**data) for data in DORIS_STORAGE_INSTANCE_DATA])
+        cluster = Cluster.objects.first()
+        storage_instance = StorageInstance.objects.filter(cluster_type=ClusterType.Doris.value)
+        cluster.storageinstance_set.set(storage_instance, clear=True)
         super().setup()
 
     def teardown(self):
---
+++ dbm-ui/backend/ticket/builders/doris/doris_scale_up.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+import logging
+
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+
+from backend.db_services.dbbase.constants import IpSource
+from backend.flow.engine.controller.doris import DorisController
+from backend.ticket import builders
+from backend.ticket.builders.common.bigdata import (
+    BaseDorisTicketFlowBuilder,
+    BigDataScaleDetailSerializer,
+    BigDataScaleUpResourceParamBuilder,
+)
+from backend.ticket.builders.doris.doris_apply import DorisApplyResourceParamBuilder
+from backend.ticket.constants import TicketType
+
+logger = logging.getLogger("root")
+
+
+class DorisScaleUpDetailSerializer(BigDataScaleDetailSerializer):
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+
+        if attrs["ip_source"] == IpSource.RESOURCE_POOL:
+            return attrs
+
+        role_nodes_list = list(attrs["nodes"].values())
+
+        node_list = []
+        for role_nodes in role_nodes_list:
+            node_list.extend(role_nodes)
+
+        instance_num_list = [node["instance_num"] for node in node_list if "instance_num" in node.keys()]
+        if instance_num_list and min(instance_num_list) <= 0:
+            raise serializers.ValidationError(_("实例数必须为正数，请确保实例的合法性"))
+
+        return attrs
+
+
+class DorisScaleUpResourceParamBuilder(BigDataScaleUpResourceParamBuilder):
+    def post_callback(self):
+        next_flow = self.ticket.next_flow()
+        DorisApplyResourceParamBuilder.fill_instance_num(
+            next_flow.details["ticket_data"], self.ticket_data, nodes_key="nodes"
+        )
+        next_flow.save(update_fields=["details"])
+
+
+class DorisScaleUpFlowParamBuilder(builders.FlowParamBuilder):
+    controller = DorisController.doris_scale_up_scene
+
+    def format_ticket_data(self):
+        """
+        {
+            "uid": 346,
+            "ticket_type": "DORIS_SCALE_UP",
+            "bk_biz_id": 2005000002,
+            "created_by": "admin",
+            "cluster_id": 123,
+            "nodes": {
+                "observer": [
+                    {
+                        "bk_cloud_id": 0,
+                        "bk_host_id": 0,
+                        "ip": "127.0.0.7"
+                    }
+                ],
+                "cold": [
+                    {
+                        "bk_cloud_id": 0,
+                        "bk_host_id": 0,
+                        "instance_num": 1,
+                        "ip": "127.0.0.2"
+                    }
+                ],
+                "hot": [
+                    {
+                        "bk_cloud_id": 0,
+                        "bk_host_id": 0,
+                        "instance_num": 1,
+                        "ip": "127.0.0.1"
+                    }
+                ]
+            },
+        }
+        """
+        super().format_ticket_data()
+
+
+@builders.BuilderFactory.register(TicketType.DORIS_SCALE_UP, is_apply=True)
+class DorisScaleUpFlowBuilder(BaseDorisTicketFlowBuilder):
+    serializer = DorisScaleUpDetailSerializer
+    inner_flow_builder = DorisScaleUpFlowParamBuilder
+    inner_flow_name = _("Doris集群扩容")
+    resource_apply_builder = DorisScaleUpResourceParamBuilder
