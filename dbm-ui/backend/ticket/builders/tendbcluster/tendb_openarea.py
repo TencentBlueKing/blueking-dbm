3e4ce61a628a2b26639ce545c9021f6b6c65d7cc
leifxu <leifxu@tencent.com>
2024-06-17 09:58:14 +0800

    feat(backend): 新增TendbCluster开区 #4968
    feat(backend): 新增TendbCluster开区 #4968

--- dbm-ui/backend/ticket/builders/tendbcluster/tendb_openarea.py
+++ dbm-ui/backend/ticket/builders/tendbcluster/tendb_openarea.py
@@ -8,32 +8,24 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from django.utils.translation import gettext_lazy as _
 
-from django.utils.translation import ugettext_lazy as _
-
-from backend.flow.engine.controller.spider import SpiderController
 from backend.ticket import builders
-from backend.ticket.builders.mysql.mysql_ha_clear import MySQLHaClearDetailSerializer
+from backend.ticket.builders.mysql.mysql_openarea import MysqlOpenAreaParamBuilder, MysqlOpenAreaDetailSerializer
 from backend.ticket.builders.tendbcluster.base import BaseTendbTicketFlowBuilder
 from backend.ticket.constants import TicketType
 
 
-class TendbClearDetailSerializer(MySQLHaClearDetailSerializer):
-    def validate(self, attrs):
-        return super().validate(attrs)
+class TendbOpenAreaDetailSerializer(MysqlOpenAreaDetailSerializer):
+    pass
 
 
-class TendbClearFlowParamBuilder(builders.FlowParamBuilder):
-    """TendbCluster清档执行单据参数"""
-
-    controller = SpiderController.truncate_database
-
-    def format_ticket_data(self):
-        pass
+class TendbOpenAreaParamBuilder(MysqlOpenAreaParamBuilder):
+    pass
 
 
-@builders.BuilderFactory.register(TicketType.TENDBCLUSTER_TRUNCATE_DATABASE)
-class TendbClearFlowBuilder(BaseTendbTicketFlowBuilder):
-    serializer = TendbClearDetailSerializer
-    inner_flow_builder = TendbClearFlowParamBuilder
-    inner_flow_name = _("TenDB Cluster 清档执行")
+@builders.BuilderFactory.register(TicketType.TENDBCLUSTER_OPEN_AREA)
+class TendbOpenAreaFlowBuilder(BaseTendbTicketFlowBuilder):
+    serializer = TendbOpenAreaDetailSerializer
+    inner_flow_builder = TendbOpenAreaParamBuilder
+    inner_flow_name = _("Tendb Cluster 开区执行")
