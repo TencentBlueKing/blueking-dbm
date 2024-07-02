cac9578cc7fb5c33dbed653a9f8e748d768a5b27
ymakedaq <996156275@qq.com>
2024-07-02 14:23:43 +0800

    fix(backend): 获取cluster_type #5309

--- dbm-ui/backend/flow/engine/bamboo/scene/mysql/mysql_migrate_cluster_remote_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/mysql/mysql_migrate_cluster_remote_flow.py
@@ -113,7 +113,7 @@ class MySQLMigrateClusterRemoteFlow(object):
             charset, db_version = get_version_and_charset(
                 bk_biz_id=cluster_class.bk_biz_id,
                 db_module_id=db_module_id,
-                cluster_type=self.data["cluster_type"],
+                cluster_type=cluster_class.cluster_type,
             )
 
             self.data["master_ip"] = master_model.machine.ip
