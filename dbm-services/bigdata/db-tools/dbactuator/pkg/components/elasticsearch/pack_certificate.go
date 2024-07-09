70504d6f0d41c72347e4c2782f585339568dd2d1
zvictorino <zvictorino@gmail.com>
2024-07-09 17:27:10 +0800

    fix(es): 扩容时同步ik分词器配置 #5473

--- dbm-services/bigdata/db-tools/dbactuator/pkg/components/elasticsearch/pack_certificate.go
+++ dbm-services/bigdata/db-tools/dbactuator/pkg/components/elasticsearch/pack_certificate.go
@@ -7,44 +7,48 @@ import (
 	"dbm-services/bigdata/db-tools/dbactuator/pkg/components"
 	"dbm-services/bigdata/db-tools/dbactuator/pkg/core/cst"
 	"dbm-services/bigdata/db-tools/dbactuator/pkg/rollback"
+	"dbm-services/bigdata/db-tools/dbactuator/pkg/util"
 	"dbm-services/bigdata/db-tools/dbactuator/pkg/util/osutil"
 	"dbm-services/common/go-pubpkg/logger"
 
 	"github.com/hashicorp/go-version"
 )
 
-// PackCerComp struct
+// PackCerComp 结构体定义了打包证书组件的参数和回滚上下文
 type PackCerComp struct {
 	GeneralParam    *components.GeneralParam
 	Params          *PackCerParams
 	RollBackContext rollback.RollBackObjects
 }
 
-// PackCerParams param
+// PackCerParams 结构体定义了打包证书组件的参数
 type PackCerParams struct {
 	EsVersion string `json:"es_version"`
 }
 
-// Init function
+// Init 初始化函数，当前仅用于日志记录
 func (d *PackCerComp) Init() (err error) {
 	logger.Info("Generate certificate fake init")
 	return nil
 }
 
-// PackCer 710 for pack es certificate files
+// PackCer 函数用于打包 Elasticsearch 证书文件
 // Usage: dbactuator es pack_certificate xxxxxx
 // For 7.10.2, copy key files and elasticsearch.yml.append  to  /tmp/
 // For 7.14.2, copy key files and elasticsearch.yml.append  to  /tmp/
 // Finally, it will output the tar.gz package in /tmp, named es_cerfiles.tar.gz
 // And the transfer files to other nodes
 func (d *PackCerComp) PackCer() (err error) {
+	// 解析 Elasticsearch 版本
 	v, _ := version.NewVersion(d.Params.EsVersion)
 	v7, _ := version.NewVersion(cst.ES7142)
 	cerFiles := cst.CerFile
+	// 根据版本选择不同的证书文件
 	if v.LessThan(v7) {
 		cerFiles = cst.CerFile710
 	}
 	confDir := cst.DefaulEsConfigDir
+	// 打包证书文件
 	if err := TarCerFiles(confDir, cerFiles); err != nil {
 		logger.Error("package cerfiles failed, msg [%s]", err)
 		return err
@@ -53,15 +57,15 @@ func (d *PackCerComp) PackCer() (err error) {
 	return nil
 }
 
-// TarCerFiles TODO
+// TarCerFiles 函数用于打包证书文件
 func TarCerFiles(dir string, cerFiles []string) error {
-	// First, change dir to /tmp
+	// 首先，切换到 /tmp 目录
 	if err := os.Chdir("/tmp"); err != nil {
 		logger.Error("Change dir to [tmp] failed %s", err)
 		return err
 	}
 
-	// For rerunable, delete ca files first
+	// 为了可重入性，先删除旧的证书文件
 	extraCmd := "rm -rf /tmp/{es_cerfiles.tar.gz,es_cerfiles}"
 	logger.Info("Delete certificate file/dir first,[%s]", extraCmd)
 	if output, err := osutil.ExecShellCommand(false, extraCmd); err != nil {
@@ -69,7 +73,7 @@ func TarCerFiles(dir string, cerFiles []string) error {
 		return err
 	}
 
-	// Create es_cerfiles dir
+	// 创建 es_cerfiles 目录
 	esCerDir := "/tmp/es_cerfiles"
 	extraCmd = fmt.Sprintf("mkdir -p %s", esCerDir)
 	logger.Info("Make dir, exec [%s]", extraCmd)
@@ -78,7 +82,7 @@ func TarCerFiles(dir string, cerFiles []string) error {
 		return err
 	}
 
-	// Copy key files and elasticsearch.yml.append file to /tmp/es_cerfiles
+	// 复制密钥文件和 elasticsearch.yml.append 文件到 /tmp/es_cerfiles
 	for _, f := range cerFiles {
 		// cp /data/esenv/es_1/config/xxx /tmp/es_cerfiles/
 		extraCmd = fmt.Sprintf("cp %s/%s %s", dir, f, esCerDir)
@@ -89,7 +93,22 @@ func TarCerFiles(dir string, cerFiles []string) error {
 		}
 	}
 
-	// In tmp dir, Tar es_cerfiles
+	// 复制 ik 或其他文件
+	for _, f := range cst.OtherESFiles {
+		ff := fmt.Sprintf("%s/%s", dir, f)
+		logger.Info(ff)
+		// 检查文件是否存在
+		if util.FileExists(ff) {
+			extraCmd = fmt.Sprintf("cp -r %s %s", ff, esCerDir)
+			logger.Info("Exec [%s]", extraCmd)
+			if output, err := osutil.ExecShellCommand(false, extraCmd); err != nil {
+				logger.Error("cmd [%s] failed, error message: [%s], error code: [%s]", extraCmd, output, err)
+				return err
+			}
+		}
+	}
+
+	// 在 /tmp 目录下打包 es_cerfiles
 	extraCmd = "tar zcf es_cerfiles.tar.gz es_cerfiles"
 	logger.Info("Tar dir [%s]", extraCmd)
 	if output, err := osutil.ExecShellCommand(false, extraCmd); err != nil {
