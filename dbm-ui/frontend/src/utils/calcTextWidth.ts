ee3a697495ab266545f213b8b2a1d2a45dffcd33
chenwenchang <479999519@qq.com>
2024-07-15 14:11:00 +0800

    feat(frontend): 健康校验失败从库实例详情表格 #5587

--- dbm-ui/frontend/src/utils/calcTextWidth.ts
+++ dbm-ui/frontend/src/utils/calcTextWidth.ts
@@ -38,3 +38,19 @@ export const calcTextWidth = (text: string, parentEl = document.body) => {
 
   return width;
 };
+
+export const calcTextHeight = (text: string, width: number, lineHeight = 40, parentEl = document.body) => {
+  const $el = document.createElement('div');
+  $el.innerText = text;
+  $el.style.position = 'absolute';
+  $el.style.width = `${width}px`;
+  $el.style.opacity = '0';
+  $el.style.zIndex = '-1';
+  $el.style.whiteSpace = 'pre-wrap';
+  $el.style.lineHeight = `${lineHeight}px`;
+  parentEl.appendChild($el);
+  const { height } = $el.getBoundingClientRect();
+  parentEl.removeChild($el);
+
+  return height;
+};
