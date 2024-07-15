8f796ee4586ea70c4fddfed94e9420348f8d603d
3octaves <873551943@qq.com>
2024-07-15 15:51:07 +0800

    feat(frontend): 新增MySQL、TendbCluster导出数据单据 #4902

--- dbm-ui/frontend/src/views/tickets/common/components/flow-content/Index.vue
+++ dbm-ui/frontend/src/views/tickets/common/components/flow-content/Index.vue
@@ -130,6 +130,7 @@
           :start-time="utcTimeToSeconds(content.start_time)"
           :value="content.cost_time" />
       </template>
+      <slot name="extra-text" />
       <template v-if="content.url">
         ，
         <a
@@ -138,7 +139,6 @@
           {{ t('查看详情') }} &gt;
         </a>
       </template>
-      <slot name="extra-text" />
     </div>
     <div
       v-if="content.end_time"
@@ -207,7 +207,7 @@
   import { useI18n } from 'vue-i18n';
 
   import TicketModel from '@services/model/ticket/ticket';
-  import { processTicketTodo, retryTicketFlow  } from '@services/source/ticket';
+  import { processTicketTodo, retryTicketFlow } from '@services/source/ticket';
   import type { FlowItem } from '@services/types/ticket';
 
   import { useUserProfile } from '@stores';
@@ -221,16 +221,15 @@
   // import FlowContentInnerFlow from './components/ContentInnerFlow.vue';
   import FlowContentTodo from './components/ContentTodo.vue';
 
-
   interface Emits {
     (e: 'fetch-data'): void;
   }
 
   interface Props {
-    ticketData: TicketModel<unknown>,
-    content: FlowItem,
-    flows?: FlowItem[],
-    isTodos?: boolean
+    ticketData: TicketModel<unknown>;
+    content: FlowItem;
+    flows?: FlowItem[];
+    isTodos?: boolean;
   }
 
   const props = withDefaults(defineProps<Props>(), {
@@ -251,7 +250,7 @@
 
   const manualNexFlowDisaply = computed(() => {
     if (props.flows.length > 0) {
-      const manualIndex = props.flows.findIndex(item => item.flow_type === 'PAUSE');
+      const manualIndex = props.flows.findIndex((item) => item.flow_type === 'PAUSE');
       if (manualIndex > -1) {
         return props.flows[manualIndex + 1].flow_type_display;
       }
@@ -266,21 +265,21 @@
 
   const getHrefTarget = (content: FlowItem) => (content.flow_type === 'BK_ITSM' ? '_blank' : '_self');
 
-  const handleConfirm = (item: FlowItem) => retryTicketFlow({
-    ticketId: item.ticket,
-    flow_id: item.id,
-  })
-    .then(() => {
+  const handleConfirm = (item: FlowItem) =>
+    retryTicketFlow({
+      ticketId: item.ticket,
+      flow_id: item.id,
+    }).then(() => {
       emits('fetch-data');
     });
 
-  const handleProcessTicket = (action: 'APPROVE' | 'TERMINATE', todoItem: FlowItem['todos'][number]) => processTicketTodo({
-    action,
-    todo_id: todoItem.id,
-    ticket_id: props.content.ticket,
-    params: {},
-  })
-    .then(() => {
+  const handleProcessTicket = (action: 'APPROVE' | 'TERMINATE', todoItem: FlowItem['todos'][number]) =>
+    processTicketTodo({
+      action,
+      todo_id: todoItem.id,
+      ticket_id: props.content.ticket,
+      params: {},
+    }).then(() => {
       emits('fetch-data');
     });
 
