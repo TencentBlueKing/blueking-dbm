2cef04f8b35ebf869759b91aa5365e94ddea3642
iSecloud <869820505@qq.com>
2024-07-17 11:30:18 +0800

    feat(backend): BF鉴权接口开发 #5670

--- dbm-ui/backend/db_services/plugin/bf/serializers.py
+++ dbm-ui/backend/db_services/plugin/bf/serializers.py
@@ -8,13 +8,17 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from rest_framework.routers import DefaultRouter
 
-from .bf.views import BFPluginViewSet
-from .mysql.authorize.views import AuthorizePluginViewSet
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
 
-routers = DefaultRouter(trailing_slash=True)
-routers.register("mysql/authorize", AuthorizePluginViewSet, basename="authorize")
-routers.register("bf", BFPluginViewSet, basename="bfplugin")
 
-urlpatterns = routers.urls
+class HasBFPrivSerializer(serializers.Serializer):
+    class BFHostSerializer(serializers.Serializer):
+        bk_biz_id = serializers.IntegerField(help_text=_("业务ID"))
+        bk_host_id = serializers.IntegerField(help_text=_("主机ID"))
+        bk_host_innerip = serializers.CharField(help_text=_("主机IP"))
+
+    username = serializers.CharField(help_text=_("用户名"))
+    account_name = serializers.CharField(help_text=_("登录用户"))
+    host = BFHostSerializer(help_text=_("登录主机信息"))
