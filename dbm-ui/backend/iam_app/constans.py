608c85aef8866a8d72f64feb4f00b5b468136960
iSecloud <869820505@qq.com>
2024-08-19 19:58:26 +0800

    fix(backend): 更新iam的标签 #6325

--- dbm-ui/backend/iam_app/constans.py
+++ dbm-ui/backend/iam_app/constans.py
@@ -19,3 +19,14 @@ MAX_ACTION_NAME_LEN = 32
 class CommonActionLabel(str, StructuredEnum):
     BIZ_READ_ONLY = EnumField("biz_read_only", _("业务只读"))
     BIZ_MAINTAIN = EnumField("biz_maintain", _("业务运维"))
+    EXTERNAL_DEVELOPER = EnumField("external_developer", _("外部开发商专用"))
+
+    MYSQL_IMPORT_SQLFILE = EnumField("mysql_import_sqlfile", _("MySQL SQL变更"))
+    MYSQL_AUTHORIZE_RULES = EnumField("mysql_authorize_rules", _("MySQL DB授权"))
+
+    TENDBCLUSTER_IMPORT_SQLFILE = EnumField("tendbcluster_import_sqlfile", _("TendbCluster SQL变更"))
+    TENDBCLUSTER_AUTHORIZE_RULES = EnumField("tendbcluster_authorize_rules", _("TendbCluster DB授权"))
+
+    KAFKA_ACCESS = EnumField("kafka_access", _("Kafka获取访问方式"))
+    ES_ACCESS = EnumField("es_access", _("ES获取访问方式"))
+    HDFS_ACCESS = EnumField("hdfs_access", _("HDFS获取访问方式"))
