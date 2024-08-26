2aa241fd118213c5834f7d1e4317f7758a6dba0f
Austin <1344583166@qq.com>
2024-08-26 19:20:21 +0800

    fix(frontend): 表格更多操作按钮交互优化 #6422

--- dbm-ui/frontend/src/components/more-action-extend/Index.vue
+++ dbm-ui/frontend/src/components/more-action-extend/Index.vue
@@ -1,121 +1,59 @@
 <template>
-  <div
-    ref="rootRef"
-    class="more-action-extend"
-    :class="{
-      active: isPopoverShow,
-    }"
-    v-bind="attrs"
-    @click.stop="handleClick">
-    <DbIcon type="more" />
-  </div>
-  <div
-    ref="popRef"
-    class="more-action-extend-popover"
-    @click="handleHide">
-    <template v-if="isPopoverShow">
-      <slot />
+  <BkDropdown
+    :popover-options="{ zIndex: 10 }"
+    trigger="click"
+    @hide="handleHidePopover">
+    <div
+      class="operation-more-main"
+      @click="handleClickMore">
+      <BkButton
+        class="mr-4"
+        text
+        theme="primary">
+        {{ t('更多') }}
+      </BkButton>
+      <DbIcon
+        class="more-icon"
+        :class="{ 'more-icon-active': isRotate }"
+        type="down-shape" />
+    </div>
+    <template #content>
+      <BkDropdownMenu>
+        <slot />
+      </BkDropdownMenu>
     </template>
-  </div>
+  </BkDropdown>
 </template>
-<script lang="ts">
-  import tippy, { type Instance, type SingleTarget } from 'tippy.js';
-  import { onBeforeUnmount, onMounted, ref, useAttrs } from 'vue';
+<script lang="ts" setup>
+  import { useI18n } from 'vue-i18n';
 
-  let activeTippyIns: Instance;
-</script>
-<script setup lang="ts">
-  const rootRef = ref();
-  const popRef = ref();
+  const { t } = useI18n();
 
-  let tippyIns: Instance | undefined;
+  const isRotate = ref(false);
 
-  const attrs = useAttrs();
-
-  const isPopoverShow = ref(false);
-
-  const handleClick = () => {
-    if (!tippyIns) {
-      return;
-    }
-    if (activeTippyIns && activeTippyIns !== tippyIns) {
-      activeTippyIns.hide();
-    }
-    tippyIns.show();
-    activeTippyIns = tippyIns;
+  const handleClickMore = () => {
+    isRotate.value = !isRotate.value;
   };
 
-  const handleHide = () => {
-    activeTippyIns.hide();
+  const handleHidePopover = () => {
+    isRotate.value = false;
   };
-
-  onMounted(() => {
-    tippyIns = tippy(rootRef.value as SingleTarget, {
-      content: popRef.value,
-      placement: 'bottom',
-      appendTo: () => document.body,
-      theme: 'light',
-      maxWidth: 'none',
-      trigger: 'manual',
-      interactive: true,
-      arrow: false,
-      offset: [0, 8],
-      zIndex: 999999,
-      hideOnClick: true,
-      onShow() {
-        isPopoverShow.value = true;
-      },
-      onHidden() {
-        isPopoverShow.value = false;
-      },
-    });
-  });
-  onBeforeUnmount(() => {
-    if (tippyIns) {
-      tippyIns.hide();
-      tippyIns.unmount();
-      tippyIns.destroy();
-      tippyIns = undefined;
-    }
-  });
 </script>
-<style lang="less">
-  .more-action-extend {
-    display: inline-flex;
-    width: 20px;
-    height: 20px;
-    font-size: 14px;
+
+<style lang="less" scoped>
+  .operation-more-main {
     color: #3a84ff;
-    cursor: pointer;
-    border-radius: 50%;
-    justify-content: center;
-    align-items: center;
-
-    &:hover,
-    &.active {
-      background: #dcdee5;
-    }
-  }
-
-  .more-action-extend-popover {
     display: flex;
-    padding: 4px 0;
-    margin: -5px -9px;
-    font-size: 12px;
-    color: #63656e;
-    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
 
-    & > * {
-      display: flex !important;
-      height: 32px;
-      justify-content: center;
-      align-items: center;
-      padding: 0 22px 0 12px;
-      cursor: pointer;
+    .more-icon {
+      transform: rotate(0deg);
+      transition: all 0.5s;
+    }
 
-      &:hover {
-        background-color: #f5f7fa;
-      }
+    .more-icon-active {
+      transform: rotate(-180deg);
     }
   }
 </style>
