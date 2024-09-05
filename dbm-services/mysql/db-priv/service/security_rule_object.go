710eb24a28ce151307a475ffafc74024ea2c4f8a
fanfanyangyang <yushang88@qq.com>
2024-09-05 19:15:12 +0800

    feat(backend): 新版本密码安全规则_各数据库组件独立管理 #6680

--- dbm-services/mysql/db-priv/service/security_rule_object.go
+++ dbm-services/mysql/db-priv/service/security_rule_object.go
@@ -2,12 +2,18 @@ package service
 
 import "time"
 
+const BigDataRule = "{\"max_length\":32,\"min_length\":6,\"include_rule\":{\"numbers\":true,\"symbols\":false,\"lowercase\":true,\"uppercase\":true},\"number_of_types\":2,\"symbols_allowed\":\"\",\"weak_password\":false,\"repeats\":4}"
+const MysqlSqlserverRule = "{\"max_length\":32,\"min_length\":6,\"include_rule\":{\"numbers\":true,\"symbols\":true,\"lowercase\":true,\"uppercase\":true},\"number_of_types\":2,\"symbols_allowed\":\"!#%&()*+,-./;<=>?[]^_{|}~@:$\",\"weak_password\":false,\"repeats\":4}"
+const RedisRule = "{\"max_length\":32,\"min_length\":8,\"include_rule\":{\"numbers\":true,\"symbols\":true,\"lowercase\":true,\"uppercase\":true},\"number_of_types\":2,\"symbols_allowed\":\"#@%=+-;\",\"weak_password\":false,\"repeats\":4}"
+const MongodbRule = "{\"max_length\":32,\"min_length\":8,\"include_rule\":{\"numbers\":true,\"symbols\":true,\"lowercase\":true,\"uppercase\":true},\"number_of_types\":2,\"symbols_allowed\":\"#%=+-;\",\"weak_password\":false,\"repeats\":4}"
+
 // SecurityRulePara 安全规则相关函数的入参
 type SecurityRulePara struct {
 	Id       int64  `json:"id"`
 	Name     string `json:"name"`
 	Rule     string `json:"rule"`
 	Operator string `json:"operator"`
+	Reset    bool   `json:"reset"`
 }
 
 // TbSecurityRules 安全规则表
@@ -23,20 +29,13 @@ type TbSecurityRules struct {
 
 // SecurityRule 安全规则
 type SecurityRule struct {
-	ExcludeContinuousRule ExcludeContinuousRule `json:"exclude_continuous_rule"` // 连续字符的规则
-	IncludeRule           IncludeRule           `json:"include_rule"`            // 密码中必须包含某些字符
-	MaxLength             int                   `json:"max_length"`              // 密码的最大长度
-	MinLength             int                   `json:"min_length"`              // 密码的最小长度
-}
-
-// ExcludeContinuousRule 密码不允许连续N位出现
-type ExcludeContinuousRule struct {
-	Limit     int  `json:"limit"`     //连续N位
-	Letters   bool `json:"letters"`   //字母顺序
-	Numbers   bool `json:"numbers"`   //数字顺序
-	Symbols   bool `json:"symbols"`   //特殊符号顺序
-	Keyboards bool `json:"keyboards"` //键盘顺序
-	Repeats   bool `json:"repeats"`   //重复的字母、数字、特殊字符
+	MaxLength      int         `json:"max_length"`      // 密码的最大长度
+	MinLength      int         `json:"min_length"`      // 密码的最小长度
+	IncludeRule    IncludeRule `json:"include_rule"`    // 密码组成
+	NumberOfTypes  int         `json:"number_of_types"` // 包含任意多少种类型
+	SymbolsAllowed string      `json:"symbols_allowed"` // 指定特殊字符
+	WeakPassword   bool        `json:"weak_password"`   // 弱密码检查，连续N位的字母顺序、数字顺序、特殊符号顺序、键盘顺序、重复的字母、数字、特殊字符
+	Repeats        int         `json:"repeats"`         // 连续N位
 }
 
 type IncludeRule struct {
@@ -52,15 +51,14 @@ type CheckPasswordComplexity struct {
 }
 
 type PasswordVerifyInfo struct {
-	LowercaseValid       bool `json:"lowercase_valid"`
-	UppercaseValid       bool `json:"uppercase_valid"`
-	NumbersValid         bool `json:"numbers_valid"`
-	SymbolsValid         bool `json:"symbols_valid"`
-	RepeatsValid         bool `json:"repeats_valid"`
-	FollowLettersValid   bool `json:"follow_letters_valid"`
-	FollowSymbolsValid   bool `json:"follow_symbols_valid"`
-	FollowKeyboardsValid bool `json:"follow_keyboards_valid"`
-	FollowNumbersValid   bool `json:"follow_numbers_valid"`
-	MinLengthValid       bool `json:"min_length_valid"`
-	MaxLengthValid       bool `json:"max_length_valid"`
+	NumberOfTypesValid   bool   `json:"number_of_types_valid"` // ”包含任意多少种类型“的检查结果
+	AllowedValid         bool   `json:"allowed_valid"`         // 是否在允许的字符列表内
+	OutOfRange           string `json:"out_of_range"`          // 不在允许列表的字符
+	RepeatsValid         bool   `json:"repeats_valid"`
+	FollowLettersValid   bool   `json:"follow_letters_valid"`
+	FollowSymbolsValid   bool   `json:"follow_symbols_valid"`
+	FollowKeyboardsValid bool   `json:"follow_keyboards_valid"`
+	FollowNumbersValid   bool   `json:"follow_numbers_valid"`
+	MinLengthValid       bool   `json:"min_length_valid"`
+	MaxLengthValid       bool   `json:"max_length_valid"`
 }
