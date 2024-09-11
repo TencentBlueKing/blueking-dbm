0cbc535eb6bcff1c9dcfa8da1125ccf9a762e4ae
zfrendo <842557664@qq.com>
2024-09-11 17:49:53 +0800

    fix: 修复获取binlog错误 #6867

--- dbm-ui/backend/flow/engine/bamboo/scene/mysql/common/get_binlog_backup.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/mysql/common/get_binlog_backup.py
@@ -19,7 +19,7 @@ logger = logging.getLogger("root")
 
 
 def get_backup_binlog(
-    cluster_id: int, start_time: datetime, end_time: datetime, binlog_info: dict, minute_range=30
+        cluster_id: int, start_time: datetime, end_time: datetime, binlog_info: dict, minute_range=30
 ) -> dict:
     result = {}
     if start_time > end_time:
@@ -27,21 +27,8 @@ def get_backup_binlog(
         return result
     # 先从别分文件的主节点查询，查询不到改为从节点查询。
     rollback_handler = FixPointRollbackHandler(cluster_id)
-    backup_binlog = rollback_handler.query_binlog_from_bklog(
-        start_time=start_time,
-        end_time=end_time,
-        host_ip=binlog_info["show_master_status"]["master_host"],
-        port=binlog_info["show_master_status"]["master_port"],
-        minute_range=minute_range,
-    )
-    result["binlog_start_file"] = binlog_info["show_master_status"]["binlog_file"]
-    result["binlog_start_pos"] = binlog_info["show_master_status"]["binlog_pos"]
-    if backup_binlog is None:
-        if "show_slave_status" not in binlog_info.keys():
-            result["query_binlog_error"] = _(
-                "获取原主节点 {} binlog失败".format(binlog_info["show_master_status"]["master_host"])
-            )
-            return result
+    backup_binlog = None
+    if "show_slave_status" in binlog_info.keys() and binlog_info.get("show_slave_status", None) is not None:
         backup_binlog = rollback_handler.query_binlog_from_bklog(
             start_time=start_time,
             end_time=end_time,
@@ -51,9 +38,21 @@ def get_backup_binlog(
         )
         result["binlog_start_file"] = binlog_info["show_slave_status"]["binlog_file"]
         result["binlog_start_pos"] = binlog_info["show_slave_status"]["binlog_pos"]
+        logger.info("master binlog is:", backup_binlog)
+
+    if backup_binlog is None or len(backup_binlog) == 0:
+        backup_binlog = rollback_handler.query_binlog_from_bklog(
+            start_time=start_time,
+            end_time=end_time,
+            host_ip=binlog_info["show_master_status"]["master_host"],
+            port=binlog_info["show_master_status"]["master_port"],
+            minute_range=minute_range,
+        )
+        result["binlog_start_file"] = binlog_info["show_master_status"]["binlog_file"]
+        result["binlog_start_pos"] = binlog_info["show_master_status"]["binlog_pos"]
         if backup_binlog is None:
-            result["query_binlog_error"] = _("获取原主节点{} 和 原从节点{} 的binlog失败").format(
-                binlog_info["show_master_status"]["master_host"], binlog_info["show_slave_status"]["master_host"]
+            result["query_binlog_error"] = _("原备份节点节点{} 的binlog失败").format(
+                binlog_info["show_master_status"]["master_host"]
             )
             return result
     result["binlog_task_ids"] = [i["task_id"] for i in backup_binlog["file_list_details"]]
