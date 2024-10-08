89602874f0d3f4c4b1e9e3a78b1d5a8c61e9cf36
jojo <royalpioneer@foxmail.com>
2024-10-08 15:03:43 +0800

    feat(frontend): 添加全局及业务下的资源标签管理功能 #7285

---
+++ dbm-ui/frontend/src/views/resource-manage/pool/components/host-list/components/HostOperationTip.vue
@@ -0,0 +1,114 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+<template>
+  <BkPopConfirm
+    ext-cls="host-operation-btn"
+    :title="title"
+    trigger="click"
+    width="360"
+    @confirm="handleConfirm">
+    <slot />
+    <template #content>
+      <div class="content">
+        <div>
+          <span>{{ t('主机') }}：</span>
+          <span class="ip">{{ data.ip }}</span>
+        </div>
+        <div class="tip">{{ tip }}</div>
+      </div>
+    </template>
+  </BkPopConfirm>
+</template>
+
+<script setup lang="tsx">
+  import { useI18n } from 'vue-i18n';
+  import { useRequest } from 'vue-request';
+
+  import DbResourceModel from '@services/model/db-resource/DbResource';
+  import { removeResource, updateResource } from '@services/source/dbresourceResource';
+
+  import { messageSuccess } from '@utils';
+
+  interface Props {
+    title: string;
+    tip: string;
+    data: DbResourceModel;
+    type: ServiceParameters<typeof removeResource>['event'] | 'public';
+  }
+
+  interface Emits {
+    (e: 'refresh'): void;
+  }
+
+  const props = defineProps<Props>();
+
+  const emits = defineEmits<Emits>();
+
+  const { t } = useI18n();
+
+  const { run: runRemove } = useRequest(removeResource, {
+    manual: true,
+    onSuccess() {
+      messageSuccess(t('操作成功'));
+      emits('refresh');
+    },
+  });
+
+  const { run: convertToPublic } = useRequest(updateResource, {
+    manual: true,
+    onSuccess() {
+      messageSuccess(t('操作成功'));
+      emits('refresh');
+    },
+  });
+
+  const handleConfirm = () => {
+    if (props.type === 'public') {
+      convertToPublic({
+        bk_host_ids: [props.data.bk_host_id],
+        for_biz: props.data.bk_biz_id,
+        rack_id: '',
+        resource_type: props.data.resource_type,
+        storage_device: {},
+      });
+      return;
+    }
+    runRemove({
+      bk_host_ids: [props.data.bk_host_id],
+      event: props.type,
+    });
+  };
+</script>
+
+<style lang="less">
+  .host-operation-btn {
+    .content {
+      font-size: 12px;
+      color: #63656e;
+
+      .ip {
+        color: #313238;
+      }
+
+      .tip {
+        margin-top: 4px;
+        margin-bottom: 14px;
+      }
+    }
+
+    .bk-pop-confirm-title {
+      font-size: 16px !important;
+      color: #313238 !important;
+    }
+  }
+</style>
