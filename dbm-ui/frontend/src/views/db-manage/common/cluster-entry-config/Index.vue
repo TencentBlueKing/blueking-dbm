08ff88cb2dab5667f2785c5ce979276effd03cd4
3octaves <873551943@qq.com>
2024-11-07 15:51:41 +0800

    fix(frontend): redis集群拓扑、访问入口问题修复 #7619

--- dbm-ui/frontend/src/views/db-manage/common/cluster-entry-config/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/common/cluster-entry-config/Index.vue
@@ -67,7 +67,7 @@
   import { useI18n } from 'vue-i18n';
   import { useRequest } from 'vue-request';
 
-  import type { DnsTargetDetails } from '@services/model/cluster-entry/cluster-entry-details';
+  import ClusterEntryDetailModel, { type DnsTargetDetails } from '@services/model/cluster-entry/cluster-entry-details';
   import { getClusterEntries } from '@services/source/clusterEntry';
 
   import type { DBTypes } from '@common/const';
@@ -121,8 +121,10 @@
           type: item.cluster_entry_type,
           entry: item.entry,
           role: item.role,
-          ips: item.isDns ? (item.target_details as DnsTargetDetails[]).map((row) => row.ip).join('\n') : '',
-          port: item.target_details[0]?.port,
+          ips: item.isDns
+            ? (item as ClusterEntryDetailModel<DnsTargetDetails>).target_details.map((row) => row.ip).join('\n')
+            : '',
+          port: (item as ClusterEntryDetailModel<DnsTargetDetails>).target_details[0]?.port,
         })),
       );
     },
