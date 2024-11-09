32a370d353f666cb15990897f0810607c4219720
Austin <1344583166@qq.com>
2024-11-09 09:57:23 +0800

    fix(frontend): 集群选择器勾选顺序问题修复 #7850 # Reviewed, transaction id: 23362

--- dbm-ui/frontend/src/services/source/dbbase.ts
+++ dbm-ui/frontend/src/services/source/dbbase.ts
@@ -38,6 +38,7 @@ export function filterClusters<
     cluster_type: string;
     master_domain: string;
     id: number;
+    major_version: string;
   },
 >(params: { bk_biz_id: number; exact_domain?: string; cluster_ids?: string }) {
   return http.get<T[]>(`${path}/filter_clusters/`, params);
