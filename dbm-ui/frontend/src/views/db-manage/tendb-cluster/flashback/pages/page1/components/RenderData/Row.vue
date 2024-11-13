7d66190b9471d914c83404c56687432ff6ab4d14
Austin <1344583166@qq.com>
2024-11-13 15:24:56 +0800

    fix(frontend): mysql、spider库表输入限制更新 #7934

--- dbm-ui/frontend/src/views/db-manage/tendb-cluster/flashback/pages/page1/components/RenderData/Row.vue
+++ dbm-ui/frontend/src/views/db-manage/tendb-cluster/flashback/pages/page1/components/RenderData/Row.vue
@@ -161,16 +161,16 @@
       message: t('不支持 *'),
       trigger: 'change',
     },
-    {
-      validator: (value: string[]) => {
-        if (_.some(value, (item) => /[*%?]/.test(item))) {
-          return value.length < 2;
-        }
-        return true;
-      },
-      message: t('含通配符的单元格仅支持输入单个对象'),
-      trigger: 'change',
-    },
+    // {
+    //   validator: (value: string[]) => {
+    //     if (_.some(value, (item) => /[*%?]/.test(item))) {
+    //       return value.length < 2;
+    //     }
+    //     return true;
+    //   },
+    //   message: t('含通配符的单元格仅支持输入单个对象'),
+    //   trigger: 'change',
+    // },
     {
       validator: (value: string[]) => _.every(value, (item) => !/^%$/.test(item)),
       message: t('% 不允许单独使用'),
