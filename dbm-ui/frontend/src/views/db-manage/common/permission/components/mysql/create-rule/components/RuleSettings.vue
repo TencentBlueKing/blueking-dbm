2df532800c21b1cd8a3a5cf3e01ad65f7a0e5c4e
Austin <1344583166@qq.com>
2024-11-19 21:11:33 +0800

    fix(frontend): 授权规则db输入校验异常修复 #8052

--- dbm-ui/frontend/src/views/db-manage/common/permission/components/mysql/create-rule/components/RuleSettings.vue
+++ dbm-ui/frontend/src/views/db-manage/common/permission/components/mysql/create-rule/components/RuleSettings.vue
@@ -213,10 +213,15 @@
       },
       {
         trigger: 'blur',
+        message: t('不允许 *'),
+        validator: (value: string) => !/\*/.test(value),
+      },
+      {
+        trigger: 'blur',
         message: t('DB名称支持通配符_如Data_区分大小写_多个使用英文逗号_分号或换行分隔'),
         validator: (value: string) => {
           const dbs = value.split(/[\n;,]/);
-          return _.every(dbs, (item) => (!item ? true : /^[0-9a-zA-Z][0-9a-zA-Z%]*$/.test(item)));
+          return _.every(dbs, (item) => (!item ? true : /^[0-9a-zA-Z%][0-9a-zA-Z_%]*$/.test(item)));
         },
       },
       {
