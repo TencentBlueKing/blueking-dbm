9f2e8004601e6a95dc961f83f5596a9b173b160d
hLinx <327159425@qq.com>
2024-11-20 16:15:51 +0800

    fix(frontend): sql执行单据详情查看 sql 文件内容时 db 显示问题 #8068

--- dbm-ui/frontend/src/views/tickets/common/components/demand-factory/components/import-sqlfile/components/render-sqlfile/Index.vue
+++ dbm-ui/frontend/src/views/tickets/common/components/demand-factory/components/import-sqlfile/components/render-sqlfile/Index.vue
@@ -17,7 +17,13 @@
             :key="item">
             {{ item }}
           </BkTag>
-          <BkTag v-if="executeObject.dbnames.length > 3">...</BkTag>
+          <BkTag
+            v-if="executeObject.dbnames.length > 3"
+            v-bk-tooltips="{
+              content: executeObject.dbnames.join('\n'),
+            }">
+            ...
+          </BkTag>
           <template v-if="executeObject.dbnames.length < 1">--</template>
         </span>
         <span class="ml-25">{{ t('忽略的 DB:') }}</span>
@@ -27,7 +33,13 @@
             :key="item">
             {{ item }}
           </BkTag>
-          <BkTag v-if="executeObject.ignore_dbnames.length > 3">...</BkTag>
+          <BkTag
+            v-if="executeObject.ignore_dbnames.length > 3"
+            v-bk-tooltips="{
+              content: executeObject.ignore_dbnames.join('\n'),
+            }">
+            ...
+          </BkTag>
           <template v-if="executeObject.ignore_dbnames.length < 1">--</template>
         </span>
       </span>
