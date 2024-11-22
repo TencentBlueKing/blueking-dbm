c3656b194965cde7a75825469c2ab767fad17fea
chenwenchang <479999519@qq.com>
2024-11-22 16:00:23 +0800

    feat(frontend): 单据管理迭代_1122 #7190

---
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/task-info/com-factory/mysql/SingleDestroy.vue
@@ -0,0 +1,21 @@
+<template>
+  <ClusterSwitch :ticket-details="ticketDetails" />
+</template>
+<script setup lang="ts">
+  import TicketModel, { type Mysql } from '@services/model/ticket/ticket';
+
+  import { TicketTypes } from '@common/const';
+
+  import ClusterSwitch from './common/ClusterSwitch.vue';
+
+  interface Props {
+    ticketDetails: TicketModel<Mysql.ClusterSwitch>;
+  }
+
+  defineProps<Props>();
+
+  defineOptions({
+    name: TicketTypes.MYSQL_SINGLE_DESTROY,
+    inheritAttrs: false,
+  });
+</script>
