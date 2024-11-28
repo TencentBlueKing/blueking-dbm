c73cc878ecf9cec4c26fc44d03e6e9644d13d8ba
xiepaup <xiepaup@gmail.com>
2024-11-28 11:50:09 +0800

    fix(redis): 自愈下架提单失败 #8252

--- dbm-ui/backend/flow/plugins/components/collections/redis/redis_ticket.py
+++ dbm-ui/backend/flow/plugins/components/collections/redis/redis_ticket.py
@@ -10,7 +10,7 @@ specific language governing permissions and limitations under the License.
 """
 from typing import List
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from pipeline.component_framework.component import Component
 from pipeline.core.flow.activity import Service
 
