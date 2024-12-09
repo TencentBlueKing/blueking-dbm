cfb761a7453981aaaf167063dee7d168b90d3438
royalpioneer <royalpioneer@foxmail.com>
2024-12-09 10:59:26 +0800

    fix(frontend): 单据协作人样式调整 #8482 # Reviewed, transaction id: 26151

--- dbm-ui/frontend/src/views/ticket-cooperation-setting/Index.vue
+++ dbm-ui/frontend/src/views/ticket-cooperation-setting/Index.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="ticket-cooperation-setting">
-    <DbCard
-      mode="collapse"
-      :title="t('单据协助')">
-      <BkForm
-        ref="formRef"
-        :model="formModel">
-        <BkFormItem :label="t('单据协助')">
-          <BkSwitcher
-            v-model="isCooperationOpen"
-            class="mr-8"
-            theme="primary" />
-          {{ t('开启后，您指定的协作人将能够协助处理单据执行、继续任务等事项，同时也会收到单据通知') }}
-        </BkFormItem>
-        <BkFormItem
-          v-if="isCooperationOpen"
-          :label="t('默认协作人')"
-          property="members"
-          required>
-          <MemberSelector v-model="formModel.members" />
-        </BkFormItem>
-      </BkForm>
-    </DbCard>
-    <AuthButton
-      action-id="biz_assistance_vars_config"
-      class="submit-btn"
-      :loading="isUpdating"
-      theme="primary"
-      @click="handleSubmit">
-      {{ t('保存') }}
-    </AuthButton>
+    <SmartAction :offset-target="getSmartActionOffsetTarget">
+      <DbCard
+        mode="collapse"
+        :title="t('单据协助')">
+        <BkForm
+          ref="formRef"
+          :model="formModel">
+          <BkFormItem :label="t('单据协助')">
+            <BkSwitcher
+              v-model="isCooperationOpen"
+              class="mr-8"
+              theme="primary" />
+            {{ t('开启后，您指定的协作人将能够协助处理单据执行、继续任务等事项，同时也会收到单据通知') }}
+          </BkFormItem>
+          <BkFormItem
+            v-if="isCooperationOpen"
+            :label="t('默认协作人')"
+            property="members"
+            required>
+            <MemberSelector v-model="formModel.members" />
+          </BkFormItem>
+        </BkForm>
+      </DbCard>
+      <template #action>
+        <AuthButton
+          action-id="biz_assistance_vars_config"
+          class="submit-btn"
+          :loading="isUpdating"
+          theme="primary"
+          @click="handleSubmit">
+          {{ t('保存') }}
+        </AuthButton>
+      </template>
+    </SmartAction>
   </div>
 </template>
 
@@ -55,6 +59,8 @@
   const formModel = reactive({
     members: [],
   });
+
+  const getSmartActionOffsetTarget = () => document.querySelector('.bk-form-content');
 
   // 是否开启单据协助
   useRequest(getBizSettingList, {
