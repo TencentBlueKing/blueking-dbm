0307a2b0b7adbb87e46a084c0bf4a7c5c0f31357
hLinx <327159425@qq.com>
2024-12-09 10:10:02 +0800

    feat(frontend): 新增 editable-table 组件 #8476

---
+++ dbm-ui/frontend/src/components/editable-table/edit/Input.vue
@@ -0,0 +1,41 @@
+<template>
+  <BkInput
+    v-model="modelValue"
+    class="bk-editable-input"
+    @blur="handleBlur"
+    @focus="handleFocus" />
+</template>
+<script setup lang="ts">
+  import { watch } from 'vue';
+
+  import useColumn from '../useColumn';
+
+  const columnContext = useColumn();
+
+  const modelValue = defineModel<string>();
+
+  watch(modelValue, () => {
+    columnContext?.validate();
+  });
+
+  const handleBlur = () => {
+    columnContext?.blur();
+  };
+
+  const handleFocus = () => {
+    columnContext?.focus();
+  };
+</script>
+<style lang="less">
+  .bk-editable-input {
+    &.bk-input {
+      height: 40px;
+      border: none;
+      box-shadow: none !important;
+    }
+
+    .bk-input--text {
+      background: transparent;
+    }
+  }
+</style>
---
+++ dbm-ui/frontend/src/components/editable-table/edit/Select.vue
@@ -0,0 +1,36 @@
+<template>
+  <BkSelect
+    class="bk-editable-select"
+    @blur="handleBlur"
+    @focus="handleFocus">
+    <slot />
+  </BkSelect>
+</template>
+<script setup lang="ts">
+  import useColumn from '../useColumn';
+
+  const columnContext = useColumn();
+
+  const handleBlur = () => {
+    columnContext?.blur();
+  };
+
+  const handleFocus = () => {
+    columnContext?.focus();
+  };
+</script>
+<style lang="less">
+  .bk-editable-select {
+    width: 100%;
+
+    .bk-input {
+      height: 40px;
+      border: none;
+      box-shadow: none !important;
+    }
+
+    .bk-input--text {
+      background: transparent;
+    }
+  }
+</style>
