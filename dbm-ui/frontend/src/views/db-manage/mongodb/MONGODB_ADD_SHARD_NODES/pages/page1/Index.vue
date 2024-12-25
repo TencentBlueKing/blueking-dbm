51d97ed16f767a500b0debf6c477156ec31dae90
3octaves <873551943@qq.com>
2024-12-25 20:27:50 +0800

    perf(frontend): mongodb工具箱重构_扩容Shard节点数 #8498

---
+++ dbm-ui/frontend/src/views/db-manage/mongodb/MONGODB_ADD_SHARD_NODES/pages/page1/Index.vue
@@ -0,0 +1,350 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
+<template>
+  <SmartAction>
+    <div class="proxy-scale-down-page">
+      <BkAlert
+        closable
+        theme="info"
+        :title="t('扩容Shard节点数：xxx')" />
+      <DbForm
+        ref="form"
+        class="toolbox-form"
+        form-type="vertical"
+        :model="formData"
+        style="margin-top: 16px">
+        <EditableTable
+          ref="editableTable"
+          class="mt16 mb16"
+          :model="tableData"
+          :rules="rules">
+          <EditableTableRow
+            v-for="(item, index) in tableData"
+            :key="index">
+            <EditClusterWithRelatedClustersColumn
+              v-model="item.cluster"
+              :cluster-types="[ClusterTypes.MONGO_REPLICA_SET, ClusterTypes.MONGO_SHARED_CLUSTER]"
+              :selected="selected"
+              @batch-edit="handleClusterBatchEdit" />
+            <EditableTableColumn
+              :label="t('集群类型')"
+              :width="200">
+              <EditBlock
+                v-model="item.cluster.cluster_type_name"
+                :placeholder="t('输入集群后自动生成')" />
+            </EditableTableColumn>
+            <EditableTableColumn
+              :label="t('当前 Shard 的节点数')"
+              :width="200">
+              <EditBlock
+                v-model="item.cluster.cluster_type_name"
+                :placeholder="t('输入集群后自动生成')">
+                {{ item.cluster.shard_node_count }}
+              </EditBlock>
+            </EditableTableColumn>
+            <TargetNumberColumn
+              v-model="item.target_num"
+              :disabled="!item.cluster.id"
+              :min="item.cluster.shard_node_count" />
+            <OperationColumn
+              :create-row-method="createRowData"
+              :table-data="tableData" />
+          </EditableTableRow>
+        </EditableTable>
+        <div class="bottom-opeartion">
+          <BkCheckbox
+            v-model="formData.is_ignore_business_access"
+            style="padding-top: 6px" />
+          <span
+            v-bk-tooltips="{
+              content: t('如忽略_有连接的情况下也会执行'),
+              theme: 'dark',
+            }"
+            class="ml-6 force-switch">
+            {{ t('忽略业务连接') }}
+          </span>
+        </div>
+        <TicketRemark v-model="formData.remark" />
+      </DbForm>
+    </div>
+    <template #action>
+      <BkButton
+        class="w-88"
+        :loading="isSubmitting"
+        theme="primary"
+        @click="handleSubmit">
+        {{ t('提交') }}
+      </BkButton>
+      <DbPopconfirm
+        :confirm-handler="handleReset"
+        :content="t('重置将会清空当前填写的所有内容_请谨慎操作')"
+        :title="t('确认重置页面')">
+        <BkButton
+          class="ml-8 w-88"
+          :disabled="isSubmitting">
+          {{ t('重置') }}
+        </BkButton>
+      </DbPopconfirm>
+    </template>
+  </SmartAction>
+</template>
+
+<script setup lang="tsx">
+  import type { ComponentProps } from 'vue-component-type-helpers';
+  import { useI18n } from 'vue-i18n';
+  import { useRouter } from 'vue-router';
+
+  import MongodbModel from '@services/model/mongodb/mongodb';
+  import type { Mongodb } from '@services/model/ticket/ticket';
+  import { createTicket } from '@services/source/ticket';
+
+  import { useTicketDetail } from '@hooks';
+
+  import { ClusterTypes, TicketTypes } from '@common/const';
+
+  import EditableTable, {
+    Block as EditBlock,
+    Column as EditableTableColumn,
+    Row as EditableTableRow,
+  } from '@components/editable-table/Index.vue';
+
+  import TicketRemark from '@views/db-manage/common/TicketRemark.vue';
+  import OperationColumn from '@views/db-manage/common/toolbox-field/operation-column/Index.vue';
+  import EditClusterColumn from '@views/db-manage/mongodb/common/toolbox-field/edit-cluster/Index.vue';
+  import EditClusterWithRelatedClustersColumn from '@views/db-manage/mongodb/common/toolbox-field/edit-cluster-with-related-clusters/Index.vue';
+
+  import TargetNumberColumn from './components/TargetNumberColumn.vue';
+
+  export interface IDataRow {
+    cluster: {
+      id?: number;
+      master_domain?: string;
+      related_clusters?: {
+        id: number;
+        domain: string;
+      }[];
+      cluster_type?: string;
+      cluster_type_name?: string;
+      shard_node_count?: number;
+      machine_instance_num?: number;
+      shard_num?: number;
+      mongodb?: MongodbModel['mongos'];
+    };
+    target_num?: number;
+  }
+
+  const createRowData = (values?: Partial<IDataRow>) => ({
+    cluster: values?.cluster ? values.cluster : ({} as IDataRow['cluster']),
+    target_num: values?.target_num,
+  });
+
+  const createDefaultFormData = () => ({
+    is_ignore_business_access: false,
+    remark: '',
+  });
+
+  const { t } = useI18n();
+  const router = useRouter();
+
+  useTicketDetail<Mongodb.AddShardNodes>(TicketTypes.MONGODB_ADD_SHARD_NODES, {
+    onSuccess(ticketDetail) {
+      const { details, remark } = ticketDetail;
+      const { infos, clusters, is_safe: isSafe } = details;
+      tableData.value = infos.map((item) => {
+        const clusterItem = clusters[item.cluster_ids[0]];
+        return createRowData({
+          cluster: {
+            master_domain: clusterItem.immute_domain,
+          },
+          target_num: item.current_shard_nodes_num + item.add_shard_nodes_num,
+        });
+      });
+      Object.assign(formData, {
+        is_ignore_business_access: !isSafe,
+        remark,
+      });
+    },
+  });
+
+  const formRef = useTemplateRef('form');
+  const editableTableRef = useTemplateRef('editableTable');
+
+  const rules = {
+    'cluster.master_domain': [
+      {
+        validator: (value: string) => {
+          if (value) {
+            const domainList = tableData.value
+              .flatMap((tableRow) => [
+                tableRow.cluster.master_domain || '',
+                ...(tableRow.cluster.related_clusters || []).map((relatedItem) => relatedItem.domain),
+              ])
+              .filter((domainItem) => domainItem);
+            return domainList.filter((domain) => domain === value).length === 1;
+          }
+          return true;
+        },
+        trigger: 'change',
+        message: t('目标集群重复'),
+      },
+    ],
+  };
+
+  const isSubmitting = ref(false);
+  const tableData = ref<IDataRow[]>([createRowData()]);
+
+  const formData = reactive(createDefaultFormData());
+
+  const selected = computed(() => {
+    const selectedClusters: ComponentProps<typeof EditClusterColumn>['selected'] = {
+      [ClusterTypes.MONGO_REPLICA_SET]: [],
+      [ClusterTypes.MONGO_SHARED_CLUSTER]: [],
+    };
+    tableData.value.forEach((tableRow) => {
+      const { id, cluster_type: clusterType, master_domain: masterDomain } = tableRow.cluster;
+      if (id && clusterType && masterDomain) {
+        selectedClusters[clusterType as keyof typeof selectedClusters].push({
+          id,
+          master_domain: masterDomain,
+        });
+      }
+    });
+    return selectedClusters;
+  });
+
+  const clusterMemo = computed(() =>
+    Object.fromEntries(
+      Object.values(selected.value).flatMap((clusters) =>
+        clusters.filter((cluster) => cluster.master_domain).map((cluster) => [cluster.master_domain, true]),
+      ),
+    ),
+  );
+
+  const handleClusterBatchEdit = (clusterList: MongodbModel[]) => {
+    const newList: IDataRow[] = [];
+    clusterList.forEach((item) => {
+      if (!clusterMemo.value[item.master_domain]) {
+        newList.push(
+          createRowData({
+            cluster: {
+              id: item.id,
+              master_domain: item.master_domain,
+              cluster_type: item.cluster_type,
+              cluster_type_name: item.cluster_type_name,
+              shard_node_count: item.shard_node_count,
+              machine_instance_num: item.machine_instance_num,
+              shard_num: item.shard_num,
+              mongodb: item.mongodb,
+            },
+          }),
+        );
+      }
+    });
+    tableData.value = [...(tableData.value[0].cluster.master_domain ? tableData.value : []), ...newList];
+    window.changeConfirm = true;
+  };
+
+  // 点击提交按钮
+  const handleSubmit = async () => {
+    try {
+      isSubmitting.value = true;
+      await formRef.value!.validate();
+      const validateResult = await editableTableRef.value!.validate();
+      if (validateResult) {
+        const params = {
+          bk_biz_id: window.PROJECT_CONFIG.BIZ_ID,
+          ticket_type: TicketTypes.MONGODB_ADD_SHARD_NODES,
+          remark: formData.remark,
+          details: {
+            is_safe: !formData.is_ignore_business_access,
+            infos: tableData.value.map((tableRow) => {
+              const cluster = tableRow.cluster as Required<IDataRow['cluster']>;
+              const targerNum = tableRow.target_num!;
+              return {
+                cluster_ids:
+                  cluster.cluster_type === ClusterTypes.MONGO_REPLICA_SET
+                    ? [cluster.id, ...cluster.related_clusters.map((relatedItem) => relatedItem.id)]
+                    : [cluster.id],
+                add_shard_nodes_num: targerNum - cluster.shard_node_count, // 增加shard节点数
+                current_shard_nodes_num: cluster.shard_node_count, // 当前shard节点数
+                node_replica_count: cluster.machine_instance_num, // 单机部署实例
+                shards_num: cluster.shard_num, // 分片数
+                resource_spec: {
+                  shard_nodes: {
+                    spec_id: cluster.mongodb[0].spec_config.id,
+                    count: (cluster.shard_num / cluster.machine_instance_num) * (targerNum - cluster.shard_node_count), // 分片数 / 每台机器的实例数 * 增加的节点数
+                  },
+                },
+              };
+            }),
+          },
+        };
+        await createTicket(params).then((data) => {
+          window.changeConfirm = false;
+          router.push({
+            name: TicketTypes.MONGODB_ADD_SHARD_NODES,
+            params: {
+              page: 'success',
+            },
+            query: {
+              ticketId: data.id,
+            },
+          });
+        });
+      }
+    } finally {
+      isSubmitting.value = false;
+    }
+  };
+
+  const handleReset = () => {
+    Object.assign(formData, createDefaultFormData());
+    tableData.value = [createRowData()];
+    window.changeConfirm = false;
+  };
+</script>
+
+<style lang="less" scoped>
+  .proxy-scale-down-page {
+    padding-bottom: 20px;
+
+    .page-action-box {
+      display: flex;
+      align-items: center;
+      margin-top: 16px;
+
+      .safe-action {
+        margin-left: auto;
+
+        .safe-action-text {
+          padding-bottom: 2px;
+          border-bottom: 1px dashed #979ba5;
+        }
+      }
+    }
+
+    .bottom-opeartion {
+      display: flex;
+      width: 100%;
+      height: 30px;
+      align-items: flex-end;
+      margin-bottom: 24px;
+
+      .force-switch {
+        font-size: 12px;
+        border-bottom: 1px dashed #63656e;
+      }
+    }
+  }
+</style>
---
+++ dbm-ui/frontend/src/views/db-manage/mongodb/common/toolbox-field/edit-cluster-with-related-clusters/Index.vue
@@ -0,0 +1,204 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
+<template>
+  <Column
+    :append-rules="rules"
+    field="cluster.master_domain"
+    fixed="left"
+    :label="t('目标集群')"
+    :loading="isLoading"
+    :min-width="300"
+    required
+    :validate-delay="300">
+    <template #headAppend>
+      <span
+        v-bk-tooltips="t('批量选择')"
+        class="batch-host-select"
+        @click="handleShowSelector">
+        <DbIcon type="batch-host-select" />
+      </span>
+    </template>
+    <div style="flex: 1">
+      <Input
+        v-model="modelValue.master_domain"
+        :placeholder="t('请输入集群域名')" />
+      <BkLoading
+        v-if="modelValue.related_clusters?.length"
+        class="related-clusters"
+        :loading="relatedClusterLoading">
+        {{ t('含n个同机关联集群', { n: modelValue.related_clusters.length }) }}
+        <p
+          v-for="item in modelValue.related_clusters"
+          :key="item.id">
+          -- {{ item.domain }}
+        </p>
+      </BkLoading>
+    </div>
+  </Column>
+  <ClusterSelector
+    :key="clusterTypes.join(',')"
+    v-model:is-show="showSelector"
+    :cluster-types="clusterTypes"
+    :selected="selected as MappedProps"
+    @change="handleSelectorChange" />
+</template>
+<script lang="ts" setup>
+  import { useI18n } from 'vue-i18n';
+  import { useRequest } from 'vue-request';
+
+  import MongodbModel from '@services/model/mongodb/mongodb';
+  import { filterClusters } from '@services/source/dbbase';
+  import { getRelatedClustersByClusterIds } from '@services/source/mongodb';
+
+  import { ClusterTypes } from '@common/const';
+  import { domainRegex } from '@common/regex';
+
+  import ClusterSelector from '@components/cluster-selector/Index.vue';
+  import { Column, Input } from '@components/editable-table/Index.vue';
+
+  type MappedProps = {
+    [K in keyof Props['selected']]: MongodbModel[];
+  };
+
+  interface Props {
+    clusterTypes: string[];
+    selected: Record<
+      string,
+      {
+        id: number;
+        master_domain: string;
+      }[]
+    >;
+  }
+
+  interface Emits {
+    (e: 'batch-edit', value: MongodbModel[]): void;
+  }
+
+  defineProps<Props>();
+
+  const emits = defineEmits<Emits>();
+
+  const modelValue = defineModel<{
+    id?: number;
+    master_domain?: string;
+    cluster_type?: string;
+    related_clusters?: {
+      id: number;
+      domain: string;
+    }[];
+  }>({
+    default: () => ({
+      domain: '',
+    }),
+  });
+
+  const { t } = useI18n();
+
+  const showSelector = ref(false);
+  const isLoading = ref(false);
+
+  const rules = [
+    {
+      validator: (value: string) => domainRegex.test(value),
+      message: t('集群域名格式不正确'),
+      trigger: 'change',
+    },
+    {
+      validator: () => Boolean(modelValue.value.id),
+      trigger: 'change',
+      message: t('目标集群不存在'),
+    },
+  ];
+
+  const { run: queryRelatedClusters, loading: relatedClusterLoading } = useRequest(getRelatedClustersByClusterIds, {
+    manual: true,
+    onSuccess: (data) => {
+      modelValue.value.related_clusters = [];
+      if (data.length) {
+        modelValue.value.related_clusters = data[0].related_clusters.map((item) => ({
+          id: item.id,
+          domain: item.master_domain,
+        }));
+      }
+    },
+  });
+
+  watch(
+    () => modelValue.value.master_domain,
+    () => {
+      if (!modelValue.value.id && modelValue.value.master_domain) {
+        isLoading.value = true;
+        modelValue.value.id = undefined;
+        filterClusters<MongodbModel>({
+          bk_biz_id: window.PROJECT_CONFIG.BIZ_ID,
+          exact_domain: modelValue.value.master_domain,
+        })
+          .then((data) => {
+            if (data.length > 0) {
+              [modelValue.value] = data;
+            }
+          })
+          .finally(() => {
+            isLoading.value = false;
+            // editableTableColumnRef.value!.validate();
+          });
+      }
+      if (!modelValue.value.master_domain) {
+        modelValue.value.id = undefined;
+      }
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  watch(
+    () => modelValue.value.id,
+    (id) => {
+      if (id && modelValue.value.cluster_type === ClusterTypes.MONGO_REPLICA_SET) {
+        queryRelatedClusters({
+          cluster_ids: [id],
+        });
+      } else {
+        modelValue.value.related_clusters = [];
+      }
+    },
+  );
+
+  const handleShowSelector = () => {
+    showSelector.value = true;
+  };
+
+  const handleSelectorChange = (selected: Record<string, MongodbModel[]>) => {
+    const clusterList = Object.values(selected).flatMap((selectedList) => selectedList);
+    emits('batch-edit', clusterList);
+  };
+</script>
+
+<style lang="less" scoped>
+  .batch-host-select {
+    font-size: 14px;
+    color: #3a84ff;
+    cursor: pointer;
+  }
+
+  .related-clusters {
+    padding: 8px;
+    font-size: 12px;
+    line-height: 20px;
+    color: #979ba5;
+    background: #fafbfd;
+  }
+</style>
