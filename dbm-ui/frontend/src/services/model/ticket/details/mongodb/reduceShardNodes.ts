905aa17f352627228eeca45a8ccd18d329ed6682
3octaves <873551943@qq.com>
2024-12-26 16:49:10 +0800

    fix(frontend): 修复mongo扩缩容shard单据详情显示问题 #8847

--- dbm-ui/frontend/src/services/model/ticket/details/mongodb/reduceShardNodes.ts
+++ dbm-ui/frontend/src/services/model/ticket/details/mongodb/reduceShardNodes.ts
@@ -3,9 +3,12 @@ import type { DetailBase, DetailClusters } from '../common';
 export interface ReduceShardNodes extends DetailBase {
   clusters: DetailClusters;
   infos: {
-    cluster_id: number;
+    cluster_ids: number[];
+    current_shard_nodes_num: number;
     reduce_shard_nodes: number;
+    shard_num: number;
+    machine_instance_num: number;
   }[];
   is_safe: boolean;
-  ip_source: string;
+  // ip_source: string;
 }
