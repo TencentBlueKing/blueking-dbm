da620b4c9923733c1e940be72682cd2626b777a3
3octaves <873551943@qq.com>
2024-12-26 17:08:51 +0800

    perf(frontend): mongodb工具箱重构_缩容Shard节点数 #8498

--- dbm-ui/frontend/src/views/db-manage/mongodb/routes.ts
+++ dbm-ui/frontend/src/views/db-manage/mongodb/routes.ts
@@ -44,14 +44,17 @@ export const mongoToolboxChildrenRoutes: RouteRecordRaw[] = [
   createRouteItem(TicketTypes.MONGODB_ADD_SHARD_NODES, {
     navName: t('扩容Shard节点数'),
   }),
-  {
-    name: 'MongoShardScaleDown',
-    path: 'shard-scale-down/:page?',
-    meta: {
-      navName: t('缩容Shard节点数'),
-    },
-    component: () => import('@views/db-manage/mongodb/shard-scale-down/Index.vue'),
-  },
+  // {
+  //   name: 'MongoShardScaleDown',
+  //   path: 'shard-scale-down/:page?',
+  //   meta: {
+  //     navName: t('缩容Shard节点数'),
+  //   },
+  //   component: () => import('@views/db-manage/mongodb/shard-scale-down/Index.vue'),
+  // },
+  createRouteItem(TicketTypes.MONGODB_REDUCE_SHARD_NODES, {
+    navName: t('缩容Shard节点数'),
+  }),
   // {
   //   name: 'MongoCapacityChange',
   //   path: 'capacity-change/:page?',
