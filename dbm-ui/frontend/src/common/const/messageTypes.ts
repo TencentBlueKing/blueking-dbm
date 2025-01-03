451eb0825ec5a8f9349dbd3025fb4b99271effce
3octaves <873551943@qq.com>
2025-01-03 16:13:41 +0800

    feat(frontend): 新增单据通知设置页面 #8335

--- dbm-ui/frontend/src/common/const/messageTypes.ts
+++ dbm-ui/frontend/src/common/const/messageTypes.ts
@@ -2,6 +2,8 @@
  * 消息通知类型
  */
 
+import { t } from '@locales/index';
+
 export enum MessageTypes {
   SMS = 'sms',
   WEIXIN = 'weixin',
@@ -12,3 +14,12 @@ export enum MessageTypes {
 }
 
 export const InputMessageTypes = [MessageTypes.WECOM_ROBOT] as string[];
+
+export const MessageTipMap: Record<string, string> = {
+  [MessageTypes.WECOM_ROBOT]: [
+    t('获取会话ID方法:'),
+    t('1. 群聊添加群机器人: 蓝鲸审批助手'),
+    t('2. 手动蓝鲸审批助手，获取会话ID'),
+    t('3. 将获取到的会话ID粘贴到输入框，多个会话ID使用逗号分隔'),
+  ].join('\n'),
+};
