7485cb1db3d11bd8db15b61189eaa452f9cc02d3
3octaves <873551943@qq.com>
2025-01-06 17:35:31 +0800

    perf(frontend): mongodb工具箱重构_库表备份 #8498

--- dbm-ui/frontend/src/views/db-manage/mongodb/MONGODB_BACKUP/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/mongodb/MONGODB_BACKUP/Index.vue
@@ -1,9 +1,32 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
 <template>
-  <div></div>
+  <Component :is="components[page]" />
 </template>
-<script setup lang="ts"></script>
-<style lang="less">
-  .root {
-    display: block;
-  }
-</style>
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+
+  import Page2 from '@views/db-manage/common/create-ticket-success/Index.vue';
+
+  import Page1 from './create/Index.vue';
+
+  const route = useRoute();
+
+  const components = {
+    create: Page1,
+    success: Page2,
+  };
+
+  const page = computed(() => (route.params.page as keyof typeof components) || 'create');
+</script>
--- dbm-ui/frontend/src/views/ticket-center/common/TicketClone.vue
+++ dbm-ui/frontend/src/views/ticket-center/common/TicketClone.vue
@@ -151,6 +151,8 @@
     [TicketTypes.TENDBCLUSTER_RESTORE_LOCAL_SLAVE]: 'spiderSlaveRebuild', // spider 重建从库-原地重建
     [TicketTypes.TENDBCLUSTER_RESTORE_SLAVE]: 'spiderSlaveRebuild', // spider 重建从库-新机重建
     [TicketTypes.MONGODB_FULL_BACKUP]: TicketTypes.MONGODB_FULL_BACKUP, // mongodb 全库备份
+    [TicketTypes.MONGODB_BACKUP]: TicketTypes.MONGODB_BACKUP, // mongodb 库表备份
+    [TicketTypes.MONGODB_REMOVE_NS]: TicketTypes.MONGODB_REMOVE_NS, // mongodb 清档
   };
 
   const isSupported = computed(() => !!ticketTypeRouteNameMap[props.data.ticket_type]);
