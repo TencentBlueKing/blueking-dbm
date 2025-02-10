a55c2755b56d8dfb30bec3eef92a87f61e5d9ca6
Austin <1344583166@qq.com>
2025-02-10 17:49:58 +0800

    feat(frontend): 巡检报告改版及个人工作台新增巡检待办 #8953 # Reviewed, transaction id: 30016

--- dbm-ui/frontend/src/views/inspection-manage/global/Index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <AuthTemplate action-id="platform_healthy_report_view">
-    <InspectionReportPage />
-  </AuthTemplate>
-</template>
-<script setup lang="ts">
-  import InspectionReportPage from '../report/Index.vue';
-</script>
