f16c3c3877fd75f7e6ef8bab6f93dfe449bbd0d6
hLinx <327159425@qq.com>
2025-02-11 19:07:28 +0800

    fix(frontend): 列表搜索交互问题 #9264

--- dbm-ui/frontend/src/views/db-manage/mongodb/replica-set-list/components/list/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/mongodb/replica-set-list/components/list/Index.vue
@@ -276,11 +276,13 @@
       name: t('访问入口'),
       id: 'domain',
       multiple: true,
+      async: false,
     },
     {
       name: t('IP 或 IP:Port'),
       id: 'instance',
       multiple: true,
+      async: false,
     },
     {
       name: 'ID',
@@ -290,6 +292,7 @@
       name: t('集群名称'),
       id: 'name',
       multiple: true,
+      async: false,
     },
     {
       name: t('管控区域'),
