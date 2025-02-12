db9f25b034498c9472dfe76ea08570d6c67e9103
hLinx <327159425@qq.com>
2025-02-12 17:09:59 +0800

    perf(frontend): cluster-selector 类型问题修复 #9295

--- dbm-ui/frontend/src/components/cluster-selector/components/tendb-cluster/Index.vue
+++ dbm-ui/frontend/src/components/cluster-selector/components/tendb-cluster/Index.vue
@@ -212,7 +212,7 @@
     },
     {
       label: t('访问入口'),
-      field: 'cluster_name',
+      field: 'master_domain',
       minWidth: 300,
       showOverflowTooltip: true,
       render: ({ data }: { data: ResourceItem }) => (
--- dbm-ui/frontend/src/components/cluster-selector/components/tendb-single/Index.vue
+++ dbm-ui/frontend/src/components/cluster-selector/components/tendb-single/Index.vue
@@ -215,7 +215,7 @@
     },
     {
       label: t('访问入口'),
-      field: 'cluster_name',
+      field: 'master_domain',
       minWidth: 250,
       fixed: 'left',
       showOverflowTooltip: true,
