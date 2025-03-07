1266fbce660e1c3c92d5603876911159672a11a5
3octaves <873551943@qq.com>
2025-03-07 14:00:40 +0800

    feat(frontend): 集群部署增加园区和调整容灾要求和增加预估成本展示 #9399

--- dbm-ui/frontend/src/services/model/function-controller/functionController.ts
+++ dbm-ui/frontend/src/services/model/function-controller/functionController.ts
@@ -54,7 +54,6 @@ export default class FunctionController {
   'bizConfigManage.ticketNoticeSetting': ControllerItem<string>;
 
   // 通用开关，涉及多个功能页面
-  'common.affinityNone': ControllerItem<string>;
   'common.specCostEstimate': ControllerItem<string>;
 
   'databaseManage.missionManage': ControllerItem<string>;
