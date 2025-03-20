722ce6d3c844bfa93c358545f73199c826a8ec69
Austin <1344583166@qq.com>
2025-03-20 17:26:33 +0800

    feat(frontend): dba工具箱新增web查询 #9430

---
+++ dbm-ui/frontend/src/views/db-manage/common/web-query/components/sql-query/components/query-result/components/result/Index.vue
@@ -0,0 +1,194 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
+<template>
+  <div class="panel-results-main">
+    <div class="results-info-main">
+      <span>{{ t('查询结果') }}</span>
+      <span class="ml-4 mr-4">:</span>
+      <I18nT
+        keypath="共m条"
+        tag="span">
+        <span style="font-weight: 700; color: #63656e">{{ queryResults.length }}</span>
+      </I18nT>
+      <span class="ml-4 mr-4">,</span>
+      <span>{{ t('耗时') }}</span>
+      <span>{{ querySeconds }}s</span>
+      <span class="ml-4 mr-4">,</span>
+      <BkButton
+        text
+        theme="primary"
+        @click="handleExport">
+        {{ t('导出结果') }}
+      </BkButton>
+    </div>
+    <DbTable
+      ref="tableRef"
+      class="query-result-table"
+      :columns="columns"
+      :container-height="500"
+      :data-source="dataSource"
+      :remote-pagination="false"
+      :row-config="{
+        isHover: false,
+        height: 28,
+      }"
+      stripe />
+  </div>
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  import { DBTypes } from '@common/const';
+
+  import { exportExcelFile, random } from '@utils';
+
+  import type { DbConsoleResults } from '../../../../Index.vue';
+
+  interface Props {
+    data?: DbConsoleResults;
+    dbType?: DBTypes;
+    querySeconds?: number;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    data: () => [],
+    dbType: DBTypes.MYSQL,
+    querySeconds: 0,
+  });
+
+  const { t } = useI18n();
+
+  // 以防跟元数据中的key发生冲突
+  const instanceId = random();
+  const seqId = random();
+
+  const tableRef = ref();
+  const columns = ref<
+    {
+      field: string;
+      fixed?: string;
+      label: string;
+      width?: number;
+    }[]
+  >([]);
+
+  const queryResults = computed(() => {
+    if (!props.data.length || !props.data[0].table_data) {
+      return [];
+    }
+
+    return props.data.reduce<Record<string, string | number>[]>((list, item) => {
+      item.table_data.forEach((row) => {
+        list.push({
+          [instanceId]: item.instance,
+          [seqId]: list.length + 1,
+          ...row,
+        });
+      });
+      return list;
+    }, []);
+  });
+
+  const dataSource = computed(() => {
+    if (!props.data.length || !columns.value.length || !queryResults.value.length) {
+      return () =>
+        Promise.resolve({
+          count: 0,
+          results: [],
+        });
+    }
+
+    return () =>
+      Promise.resolve({
+        count: queryResults.value.length,
+        results: queryResults.value,
+      });
+  });
+
+  watch(
+    () => props.data,
+    () => {
+      if (props.data.length && props.data[0].table_data) {
+        const dataKeys = Object.keys(props.data[0].table_data[0]).map((key) => ({
+          field: key,
+          label: key,
+        }));
+        columns.value = [
+          {
+            field: seqId,
+            fixed: 'left',
+            label: t('序号'),
+            width: 120,
+          },
+          {
+            field: instanceId,
+            fixed: 'left',
+            label: 'Instance',
+          },
+          ...dataKeys,
+        ];
+      } else {
+        columns.value = [];
+      }
+    },
+    { immediate: true },
+  );
+
+  watch(
+    dataSource,
+    () => {
+      setTimeout(() => {
+        tableRef.value?.fetchData();
+      });
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  const handleExport = () => {
+    const formatData = queryResults.value.map((item) =>
+      columns.value.reduce<Record<string, string>>((results, column) => {
+        Object.assign(results, { [column.label]: item[column.field] });
+        return results;
+      }, {}),
+    );
+    const colsWidths = columns.value.map(() => ({ width: 30 }));
+
+    exportExcelFile(formatData, colsWidths, 'Sheet1', `${props.dbType}_${t('Web 查询')}.xlsx`);
+  };
+</script>
+<style lang="less" scoped>
+  .panel-results-main {
+    display: flex;
+    flex-direction: column;
+
+    .results-info-main {
+      display: flex;
+      margin-bottom: 15px;
+      font-size: 12px;
+    }
+  }
+</style>
+<style lang="less">
+  .query-result-table {
+    .vxe-table--header-inner-wrapper {
+      height: 28px !important;
+    }
+
+    .vxe-header--column {
+      padding: 3px 0 !important;
+    }
+  }
+</style>
