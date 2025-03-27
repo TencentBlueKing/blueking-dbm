3247673ddfa5a5be5486bd3ea92a01e797678576
chenwenchang <479999519@qq.com>
2025-03-27 10:17:41 +0800

    feat(frontend): dba工具箱 #9859 # Reviewed, transaction id: 47171

---
+++ dbm-ui/frontend/src/views/db-manage/redis/dba-manage/Index.vue
@@ -0,0 +1,40 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
+<template>
+  <DbaManageMenu
+    :routes="routes"
+    sub-title="Redis" />
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  import { TicketTypes } from '@common/const';
+
+  import DbaManageMenu from '@views/db-manage/common/dba-manage-menu/Index.vue';
+
+  const { t } = useI18n();
+
+  const routes = [
+    {
+      dbConsoleValue: 'dbaManage.redis.clusterCutoff',
+      id: `DBA_${TicketTypes.REDIS_CLUSTER_CUTOFF}`,
+      name: t('整机替换'),
+    },
+    {
+      dbConsoleValue: 'dbaManage.redis.clusterReinstallDbmon',
+      id: `DBA_${TicketTypes.REDIS_CLUSTER_REINSTALL_DBMON}`,
+      name: t('集群标准化'),
+    },
+  ];
+</script>
