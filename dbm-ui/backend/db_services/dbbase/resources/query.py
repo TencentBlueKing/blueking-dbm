cbba41bc0b047cc492063dff9d38eb7a7076573f
ycggyao <yao18274936495@163.com>
2025-04-11 15:45:49 +0800

    fix(backend): 集群列表支持批量id过滤 #9847 # Reviewed, transaction id: 38506

--- dbm-ui/backend/db_services/dbbase/resources/query.py
+++ dbm-ui/backend/db_services/dbbase/resources/query.py
@@ -388,6 +388,11 @@ class ListRetrieveResource(BaseListRetrieveResource):
         @param filter_params_map: 过滤参数map
         @param filter_func_map: 过滤函数map，每个函数必须接受query_params和cluster、proxy、storage三个query参数
         """
+
+        cluster_ids = query_params.get("cluster_ids", [])
+        if isinstance(cluster_ids, str):
+            cluster_ids = [int(x) for x in cluster_ids.split(",")]
+
         query_filters = Q(bk_biz_id=bk_biz_id, cluster_type__in=cls.cluster_types)
         proxy_queryset = ProxyInstance.objects.filter(query_filters)
         storage_queryset = StorageInstance.objects.filter(query_filters)
@@ -410,7 +415,7 @@ class ListRetrieveResource(BaseListRetrieveResource):
             "city": Q(region__in=query_params.get("city", "").split(",")),
             "region": Q(region__in=query_params.get("region", "").split(",")),
             # 集群id列表
-            "cluster_ids": Q(id__in=query_params.get("cluster_ids")),
+            "cluster_ids": Q(id__in=cluster_ids),
             # 创建者
             "creator": Q(creator__icontains=query_params.get("creator")),
             # 所属DB模块
