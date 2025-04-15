7223a786d47224b50e88349ade4bc12bb17f412e
chenwenchang <479999519@qq.com>
2025-04-15 14:44:48 +0800

    fix(frontend): 单据详情关联集群字段同步改动 #9917

--- dbm-ui/frontend/src/services/model/ticket/details/common.ts
+++ dbm-ui/frontend/src/services/model/ticket/details/common.ts
@@ -71,25 +71,7 @@ export interface DetailSpecs {
 export interface DetailMachines {
   [key: string]: {
     instance_role: string;
-    related_clusters: {
-      alias: string;
-      bk_biz_id: number;
-      bk_cloud_id: number;
-      cluster_type: string;
-      cluster_type_name: string;
-      creator: string;
-      db_module_id: number;
-      disaster_tolerance_level: string;
-      id: number;
-      immute_domain: string;
-      major_version: string;
-      name: string;
-      phase: string;
-      region: string;
-      status: string;
-      time_zone: string;
-      updater: string;
-    }[];
+    related_clusters: number[];
     related_instances: {
       bk_biz_id: number;
       bk_cloud_id: number;
