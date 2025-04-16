ec789071b6092c8ace0b84ce8827cb0c7807a5f8
3octaves <873551943@qq.com>
2025-04-16 14:38:30 +0800

    feat(frontend): 统一业务选择框 #9905

--- dbm-ui/frontend/src/components/db-app-select/Index.vue
+++ dbm-ui/frontend/src/components/db-app-select/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <AppSelect
     v-bind="{ ...attrs, ...props }"
-    :data="withFavorBizList"
+    :data="dataList"
     :generate-key="(item: IAppItem) => item.bk_biz_id"
     :generate-name="(item: IAppItem) => item.display_name"
     :search-extension-method="searchExtensionMethod"
@@ -25,6 +25,21 @@
         <span style="color: #979ba5">
           (#{{ data.bk_biz_id }}{{ data.english_name ? `, ${data.english_name}` : '' }})
         </span>
+        <template
+          v-if="data.bk_biz_id !== publicBiz.bk_biz_id"
+          #append>
+          <DbIcon
+            v-if="favorBizIdMap[data.bk_biz_id]"
+            class="unfavor-btn ml-4"
+            style="color: #ffb848"
+            type="star-fill"
+            @click.stop="handleUnfavor(data.bk_biz_id)" />
+          <DbIcon
+            v-else
+            class="favor-btn ml-4"
+            type="star"
+            @click.stop="handleFavor(data.bk_biz_id)" />
+        </template>
       </TextOverflowLayout>
     </template>
   </AppSelect>
@@ -38,7 +53,7 @@
 
   import { getBizs } from '@services/source/cmdb';
 
-  import { useUserProfile } from '@stores';
+  import { useGlobalBizs, useUserProfile } from '@stores';
 
   import { UserPersonalSettings } from '@common/const';
 
@@ -52,11 +67,14 @@
 
   interface Props {
     list: IAppItem[];
+    showPublicBiz?: boolean;
   }
 
   type Emits = (e: 'change', value?: IAppItem) => void;
 
-  const props = defineProps<Props>();
+  const props = withDefaults(defineProps<Props>(), {
+    showPublicBiz: true,
+  });
 
   const emits = defineEmits<Emits>();
 
@@ -68,10 +86,18 @@
 
   const attrs = useAttrs();
   const userProfile = useUserProfile();
+  const { publicBiz } = useGlobalBizs();
 
-  const favorBizIdMap = makeMap(userProfile.profile[UserPersonalSettings.APP_FAVOR] || []);
+  const favorBizIdMap = shallowRef(makeMap(userProfile.profile[UserPersonalSettings.APP_FAVOR] || []));
 
-  const withFavorBizList = computed(() => _.sortBy(props.list, (item) => favorBizIdMap[item.bk_biz_id]));
+  const dataList = computed(() => {
+    const sortedList = _.sortBy(props.list, (item) => favorBizIdMap.value[item.bk_biz_id]);
+
+    if (props.showPublicBiz) {
+      sortedList.unshift(publicBiz);
+    }
+    return sortedList;
+  });
 
   const searchExtensionMethod = (data: IAppItem, keyword: string) => {
     const rule = new RegExp(encodeRegexp(keyword), 'i');
@@ -82,6 +108,28 @@
   const handleAppChange = (appInfo?: IAppItem) => {
     modelValue.value = appInfo;
     emits('change', appInfo);
+  };
+
+  const handleUnfavor = (bizId: number) => {
+    const lastFavorBizIdMap = { ...favorBizIdMap.value };
+    delete lastFavorBizIdMap[bizId];
+    favorBizIdMap.value = lastFavorBizIdMap;
+
+    userProfile.updateProfile({
+      label: UserPersonalSettings.APP_FAVOR,
+      values: Object.keys(lastFavorBizIdMap),
+    });
+  };
+
+  const handleFavor = (bizId: number) => {
+    favorBizIdMap.value = {
+      ...favorBizIdMap.value,
+      [bizId]: true,
+    };
+    userProfile.updateProfile({
+      label: UserPersonalSettings.APP_FAVOR,
+      values: Object.keys(favorBizIdMap.value),
+    });
   };
 </script>
 
@@ -98,8 +146,25 @@
 
   .tippy-box[data-theme='bk-app-select-menu'] {
     .db-select-no-permission-item {
+      width: 100%;
+
+      .layout-content {
+        width: 100%;
+      }
+
       & span {
         display: inline !important;
+      }
+
+      &:hover {
+        .favor-btn {
+          opacity: 100%;
+        }
+      }
+
+      .favor-btn {
+        opacity: 0%;
+        transition: all 0.1s;
       }
     }
   }
