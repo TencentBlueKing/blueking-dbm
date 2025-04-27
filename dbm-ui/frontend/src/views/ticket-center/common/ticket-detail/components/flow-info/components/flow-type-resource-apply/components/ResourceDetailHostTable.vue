adb994eba55e8e69902b01fde8090ceb851ec264
hLinx <327159425@qq.com>
2025-04-27 12:19:59 +0800

    fix(frontend): 单据详情——flow资源明细主从分组展示 #10294

---
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/flow-info/components/flow-type-resource-apply/components/ResourceDetailHostTable.vue
@@ -0,0 +1,96 @@
+<template>
+  <BkTable
+    :data="data"
+    :row-config="{
+      useKey: true,
+      keyField: 'ip',
+    }">
+    <BkTableColumn
+      field="ip"
+      fixed="left"
+      label="IP"
+      :width="200">
+      <template #default="{ data }: { data: IResouce & { tag: string } }">
+        {{ data.ip }}
+        <BkTag v-if="data.tag">{{ data.tag }}</BkTag>
+      </template>
+    </BkTableColumn>
+    <BkTableColumn
+      field="city"
+      :label="t('地域')"
+      :min-width="100">
+      <template #default="{ data }: { data: IResouce }">
+        {{ data.city || '--' }}
+      </template>
+    </BkTableColumn>
+    <BkTableColumn
+      field="sub_zone"
+      :label="t('园区')"
+      :min-width="100">
+      <template #default="{ data }: { data: IResouce }">
+        {{ data.sub_zone || '--' }}
+      </template>
+    </BkTableColumn>
+    <BkTableColumn
+      field="rack_id"
+      :label="t('机架')"
+      :min-width="100">
+      <template #default="{ data }: { data: IResouce }">
+        {{ data.rack_id || '--' }}
+      </template>
+    </BkTableColumn>
+    <BkTableColumn
+      field="os_type"
+      :label="t('操作系统名称')"
+      :min-width="180">
+      <template #default="{ data }: { data: IResouce }">
+        {{ data.os_type || '--' }}
+      </template>
+    </BkTableColumn>
+    <BkTableColumn
+      field="device_class"
+      :label="t('机型')"
+      :min-width="100">
+      <template #default="{ data }: { data: IResouce }">
+        {{ data.device_class || '--' }}
+      </template>
+    </BkTableColumn>
+    <BkTableColumn
+      field="bk_cpu"
+      :label="t('CPU_核_')"
+      :min-width="100" />
+    <BkTableColumn
+      field="bk_mem"
+      :label="t('内存M')"
+      :min-width="100" />
+    <BkTableColumn
+      field="bk_disk"
+      :label="t('磁盘G')"
+      :min-width="100" />
+  </BkTable>
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  export interface IResouce {
+    bk_cloud_id: number;
+    bk_cpu: number;
+    bk_disk: number;
+    bk_mem: number;
+    city: string;
+    device_class: string;
+    ip: string;
+    os_name: string;
+    os_type: string;
+    rack_id: string;
+    sub_zone: string;
+  }
+
+  interface Props {
+    data: IResouce[];
+  }
+
+  defineProps<Props>();
+
+  const { t } = useI18n();
+</script>
