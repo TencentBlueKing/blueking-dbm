8e518872890abcb6795104246f9704a063bbc40a
chenwenchang <479999519@qq.com>
2025-05-13 16:17:50 +0800

    fix(frontend): sqlserver数据迁移无法单据克隆修复 #10497 # Reviewed, transaction id: 42227

--- dbm-ui/frontend/src/views/ticket-center/common/TicketClone.vue
+++ dbm-ui/frontend/src/views/ticket-center/common/TicketClone.vue
@@ -148,9 +148,9 @@
     [TicketTypes.SQLSERVER_CLEAR_DBS]: TicketTypes.SQLSERVER_CLEAR_DBS, // sqlserver 清档
     [TicketTypes.SQLSERVER_DATA_MIGRATE]: 'sqlServerDataMigrate', // sqlserver 数据迁移
     [TicketTypes.SQLSERVER_DBRENAME]: TicketTypes.SQLSERVER_DBRENAME, // sqlserver DB重命名
-    [TicketTypes.SQLSERVER_FULL_MIGRATE]: 'sqlServerDataMigrate', // sqlserver 全库迁移
+    [TicketTypes.SQLSERVER_FULL_MIGRATE]: TicketTypes.SQLSERVER_FULL_MIGRATE, // sqlserver 全库迁移
     [TicketTypes.SQLSERVER_IMPORT_SQLFILE]: 'sqlServerExecute', // sqlserver 变更SQL执行
-    [TicketTypes.SQLSERVER_INCR_MIGRATE]: 'sqlServerDataMigrate', // sqlserver 增量迁移
+    [TicketTypes.SQLSERVER_INCR_MIGRATE]: TicketTypes.SQLSERVER_INCR_MIGRATE, // sqlserver 增量迁移
     [TicketTypes.SQLSERVER_MASTER_FAIL_OVER]: 'sqlServerMasterFailover', // sqlserver 主库故障切换
     [TicketTypes.SQLSERVER_MASTER_SLAVE_SWITCH]: 'sqlServerMasterSlaveSwap', // sqlserver 主从互切
     [TicketTypes.SQLSERVER_RESTORE_LOCAL_SLAVE]: TicketTypes.SQLSERVER_RESTORE_LOCAL_SLAVE, // sqlserver 重建从库_原地重建
