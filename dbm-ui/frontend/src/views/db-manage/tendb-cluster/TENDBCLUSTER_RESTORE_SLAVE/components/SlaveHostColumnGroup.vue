016f08842c6ec5384a65f9114f110f297270d119
chenwenchang <479999519@qq.com>
2025-06-06 16:16:44 +0800

    feat(frontend): tendbcluster替换接入层 #10933 # Reviewed, transaction id: 47436

--- dbm-ui/frontend/src/views/db-manage/tendb-cluster/TENDBCLUSTER_RESTORE_SLAVE/components/SlaveHostColumnGroup.vue
+++ dbm-ui/frontend/src/views/db-manage/tendb-cluster/TENDBCLUSTER_RESTORE_SLAVE/components/SlaveHostColumnGroup.vue
@@ -58,7 +58,7 @@
 
   import { checkInstance } from '@services/source/dbbase';
 
-  import type { ClusterTypes } from '@common/const';
+  import { ClusterTypes, DBTypes } from '@common/const';
   import { ipv4 } from '@common/regex';
 
   import InstanceSelector, {
