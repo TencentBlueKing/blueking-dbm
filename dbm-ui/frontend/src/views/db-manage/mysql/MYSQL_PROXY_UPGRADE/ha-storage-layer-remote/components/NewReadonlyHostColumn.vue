43d194dd32464c7fb5e379a35c00db3e32ce6bbf
Austin <1344583166@qq.com>
2025-06-17 16:25:44 +0800

    fix(frontend): mysql版本升级主机交互问题修复 #11111 # Reviewed, transaction id: 46437

--- dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_PROXY_UPGRADE/ha-storage-layer-remote/components/NewReadonlyHostColumn.vue
+++ dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_PROXY_UPGRADE/ha-storage-layer-remote/components/NewReadonlyHostColumn.vue
@@ -14,18 +14,18 @@
 <template>
   <EditableColumn
     :append-rules="rules"
-    field="new_readonly_host.ip"
+    field="new_readonly_host"
     :label="t('新只读主机')"
     :loading="loading"
     :min-width="200">
     <template #headAppend> <span class="required-icon" /> </template>
     <EditableBlock
-      v-if="cluster.id && !cluster.readonly_host"
+      v-if="cluster.id && !cluster.readonly_host.length"
       :placeholder="t('无只读主机')" />
     <EditableInput
       v-else
-      v-model="modelValue.ip"
-      :placeholder="t('请输入n个主机IP', { n: limit })"
+      v-model="inputIps"
+      :placeholder="t('请输入n个主机IP', { n: hostLimit })"
       @change="handleInputChange">
       <template #append>
         <DbIcon
@@ -39,7 +39,7 @@
   <ResourceHostSelector
     v-model="selected"
     v-model:is-show="showSelector"
-    :limit="limit"
+    :limit="hostLimit"
     :params="{
       for_bizs: [currentBizId, 0],
       resource_types: [DBTypes.MYSQL, 'PUBLIC'],
@@ -65,7 +65,7 @@
         bk_cloud_id: number;
         bk_host_id: number;
         ip: string;
-      };
+      }[];
     };
   }
 
@@ -74,60 +74,68 @@
   /**
    * 绑定的modelValue须包含ip
    */
-  const modelValue = defineModel<{
-    bk_biz_id?: number;
-    bk_cloud_id?: number;
-    bk_host_id?: number;
-    ip: string;
-  }>({
-    default: () => ({
-      bk_biz_id: undefined,
-      bk_cloud_id: undefined,
-      bk_host_id: undefined,
-      ip: '',
-    }),
+  const modelValue = defineModel<
+    {
+      bk_biz_id?: number;
+      bk_cloud_id?: number;
+      bk_host_id?: number;
+      ip: string;
+    }[]
+  >({
+    default: () => [
+      {
+        bk_biz_id: undefined,
+        bk_cloud_id: undefined,
+        bk_host_id: undefined,
+        ip: '',
+      },
+    ],
   });
 
   const { t } = useI18n();
 
   const currentBizId = window.PROJECT_CONFIG.BIZ_ID;
 
-  const limit = 1;
+  const inputIps = ref('');
   const showSelector = ref(false);
-  const selected = computed(() => (modelValue.value.bk_host_id ? ([modelValue.value] as IValue[]) : ([] as IValue[])));
+
+  const hostLimit = computed(() => props.cluster.readonly_host.length);
+  const selected = computed(() =>
+    modelValue.value.filter((item) => !!item.ip).length ? (modelValue.value as IValue[]) : ([] as IValue[]),
+  );
 
   const rules = [
     {
       message: t('新只读主机不能为空'),
       trigger: 'change',
-      validator: (value: string) => !props.cluster.readonly_host || !!value,
+      validator: (value: Props['cluster']['readonly_host']) => value.every((item) => !!item.ip),
     },
     {
       message: t('IP 格式不符合IPv4标准'),
       trigger: 'change',
-      validator: (value: string) => !props.cluster.readonly_host || ipv4.test(value),
+      validator: (value: Props['cluster']['readonly_host']) => value.every((item) => ipv4.test(item.ip)),
     },
     {
-      message: t('最多输入n个主机IP', { n: limit }),
+      message: t('新只读主机数与旧只读主机数不一致'),
       trigger: 'blur',
-      validator: (value: string) => !props.cluster.readonly_host || value.split(batchSplitRegex).length <= limit,
+      validator: (value: Props['cluster']['readonly_host']) => value.length === hostLimit.value,
     },
     {
       message: t('目标主机不存在'),
       trigger: 'blur',
-      validator: () => !props.cluster.readonly_host || Boolean(modelValue.value.bk_host_id),
+      validator: () => modelValue.value.every((item) => Boolean(item.bk_host_id)),
     },
   ];
 
   const { loading, run: queryHost } = useRequest(fetchList, {
     manual: true,
     onSuccess: (data) => {
-      const [currentHost] = data.results;
-      if (currentHost) {
-        modelValue.value.bk_biz_id = currentHost.dedicated_biz;
-        modelValue.value.bk_cloud_id = currentHost.bk_cloud_id;
-        modelValue.value.bk_host_id = currentHost.bk_host_id;
-      }
+      modelValue.value = data.results.map((item) => ({
+        bk_biz_id: item.dedicated_biz,
+        bk_cloud_id: item.bk_cloud_id,
+        bk_host_id: item.bk_host_id,
+        ip: item.ip,
+      }));
     },
   });
 
@@ -136,31 +144,33 @@
   };
 
   const handleInputChange = (value: string) => {
-    modelValue.value = {
-      bk_biz_id: undefined,
-      bk_cloud_id: undefined,
-      bk_host_id: undefined,
-      ip: value,
-    };
+    modelValue.value = [
+      {
+        bk_biz_id: undefined,
+        bk_cloud_id: undefined,
+        bk_host_id: undefined,
+        ip: value,
+      },
+    ];
     if (value) {
       queryHost({
         bk_biz_id: window.PROJECT_CONFIG.BIZ_ID,
-        hosts: value,
-        limit,
+        hosts: value.split(batchSplitRegex).join(','),
+        limit: hostLimit.value,
         offset: 0,
       });
     }
   };
 
   const handleSelectorChange = (hostList: IValue[]) => {
-    const [currentHost] = hostList;
-    if (currentHost) {
-      modelValue.value = {
-        bk_biz_id: currentHost.dedicated_biz || currentHost.bk_biz_id,
-        bk_cloud_id: currentHost.bk_cloud_id,
-        bk_host_id: currentHost.bk_host_id,
-        ip: currentHost.ip,
-      };
+    if (hostList.length) {
+      inputIps.value = hostList.map((item) => item.ip).join(',');
+      modelValue.value = hostList.map((item) => ({
+        bk_biz_id: item.dedicated_biz || item.bk_biz_id,
+        bk_cloud_id: item.bk_cloud_id,
+        bk_host_id: item.bk_host_id,
+        ip: item.ip,
+      }));
     }
   };
 </script>
