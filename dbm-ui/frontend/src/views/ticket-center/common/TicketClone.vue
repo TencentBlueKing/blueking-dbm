8860235c8125e3033543d37f81dbe8bbab7fd100
chenwenchang <479999519@qq.com>
2025-06-20 17:06:01 +0800

    feat(frontend): mysql单据资源池自动匹配和重选规格_tendbcluster部署只读接入层 #11086

--- dbm-ui/frontend/src/views/ticket-center/common/TicketClone.vue
+++ dbm-ui/frontend/src/views/ticket-center/common/TicketClone.vue
@@ -70,6 +70,7 @@
     [TicketTypes.TENDBCLUSTER_SPIDER_ADD_NODES]: TicketTypes.TENDBCLUSTER_SPIDER_ADD_NODES, // Spider扩容接入层
     [TicketTypes.TENDBCLUSTER_SPIDER_MNT_APPLY]: TicketTypes.TENDBCLUSTER_SPIDER_MNT_APPLY, // Spider 添加运维节点
     [TicketTypes.TENDBCLUSTER_SPIDER_REDUCE_NODES]: TicketTypes.TENDBCLUSTER_SPIDER_REDUCE_NODES, // spider 缩容接入层
+    [TicketTypes.TENDBCLUSTER_SPIDER_SLAVE_APPLY]: TicketTypes.TENDBCLUSTER_SPIDER_SLAVE_APPLY, // Spider 部署只读接入层
     [TicketTypes.TENDBCLUSTER_SPIDER_SWITCH_NODES]: TicketTypes.TENDBCLUSTER_SPIDER_SWITCH_NODES, // spider 替换接入层
   };
 
@@ -193,7 +194,6 @@
     [TicketTypes.TENDBCLUSTER_RESTORE_LOCAL_SLAVE]: TicketTypes.TENDBCLUSTER_RESTORE_LOCAL_SLAVE, // spider 重建从库-原地重建
     [TicketTypes.TENDBCLUSTER_ROLLBACK_CLUSTER]: TicketTypes.TENDBCLUSTER_ROLLBACK_CLUSTER, // Spider 定点回档
     [TicketTypes.TENDBCLUSTER_SPIDER_MNT_APPLY]: TicketTypes.TENDBCLUSTER_SPIDER_MNT_APPLY, // Spider 添加运维节点
-    [TicketTypes.TENDBCLUSTER_SPIDER_SLAVE_APPLY]: 'SpiderProxySlaveApply', // Spider 部署只读接入层
     [TicketTypes.TENDBCLUSTER_TRUNCATE_DATABASE]: 'spiderDbClear', // Spider tendbcluster 清档
   };
 
