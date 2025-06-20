e2c1e914ccc318478c280cb25b19be6f5381c81e
groobyming <lidanzhuang001@gmail.com>
2025-06-20 15:26:09 +0800

    fix(k8s-dbs): 修复测试用例问题 #11189

--- dbm-services/k8s-dbs/metadata/dbaccess/tests/cluster_dbaccess_test.go
+++ dbm-services/k8s-dbs/metadata/dbaccess/tests/cluster_dbaccess_test.go
@@ -214,7 +214,7 @@ func TestListCluster(t *testing.T) {
 		Limit: 10,
 	}
 
-	clusterList, rows, err := dbAccess.ListByPage(params, pagination)
+	clusterList, rows, err := dbAccess.ListByPage(params, &pagination)
 	assert.NoError(t, err)
 	assert.Equal(t, uint64(2), rows)
 	assert.Equal(t, len(clusterList), len(clusterList))
