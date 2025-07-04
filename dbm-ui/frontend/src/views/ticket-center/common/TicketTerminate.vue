1c88206f2447e8fa238331a5d441bc0d8f662a86
hLinx <327159425@qq.com>
2025-07-04 17:00:28 +0800

    fix(frontend): 单据中的资源明细问题 #11447

---
+++ dbm-ui/frontend/src/views/ticket-center/common/TicketTerminate.vue
@@ -0,0 +1,27 @@
+<template>
+  <ProcessFailedTerminate :data="data">
+    <BkButton theme="danger">
+      {{ t('终止单据') }}
+    </BkButton>
+  </ProcessFailedTerminate>
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  import TicketModel from '@services/model/ticket/ticket';
+
+  import { useUserProfile } from '@stores';
+
+  import ProcessFailedTerminate from '@views/ticket-center/common/action-confirm/ProcessFailedTerminate.vue';
+
+  interface Props {
+    data: TicketModel;
+  }
+
+  const props = defineProps<Props>();
+
+  const { username } = useUserProfile();
+  const { t } = useI18n();
+
+  console.log(props, username);
+</script>
