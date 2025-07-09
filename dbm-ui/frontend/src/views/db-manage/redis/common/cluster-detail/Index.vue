73fc6f5b1b93ee658c5d449283c75d2400fef63c
hLinx <327159425@qq.com>
2025-07-09 11:24:10 +0800

    fix(frontend): redis 集群禁用 cluster_type 传参错误 #11552

--- dbm-ui/frontend/src/views/db-manage/redis/common/cluster-detail/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/redis/common/cluster-detail/Index.vue
@@ -341,7 +341,7 @@
   };
 
   const { handleDeleteCluster, handleDisableCluster, handleEnableCluster } = useOperateClusterBasic(
-    ClusterTypes.TENDBHA,
+    ClusterTypes.REDIS_CLUSTER,
     {
       onSuccess: () => {
         fetchDetailData();
