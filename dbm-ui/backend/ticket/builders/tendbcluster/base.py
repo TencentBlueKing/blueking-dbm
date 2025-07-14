4383446f9b0454007f1ce1dd1344aab68661d14f
yksitu <1297650644@qq.com>
2025-07-14 16:55:37 +0800

    fix(mysql): 优化spider缩容单据，spider master 下限调整为1 #11682

--- dbm-ui/backend/ticket/builders/tendbcluster/base.py
+++ dbm-ui/backend/ticket/builders/tendbcluster/base.py
@@ -9,13 +9,14 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 from django.db.models import Q
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 from backend.configuration.constants import DBType
 from backend.db_meta.enums import ClusterTenDBClusterStatusFlag, ClusterType, TenDBClusterSpiderRole
 from backend.db_meta.models import Cluster
-from backend.flow.consts import MAX_SPIDER_MASTER_COUNT, MIN_SPIDER_MASTER_COUNT, MIN_SPIDER_SLAVE_COUNT
+from backend.flow.consts import MIN_SPIDER_MASTER_COUNT, MIN_SPIDER_SLAVE_COUNT
+from backend.flow.utils.spider.spider_bk_config import calc_spider_max_count, get_spider_version_and_charset
 from backend.ticket import builders
 from backend.ticket.builders import TicketFlowBuilder
 from backend.ticket.builders.common.base import (
@@ -99,6 +100,16 @@ class TendbBaseOperateDetailSerializer(MySQLBaseOperateDetailSerializer):
                 continue
 
             # 获取当前存在的spider master/spider mnt 节点数量 以及 新加入的节点数量
+            # 获取集群当前spider_master部署数量上限
+            # 获取Spider版本号
+            __, spider_version = get_spider_version_and_charset(cluster.bk_biz_id, cluster.db_module_id)
+            upper_limit_count = calc_spider_max_count(
+                bk_biz_id=cluster.bk_biz_id,
+                db_module_id=cluster.db_module_id,
+                db_version=spider_version,
+                immute_domain=cluster.immute_domain,
+            )
+
             spider_master_mnt_count = cluster.proxyinstance_set.filter(
                 Q(tendbclusterspiderext__spider_role=TenDBClusterSpiderRole.SPIDER_MASTER)
                 | Q(tendbclusterspiderext__spider_role=TenDBClusterSpiderRole.SPIDER_MNT)
@@ -112,8 +123,11 @@ class TendbBaseOperateDetailSerializer(MySQLBaseOperateDetailSerializer):
                     spider_ip_list["count"] = len(spider_ip_list["hosts"])
                 new_add_count = spider_ip_list["count"]
 
-            if spider_master_mnt_count + new_add_count > MAX_SPIDER_MASTER_COUNT:
-                raise serializers.ValidationError(_("【{}】请保证集群部署的接入层主节点和运维节点的总和小于37").format(cluster.name))
+            if (spider_master_mnt_count + new_add_count) * 2 > upper_limit_count:
+
+                raise serializers.ValidationError(
+                    _("【{}】请确保集群扩容接入层后主节点和运维节点的总和小于等于集群【{}】上限的一半").format(cluster.immute_domain, upper_limit_count)
+                )
 
     def validate_min_spider_count(self, attrs):
         """校验缩容后，spider节点能满足最小限度"""
