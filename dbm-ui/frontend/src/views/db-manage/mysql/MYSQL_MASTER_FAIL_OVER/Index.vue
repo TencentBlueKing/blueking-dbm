c0a6838730152e787dd0dbce0e5b0ddb34aa7c37
chenwenchang <479999519@qq.com>
2025-07-16 14:47:54 +0800

    fix(frontend): mysql主库故障切换交互优化 #9859 # Reviewed, transaction id: 50557

--- dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_MASTER_FAIL_OVER/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_MASTER_FAIL_OVER/Index.vue
@@ -42,7 +42,7 @@
       :model="formData">
       <EditableTable
         ref="table"
-        class="mb-10"
+        class="mb-16"
         :model="formData.tableData">
         <EditableRow
           v-for="(item, index) in formData.tableData"
