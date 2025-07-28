9b8cc75d61fe82c9ad0e2a25c083ff984d20d55b
OMG-By <504094596@qq.com>
2025-07-28 17:32:07 +0800

    feat(redis): redis zone_list #11932

--- dbm-ui/backend/db_meta/api/cluster/tendissingle/handler.py
+++ dbm-ui/backend/db_meta/api/cluster/tendissingle/handler.py
@@ -43,6 +43,7 @@ class TendisSingleHandler(ClusterHandler):
         bk_cloud_id: int = DEFAULT_BK_CLOUD_ID,
         region: str = "",
         disaster_tolerance_level: str = "",
+        zone_list: list = None,
     ):
         """「必须」创建集群"""
         pkg_create_single(
@@ -58,6 +59,7 @@ class TendisSingleHandler(ClusterHandler):
             bk_cloud_id=bk_cloud_id,
             region=region,
             disaster_tolerance_level=disaster_tolerance_level,
+            zone_list=zone_list if zone_list else [],
         )
 
     @transaction.atomic
--- dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_cluster_migrate_load.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_cluster_migrate_load.py
@@ -321,6 +321,7 @@ class RedisClusterMigrateLoadFlow(object):
                 "created_by": self.data["created_by"],
                 "region": params["clusterinfo"]["region"],
                 "disaster_tolerance_level": self.data.get("disaster_tolerance_level", AffinityEnum.CROS_SUBZONE),
+                "zone_list": self.data.get("zone_list", []),
             }
 
             # 建立集群关系，不同类型走不通方式
@@ -746,6 +747,7 @@ class RedisInsMigrateLoadFlow(object):
                     "region": ins["clusterinfo"]["region"],
                     "meta_func_name": RedisDBMeta.redis_instance.__name__,
                     "disaster_tolerance_level": self.data.get("disaster_tolerance_level", AffinityEnum.CROS_SUBZONE),
+                    "zone_list": self.data.get("zone_list", []),
                 }
                 acts_list.append(
                     {
