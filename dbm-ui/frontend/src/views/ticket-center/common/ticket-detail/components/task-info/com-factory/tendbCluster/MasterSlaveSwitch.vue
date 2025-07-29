b059782b91549205c77468628567df9663cba6ef
chenwenchang <479999519@qq.com>
2025-07-29 11:51:43 +0800

    perf(frontend): mysql、tendbcluster主库故障切换、主从互切去掉同步延迟检查 #11425 # Reviewed, transaction id: 52055

--- dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/task-info/com-factory/tendbCluster/MasterSlaveSwitch.vue
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/task-info/com-factory/tendbCluster/MasterSlaveSwitch.vue
@@ -33,9 +33,6 @@
     <InfoItem :label="t('检查业务来源的连接')">
       {{ ticketDetails.details.is_check_process ? t('是') : t('否') }}
     </InfoItem>
-    <InfoItem :label="t('检查主从同步延迟')">
-      {{ ticketDetails.details.is_check_delay ? t('是') : t('否') }}
-    </InfoItem>
     <InfoItem :label="t('检查主从数据校验结果')">
       {{ ticketDetails.details.is_verify_checksum ? t('是') : t('否') }}
     </InfoItem>
