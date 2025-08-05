f0346a4ffb488b7d76f8e05605f9ed6d2202e3f6
groobyming <lidanzhuang001@gmail.com>
2025-08-05 12:23:32 +0800

    refactor(k8s-dbs): 操作日志列表新增操作别名描述 #12140

--- dbm-services/k8s-dbs/metadata/vo/response/cluster_operation_log_response.go
+++ dbm-services/k8s-dbs/metadata/vo/response/cluster_operation_log_response.go
@@ -20,22 +20,67 @@ limitations under the License.
 package response
 
 import (
+	"encoding/json"
 	commtypes "k8s-dbs/common/types"
 )
 
+var requestTypeMapping = map[string]string{
+	"CreateCluster":        "创建集群",
+	"DeleteCluster":        "删除集群",
+	"UpdateCluster":        "全量更新集群",
+	"PartialUpdateCluster": "局部更新集群",
+	"StartCluster":         "启动集群",
+	"StopCluster":          "停止集群",
+	"RestartCluster":       "重启集群",
+	"StartComponent":       "启动组件",
+	"StopComponent":        "停止组件",
+	"RestartComponent":     "重启组件",
+	"VerticalScaling":      "垂直扩容",
+	"HorizontalScaling":    "水平扩容",
+	"VolumeExpansion":      "磁盘扩容",
+	"UpgradeComp":          "升级组件",
+	"ExposeService":        "暴露服务",
+	"Undefined":            "未知操作",
+	"CreateK8sNs":          "创建命名空间",
+	"DeleteK8sPod":         "删除 Pod 实例",
+}
+
 // ClusterOperationLogResponse response vo 定义
 type ClusterOperationLogResponse struct {
-	ID             uint64                 `json:"id"`
-	RequestID      string                 `json:"requestId"`
-	K8sClusterName string                 `json:"k8sClusterName"`
-	ClusterName    string                 `json:"clusterName"`
-	NameSpace      string                 `json:"namespace"`
-	RequestType    string                 `json:"requestType"`
-	RequestParams  string                 `json:"requestParams"`
-	Status         string                 `json:"status"`
-	Description    string                 `json:"description"`
-	CreatedBy      string                 `json:"createdBy"`
-	CreatedAt      commtypes.JSONDatetime `json:"createdAt"`
-	UpdatedBy      string                 `json:"updatedBy"`
-	UpdatedAt      commtypes.JSONDatetime `json:"updatedAt"`
+	ID               uint64                 `json:"id"`
+	RequestID        string                 `json:"requestId"`
+	K8sClusterName   string                 `json:"k8sClusterName"`
+	ClusterName      string                 `json:"clusterName"`
+	NameSpace        string                 `json:"namespace"`
+	RequestType      string                 `json:"requestType"`
+	RequestTypeAlias string                 `json:"requestTypeAlias"`
+	RequestParams    string                 `json:"requestParams"`
+	Status           string                 `json:"status"`
+	Description      string                 `json:"description"`
+	CreatedBy        string                 `json:"createdBy"`
+	CreatedAt        commtypes.JSONDatetime `json:"createdAt"`
+	UpdatedBy        string                 `json:"updatedBy"`
+	UpdatedAt        commtypes.JSONDatetime `json:"updatedAt"`
+}
+
+// MarshalJSON 自定义 ClusterOperationLogResponse JSON 序列化逻辑
+func (k ClusterOperationLogResponse) MarshalJSON() ([]byte, error) {
+	k.RequestTypeAlias = requestTypeMapping[k.RequestType]
+	output := map[string]interface{}{
+		"id":               k.ID,
+		"requestId":        k.RequestID,
+		"k8sClusterName":   k.NameSpace,
+		"clusterName":      k.ClusterName,
+		"nameSpace":        k.NameSpace,
+		"requestType":      k.RequestType,
+		"requestTypeAlias": k.RequestTypeAlias,
+		"requestParams":    k.RequestParams,
+		"status":           k.Status,
+		"description":      k.Description,
+		"createdBy":        k.CreatedBy,
+		"createdAt":        k.CreatedAt,
+		"updatedBy":        k.UpdatedBy,
+		"updatedAt":        k.UpdatedAt,
+	}
+	return json.Marshal(output)
 }
