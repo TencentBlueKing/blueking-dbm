5dc8f4a354841d983a9136ba04409d62424bd33b
3octaves <873551943@qq.com>
2025-08-26 11:59:56 +0800

    feat(frontend): redis列表和详情页增加负载 #12557

--- dbm-ui/frontend/src/views/db-manage/redis/cluster-ha-list/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/redis/cluster-ha-list/Index.vue
@@ -242,7 +242,14 @@
           :label="t('主访问入口')"
           :selected-list="selected"
           @go-detail="handleToDetails"
-          @refresh="fetchData" />
+          @refresh="fetchData">
+          <template #append="{ data }">
+            <ClusterLoad
+              :cluster-type="ClusterTypes.REDIS_INSTANCE"
+              :domain="data.master_domain"
+              size="small" />
+          </template>
+        </MasterDomainColumn>
       </template>
       <template #slaveDomain>
         <SlaveDomainColumn
@@ -318,6 +325,7 @@
   import ClusterBatchOperation from '@views/db-manage/common/cluster-batch-opration/Index.vue';
   import ClusterDomainDnsRelation from '@views/db-manage/common/cluster-domain-dns-relation/Index.vue';
   import ClusterIpCopy from '@views/db-manage/common/cluster-ip-copy/Index.vue';
+  import ClusterLoad from '@views/db-manage/common/cluster-load/Index.vue';
   import ClusterTable, {
     MasterDomainColumn,
     OperationColumn,
