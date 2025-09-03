53da0ef961942a93ea69572a41582d1caa41a504
3octaves <873551943@qq.com>
2025-09-03 16:16:01 +0800

    feat(frontend): mongodb库表备份支持批量录入 #12735

--- dbm-ui/frontend/src/views/db-manage/mongodb/MONGODB_BACKUP/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/mongodb/MONGODB_BACKUP/Index.vue
@@ -15,15 +15,20 @@
   <SmartAction>
     <div class="mongo-db-table-backup-page">
       <BkAlert
+        class="mb-16"
         theme="info"
         :title="t('库表备份：指定库表备份，支持模糊匹配')" />
+      <BatchInput
+        :config="batchInputConfig"
+        @change="handleBatchInput" />
       <DbForm
         ref="form"
-        class="toolbox-form"
+        class="toolbox-form mt-16"
         form-type="vertical"
         :model="formData"
         style="margin-top: 16px">
         <EditableTable
+          :key="tableKey"
           ref="editableTable"
           class="mt-16 mb-16"
           :model="formData.tableData">
@@ -127,12 +132,15 @@
 
   import { TicketTypes } from '@common/const';
 
+  import BatchInput from '@views/db-manage/common/batch-input/Index.vue';
   import TicketPayload, {
     createTickePayload,
   } from '@views/db-manage/common/toolbox-field/form-item/ticket-payload/Index.vue';
   import ClusterColumn from '@views/db-manage/mongodb/common/toolbox-field/cluster-column/Index.vue';
   import DbNameColumn from '@views/db-manage/mongodb/common/toolbox-field/db-name-column/Index.vue';
   import TableNameColumn from '@views/db-manage/mongodb/common/toolbox-field/table-name-column/Index.vue';
+
+  import { random } from '@utils';
 
   interface IDataRow {
     cluster: {
@@ -170,6 +178,34 @@
   });
 
   const { t } = useI18n();
+
+  const batchInputConfig = [
+    {
+      case: 'mongodb.test.dba.db',
+      key: 'domain',
+      label: t('目标集群'),
+    },
+    {
+      case: 'db1,db2',
+      key: 'db_patterns',
+      label: t('指定 DB 名'),
+    },
+    {
+      case: 'db1,db2',
+      key: 'ignore_dbs',
+      label: t('忽略 DB 名'),
+    },
+    {
+      case: 'table1,table2',
+      key: 'table_patterns',
+      label: t('指定表名'),
+    },
+    {
+      case: 'table1,table2',
+      key: 'ignore_tables',
+      label: t('忽略表名'),
+    },
+  ];
 
   useTicketDetail<Mongodb.Backup>(TicketTypes.MONGODB_BACKUP, {
     onSuccess(ticketDetail) {
@@ -209,6 +245,8 @@
 
   const formRef = useTemplateRef('form');
   const editableTableRef = useTemplateRef('editableTable');
+
+  const tableKey = ref(random());
 
   const formData = reactive(createDefaultFormData());
 
@@ -257,6 +295,27 @@
 
     formData.tableData = [...(selected.value.length ? formData.tableData : []), ...newList];
     window.changeConfirm = true;
+  };
+
+  const handleBatchInput = (data: Record<string, any>[], isClear: boolean) => {
+    const dataList = data.map((item) =>
+      createRowData({
+        cluster: {
+          master_domain: item.domain,
+        } as IDataRow['cluster'],
+        db_patterns: item.db_patterns ? item.db_patterns.split(',') : [],
+        ignore_dbs: item.ignore_dbs ? item.ignore_dbs.split(',') : [],
+        ignore_tables: item.ignore_tables ? item.ignore_tables.split(',') : [],
+        table_patterns: item.table_patterns ? item.table_patterns.split(',') : [],
+      }),
+    );
+
+    if (isClear) {
+      tableKey.value = random();
+      formData.tableData = [...dataList];
+    } else {
+      formData.tableData = [...(selected.value.length ? formData.tableData : []), ...dataList];
+    }
   };
 
   const handleDbTableBatchEdit = (value: string[], field: string) => {
