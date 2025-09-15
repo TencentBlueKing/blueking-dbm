3d1438f6b10569de559d75b57329fec9f50d2df8
3octaves <873551943@qq.com>
2025-09-15 17:32:23 +0800

    feat(frontend): mongodb新增分片集群增加分片数单据 #12936

---
+++ dbm-ui/frontend/src/services/model/ticket/details/mongodb/addShard.ts
@@ -0,0 +1,24 @@
+import type { DetailBase, DetailClusters, DetailSpecs } from '../common';
+
+export interface AddShard extends DetailBase {
+  clusters: DetailClusters;
+  infos: {
+    add_shards_num: number; // 新增分片数
+    city_code: string;
+    cluster_id: number;
+    current_shard_nodes_num: number; // 当前每分片节点数
+    current_shards_num: number; // 展示用
+    db_version: string;
+    disaster_tolerance_level: string; // 亲和性
+    node_replicaset_count: number; // 单机部署实例数
+    resource_spec: {
+      shard_nodes: {
+        count: number; // 台数
+        spec_id: number;
+      };
+    };
+    single_host_shard_num: number; // 展示用
+  }[];
+  ip_source: 'resource_pool';
+  specs: DetailSpecs;
+}
