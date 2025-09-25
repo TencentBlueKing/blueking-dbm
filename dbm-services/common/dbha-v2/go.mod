715b49c6a8d683090dea4f66396ecce53593e55b
uriwang <uriwang@tencent.com>
2025-09-25 17:14:29 +0800

    feat(backend): Redefine the Prometheus metrics for the DBHA-V2. issue: #12991

--- dbm-services/common/dbha-v2/go.mod
+++ dbm-services/common/dbha-v2/go.mod
@@ -27,12 +27,15 @@ require (
 require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/hako/durafmt v0.0.0-20210608085754-5c1018a4e16b
+	github.com/prometheus/client_golang v1.23.2
 	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.63.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
@@ -48,12 +51,17 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.66.1 // indirect
+	github.com/prometheus/procfs v0.16.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel v1.38.0 // indirect
 	go.opentelemetry.io/otel/metric v1.38.0 // indirect
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	golang.org/x/arch v0.20.0 // indirect
 )
 
--- dbm-services/common/dbha-v2/pkg/haapm/gauge.go
+++ dbm-services/common/dbha-v2/pkg/haapm/gauge.go
@@ -23,3 +23,170 @@
  */
 
 package haapm
+
+import (
+	"dbm-services/common/go-pubpkg/apm/metric"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// HaGauge is a Metric that represents a single numerical value that can
+// arbitrarily go up and down.
+//
+// A HaGauge is typically used for measured values like temperatures or current
+// memory usage, but also "counts" that can go up and down, like the number of
+// running goroutines.
+//
+// To create HaGauge instances, use NewHaGauge.
+type HaGauge struct {
+	metric      *metric.Metric
+	labelNames  []string
+	labelValues map[string]string
+}
+
+func (m *HaGauge) ToMetric() *metric.Metric {
+	return (*metric.Metric)(m.metric)
+}
+
+func (m *HaGauge) UpdateLabel(lvs map[string]string) *HaGauge {
+	if len(m.labelNames) == 0 {
+		panic("Update the gauge value with the new label values.")
+	}
+
+	for key, val := range lvs {
+		m.labelValues[key] = val
+	}
+
+	return m
+}
+
+func (m *HaGauge) Set(val float64) {
+	if len(m.labelNames) == 0 {
+		m.metric.Collector.(prometheus.Gauge).Set(val)
+		return
+	}
+
+	// Must keep the label value sequence the same as the label names.
+	values := m.getLabelValues()
+
+	if len(values) == len(m.labelNames) && len(m.labelValues) == len(m.labelNames) {
+		m.metric.Collector.(*prometheus.GaugeVec).WithLabelValues(values...).Set(val)
+		return
+	}
+
+	// Reset the label values.
+	m.resetLabelValues()
+}
+
+func (m *HaGauge) Inc() {
+	if len(m.labelNames) == 0 {
+		m.metric.Collector.(prometheus.Gauge).Inc()
+	}
+
+	// Must keep the label value sequence the same as the label names.
+	values := m.getLabelValues()
+
+	if len(values) == len(m.labelNames) && len(m.labelValues) == len(m.labelNames) {
+		m.metric.Collector.(*prometheus.GaugeVec).WithLabelValues(values...).Inc()
+		return
+	}
+
+	// Reset the label values.
+	m.resetLabelValues()
+}
+
+func (m *HaGauge) Dec() {
+	if len(m.labelNames) == 0 {
+		m.metric.Collector.(prometheus.Gauge).Dec()
+		return
+	}
+
+	// Must keep the label value sequence the same as the label names.
+	values := m.getLabelValues()
+
+	if len(values) == len(m.labelNames) && len(m.labelValues) == len(m.labelNames) {
+		m.metric.Collector.(*prometheus.GaugeVec).WithLabelValues(values...).Dec()
+		return
+	}
+
+	// Reset the label values.
+	m.resetLabelValues()
+}
+
+func (m *HaGauge) Add(val float64) {
+	if len(m.labelNames) == 0 {
+		m.metric.Collector.(prometheus.Gauge).Add(val)
+		return
+	}
+
+	// Must keep the label value sequence the same as the label names.
+	values := m.getLabelValues()
+
+	if len(values) == len(m.labelNames) && len(m.labelValues) == len(m.labelNames) {
+		m.metric.Collector.(*prometheus.GaugeVec).WithLabelValues(values...).Add(val)
+		return
+	}
+
+	// Reset the label values.
+	m.resetLabelValues()
+}
+
+func (m *HaGauge) Sub(val float64) {
+	if len(m.labelNames) == 0 {
+		m.metric.Collector.(prometheus.Gauge).Sub(val)
+		return
+	}
+
+	// Must keep the label value sequence the same as the label names.
+	values := m.getLabelValues()
+
+	if len(values) == len(m.labelNames) && len(m.labelValues) == len(m.labelNames) {
+		m.metric.Collector.(*prometheus.GaugeVec).WithLabelValues(values...).Sub(val)
+		return
+	}
+
+	// Reset the label values.
+	m.resetLabelValues()
+}
+
+func (m *HaGauge) getLabelValues() []string {
+	values := []string{}
+
+	for _, name := range m.labelNames {
+		if val, ok := m.labelValues[name]; ok {
+			values = append(values, val)
+		}
+	}
+
+	return values
+}
+
+func (m *HaGauge) resetLabelValues() {
+	m.labelValues = map[string]string{}
+	for _, name := range m.labelNames {
+		m.labelValues[name] = "" // set default label value
+	}
+}
+
+func NewHaGauge(name, help string, labelNames ...string) *HaGauge {
+	gauge := &HaGauge{}
+
+	gauge.metric = &metric.Metric{
+		ID:          name,
+		Name:        name,
+		Description: help,
+	}
+
+	if len(labelNames) == 0 {
+		gauge.metric.Type = MetricTypeGauge.String()
+		return gauge
+	}
+
+	gauge.metric.Type = MetricTypeGaugeVec.String()
+	gauge.labelNames = append(gauge.labelNames, labelNames...)
+	gauge.metric.Labels = gauge.labelNames
+
+	gauge.resetLabelValues()
+
+	return gauge
+}
