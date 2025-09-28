1474a5330188b1131e4cbe8df2417611ed202d17
huangwentai-wendel <huangwentai96@163.com>
2025-09-28 15:23:38 +0800

    fix(backend): mysql proxy替换合并同集群proxy #13332

--- dbm-ui/backend/ticket/builders/mysql/mysql_proxy_switch.py
+++ dbm-ui/backend/ticket/builders/mysql/mysql_proxy_switch.py
@@ -13,7 +13,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from backend.db_meta.enums import MachineType
-from backend.db_services.dbbase.constants import IpSource, SourceType
+from backend.db_services.dbbase.constants import IpSource
 from backend.flow.engine.controller.mysql import MySQLController
 from backend.ticket import builders
 from backend.ticket.builders.common.base import (
@@ -34,22 +34,18 @@ from backend.ticket.constants import FlowRetryType, TicketType
 class MysqlProxySwitchDetailSerializer(MySQLBaseOperateDetailSerializer):
     class SwitchInfoSerializer(DisplayInfoSerializer):
         class OldProxySerializer(serializers.Serializer):
-            origin_proxy = serializers.ListSerializer(child=InstanceInfoSerializer())
+            proxy = serializers.ListSerializer(child=InstanceInfoSerializer())
 
         cluster_ids = serializers.ListField(help_text=_("集群ID列表"), child=serializers.IntegerField())
         old_nodes = OldProxySerializer(help_text=_("旧Proxy实例信息"))
-        target_proxy_pkg_id = serializers.IntegerField(help_text=_("新机器部署的介质包ID，在FLow计算赋值"), required=False, default=0)
         resource_spec = serializers.JSONField(help_text=_("资源规格"))
         related_instances = serializers.ListSerializer(
             help_text=_("关联的实例"), child=serializers.JSONField(), required=False
         )
-        origin_proxy_ip = serializers.JSONField(help_text=_("原始proxy信息"))
+        origin_proxy_ip = serializers.JSONField(help_text=_("原始proxy信息"), required=False)
 
     ip_source = serializers.ChoiceField(
         help_text=_("机器来源"), choices=IpSource.get_choices(), required=False, default=IpSource.RESOURCE_POOL
-    )
-    source_type = serializers.ChoiceField(
-        help_text=_("资源来源类型"), choices=SourceType.get_choices(), required=False, default=SourceType.RESOURCE_AUTO
     )
     ip_recycle = HostRecycleSerializer(help_text=_("主机回收信息"), default=HostRecycleSerializer.DEFAULT)
     is_safe = serializers.BooleanField(help_text=_("是否安全模式"), required=False, default=True)
@@ -62,34 +58,12 @@ class MysqlProxySwitchParamBuilder(builders.FlowParamBuilder):
     controller = MySQLController.mysql_proxy_switch_scene
     validator = MySQLController.mysql_proxy_switch_scene.validator
 
-    @classmethod
-    def merge_same_proxy_clusters(cls, infos):
-        """聚合替换相同的proxy的集群"""
-        switch_proxy_cluster_map = {}
-        for info in infos:
-            switch_key = f"{info['origin_proxy_ip']['bk_host_id']}--{info['target_proxy_ip']['bk_host_id']}"
-            if switch_key not in switch_proxy_cluster_map:
-                switch_proxy_cluster_map[switch_key] = {**info, "cluster_ids": []}
-            switch_proxy_cluster_map[switch_key]["cluster_ids"].extend(info["cluster_ids"])
-        return list(switch_proxy_cluster_map.values())
-
 
 class MysqlProxySwitchResourceParamBuilder(BaseOperateResourceParamBuilder):
     def format(self):
         self.patch_info_common_affinity(
-            role="target_proxy", remain_machine_type=MachineType.PROXY, replace_key="origin_proxy", tolerance=0.5
+            role="target_proxys", remain_machine_type=MachineType.PROXY, replace_key="proxy", tolerance=0.5
         )
-
-    def post_callback(self):
-        next_flow = self.ticket.next_flow()
-        ticket_data = next_flow.details["ticket_data"]
-        for info in ticket_data["infos"]:
-            info["target_proxy"] = info.pop("target_proxy")[0]
-            info["target_proxy_ip"] = info["target_proxy"]
-        # 聚合集群
-        infos = MysqlProxySwitchParamBuilder.merge_same_proxy_clusters(ticket_data["infos"])
-        next_flow.details["ticket_data"]["infos"] = infos
-        next_flow.save(update_fields=["details"])
 
 
 @builders.BuilderFactory.register(TicketType.MYSQL_PROXY_SWITCH, is_apply=True, is_recycle=True)
