b79f5242a34adc26d4ab02e10d741b3315b95b7e
zfrendo <842557664@qq.com>
2025-09-28 10:58:33 +0800

    fix: 临时指定回档指定binlog数量 #13317

--- dbm-ui/backend/flow/utils/mysql/mysql_act_playload.py
+++ dbm-ui/backend/flow/utils/mysql/mysql_act_playload.py
@@ -1745,7 +1745,7 @@ class MysqlActPayload(PayloadHandler, ProxyActPayload, TBinlogDumperActPayload):
         if binlog_start_file not in binlog_files_list:
             logger.error("start binlog {} not exist".format(binlog_start_file))
             raise TendbGetBackupInfoFailedException(message=_("start binlog  {} not exist".format(binlog_start_file)))
-        binlog_files_list_simple = [self.cluster["binlog_start_file"], binlog_files[0]]
+        binlog_files_list_simple = [self.cluster["binlog_start_file"], binlog_files_list[0]]
         payload = {
             "db_type": DBActuatorTypeEnum.MySQL.value,
             "action": DBActuatorActionEnum.RecoverBinlog.value,
