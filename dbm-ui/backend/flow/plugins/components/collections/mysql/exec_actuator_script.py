a28a8a2a2efc8c018ea20ebf40ddf5586e95672f
yksitu <1297650644@qq.com>
2025-09-28 18:58:00 +0800

    fix(mysql): proxy流程的V2版本调整 #13334

--- dbm-ui/backend/flow/plugins/components/collections/mysql/exec_actuator_script.py
+++ dbm-ui/backend/flow/plugins/components/collections/mysql/exec_actuator_script.py
@@ -104,7 +104,7 @@ class ExecuteDBActuatorScriptService(BkJobService):
             trans_data = asdict(trans_data)
 
         db_act_template = getattr(mysql_act_payload, kwargs["get_mysql_payload_func"])(
-            ip=exec_ips[0], trans_data=trans_data, custom_params=kwargs.get("custom_params", {})
+            ip=exec_ips[0], trans_data=trans_data, **kwargs.get("component_kwargs", {})
         )
         db_act_template["root_id"] = root_id
         db_act_template["node_id"] = node_id
