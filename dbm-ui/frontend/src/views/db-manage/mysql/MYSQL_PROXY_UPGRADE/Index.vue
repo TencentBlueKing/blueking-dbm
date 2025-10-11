9589fbe75beb2821804910c35b0e4235b8e0f5f9
chenwenchang <479999519@qq.com>
2025-10-11 15:40:48 +0800

    fix(frontend): mysql单据force参数纠正 #13482 # Reviewed, transaction id: 60151

--- dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_PROXY_UPGRADE/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_PROXY_UPGRADE/Index.vue
@@ -48,8 +48,8 @@
       <BkFormItem>
         <BkCheckbox
           v-model="formData.force"
-          :false-label="false"
-          true-label>
+          false-label
+          :true-label="false">
           <span
             v-bk-tooltips="t('存在业务连接时需要人工确认')"
             class="safe-action-text">
@@ -134,7 +134,7 @@
   });
 
   const defaultData = () => ({
-    force: true,
+    force: false,
     payload: createTickePayload(),
     tableData: [createTableRow()],
   });
