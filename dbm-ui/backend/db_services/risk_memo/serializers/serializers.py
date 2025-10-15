ac252f4fe23ba966b6ad3dc0490986cc33e884e3
ycggyao <yao18274936495@163.com>
2025-10-15 12:41:11 +0800

    feat(backend): 风险备忘录开发【saas部分】 #13516 # Reviewed, transaction id: 60591

---
+++ dbm-ui/backend/db_services/risk_memo/serializers/serializers.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from django.utils.translation import ugettext as _
+from rest_framework import serializers
+
+from backend.db_services.risk_memo.constants import RiskOpType, Status
+from backend.db_services.risk_memo.models.risk_memo import RiskMemo, RiskMemoFollowUp, RiskOperateRecord
+
+
+class RiskMemoSerializer(serializers.ModelSerializer):
+    followup_update_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = RiskMemo
+        exclude = ["creator", "updater"]
+        extra_kwargs = {
+            "followup_update_at": {"read_only": True},
+        }
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["biz_inpact"] = representation["biz_inpact"].split(",")
+        representation["inpact_cluster"] = representation["inpact_cluster"].split(",")
+        return representation
+
+
+class RiskMemoDtailSerializer(serializers.ModelSerializer):
+    follow_ups = serializers.SerializerMethodField()
+
+    class Meta:
+        model = RiskMemo
+        fields = "__all__"
+
+    def get_follow_ups(self, obj):
+        follow_ups = obj.riskmemofollowup_set.all().order_by("-create_at")
+
+        return RiskMemoFollowUpSerializer(follow_ups, many=True, context=self.context).data
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["biz_inpact"] = representation["biz_inpact"].split(",")
+        representation["inpact_cluster"] = representation["inpact_cluster"].split(",")
+        representation["followup_update_at"] = (
+            representation["follow_ups"][0]["update_at"] if representation.get("follow_ups") else None
+        )
+        return representation
+
+
+class RiskMemoFollowUpSerializer(serializers.ModelSerializer):
+    is_follow_up_owner = serializers.SerializerMethodField(help_text=_("是否跟进创建者"))
+
+    class Meta:
+        model = RiskMemoFollowUp
+        fields = "__all__"
+        read_only_fields = model.AUDITED_FIELDS + ("is_follow_up_owner",)
+
+    def get_is_follow_up_owner(self, obj):
+        """根据跟进类型调用相应管理器方法返回是否创建者。"""
+        request = self.context["request"]
+        return RiskMemoFollowUp.objects.get_is_follow_up_owner(request, obj)
+
+
+class UpdateRiskMemoFollowUpSerializer(serializers.ModelSerializer):
+    """修改跟进序列化器"""
+
+    class Meta:
+        model = RiskMemoFollowUp
+        fields = "__all__"
+        read_only_fields = model.AUDITED_FIELDS
+
+    def validate(self, attrs):
+        # 从上下文中获取request对象
+        request = self.context.get("request")
+
+        if attrs["risk"].status != Status.DOING.value:
+            raise serializers.ValidationError(_("状态非[进行中]时，不支持修改跟进。"))
+        if not request.user.is_superuser or self.instance.creator != request.user.username:
+            raise serializers.ValidationError(_("暂不支持修改跟进。"))
+        return attrs
+
+
+class DeleteFollowUpSerializer(serializers.ModelSerializer):
+    """删除跟进序列化器"""
+
+    class Meta:
+        model = RiskMemoFollowUp
+        fields = ("risk",)
+
+    def validate(self, attrs):
+        # 从上下文中获取request对象
+        request = self.context.get("request")
+        if not request.user.is_superuser or self.instance.creator != request.user.username:
+            raise serializers.ValidationError(_("暂不支持删除跟进。"))
+        return attrs
+
+
+class UpdateRiskStatusSerializer(serializers.ModelSerializer):
+    """更新风险状态序列化器"""
+
+    final_content = serializers.CharField(required=False, help_text=_("结项内容"))
+
+    class Meta:
+        model = RiskMemo
+        fields = ["status", "final_content"]
+
+
+class RiskOpSerializer(serializers.ModelSerializer):
+    oper_type_value = serializers.SerializerMethodField(help_text=_("操作类型"))
+
+    class Meta:
+        model = RiskOperateRecord
+        fields = "__all__"
+
+    def get_oper_type_value(self, obj):
+        return RiskOpType.get_choice_label(obj.oper_type)
---
+++ dbm-ui/backend/db_services/risk_memo/viewsets/risk_memo.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from bkstorages.backends.bkrepo import BKRepoStorage
+from django.db.models import OuterRef, Subquery
+from django.db.models.functions import Coalesce
+from django.http import JsonResponse
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from backend.bk_web import viewsets
+from backend.bk_web.pagination import AuditedLimitOffsetPagination
+from backend.bk_web.swagger import PaginatedResponseSwaggerAutoSchema, common_swagger_auto_schema
+from backend.db_services.risk_memo.constants import (
+    BKREPO_RISK_MEMO_PATH,
+    RISK_REQUIRE_MAP,
+    BizImpact,
+    RiskOpType,
+    Status,
+)
+from backend.db_services.risk_memo.filters import RiskMemoListFilter, RiskOpRecordListFilter
+from backend.db_services.risk_memo.handler import log_operation
+from backend.db_services.risk_memo.models.risk_memo import RiskMemo, RiskMemoFollowUp, RiskOperateRecord
+from backend.db_services.risk_memo.serializers import (
+    RiskMemoDtailSerializer,
+    RiskMemoSerializer,
+    RiskOpSerializer,
+    UpdateRiskStatusSerializer,
+    UploadImageSerializer,
+)
+from backend.iam_app.dataclass import ActionEnum, ResourceEnum
+from backend.iam_app.handlers.drf_perm.risk_memo import ListRiskMemoPermission, RiskMemoPermission
+from backend.iam_app.handlers.permission import Permission
+from backend.utils.string import make_unique_key
+
+RISK_MEMO_VIEW_TAGS = ["risk_memo"]
+
+
+class RiskMemoViewSet(viewsets.AuditedModelViewSet):
+    """
+    风险备忘录跟进视图
+    """
+
+    action_permission_map = {
+        ("list",): [ListRiskMemoPermission()],
+        ("update", "create", "update_risk_status", "retrieve"): [RiskMemoPermission()],
+        ("get_risk_operate_records", "get_biz_inpact_list", "images"): [],
+    }
+
+    queryset = RiskMemo.objects.all().order_by("-update_at")
+    pagination_class = AuditedLimitOffsetPagination
+    serializer_class = RiskMemoSerializer
+    filter_class = RiskMemoListFilter
+
+    def get_queryset(self):
+        # queryset获取关联跟进的最新update_at
+        followup_update_at_subquery = (
+            RiskMemoFollowUp.objects.filter(risk=OuterRef("pk")).order_by("-update_at").values("update_at")[:1]
+        )
+
+        queryset = (
+            RiskMemo.objects.all()
+            .annotate(followup_update_at=Coalesce(Subquery(followup_update_at_subquery), None))
+            .order_by("-followup_update_at", "-update_at")
+        )
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return RiskMemoDtailSerializer
+        elif self.action == "update_risk_status":
+            return UpdateRiskStatusSerializer
+        return RiskMemoSerializer
+
+    @common_swagger_auto_schema(
+        operation_summary=_("风险列表"),
+        auto_schema=PaginatedResponseSwaggerAutoSchema,
+        tags=RISK_MEMO_VIEW_TAGS,
+    )
+    @Permission.decorator_external_permission_field(
+        param_field=lambda d: d["bk_biz_id"],
+        actions=[ActionEnum.RISK_MEMO_CREATE, ActionEnum.RISK_MEMO_MANAGE],
+        resource_meta=ResourceEnum.BUSINESS,
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("新建风险"),
+        responses={status.HTTP_200_OK: RiskMemoSerializer(label=_("新建风险"))},
+        tags=RISK_MEMO_VIEW_TAGS,
+    )
+    @log_operation(oper_type=RiskOpType.CREATE_RISK.value)
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("风险详情"),
+        responses={status.HTTP_200_OK: RiskMemoSerializer(label=_("风险详情"))},
+        tags=RISK_MEMO_VIEW_TAGS,
+    )
+    def retrieve(self, request, *args, **kwargs):
+        """风险详情"""
+        return super().retrieve(request, *args, **kwargs)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("更新风险"),
+        responses={status.HTTP_200_OK: RiskMemoSerializer(label=_("更新风险"))},
+        tags=RISK_MEMO_VIEW_TAGS,
+    )
+    @log_operation(oper_type=RiskOpType.UPDATE_RISK.value)
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("更新风险状态"),
+        request_body=UpdateRiskStatusSerializer(),
+        tags=RISK_MEMO_VIEW_TAGS,
+    )
+    @action(detail=True, methods=["POST"], serializer_class=UpdateRiskStatusSerializer)
+    def update_risk_status(self, request, *args, **kwargs):
+        """更新风险状态"""
+
+        def get_operation_type(status, is_special):
+            # 状态与操作类型映射
+            status_op_map = {
+                Status.DONE.value: RiskOpType.FINAL.value,
+                Status.DOING.value: RiskOpType.RESTART_RISK.value,
+            }
+
+            oper_type = status_op_map.get(status)
+
+            # 如果是特殊业务要求，返回特殊的操作类型
+            if is_special:
+                return RISK_REQUIRE_MAP.get(oper_type, oper_type)
+
+            return oper_type
+
+        validated_data = self.params_validate(self.get_serializer_class())
+        risk = self.get_object()
+
+        # 更新对象
+        try:
+            RiskMemo.objects.handler_risk_status(request=request, validated_data=validated_data, risk=risk)
+        except Exception as e:
+            return JsonResponse({"msg": "{}".format(e), "code": 1, "data": ""})
+
+        oper_type = get_operation_type(validated_data["status"], risk.is_special)
+
+        # 创建risk操作记录
+        RiskOperateRecord.objects.create(creator=request.user.username, oper_type=oper_type, risk=risk)
+
+        serializer = RiskMemoSerializer(instance=risk)
+        return Response(serializer.data)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("获取风险记录日志"),
+        tags=[RISK_MEMO_VIEW_TAGS],
+    )
+    @action(
+        methods=["GET"],
+        detail=False,
+        serializer_class=RiskOpSerializer,
+        queryset=RiskOperateRecord.objects.select_related("risk").order_by("-create_at"),
+        filter_class=RiskOpRecordListFilter,
+    )
+    def get_risk_operate_records(self, request, *args, **kwargs):
+        op_records_page_qs = self.paginate_queryset(super().filter_queryset(self.queryset))
+        op_records_page_data = self.serializer_class(op_records_page_qs, many=True).data
+        return self.get_paginated_response(data=op_records_page_data)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("获取业务影响类型"),
+        tags=[RISK_MEMO_VIEW_TAGS],
+    )
+    @action(methods=["GET"], detail=False, serializer_class=None, filter_class=None)
+    def get_biz_inpact_list(self, request, *args, **kwargs):
+        data = [{"value": impact.value, "label": impact.get_choice_label(impact.value)} for impact in BizImpact]
+        return Response(data)
+
+    @common_swagger_auto_schema(
+        operation_summary=_("上传图片到制品库"),
+        tags=[RISK_MEMO_VIEW_TAGS],
+        request_body=UploadImageSerializer(),
+    )
+    @action(detail=False, methods=["POST"], serializer_class=UploadImageSerializer)
+    def images(self, request):
+        """上传图片"""
+        file_obj = request.data.get("file", None)
+        bk_biz_id = request.data.get("bk_biz_id", 0)
+        try:
+            # 生成文件名
+            file_name = make_unique_key(file_obj)
+            storage = BKRepoStorage()
+            bkrepo_path = BKREPO_RISK_MEMO_PATH.format(biz=bk_biz_id, file=file_name)
+            storage.save(name=bkrepo_path, content=file_obj)
+
+            url = storage.url(bkrepo_path)
+
+        except Exception as e:
+            return JsonResponse({"msg": "{}".format(e), "code": 1, "data": ""})
+
+        return Response({"url": url})
