620124faf981e04521b52d0fcc688820b4d776d1
seanlook <seanlook7@gmail.com>
2025-10-21 19:45:26 +0800

    fix(mysql): mysql 备份巡检问题修复 #13721

--- dbm-ui/backend/db_periodic_task/local_tasks/mysql_backup/backup_failed_alert.py
+++ dbm-ui/backend/db_periodic_task/local_tasks/mysql_backup/backup_failed_alert.py
@@ -8,16 +8,27 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+import logging
 import time
+from collections import defaultdict
 
 import pytz
 from django.utils import timezone
+from django.utils.translation import gettext as _
 
 from backend.components.bkmonitorv3.client import BKMonitorV3EventApi
+from backend.configuration.constants import SystemSettingsEnum
+from backend.configuration.models import SystemSettings
+from backend.core.notify.constants import MsgType
+from backend.core.notify.handlers import BkChatHandler
 from backend.db_monitor.constants import MonitorEventType
 from backend.db_monitor.dataclass import BaseEventBody, MonitorEvent
+from backend.db_monitor.exceptions import DutyNoticeScheduleException
 from backend.db_report.enums import MysqlBackupCheckSubType, ReportStateType
 from backend.db_report.models import MysqlBackupCheckReport, MysqlBackupProgress
+from backend.exceptions import ApiResultError
+
+logger = logging.getLogger("celery")
 
 
 def mysql_backup_failed_alert():
@@ -32,6 +43,7 @@ def mysql_backup_failed_alert():
         status="Failed", event_create_timestamp__gte=today_time_zero_ms
     ).order_by("bk_biz_id", "cluster_domain")
 
+    failed_cluster = defaultdict(set)
     for backup in failed_backups:
         dim = {
             "appid": backup.bk_biz_id,
@@ -41,6 +53,8 @@ def mysql_backup_failed_alert():
             "instance_role": backup.mysql_role,
             "is_full_backup": backup.is_full_backup,
         }
+        k = "app={}, cluster={}".format(dim["appid"], dim["cluster_domain"])
+        failed_cluster[k].add(backup.backup_host)
         content = "{}\nevent_create_time_utc{}".format(
             backup.status_detail, int(backup.event_create_timestamp / 1000 / 1000)
         )
@@ -53,6 +67,37 @@ def mysql_backup_failed_alert():
         )
         time.sleep(0.01)  # 避免请求过快
         BKMonitorV3EventApi.send_event([event])
+
+    db_type = "mysql"
+    msg_type = MsgType.WECOM_ROBOT
+    notice_cfg = SystemSettings.get_setting_value(SystemSettingsEnum.BKM_DUTY_NOTICE.value, default={}).get(db_type)
+    if not notice_cfg:
+        raise DutyNoticeScheduleException(_("巡检通知配置[{}]不存在").format(db_type))
+    title = _("MySQL 备份失败")
+    msg_content = ""
+    batch_size = 30
+    cnt = 0
+    for k, v in failed_cluster.items():
+        msg_content += "{}: {}\n".format(k, v)
+        cnt += 1
+        if cnt >= batch_size:
+            if notice_cfg["enabled"]:
+                receivers = [notice_cfg["channels"][msg_type]]
+                msg_content += _("\n<@所有人>")
+                try:
+                    BkChatHandler(title, msg_content, receivers).send_custom_msg()
+                except (ApiResultError, Exception) as e:
+                    logger.error("[%s]send_inspect_result error: %s", msg_type, e)
+            msg_content = ""
+            cnt = 0
+    if cnt > 0:
+        if notice_cfg["enabled"]:
+            receivers = [notice_cfg["channels"][msg_type]]
+            msg_content += _("\n<@所有人>")
+            try:
+                BkChatHandler(title, msg_content, receivers).send_custom_msg()
+            except (ApiResultError, Exception) as e:
+                logger.error("[%s]send_inspect_result error: %s", msg_type, e)
 
     # if failed_days >=2: call dba
     backup_inspect_failed = MysqlBackupCheckReport.objects.filter(
