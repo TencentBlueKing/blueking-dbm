1b967054f7903d53920657e47e182ebe52732c8c
iSecloud <869820505@qq.com>
2025-10-22 12:40:44 +0800

    fix(backend): 修复redis整机替换亲和性 #13738 # Reviewed, transaction id: 61465

--- dbm-ui/backend/ticket/builders/redis/redis_toolbox_add_slave.py
+++ dbm-ui/backend/ticket/builders/redis/redis_toolbox_add_slave.py
@@ -110,13 +110,15 @@ class RedisAddSlaveFlowBuilder(BaseRedisTicketFlowBuilder):
                     "sub_zone_ids": [],
                 }
                 pair["redis_slave"].update(affinity=cluster.disaster_tolerance_level)
+                # 同园区，则slave与master在相同的subzone，跨园区，则排除master的subzone
                 if cluster.disaster_tolerance_level == AffinityEnum.CROS_SUBZONE:
-                    cluster_zone_list = cluster.zone_list
-                    sub_zone_ids = [
-                        subzone_id for subzone_id in cluster_zone_list if subzone_id != master_machine.bk_sub_zone_id
-                    ]
+                    master_subzone = master_machine.bk_sub_zone_id
+                    if not cluster.zone_list:
+                        is_include, sub_zone_ids = False, [master_subzone]
+                    else:
+                        is_include, sub_zone_ids = True, list(set(cluster.zone_list) - {master_subzone})
                     pair["redis_slave"]["location_spec"].update(
-                        sub_zone_ids=sub_zone_ids, include_or_exclue=bool(sub_zone_ids)
+                        sub_zone_ids=sub_zone_ids, include_or_exclue=is_include
                     )
                 elif cluster.disaster_tolerance_level in [
                     AffinityEnum.SAME_SUBZONE,
