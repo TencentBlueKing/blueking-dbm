3353cdfb0925350182f3e72204f8f4f4d97b692d
3octaves <873551943@qq.com>
2025-10-30 10:54:40 +0800

    feat(frontent): 单据详情表格优化 #13864

--- dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/task-info/com-factory/resource-pool-strategy/RecycleOldHost.vue
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/task-info/com-factory/resource-pool-strategy/RecycleOldHost.vue
@@ -25,76 +25,69 @@
       </BkButton>
     </InfoItem>
     <InfoItem :label="t('已下架主机')">
-      <PrimaryTable
+      <InfoTable
         :data="ticketDetails.details.recycle_hosts"
         row-key="bk_host_id">
-        <TableColumn
+        <InfoTableColumn
           col-key="ip"
           fixed="left"
+          :get-copy-value="(item: RowData) => item.ip"
           :min-width="150"
           title="IP">
-          <template #title>
-            <div class="ip-header">
-              IP
-              <DbIcon
-                type="copy"
-                @click="copyAllIp" />
-            </div>
-          </template>
-        </TableColumn>
-        <TableColumn
+        </InfoTableColumn>
+        <InfoTableColumn
           col-key="bk_cloud_name"
           :min-width="120"
           :title="t('管控区域')" />
-        <TableColumn
+        <InfoTableColumn
           col-key="status"
           :min-width="120"
           :title="t('Agent 状态')">
           <template #default="{ row }: { row: RowData }">
             <HostAgentStatus :data="row.status" />
           </template>
-        </TableColumn>
-        <TableColumn
+        </InfoTableColumn>
+        <InfoTableColumn
           col-key="city"
           :min-width="120"
           :title="t('地域')">
           <template #default="{ row }: { row: RowData }">
             {{ row.city || '--' }}
           </template>
-        </TableColumn>
-        <TableColumn
+        </InfoTableColumn>
+        <InfoTableColumn
           col-key="sub_zone"
           :min-width="120"
           :title="t('园区')">
           <template #default="{ row }: { row: RowData }">
             {{ row.sub_zone || '--' }}
           </template>
-        </TableColumn>
-        <TableColumn
+        </InfoTableColumn>
+        <InfoTableColumn
           col-key="rack_id"
           :min-width="120"
           :title="t('机架')">
           <template #default="{ row }: { row: RowData }">
             {{ row.rack_id || '--' }}
           </template>
-        </TableColumn>
-        <TableColumn
+        </InfoTableColumn>
+        <InfoTableColumn
           col-key="bk_os_name"
           :min-width="120"
           :title="t('操作系统')">
           <template #default="{ row }: { row: RowData }">
             {{ row.bk_os_name || '--' }}
           </template>
-        </TableColumn>
-        <TableColumn
+        </InfoTableColumn>
+        <InfoTableColumn
           col-key="device_class"
           :min-width="120"
           :title="t('机型')">
           <template #default="{ row }: { row: RowData }">
             {{ row.device_class || '--' }}
           </template>
-        </TableColumn>
-      </PrimaryTable>
+        </InfoTableColumn>
+      </InfoTable>
     </InfoItem>
   </InfoList>
 </template>
@@ -108,9 +101,10 @@
 
   import HostAgentStatus from '@components/host-agent-status/Index.vue';
 
-  import { execCopy, getBusinessHref } from '@utils';
+  import { getBusinessHref } from '@utils';
 
   import InfoList, { Item as InfoItem } from '../components/info-list/Index.vue';
+  import InfoTable, { InfoTableColumn } from '../components/info-table/Index.vue';
 
   interface Props {
     ticketDetails: TicketModel<Common.ResourcePoolRecycle>;
@@ -136,13 +130,6 @@
       },
     });
     window.open(getBusinessHref(href, props.ticketDetails.bk_biz_id), '_blank');
-  };
-
-  const copyAllIp = () => {
-    const ips = props.ticketDetails.details.recycle_hosts.map((item) => item.ip);
-    if (ips.length > 0) {
-      execCopy(ips.join('\n'), t('复制成功，共n条', { n: ips.length }));
-    }
   };
 </script>
 <style lang="less" scoped>
