ff51dfd75ec2c8d76b7391d93f2fc36600b73ff4
ycggyao <yao18274936495@163.com>
2025-10-31 15:25:11 +0800

    feat(backend): redis即时内存分析【saas部分】 #13920 # Reviewed, transaction id: 62842

--- dbm-ui/backend/db_services/redis/redis_keystat_report/models/redis_keystat_report.py
+++ dbm-ui/backend/db_services/redis/redis_keystat_report/models/redis_keystat_report.py
@@ -25,7 +25,7 @@ class ReportRecord(AuditedModel):
     定义内存分析统计记录表，存储内存分析统计生成每一条分析报告信息
     """
 
-    record_id = models.BigIntegerField(default=0, help_text=_("主键id"), primary_key=True)
+    record_id = models.AutoField(help_text=_("主键id"), primary_key=True)
     ticket_id = models.PositiveIntegerField(default=0, help_text=_("关联的单据id"))
     root_id = models.CharField(max_length=64, default="", help_text=_("关联root_id"))
     bk_biz_id = models.IntegerField(default=0, help_text=_("关联的业务id"))
@@ -61,7 +61,7 @@ class ReportProgress(models.Model):
     定义内存分析统计进度记录表，存储内存分析统计生成每一条进度信息
     """
 
-    id = models.BigIntegerField(default=0, help_text=_("主键id"), primary_key=True)
+    id = models.AutoField(help_text=_("主键id"), primary_key=True)
     create_at = models.DateTimeField(_("创建时间"), auto_now_add=True)
     record_id = models.BigIntegerField(default=0, help_text=_("关联的记录id"))
     progress = models.CharField(max_length=255, default="", help_text=_("进度, 0-100"))
@@ -88,11 +88,13 @@ class ReportItem(models.Model):
     count = models.IntegerField(default=0, help_text=_("数量"))
     count_with_ttl = models.IntegerField(default=0, help_text=_("带TTL的数量"))
     member_max_count = models.IntegerField(default=0, help_text=_("成员最大数量"))
+    avg_key_length = models.IntegerField(default=0, help_text=_("平均成员数量"))
     avg_ttl = models.IntegerField(default=0, help_text=_("平均TTL"))
     avg_ttl_human = models.CharField(max_length=255, default="", help_text=_("平均TTL人类化描述"))
     min_idletime_human = models.CharField(max_length=255, default="", help_text=_("最小空闲时间人类化描述"))
     so_min_idletime_human = models.CharField(max_length=255, default="", help_text=_("最小空闲时间人类化描述ForSo"))
     min_idletime = models.IntegerField(default=0, help_text=_("最小空闲时间"))
+    avg_key_used_bytes = models.IntegerField(default=0, help_text=_("单key平均占用内存"))
     mem_used_bytes = models.IntegerField(default=0, help_text=_("内存使用量字节数"))
     mem_used_pct = models.FloatField(max_length=255, default=0, help_text=_("内存使用量字节数占比"))
 
@@ -118,6 +120,7 @@ class RankItem(models.Model):
     atime = models.IntegerField(default=0, help_text=_("最后访问时间"))
     member = models.IntegerField(default=0, help_text=_("成员的数量"))
     member_len = models.IntegerField(default=0, help_text=_("成员的平均长度"))
+    key_length = models.IntegerField(default=0, help_text=_("key的长度"))
     value_size = models.IntegerField(default=0, help_text=_("value的长度或者成员value的长度"))
     db = models.IntegerField(default=0, help_text=_("db"))
     memory_size = models.IntegerField(default=0, help_text=_("基础内存占用, 复合类型中是采样计算结果"))
