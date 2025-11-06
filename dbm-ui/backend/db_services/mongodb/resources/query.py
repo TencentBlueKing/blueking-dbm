25e0d705f9a7ff535eb9c7c46a27d3c6d7b21f65
huangwentai-wendel <huangwentai96@163.com>
2025-11-06 18:05:39 +0800

    fix(backend): 兼容mongodb列表报错的问题 #14086

--- dbm-ui/backend/db_services/mongodb/resources/query.py
+++ dbm-ui/backend/db_services/mongodb/resources/query.py
@@ -230,7 +230,8 @@ class MongoDBListRetrieveResource(query.ListRetrieveResource, MongoDBExportQuery
         # 获取shard分片规格
         mongodb_spec = mongodb_insts[0].machine.spec_config
         mount_point__size = {
-            disk["mount_point"]: disk.get("min") or disk.get("size") for disk in mongodb_spec["storage_spec"]
+            disk["mount_point"]: disk["min"] if "min" in disk else disk.get("size")
+            for disk in mongodb_spec["storage_spec"]
         }
         mongodb_spec.update(
             capacity=mount_point__size.get("/data1") or mount_point__size["/data"] / 2,
