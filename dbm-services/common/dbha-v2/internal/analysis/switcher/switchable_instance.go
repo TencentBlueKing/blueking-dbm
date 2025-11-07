17bdd24978d05925097bc653a8c0ea8685952250
hanzaixiao <hanzaixiao@tencent.com>
2025-11-07 12:41:57 +0800

    fix(backend): Fix obvious bugs in MySQL master-slave cluster switch process. issue: #14099

--- dbm-services/common/dbha-v2/internal/analysis/switcher/switchable_instance.go
+++ dbm-services/common/dbha-v2/internal/analysis/switcher/switchable_instance.go
@@ -4,13 +4,13 @@
  * Copyright (c) 2023 腾讯蓝鲸
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of sw software and associated documentation files (the "Software"), to deal
+ * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and sw permission notice shall be included in all
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -112,7 +112,7 @@ func SwitchSingleInstance(ins SwitchableInstance) (success bool, retErr error) {
 	ins.ReportLog(SwitchInfo, "pre-check pass, start to do switch")
 
 	if err := ins.DoSwitch(); err != nil {
-		retErr = gerrors.New(gerrors.Failure, fmt.Sprintf("do switch failed: %s", err.Error()))
+		retErr = gerrors.New(gerrors.Failure, fmt.Sprintf("failed to do switch: %s", err.Error()))
 		logger.Error("%s, instance{%s}", retErr.Error(), ins.GetInstanceInfo())
 		success = false
 		return
