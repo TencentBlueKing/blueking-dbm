839e1c4afeb647f499e2f61375288ea3f2b5ce7b
3octaves <873551943@qq.com>
2025-11-10 17:53:57 +0800

    fix(frontend): oracle添加规格类型 #14152

--- dbm-ui/frontend/src/common/const/dbTypesInfos/oracle.ts
+++ dbm-ui/frontend/src/common/const/dbTypesInfos/oracle.ts
@@ -1,14 +1,19 @@
-// import { t } from '@locales/index';
+import { t } from '@locales/index';
 
 import { DBTypes } from '../dbTypes';
+import { MachineTypes } from '../machineTypes';
 
-// import { MachineTypes } from '../machineTypes';
 import { type DbInfoType } from './index';
 
 export const oracle: DbInfoType = {
   [DBTypes.ORACLE]: {
     id: DBTypes.ORACLE,
-    machineList: [],
+    machineList: [
+      {
+        label: t('后端存储'),
+        value: MachineTypes.ORACLE_BACKEND,
+      },
+    ],
     moduleId: 'oracle',
     name: 'Oracle',
     routeIndexName: 'OracleManage',
