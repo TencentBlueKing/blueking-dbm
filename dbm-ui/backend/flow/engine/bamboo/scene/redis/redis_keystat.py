c4171c01debf4400883ab736bf348f450f68cda4
cycker <cycker@gmail.com>
2025-11-14 18:18:32 +0800

    feat(redis): 内存分析 #12239

--- dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_keystat.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_keystat.py
@@ -28,6 +28,8 @@ from backend.flow.engine.bamboo.scene.common.builder import Builder
 from backend.flow.engine.bamboo.scene.common.get_file_list import GetFileList
 from backend.flow.plugins.components.collections.redis.trans_flies import TransFileComponent
 from backend.flow.utils.redis.redis_context_dataclass import ActKwargs, CommonContext
+from backend.flow.utils.base.payload_handler import PayloadHandler
+
 
 logger = logging.getLogger("flow")
 
@@ -187,8 +189,9 @@ class RedisKeystatFlow(object):
 
     @classmethod
     def make_kwargs(cls, cluster: Cluster, info: dict, exec_ip: str, nginx_ip: str, db_cloud_token: str) -> dict:
-        proxy_pwd = "proxy_pwd"
-        redis_pwd = "redis_pwd"
+        proxy_pwd = "not_used"
+        passwd_ret = PayloadHandler.redis_get_password_by_domain(cluster.immute_domain)
+        redis_pwd = passwd_ret["redis_password"]
         return {
             "set_trans_data_dataclass": CommonContext.__name__,
             "get_trans_data_ip_var": None,
