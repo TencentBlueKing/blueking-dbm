6f8dabd5bcac747951c79df7622a4d15e1818c3e
huangwentai-wendel <huangwentai96@163.com>
2025-11-18 17:36:40 +0800

    fix(backend): mongo亲和性优化调整最终版 #14312

--- dbm-ui/backend/ticket/builders/mongodb/mongo_shard_migrate.py
+++ dbm-ui/backend/ticket/builders/mongodb/mongo_shard_migrate.py
@@ -60,7 +60,9 @@ class MongoDBShardMigrateFlowParamBuilder(builders.FlowParamBuilder):
 class MongoDBShardMigrateResourceParamBuilder(BaseMongoDBOperateResourceParamBuilder):
     def format(self):
         # 资源申请的一些参数补充
-        self.patch_info_common_affinity(role="mongodb", tolerance=get_mongodb_cluster_tolerance)
+        self.patch_info_common_affinity(
+            role="mongodb", tolerance=get_mongodb_cluster_tolerance, tolerance_type="mongodb"
+        )
 
 
 @builders.BuilderFactory.register(TicketType.MONGODB_SHARD_MIGRATE, is_recycle=True)
