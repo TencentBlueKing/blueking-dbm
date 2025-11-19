8ef955fb514f2d60fc7fbea18b48ea643a72657b
chenwenchang <479999519@qq.com>
2025-11-19 10:54:37 +0800

    feat(frontend): mysql单节点变更 #14323

---
+++ dbm-ui/frontend/src/services/model/ticket/details/mysql/resource-pool/migrateSingle.ts
@@ -0,0 +1,35 @@
+import type { BackupSourceType } from '@services/types';
+
+import type { ResourcePoolDetailBase } from '../../common';
+
+/**
+ * MySQL 单节点迁移
+ */
+
+export interface MigrateSingle extends ResourcePoolDetailBase {
+  migrate_type: 'instance' | 'machine' | 'failover';
+  backup_source: BackupSourceType;
+  infos: {
+    cluster_ids: number[];
+    old_orphan: {
+      bk_biz_id: number;
+      bk_cloud_id: number;
+      bk_host_id: number;
+      ip: string;
+    };
+    resource_spec: {
+      bk_new_orphan: {
+        spec_id: number;
+        labels: string[];
+        label_names: string[];
+      };
+    };
+    related_cluster_infos: {
+      cluster_id: number;
+      instance_address: string;
+      master_domain: string;
+    }[];
+  }[];
+  ip_source: 'resource_pool';
+  orphan_restore_type: string;
+}
---
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/task-info/com-factory/resource-pool-strategy/mysql/migrate-single/machine-migrate/Index.vue
@@ -0,0 +1,111 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
+<template>
+  <InfoList>
+    <InfoItem :label="t('变更方式')">
+      {{ t('整机迁移') }}
+    </InfoItem>
+    <InfoItem :label="t('迁移方式')">
+      {{ restoreTypeMap[ticketDetails.details.orphan_restore_type] }}
+    </InfoItem>
+    <InfoList>
+      <InfoItem :label="t('备份源')">
+        {{ ticketDetails.details.backup_source === 'local' ? t('本地备份') : t('远程备份') }}
+      </InfoItem>
+    </InfoList>
+  </InfoList>
+  <TicketInfoTable
+    :data="ticketDetails.details.infos"
+    row-key="cluster_ids">
+    <TicketInfoTableColumn
+      col-key="old_orphan"
+      :get-copy-value="(item: RowData) => item.old_orphan?.ip || ''"
+      :min-width="260"
+      :title="t('目标主机')">
+      <template #default="{ row }: { row: RowData }">
+        {{ row.old_orphan.ip }}
+      </template>
+    </TicketInfoTableColumn>
+    <TicketInfoTableColumn
+      col-key="related_cluster_infos"
+      :get-copy-value="
+        (item: RowData) =>
+          item.cluster_ids.map((clusterId) => ticketDetails.details.clusters?.[clusterId]?.immute_domain || '')
+      "
+      :min-width="260"
+      :title="t('关联集群实例')">
+      <template #default="{ row }: { row: RowData }">
+        <div
+          v-for="item in row.related_cluster_infos"
+          :key="item.instance_address"
+          style="line-height: 20px">
+          <p>
+            {{ item.master_domain }}
+          </p>
+          <p style="color: #979ba5">-- {{ item.instance_address }}</p>
+        </div>
+      </template>
+    </TicketInfoTableColumn>
+    <TicketInfoTableColumn
+      col-key="spec_id"
+      :min-width="120"
+      :title="t('规格')">
+      <template #default="{ row }: { row: RowData }">
+        {{ ticketDetails.details.specs?.[row.resource_spec.bk_new_orphan?.spec_id]?.name || '--' }}
+      </template>
+    </TicketInfoTableColumn>
+    <TicketInfoTableColumn
+      col-key="label_names"
+      :min-width="200"
+      :title="t('资源标签')">
+      <template #default="{ row }: { row: RowData }">
+        <template v-if="row.resource_spec.bk_new_orphan?.label_names?.length">
+          <BkTag
+            v-for="item in row.resource_spec.bk_new_orphan.label_names"
+            :key="item">
+            {{ item }}
+          </BkTag>
+        </template>
+        <BkTag
+          v-else
+          theme="success">
+          {{ t('通用无标签') }}
+        </BkTag>
+      </template>
+    </TicketInfoTableColumn>
+  </TicketInfoTable>
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  import TicketModel, { type Mysql } from '@services/model/ticket/ticket';
+
+  import InfoList, { Item as InfoItem } from '../../../../components/info-list/Index.vue';
+
+  interface Props {
+    ticketDetails: TicketModel<Mysql.ResourcePool.MigrateSingle>;
+  }
+
+  type RowData = Props['ticketDetails']['details']['infos'][number];
+
+  defineProps<Props>();
+
+  const { t } = useI18n();
+
+  const restoreTypeMap: Record<string, string> = {
+    replicate_with_data: t('包含数据+主从同步'),
+    replicate_with_struct: t('仅表结构+主从同步'),
+    restore_from_flow_backup: t('仅表结构'),
+  };
+</script>
