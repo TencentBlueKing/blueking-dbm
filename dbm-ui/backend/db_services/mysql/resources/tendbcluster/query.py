b73d069d708d8be595bac46e94ffc542e662b5a0
huangwentai-wendel <huangwentai96@163.com>
2025-11-20 15:49:15 +0800

    fix(backend): 主机列表枚举接口额外增加字段 #14365

--- dbm-ui/backend/db_services/mysql/resources/tendbcluster/query.py
+++ dbm-ui/backend/db_services/mysql/resources/tendbcluster/query.py
@@ -335,7 +335,8 @@ class ListRetrieveResource(MysqlListRetrieveResource, TenDBClusterCommonQueryRes
         **kwargs,
     ) -> ResourceList:
         filter_params_map = {
-            "spider_role": Q(proxyinstance__tendbclusterspiderext__spider_role=query_params.get("spider_role"))
+            "spider_role": Q(proxyinstance__tendbclusterspiderext__spider_role=query_params.get("spider_role")),
+            "instance_role": Q(proxyinstance__tendbclusterspiderext__spider_role=query_params.get("instance_role")),
         }
         resource_list = super()._list_machines(bk_biz_id, query_params, limit, offset, filter_params_map, **kwargs)
         proxy_ips = [
