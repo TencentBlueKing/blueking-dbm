0a1c08c58d6280cd80be9be7041da8485ef859cc
chenwenchang <479999519@qq.com>
2025-11-21 15:31:24 +0800

    feat(frontend): sqlserver迁移 #14405 # Reviewed, transaction id: 67056

---
+++ dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_HOST_MIGRATE/components/HostColumnGroup.vue
@@ -0,0 +1,251 @@
+<!--
+ * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+ *
+ * Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the License.
+-->
+
+<template>
+  <EditableColumn
+    :append-rules="rules"
+    field="host.ip"
+    fixed="left"
+    :label="t('目标主机')"
+    :loading="loading"
+    :min-width="220"
+    required>
+    <template #headAppend>
+      <span
+        v-bk-tooltips="t('批量选择')"
+        class="batch-host-select"
+        @click="handleShowSelector">
+        <DbIcon type="batch-host-select" />
+      </span>
+    </template>
+    <EditableInput
+      v-model.trim="modelValue.ip"
+      :placeholder="t('请输入IP')"
+      @change="handleInputChange" />
+  </EditableColumn>
+  <EditableColumn
+    field="host.related_instances"
+    :label="t('关联集群实例')"
+    :loading="loading"
+    :min-width="220"
+    readonly>
+    <EditableBlock :placeholder="t('自动生成')">
+      <div
+        v-for="item in modelValue.related_instances"
+        :key="item.instance_address">
+        <p>
+          {{ item.master_domain }}
+        </p>
+        <p style="color: #979ba5">--{{ item.instance_address }}</p>
+      </div>
+    </EditableBlock>
+  </EditableColumn>
+  <InstanceSelector
+    v-model:is-show="showSelector"
+    :cluster-types="[ClusterTypes.SQLSERVER_HA, ClusterTypes.SQLSERVER_SINGLE]"
+    hide-manual-input
+    :selected="selectedInstances"
+    :tab-list-config="tabListConfig"
+    @change="handleSelectorChange" />
+</template>
+<script lang="ts" setup>
+  import { useI18n } from 'vue-i18n';
+  import { useRequest } from 'vue-request';
+
+  import SqlserverHaModel from '@services/model/sqlserver/sqlserver-ha';
+  import { checkInstance } from '@services/source/dbbase';
+
+  import { ClusterTypes, DBTypes } from '@common/const';
+  import { ipv4 } from '@common/regex';
+
+  import InstanceSelector, {
+    type InstanceSelectorValues,
+    type IValue,
+    type PanelListType,
+  } from '@components/instance-selector/Index.vue';
+
+  export type SelectorHost = IValue;
+
+  interface Props {
+    selected: {
+      cluster_type: ClusterTypes;
+      ip: string;
+    }[];
+  }
+
+  type Emits = (e: 'batch-edit', list: IValue[]) => void;
+
+  const props = defineProps<Props>();
+
+  const emits = defineEmits<Emits>();
+
+  const modelValue = defineModel<{
+    bk_cloud_id: number;
+    bk_host_id: number;
+    bk_idc_city_name: string;
+    bk_sub_zone: string;
+    cluster_type: ClusterTypes;
+    ip: string;
+    related_instances: ServiceReturnType<typeof checkInstance>;
+    spec: SqlserverHaModel['masters'][0]['spec_config'];
+  }>({
+    required: true,
+  });
+
+  const { t } = useI18n();
+
+  const tabListConfig = {
+    [ClusterTypes.SQLSERVER_HA]: [
+      {
+        id: ClusterTypes.SQLSERVER_HA,
+        name: t('SqlServer 主从'),
+        previewConfig: {
+          displayKey: 'ip',
+          showTitle: true,
+        },
+        tableConfig: {
+          firsrColumn: {
+            field: 'ip',
+            label: t('目标主机'),
+            role: 'backend_master',
+          },
+        },
+      },
+    ],
+    [ClusterTypes.SQLSERVER_SINGLE]: [
+      {
+        id: ClusterTypes.SQLSERVER_SINGLE,
+        name: t('SqlServer 单节点'),
+        previewConfig: {
+          displayKey: 'ip',
+          showTitle: true,
+        },
+        tableConfig: {
+          firsrColumn: {
+            field: 'ip',
+            label: t('目标主机'),
+            role: 'orphan',
+          },
+        },
+      },
+    ],
+  } as unknown as Record<ClusterTypes, PanelListType>;
+
+  const showSelector = ref(false);
+  const selectedInstances = computed<InstanceSelectorValues<IValue>>(() => ({
+    [ClusterTypes.SQLSERVER_HA]: props.selected
+      .filter((item) => item.cluster_type === ClusterTypes.SQLSERVER_HA)
+      .map(
+        (item) =>
+          ({
+            ip: item.ip,
+          }) as IValue,
+      ),
+    [ClusterTypes.SQLSERVER_SINGLE]: props.selected
+      .filter((item) => item.cluster_type === ClusterTypes.SQLSERVER_SINGLE)
+      .map(
+        (item) =>
+          ({
+            ip: item.ip,
+          }) as IValue,
+      ),
+  }));
+
+  const rules = [
+    {
+      message: t('IP格式有误，请输入合法IP'),
+      trigger: 'change',
+      validator: (value: string) => !value || ipv4.test(value),
+    },
+    {
+      message: t('目标主机重复'),
+      trigger: 'change',
+      validator: (value: string) => !value || props.selected.filter((item) => item.ip === value).length < 2,
+    },
+    {
+      message: t('目标主机不存在'),
+      trigger: 'blur',
+      validator: (value: string) => !value || Boolean(modelValue.value.bk_host_id),
+    },
+  ];
+
+  const { loading, run: queryHost } = useRequest(checkInstance, {
+    manual: true,
+    onSuccess: (data) => {
+      const relatedInstances = data;
+      const [currentHost] = data;
+      if (currentHost) {
+        modelValue.value = {
+          bk_cloud_id: currentHost.bk_cloud_id,
+          bk_host_id: currentHost.bk_host_id,
+          bk_idc_city_name: currentHost.host_info?.bk_idc_city_name || '',
+          bk_sub_zone: currentHost.host_info?.bk_sub_zone || '',
+          cluster_type: currentHost.cluster_type,
+          ip: currentHost.ip,
+          related_instances: relatedInstances,
+          spec: currentHost.spec_config,
+        };
+      }
+    },
+  });
+
+  const handleShowSelector = () => {
+    showSelector.value = true;
+  };
+
+  const handleInputChange = (value: string) => {
+    modelValue.value = Object.assign({} as typeof modelValue.value, {
+      bk_cloud_id: 0,
+      bk_host_id: 0,
+      bk_idc_city_name: '',
+      bk_sub_zone: '',
+      ip: value,
+      related_instances: [],
+      spec: {
+        id: 0,
+      },
+    });
+  };
+
+  const handleSelectorChange = (selected: InstanceSelectorValues<IValue>) => {
+    emits('batch-edit', [
+      ...(selected && selected[ClusterTypes.SQLSERVER_HA] ? selected[ClusterTypes.SQLSERVER_HA] : []),
+      ...(selected && selected[ClusterTypes.SQLSERVER_SINGLE] ? selected[ClusterTypes.SQLSERVER_SINGLE] : []),
+    ]);
+  };
+
+  watch(
+    modelValue,
+    () => {
+      if (modelValue.value.ip && !modelValue.value.bk_host_id) {
+        queryHost({
+          bk_biz_id: window.PROJECT_CONFIG.BIZ_ID,
+          cluster_type: [ClusterTypes.SQLSERVER_HA, ClusterTypes.SQLSERVER_SINGLE],
+          db_type: DBTypes.SQLSERVER,
+          instance_addresses: [modelValue.value.ip],
+          instance_role: ['backend_master', 'orphan'],
+        });
+      }
+    },
+    {
+      immediate: true,
+    },
+  );
+</script>
+<style lang="less" scoped>
+  .batch-host-select {
+    font-size: 14px;
+    color: #3a84ff;
+    cursor: pointer;
+  }
+</style>
