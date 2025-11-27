ae204e7511d6fe400f0d38057b24c9c3b54723c3
cycker <cycker@gmail.com>
2025-11-27 12:28:58 +0800

    feat(redis): 内存分析 #12239

--- dbm-ui/backend/db_services/redis/capacity_evaluate_service/util.py
+++ dbm-ui/backend/db_services/redis/capacity_evaluate_service/util.py
@@ -6,6 +6,28 @@ logger = logging.getLogger("root")
 # if REPO_VERSION_FOR_DEV is set, it means the env is in dev environment
 dev_env = str(env.REPO_VERSION_FOR_DEV)
 
+UNIFY_QUERY_PARAMS = {
+    "bk_biz_id": 0,  # 替换为实际的biz_id
+    "query_configs": [
+        {
+            "data_source_label": "prometheus",
+            "data_type_label": "time_series",
+            "promql": "",
+            "interval": 60,
+            "alias": "a",
+        }
+    ],
+    "expression": "a",
+    "alias": "a",
+    # 单位：s
+    "start_time": 1697100405,
+    "end_time": 1697101305,
+    "slimit": 500,
+    "down_sample_range": "1s",
+    # 取最新的几个周期，可以加速查询（如果指标数据不连续，则查不出数据）
+    "type": "instant",
+}
+
 
 def logger_debug(msg: str):
     """
