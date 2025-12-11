93c1710cbbd164d3d6e17166c4152c3360e9cca0
3octaves <873551943@qq.com>
2025-12-11 17:45:03 +0800

    fix(frontend): 修复redis接入层缩容详情展示问题 #14923

--- dbm-ui/frontend/src/services/model/ticket/details/redis/proxyScaleDown.ts
+++ dbm-ui/frontend/src/services/model/ticket/details/redis/proxyScaleDown.ts
@@ -4,14 +4,16 @@ export interface ProxyScaleDown extends DetailBase {
   clusters: DetailClusters;
   infos: {
     cluster_id: number;
+    old_nodes: {
+      proxy_reduced_hosts: {
+        bk_biz_id: number;
+        bk_cloud_id: number;
+        bk_host_id: number;
+        ip: string;
+      }[];
+    };
     online_switch_type: 'user_confirm' | 'no_confirm';
-    proxy_reduced_hosts?: {
-      bk_biz_id: number;
-      bk_cloud_id: number;
-      bk_host_id: number;
-      ip: string;
-    }[];
-    target_proxy_count?: number;
+    target_proxy_count: number;
   }[];
-  ip_source: 'resource_pool';
+  shrink_type: 'HOST' | 'QUANTITY';
 }
