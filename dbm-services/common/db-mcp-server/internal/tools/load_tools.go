b86a577db28e0f6683715228eb64eb337c0f50a1
xfwduke <xfwduke@gmail.com>
2025-12-15 08:31:14 +0800

    feat(backend): optimize-mcp-server-error-process #14964

--- dbm-services/common/db-mcp-server/internal/tools/load_tools.go
+++ dbm-services/common/db-mcp-server/internal/tools/load_tools.go
@@ -3,32 +3,31 @@ package tools
 import (
 	"dbm-services/common/go-pubpkg/logger"
 	"slices"
-	"time"
 
-	"github.com/avast/retry-go/v4"
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 )
 
 func LoadTools(s *server.MCPServer) (err error) {
 	var tds []*toolsDefinition
-	err = retry.Do(
-		func() error {
-			tds, err = discover()
-			if err != nil {
-				return err
-			}
-			return nil
-		},
-		retry.Attempts(4),
-		retry.Delay(5*time.Second),
-		retry.DelayType(retry.FixedDelay),
-		retry.OnRetry(
-			func(n uint, err error) {
-				logger.Warn("retry load tools %d on %s", n, err.Error())
-			},
-		),
-	)
+	tds, err = discover()
+	//err = retry.Do(
+	//	func() error {
+	//		tds, err = discover()
+	//		if err != nil {
+	//			return err
+	//		}
+	//		return nil
+	//	},
+	//	retry.Attempts(1),
+	//	retry.Delay(5*time.Second),
+	//	retry.DelayType(retry.FixedDelay),
+	//	retry.OnRetry(
+	//		func(n uint, err error) {
+	//			logger.Warn("retry load tools %d on %s", n, err.Error())
+	//		},
+	//	),
+	//)
 
 	if err != nil {
 		return err
@@ -51,7 +50,7 @@ func LoadTools(s *server.MCPServer) (err error) {
 		)
 		handler := toolHandlerFactory(td)
 		s.AddTool(tool, handler)
-		//logger.Info("load tool: %s", td.String())
+		logger.Info("load tool: %s", td.OperationId)
 	}
 	return nil
 }
