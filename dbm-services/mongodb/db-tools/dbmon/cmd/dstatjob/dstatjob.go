5799c9df72fdba74d404c5b15fd819ecd22b8e54
cycker <cycker@gmail.com>
2025-12-23 16:30:42 +0800

    fix(mongodb): bk-dbmon备份时间调整 #14995

--- dbm-services/mongodb/db-tools/dbmon/cmd/dstatjob/dstatjob.go
+++ dbm-services/mongodb/db-tools/dbmon/cmd/dstatjob/dstatjob.go
@@ -6,6 +6,7 @@ import (
 	"dbm-services/mongodb/db-tools/dbmon/cmd/basejob"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
 	"sync"
 	"time"
@@ -43,7 +44,7 @@ func GetJob(conf *config.DbMonConfig, logger *zap.Logger, jobName string, workDi
 
 // Run 执行例行心跳metric上报 会带第一个实例的维度信息
 func (job *Job) Run() {
-	job.LoopTimes++
+
 	job.Logger.Info("start", zap.Uint64("loopTimes", job.LoopTimes))
 
 	if err := job.UpdateConf(); err != nil {
@@ -56,12 +57,26 @@ func (job *Job) Run() {
 		return
 	}
 
-	err := job.runDstat()
+	err := job.runDstatLoop()
 	if err != nil {
-		job.Logger.Warn(fmt.Sprintf("SendHeartBeat return err %s", err.Error()),
+		job.Logger.Warn(fmt.Sprintf("runDstat return err %s", err.Error()),
 			zap.Uint64("loopTimes", job.LoopTimes))
 	} else {
 		job.Logger.Info("done", zap.Uint64("loopTimes", job.LoopTimes))
+	}
+}
+
+// runDstatLoop 执行dstat循环
+// 每次循环执行dstat命令，并等待1秒后继续循环. 这样保证了dstat命令的执行间隔为1秒.
+// 如果dstat命令执行失败，则等待30秒后重试.
+func (job *Job) runDstatLoop() error {
+	for {
+		job.LoopTimes++
+		err := job.runDstat()
+		if err != nil {
+			return err
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -106,6 +121,14 @@ func (job *Job) runDstat() error {
 			job.Logger.Info("deleteOldDstatLogs done")
 		}
 	}
+	// 如果没有dstat 命令,则不执行
+	if _, err := exec.LookPath("dstat"); err != nil {
+		job.Logger.Warn("dstat command not found", zap.Error(err))
+		// sleep 60 seconds
+		time.Sleep(60 * time.Second) // 等待60秒后重试. 如果这里sleep 太短时间，会导致频繁地尝试.
+		return fmt.Errorf("dstat command not found: %w", err)
+	}
+	// 执行dstat命令, 将结果写入到日志文件中
 	ymd := time.Now().Format("20060102")
 	dstatLogFile := path.Join(job.WorkDir, "logs", fmt.Sprintf("dstat.%s", ymd))
 	cmd, err := mycmd.NewMyExec(
@@ -118,9 +141,11 @@ func (job *Job) runDstat() error {
 
 	if err != nil {
 		job.Logger.Warn("runDstat return err", zap.Error(err))
-		return err
+		time.Sleep(30 * time.Second) // 等待60秒后重试. 如果这里sleep 太短时间，会导致频繁地尝试.
+		return fmt.Errorf("runDstat return err: %w", err)
 	}
 	if err := cmd.Run(); err != nil {
+		time.Sleep(30 * time.Second) // 等待60秒后重试. 如果这里sleep 太短时间，会导致频繁地尝试.
 		job.Logger.Warn("runDstat return err", zap.Error(err))
 		return err
 	}
