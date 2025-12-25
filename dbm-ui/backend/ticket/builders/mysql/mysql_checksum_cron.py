3dfb65748bb0b5f5efe88e05fbb3f337973130f2
teng00213 <1047936553@qq.com>
2025-12-25 03:59:42 +0000

    feat(backend): 提交Mysql/tendb 数据校验单据，取消flow中的定时 #15245

---
+++ dbm-ui/backend/ticket/builders/mysql/mysql_checksum_cron.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from backend.ticket import builders
+from backend.ticket.builders.mysql.mysql_checksum import MySQLChecksumDetailSerializer, MySQLChecksumFlowBuilder
+from backend.ticket.constants import TicketType
+
+
+class MySQLChecksumCronDetailSerializer(MySQLChecksumDetailSerializer):
+    skip_timer = serializers.BooleanField(help_text=_("是否是定时"))
+
+
+@builders.BuilderFactory.register(TicketType.MYSQL_CHECKSUM_CRON)
+class MySQLChecksumCronFlowBuilder(MySQLChecksumFlowBuilder):
+    serializer = MySQLChecksumCronDetailSerializer
+
+    @property
+    def need_itsm(self):
+        return False
+
+    @property
+    def need_timer(self):
+        if self.ticket.details["skip_timer"] is True:
+            return False
+        return True
