d10d83956e09d84f34e598ece220542e9e38e759
3octaves <873551943@qq.com>
2025-12-29 15:37:23 +0800

    feat(frontend): 实例选择器优化 #14892

--- dbm-ui/frontend/src/components/instance-selector-new/components/Table.vue
+++ dbm-ui/frontend/src/components/instance-selector-new/components/Table.vue
@@ -22,12 +22,40 @@
       <TableColumn
         col-key="instance_address"
         fixed="left"
-        :min-width="250"
+        :min-width="160"
         :title="t('实例')">
         <template #default="{ row }: { row: IRowData }">
           {{ row.instance_address || '--' }}
         </template>
       </TableColumn>
+      <TableColumn
+        col-key="id"
+        :filter="columnFilter?.['id']"
+        title="ID"
+        :width="80">
+        <template #default="{ row }: { row: IRowData }">
+          {{ row.id }}
+        </template>
+      </TableColumn>
+      <template v-if="isMongodb">
+        <TableColumn
+          col-key="master_domain"
+          :min-width="250"
+          :title="t('域名')">
+          <template #default="{ row }: { row: IRowData }">
+            {{ row.master_domain || '--' }}
+          </template>
+        </TableColumn>
+        <TableColumn
+          col-key="shard"
+          :filter="columnFilter?.['shard']"
+          :title="t('分片名')"
+          :width="150">
+          <template #default="{ row }: { row: IRowData }">
+            {{ row.shard || '--' }}
+          </template>
+        </TableColumn>
+      </template>
       <TableColumn
         col-key="status"
         :filter="columnFilter?.['status']"
@@ -56,10 +84,45 @@
         </template>
       </TableColumn>
       <TableColumn
-        col-key="master_domain"
-        :title="t('所属集群')">
+        :col-key="isMongodb ? 'cluster_name' : 'master_domain'"
+        :min-width="250"
+        :title="t('关联集群')">
         <template #default="{ row }: { row: IRowData }">
-          {{ row.master_domain || '--' }}
+          {{ isMongodb ? row.cluster_name : row.master_domain }}
+        </template>
+      </TableColumn>
+      <TableColumn
+        col-key="ip"
+        :filter="columnFilter?.['ip']"
+        :title="t('主机 IP')"
+        :width="140">
+      </TableColumn>
+      <TableColumn
+        col-key="bk_sub_zone"
+        :filter="columnFilter?.['bk_sub_zone']"
+        :title="t('园区')"
+        :width="140">
+        <template #default="{ row }: { row: IRowData }">
+          {{ row.bk_sub_zone || '--' }}
+        </template>
+      </TableColumn>
+      <TableColumn
+        col-key="bk_os_name"
+        :filter="columnFilter?.['bk_os_name']"
+        :title="t('操作系统')"
+        :width="140">
+        <template #default="{ row }: { row: IRowData }">
+          {{ row.bk_os_name || '--' }}
+        </template>
+      </TableColumn>
+      <TableColumn
+        col-key="create_at"
+        :filter="columnFilter?.['create_at']"
+        sorter
+        :title="t('部署时间')"
+        :width="140">
+        <template #default="{ row }: { row: IRowData }">
+          {{ row.createAtDisplay || '--' }}
         </template>
       </TableColumn>
     </DbTable>
@@ -70,6 +133,8 @@
   import { useI18n } from 'vue-i18n';
 
   import { useInstanceColumnFilter, useInstanceQuickSearch } from '@hooks';
+
+  import { ClusterTypes } from '@common/const';
 
   import ClusterInstanceStatus from '@components/cluster-instance-status/Index.vue';
   import DbTable from '@components/db-table/IndexNew.vue';
@@ -103,6 +168,8 @@
   });
 
   const instanceTableRef = useTemplateRef('instanceTable');
+
+  const isMongodb = [ClusterTypes.MONGO_REPLICA_SET, ClusterTypes.MONGO_SHARED_CLUSTER].includes(props.clusterType);
 
   const realDataSource = (params: any) => (props.dataSource || requestHandler)(params);
 
