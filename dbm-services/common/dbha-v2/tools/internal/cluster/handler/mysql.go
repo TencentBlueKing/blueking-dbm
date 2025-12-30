241f1dfd7fc2a881d51e32535aafb3efb42409fa
hanzaixiao <hanzaixiao@tencent.com>
2025-12-30 11:42:17 +0800

    feat(backend): Fix connection config to avoid extra SQL on connect for proxy nodes. issue: #15359

--- dbm-services/common/dbha-v2/tools/internal/cluster/handler/mysql.go
+++ dbm-services/common/dbha-v2/tools/internal/cluster/handler/mysql.go
@@ -372,6 +372,9 @@ func (hdl *MysqlClusterHandler) switchProxyBackend(proxyIp string, proxyAdminPor
 		hamysql.OptionPort(proxyAdminPort),
 		hamysql.OptionUser(config.ClusterConfig.AuthInfo.ProxyUser),
 		hamysql.OptionPassword(config.ClusterConfig.AuthInfo.ProxyPassword),
+		hamysql.OptionSkipInitializeWithVersion(false),
+		hamysql.OptionDisableDatetimePrecision(true),
+		hamysql.OptionCharset(""),
 	)
 	if err != nil {
 		return gerrors.Newf(gerrors.Failure, "failed to connect to proxy(%s:%d), errmsg: %s",
