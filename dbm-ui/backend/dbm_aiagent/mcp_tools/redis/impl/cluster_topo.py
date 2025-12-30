61af744565a72006c080758a3728e432e781c274
xiepaup <xiepaup@gmail.com>
2025-12-30 19:14:43 +0800

    feat(redis): redis_ai_meta_q #15358

--- dbm-ui/backend/dbm_aiagent/mcp_tools/redis/impl/cluster_topo.py
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/redis/impl/cluster_topo.py
@@ -7,19 +7,119 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from typing import Dict
+from typing import Dict, List
 
-from backend.db_meta.models import Cluster
+from backend.db_meta.enums import AccessLayer, ClusterType, InstanceRole
+from backend.db_meta.models import Cluster, ClusterEntry, Machine, ProxyInstance, StorageInstance, StorageInstanceTuple
 
 
-def redis_cluster_topo(immute_domain: str) -> Dict:
+def redis_list_clusters(bk_biz_id: int) -> List:
+    clusters = Cluster.objects.filter(
+        bk_biz_id=bk_biz_id,
+        cluster_type__in=[
+            ClusterType.TendisPredixyRedisCluster,
+            ClusterType.TendisPredixyTendisplusCluster,
+            ClusterType.TwemproxyTendisSSDInstance,
+            ClusterType.TendisTwemproxyRedisInstance,
+            ClusterType.RedisInstance,
+        ],
+    )
+
+    return [
+        {
+            "cluster_id": c.id,
+            "bk_cloud_id": c.bk_cloud_id,
+            "cluster_type": c.cluster_type,
+            "immute_domain": c.immute_domain,
+            "alias": c.alias,
+        }
+        for c in clusters
+    ]
+
+
+def cluster_overview(immute_domain: str) -> Dict:
     cluster_obj = Cluster.objects.get(immute_domain=immute_domain)
-    storage_instances = cluster_obj.storageinstance_set.all()
-    proxy_instances = cluster_obj.proxyinstance_set.all()
 
     return {
+        "bk_cloud_id": cluster_obj.bk_cloud_id,
         "cluster_type": cluster_obj.cluster_type,
         "cluster_domain": immute_domain,
-        "proxy": [{"address": "{}:{}".format(s.machine.ip, s.port), "status": s.status} for s in proxy_instances],
-        "storage": [{"address": "{}:{}".format(s.machine.ip, s.port), "status": s.status} for s in storage_instances],
+        "region": cluster_obj.region,
+        "major_version": cluster_obj.major_version,
+        "proxy_count": len(cluster_obj.proxyinstance_set.all()),
+        "master_count": len(cluster_obj.storageinstance_set.filter(instance_role=InstanceRole.REDIS_MASTER.value)),
+        "cluster_entries": [
+            {"entry_type": ce.cluster_entry_type, "entry_addr": ce.entry}
+            for ce in ClusterEntry.objects.filter(cluster=cluster_obj)
+        ],
     }
+
+
+def cluster_proxies(immute_domain: str) -> List:
+    """集群proxy 列表"""
+    c_obj = Cluster.objects.get(immute_domain=immute_domain)
+    proxy_instances = c_obj.proxyinstance_set.all()
+
+    return [{"address": "{}:{}".format(s.machine.ip, s.port), "status": s.status} for s in proxy_instances]
+
+
+def cluster_masters(immute_domain: str) -> List:
+    """集群 master节点 列表"""
+    c_obj = Cluster.objects.get(immute_domain=immute_domain)
+    master_objs = c_obj.storageinstance_set.filter(instance_role=InstanceRole.REDIS_MASTER.value)
+
+    master_infos = {}
+    for ins_obj in master_objs:
+        if not master_infos.get(ins_obj.machine.ip):
+            master_infos[ins_obj.machine.ip] = []
+        else:
+            master_infos[ins_obj.machine.ip].append(ins_obj.port)
+
+    return [{"ip": ip, "ports": ports} for ip, ports in master_infos.items()]
+
+
+def instance_tuple(addr: str) -> List:
+    """查找实例的 主从 信息
+    1. 可以是主节点, 查slave
+    2. 也可以是从节点, 查master"""
+    ad = addr.split(":")
+    ip, port = ad[0], ad[1]
+    machine_objs = Machine.objects.filter(ip=ip)
+    instance_tuples = {}
+    for m_boj in machine_objs:
+        if m_boj.access_layer == AccessLayer.PROXY.value:
+            p_obj = ProxyInstance.objects.get(machine_id=m_boj.id)
+            cluster = p_obj.cluster.get()
+            if not instance_tuples.get(cluster.immute_domain):
+                instance_tuples[cluster.immute_domain] = []
+            instance_tuples[cluster.immute_domain].append(
+                [{"proxy": "{}:{}".format(m_boj.ip, p_obj.port), "immute_domain": cluster.immute_domain}]
+            )
+        else:
+            inst_obj = StorageInstance.objects.get(machine__ip=ip, port=port)
+            for otr in StorageInstanceTuple.objects.filter(ejector=inst_obj):
+                cluster = otr.ejector.cluster.get()
+                m_obj, s_obj = otr.ejector, otr.receiver
+                if not instance_tuples.get(cluster.immute_domain):
+                    instance_tuples[cluster.immute_domain] = []
+                instance_tuples[cluster.immute_domain].append(
+                    {
+                        "master": "{}:{}".format(m_boj.ip, m_obj.port),
+                        "slave": "{}:{}".format(m_boj.ip, s_obj.port),
+                        "cluster": cluster.immute_domain,
+                    }
+                )
+            for otr in StorageInstanceTuple.objects.filter(receiver=inst_obj):
+                cluster = otr.receiver.cluster.get()
+                m_obj, s_obj = otr.ejector, otr.receiver
+                if not instance_tuples.get(cluster.immute_domain):
+                    instance_tuples[cluster.immute_domain] = []
+                instance_tuples[cluster.immute_domain].append(
+                    {
+                        "master": "{}:{}".format(m_boj.ip, m_obj.port),
+                        "slave": "{}:{}".format(m_boj.ip, s_obj.port),
+                        "cluster": cluster.immute_domain,
+                    }
+                )
+
+    return instance_tuples
--- dbm-ui/backend/dbm_aiagent/mcp_tools/redis/views/query_meta.py
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/redis/views/query_meta.py
@@ -12,8 +12,19 @@ from rest_framework.response import Response
 
 from backend.dbm_aiagent.mcp_tools.constants import DBMAMcpTools, DBMMCPTags
 from backend.dbm_aiagent.mcp_tools.decorators import mcp_tools_api_decorator
-from backend.dbm_aiagent.mcp_tools.redis.impl.cluster_topo import redis_cluster_topo
+from backend.dbm_aiagent.mcp_tools.redis.impl.cluster_topo import (
+    cluster_masters,
+    cluster_overview,
+    cluster_proxies,
+    instance_tuple,
+    redis_list_clusters,
+)
 from backend.dbm_aiagent.mcp_tools.redis.serializers.cluster_topo import (
+    RedisAddrSerializer,
+    RedisBizInputSerializer,
+    RedisClustersOutputSerializer,
+    RedisInstanceTupleSerializer,
+    RedisNodesSummarySerializer,
     RedisTopoInputSerializer,
     RedisTopoOutputSerializer,
 )
@@ -29,6 +40,19 @@ class RedisQueryMetaMcpToolsViewSet(McpToolsViewSet):
     default_permission_class = [RejectPermission()]
 
     @mcp_tools_api_decorator(
+        description=str(_("查询业务下的Redis集群列表")),
+        request_slz=RedisBizInputSerializer,
+        response_slz=RedisClustersOutputSerializer,
+        tags=[DBMMCPTags.READ],
+        mcp=[DBMAMcpTools.REDIS_QUERY_META],
+        name_prefix="redis_query_meta",
+    )
+    def list_redis_clusters(self, request, *args, **kwargs):
+        bk_biz_id = self.get_param("bk_biz_id")
+
+        return Response(redis_list_clusters(bk_biz_id=bk_biz_id))
+
+    @mcp_tools_api_decorator(
         description=str(_("查询 Redis 集群元数据")),
         request_slz=RedisTopoInputSerializer,
         response_slz=RedisTopoOutputSerializer,
@@ -36,6 +60,42 @@ class RedisQueryMetaMcpToolsViewSet(McpToolsViewSet):
         mcp=[DBMAMcpTools.REDIS_QUERY_META],
         name_prefix="redis_query_meta",
     )
-    def cluster_topo(self, request, *args, **kwargs):
+    def cluster_overview(self, request, *args, **kwargs):
         immute_domain = self.get_param("immute_domain")
-        return Response(redis_cluster_topo(immute_domain=immute_domain))
+        return Response(cluster_overview(immute_domain=immute_domain))
+
+    @mcp_tools_api_decorator(
+        description=str(_("查询 Redis 集群的Proxy节点信息")),
+        request_slz=RedisTopoInputSerializer,
+        response_slz=RedisTopoOutputSerializer,
+        tags=[DBMMCPTags.READ],
+        mcp=[DBMAMcpTools.REDIS_QUERY_META],
+        name_prefix="redis_query_meta",
+    )
+    def list_cluster_proxies(self, request, *args, **kwargs):
+        immute_domain = self.get_param("immute_domain")
+        return Response(cluster_proxies(immute_domain=immute_domain))
+
+    @mcp_tools_api_decorator(
+        description=str(_("查询 Redis 集群的Master节点信息")),
+        request_slz=RedisTopoInputSerializer,
+        response_slz=RedisNodesSummarySerializer,
+        tags=[DBMMCPTags.READ],
+        mcp=[DBMAMcpTools.REDIS_QUERY_META],
+        name_prefix="redis_query_meta",
+    )
+    def list_cluster_masters(self, request, *args, **kwargs):
+        immute_domain = self.get_param("immute_domain")
+        return Response(cluster_masters(immute_domain=immute_domain))
+
+    @mcp_tools_api_decorator(
+        description=str(_("查询 Redis 实例的主从关系")),
+        request_slz=RedisAddrSerializer,
+        response_slz=RedisInstanceTupleSerializer,
+        tags=[DBMMCPTags.READ],
+        mcp=[DBMAMcpTools.REDIS_QUERY_META],
+        name_prefix="redis_query_meta",
+    )
+    def list_instance_tuple(self, request, *args, **kwargs):
+        address = self.get_param("address")
+        return Response(instance_tuple(addr=address))
