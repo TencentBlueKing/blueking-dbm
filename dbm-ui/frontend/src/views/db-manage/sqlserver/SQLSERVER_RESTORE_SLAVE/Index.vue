49e57854728c19696a84392e7a83b9634854d537
3octaves <873551943@qq.com>
2025-12-30 15:13:51 +0800

    feat(frontend): sqlserver新机重建支持按规格自动匹配资源 #15353

--- dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_RESTORE_SLAVE/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_RESTORE_SLAVE/Index.vue
@@ -13,22 +13,32 @@
 
 <template>
   <SmartAction>
-    <BkForm
+    <BkAlert
       class="mb-20"
+      closable
+      :title="t('重建从库_原机器或新机器重新同步数据及权限_并且将域名解析指向同步好的机器')" />
+    <BkForm
+      class="toolbox-form mt-16 mb-20"
       form-type="vertical"
       :model="formData">
+      <SlaveRestoreFormItem v-model="formData.restore_type" />
+      <BatchInput
+        :config="batchInputConfig"
+        @change="handleBatchInput" />
       <EditableTable
+        :key="tableKey"
         ref="table"
-        class="mb-20"
+        class="mt-16 mb-20"
         :model="formData.tableData">
         <EditableRow
           v-for="(item, index) in formData.tableData"
           :key="index">
           <SlaveHostColumnGroup
             v-model="item.slave"
+            v-model:spec-id="item.specId"
             :selected="selected"
-            @batch-edit="handleBatchEdit" />
-          <SingleResourceHostColumn
+            @batch-edit="handleBatchEditSlave" />
+          <!-- <SingleResourceHostColumn
             v-model="item.newSlave"
             field="newSlave.ip"
             :label="t('新从库主机')"
@@ -36,6 +46,24 @@
               for_bizs: [currentBizId, 0],
               os_names: item.slave.system_version.split(','),
               resource_types: [DBTypes.SQLSERVER, 'PUBLIC'],
+            }" /> -->
+          <SpecColumn
+            v-model="item.specId"
+            :cluster-type="DBTypes.SQLSERVER"
+            :machine-type="MachineTypes.SQLSERVER"
+            required
+            selectable
+            @batch-edit="handleBatchEditColumn" />
+          <ResourceTagColumn
+            v-model="item.labels"
+            @batch-edit="handleBatchEditColumn" />
+          <AvailableResourceColumn
+            :params="{
+              city: item.slave.related_clusters?.[0]?.region,
+              for_bizs: [currentBizId, 0],
+              resource_types: [DBTypes.SQLSERVER, 'PUBLIC'],
+              spec_id: item.specId,
+              labels: item.labels.map((item) => item.id).join(','),
             }" />
           <OperationColumn
             v-model:table-data="formData.tableData"
@@ -67,27 +95,31 @@
 </template>
 
 <script lang="ts" setup>
+  import type { ComponentProps } from 'vue-component-type-helpers';
   import { useI18n } from 'vue-i18n';
 
-  import TicketModel, { type Sqlserver } from '@services/model/ticket/ticket';
+  import { type Sqlserver } from '@services/model/ticket/ticket';
 
-  import { useCreateTicket } from '@hooks';
+  import { useCreateTicket, useTicketDetail } from '@hooks';
 
-  import { DBTypes, TicketTypes } from '@common/const';
+  import { DBTypes, MachineTypes, TicketTypes } from '@common/const';
 
-  import SingleResourceHostColumn from '@views/db-manage/common/toolbox-field/column/single-resource-host-column/Index.vue';
+  import BatchInput from '@views/db-manage/common/batch-input/Index.vue';
+  import AvailableResourceColumn from '@views/db-manage/common/toolbox-field/column/available-resource-column/Index.vue';
+  import ResourceTagColumn from '@views/db-manage/common/toolbox-field/column/resource-tag-column/Index.vue';
+  import SpecColumn from '@views/db-manage/common/toolbox-field/column/spec-column/Index.vue';
+  // import SingleResourceHostColumn from '@views/db-manage/common/toolbox-field/column/single-resource-host-column/Index.vue';
   import TicketPayload, {
     createTickePayload,
   } from '@views/db-manage/common/toolbox-field/form-item/ticket-payload/Index.vue';
+  import SlaveRestoreFormItem from '@views/db-manage/sqlserver/common/slave-restore-form-item/Index.vue';
+
+  import { random } from '@utils';
 
   import SlaveHostColumnGroup, { type SelectorHost } from './components/SlaveHostColumnGroup.vue';
 
   interface RowData {
-    newSlave: {
-      bk_cloud_id: number;
-      bk_host_id: number;
-      ip: string;
-    };
+    labels: ComponentProps<typeof ResourceTagColumn>['modelValue'];
     slave: {
       bk_cloud_id: number;
       bk_host_id: number;
@@ -96,16 +128,12 @@
       related_clusters: {
         id: number;
         master_domain: string;
+        region: string;
       }[];
-      system_version: string;
+      // system_version: string;
     };
+    specId: number;
   }
-
-  interface Props {
-    ticketDetails?: TicketModel<Sqlserver.ResourcePool.RestoreSlave>;
-  }
-
-  const props = defineProps<Props>();
 
   const { t } = useI18n();
   const tableRef = useTemplateRef('table');
@@ -113,29 +141,64 @@
   const currentBizId = window.PROJECT_CONFIG.BIZ_ID;
 
   const createTableRow = (data = {} as Partial<RowData>) => ({
-    newSlave: data.newSlave || {
-      bk_cloud_id: 0,
-      bk_host_id: 0,
-      ip: '',
-    },
-    slave: data.slave || {
-      bk_cloud_id: 0,
-      bk_host_id: 0,
-      db_module_id: 0,
-      ip: '',
-      related_clusters: [],
-      system_version: '',
-    },
+    labels: (data.labels || []) as RowData['labels'],
+    slave: Object.assign(
+      {
+        bk_cloud_id: 0,
+        bk_host_id: 0,
+        db_module_id: 0,
+        ip: '',
+        related_clusters: [] as RowData['slave']['related_clusters'],
+        // system_version: '',
+      },
+      data.slave,
+    ),
+    specId: data.specId || 0,
   });
 
   const defaultData = () => ({
     payload: createTickePayload(),
+    restore_type: TicketTypes.SQLSERVER_RESTORE_SLAVE,
     tableData: [createTableRow()],
   });
 
+  const batchInputConfig = [
+    {
+      case: '192.168.10.2',
+      key: 'ip',
+      label: t('目标从库主机'),
+    },
+    {
+      case: '2核_4G_50G',
+      key: 'spec_name',
+      label: t('目标规格'),
+    },
+    {
+      case: '标签1,标签2',
+      key: 'labels',
+      label: t('资源标签'),
+    },
+  ];
+
+  const tableKey = ref(random());
   const formData = reactive(defaultData());
+
   const selected = computed(() => formData.tableData.filter((item) => item.slave.bk_host_id).map((item) => item.slave));
   const selectedMap = computed(() => Object.fromEntries(selected.value.map((cur) => [cur.ip, true])));
+
+  useTicketDetail<Sqlserver.ResourcePool.RestoreSlave>(TicketTypes.SQLSERVER_RESTORE_SLAVE, {
+    onSuccess(ticketDetail) {
+      const { details } = ticketDetail;
+      Object.assign(formData, {
+        payload: createTickePayload(ticketDetail),
+        tableData: details.infos.map((item) =>
+          createTableRow({
+            slave: { ip: item.old_nodes.old_slave_host[0].ip } as RowData['slave'],
+          }),
+        ),
+      });
+    },
+  });
 
   const { loading: isSubmitting, run: createTicketRun } = useCreateTicket<{
     infos: {
@@ -149,45 +212,15 @@
       };
       resource_spec: {
         sqlserver_ha: {
-          hosts: {
-            bk_cloud_id: number;
-            bk_host_id: number;
-            ip: string;
-          }[];
+          count: number;
+          label_names: string[]; // 标签名称列表，单据详情回显用
+          labels: string[]; // 标签id列表
           spec_id: number;
         };
       };
     }[];
     ip_source: 'resource_pool';
   }>(TicketTypes.SQLSERVER_RESTORE_SLAVE);
-
-  watch(
-    () => props.ticketDetails,
-    () => {
-      if (props.ticketDetails) {
-        const { clusters, infos } = props.ticketDetails.details;
-        Object.assign(formData, {
-          ...createTickePayload(props.ticketDetails),
-        });
-        if (infos.length > 0) {
-          formData.tableData = infos.map((item) =>
-            createTableRow({
-              newSlave: item.resource_spec.sqlserver_ha.hosts[0],
-              slave: {
-                ...item.old_nodes.old_slave_host[0],
-                db_module_id: clusters[item.cluster_ids[0]].db_module_id,
-                related_clusters: item.cluster_ids.map((id) => ({
-                  id: id,
-                  master_domain: clusters[id].immute_domain,
-                })),
-                system_version: '',
-              },
-            }),
-          );
-        }
-      }
-    },
-  );
 
   const handleSubmit = async () => {
     const valid = await tableRef.value!.validate();
@@ -207,8 +240,10 @@
             },
             resource_spec: {
               sqlserver_ha: {
-                hosts: [item.newSlave],
-                spec_id: 0,
+                count: 1,
+                label_names: item.labels.map((item) => item.value),
+                labels: item.labels.map((item) => String(item.id)),
+                spec_id: item.specId,
               },
             },
           })),
@@ -219,11 +254,7 @@
     }
   };
 
-  const handleReset = () => {
-    Object.assign(formData, defaultData());
-  };
-
-  const handleBatchEdit = (list: SelectorHost[]) => {
+  const handleBatchEditSlave = (list: SelectorHost[]) => {
     const dataList = list.reduce<RowData[]>((acc, item) => {
       if (!selectedMap.value[item.ip]) {
         acc.push(
@@ -236,14 +267,48 @@
               related_clusters: item.related_clusters.map((cluster) => ({
                 id: cluster.id,
                 master_domain: cluster.master_domain,
+                region: cluster.region,
               })),
-              system_version: '',
+              // system_version: '',
             },
+            specId: item.spec_config.id,
           }),
         );
       }
       return acc;
     }, []);
     formData.tableData = [...(selected.value.length ? formData.tableData : []), ...dataList];
+  };
+
+  const handleBatchInput = (data: Record<string, any>[], isClear: boolean) => {
+    const dataList = data.reduce<RowData[]>((acc, item) => {
+      acc.push(
+        createTableRow({
+          labels: (item.labels as string)?.split(',').map((item) => ({ value: item })),
+          slave: { ip: item.ip } as RowData['slave'],
+          specId: item.spec_name,
+        }),
+      );
+      return acc;
+    }, []);
+
+    if (isClear) {
+      tableKey.value = random();
+      formData.tableData = [...dataList];
+    } else {
+      formData.tableData = [...(selected.value.length ? formData.tableData : []), ...dataList];
+    }
+  };
+
+  const handleBatchEditColumn = (value: any, field: string) => {
+    formData.tableData.forEach((rowData) => {
+      Object.assign(rowData, {
+        [field]: value,
+      });
+    });
+  };
+
+  const handleReset = () => {
+    Object.assign(formData, defaultData());
   };
 </script>
