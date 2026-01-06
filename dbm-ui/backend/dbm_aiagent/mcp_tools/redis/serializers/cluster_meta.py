a8d734fea83c8f19233b8eb15007da0aa2f46573
xiepaup <xiepaup@gmail.com>
2026-01-06 15:11:41 +0800

    feat(redis): redis_mcp_2redis_init #15491

---
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/redis/serializers/cluster_meta.py
@@ -0,0 +1,79 @@
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from backend.db_meta.enums import InstanceStatus
+
+
+class RedisAddrSerializer(serializers.Serializer):
+    address = serializers.CharField(help_text=_("ip:port 形式的实例地址"))
+
+
+class RedisBaseInstanceSerializer(serializers.Serializer):
+    address = RedisAddrSerializer(help_text=_("ip:port 形式的实例地址"))
+    status = serializers.ChoiceField(choices=InstanceStatus.get_choices(), help_text=_("实例状态"))
+    machine_type = serializers.CharField(help_text=_("实例机器类型"))
+
+
+class RedisInstanceSummarySerializer(serializers.Serializer):
+    IP = serializers.CharField(help_text=_("机器IP"))
+    Port = serializers.IntegerField(help_text=_("实例端口"))
+
+
+class RedisStorageInstanceSerializer(RedisBaseInstanceSerializer):
+    is_stand_by = serializers.BooleanField(default=True, help_text=_("dbha 切换备选标志"))
+
+
+class RedisEntrySerializer(RedisBaseInstanceSerializer):
+    entry_type = serializers.CharField(help_text=_("访问方式"))
+    entry_addr = serializers.CharField(help_text=_("访问地址"))
+
+
+class RedisTopoInputSerializer(serializers.Serializer):
+    immute_domain = serializers.CharField(help_text=_("集群域名"))
+
+
+class RedisTopoOutputSerializer(serializers.Serializer):
+    cluster_type = serializers.CharField(help_text=_("集群类型"))
+    region = serializers.CharField(help_text=_("所在地域"))
+    major_version = serializers.CharField(help_text=_("后端版本"))
+    immute_domain = serializers.CharField(help_text=_("集群域名"))
+    proxy_count = serializers.IntegerField(help_text=_("Proxy节点数"))
+    master_count = serializers.IntegerField(help_text=_("Master节点数"))
+    cluster_entries = serializers.ListSerializer(child=RedisEntrySerializer(), help_text=_("集群连接地址"))
+
+
+class RedisNodesSummarySerializer(serializers.Serializer):
+    cluster_masters = serializers.ListSerializer(child=RedisInstanceSummarySerializer(), help_text=_("存储层实例汇总信息"))
+
+
+class RedisBizInputSerializer(serializers.Serializer):
+    bk_biz_id = serializers.IntegerField(help_text=_("业务ID"))
+
+
+class RedisClustersOutputSerializer(serializers.Serializer):
+    bk_cloud_id = serializers.IntegerField(help_text=_("云区域ID"))
+    cluster_type = serializers.CharField(help_text=_("集群类型"))
+    cluster_id = serializers.IntegerField(help_text=_("集群ID"))
+    immute_domain = serializers.CharField(help_text=_("集群域名"))
+    alias = serializers.CharField(help_text=_("集群别名"))
+
+
+class RedisTupleInfoSerializer(serializers.Serializer):
+    immute_domain = serializers.CharField(help_text=_("集群域名"))
+    master = serializers.CharField(help_text=_("主节点"))
+    slave = serializers.CharField(help_text=_("从节点"))
+    proxy = serializers.CharField(help_text=_("Proxy节点"))
+
+
+class RedisInstanceTupleSerializer(serializers.Serializer):
+    immute_domain = serializers.CharField(help_text=_("集群域名"))
+    instances = serializers.ListSerializer(child=RedisTupleInfoSerializer(), help_text=_("实例关系对"))
---
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/redis/urls.py
@@ -0,0 +1,27 @@
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from rest_framework.routers import DefaultRouter
+
+from backend.dbm_aiagent.mcp_tools.redis.views.query_meta import RedisQueryMetaMcpToolsViewSet
+from backend.dbm_aiagent.mcp_tools.redis.views.query_status import RedisQueryStatusMcpToolsViewSet
+from backend.dbm_aiagent.mcp_tools.redis.views.redis_bill_mcp import RedisBillMcpToolsViewSet
+
+routers = DefaultRouter(trailing_slash=True)
+
+# 与 元数据相关的 query
+routers.register(r"", RedisQueryMetaMcpToolsViewSet, basename="mcp-redis-query-meta")
+# 与 实力状态相关的 query ； 需要登陆实例才能获取的信息
+routers.register(r"", RedisQueryStatusMcpToolsViewSet, basename="mcp-redis-query-status")
+# 与 dbm 交互 创建单据类的 操作
+routers.register(r"", RedisBillMcpToolsViewSet, basename="mcp-redis-bill")
+# 与 其他组件----
+# routers.register(r"", RedisMetaQueryMcpToolsViewSet, basename="mcp-redis-meta-query")
+
+urlpatterns = routers.urls
