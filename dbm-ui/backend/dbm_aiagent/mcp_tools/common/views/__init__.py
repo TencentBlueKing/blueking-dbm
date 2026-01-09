a02acf028e4f69352487e2456ebc655c0e439fe4
yksitu <1297650644@qq.com>
2026-01-09 19:21:28 +0800

    fix(other): 修复单据值守的mcp注册问题 #15635

--- dbm-ui/backend/dbm_aiagent/mcp_tools/common/views/__init__.py
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/common/views/__init__.py
@@ -8,6 +8,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from .alram_query import MonitorQueryMcpToolsViewSet
 from .bill_query import BillQueryMcpToolsViewSet
 from .dbmeta_query import DBMetaQueryMcpToolsViewSet
 from .resource_param_query import ResourceParamQueryMcpToolsViewSet
