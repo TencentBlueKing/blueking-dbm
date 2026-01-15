91435f48cb34624665b6f3050229bc2ee1d2cd4b
zvictorino <zvictorino@gmail.com>
2026-01-15 16:57:11 +0800

    feat(kafka): 缩容/替换流程，清理机器前，检查数据是否搬空 close #15714

--- dbm-ui/backend/flow/engine/bamboo/scene/kafka/kafka_replace_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/kafka/kafka_replace_flow.py
@@ -470,6 +470,16 @@ class KafkaReplaceFlow(object):
                     kwargs=asdict(act_kwargs),
                 )
 
+                # 检查broker是否为空
+                act_kwargs.template = get_base_payload(
+                    action=KafkaActuatorActionEnum.BrokerIsEmpty.value, host=broker["ip"]
+                )
+                sub_pipeline.add_act(
+                    act_name=_("检查broker是否为空-{}").format(broker["ip"]),
+                    act_component_code=ExecuteDBActuatorScriptComponent.code,
+                    kwargs=asdict(act_kwargs),
+                )
+
                 act_kwargs.template = get_base_payload(
                     action=KafkaActuatorActionEnum.StopProcess.value,
                     host=broker["ip"],
