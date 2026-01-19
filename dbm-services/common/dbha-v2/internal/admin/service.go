25decb54096acad7d72fa64b0b2707e62ae28447
F-cq <2036478450@qq.com>
2026-01-19 15:40:12 +0800

    fix(backend): Fix admin api comments and add timeout config. issue: #15717

--- dbm-services/common/dbha-v2/internal/admin/service.go
+++ dbm-services/common/dbha-v2/internal/admin/service.go
@@ -309,8 +309,8 @@ func (s *Service) createWebServer() error {
 	serverConfig := &hanet.GinServerConfig{
 		Host:         config.Cfg.Web.Host,
 		Port:         config.Cfg.Web.Port,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  config.Cfg.Web.ReadTimeout * time.Second,
+		WriteTimeout: config.Cfg.Web.WriteTimeout * time.Second,
 	}
 
 	server := hanet.NewGinHTTPServer(serverConfig)
