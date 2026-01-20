16e19c7904ff95084ce2b16f8cdf9e3b38c5be4e
hLinx <327159425@qq.com>
2026-01-20 16:11:13 +0800

    feat(frontend): 单据详情增加 ai 日志分析 #15845

---
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/flow-info/components/flow-type-common/components/render-error-message/Index.vue
@@ -0,0 +1,82 @@
+<template>
+  <FlowCollapse
+    danger
+    :title="t('失败原因')">
+    <!-- ENABLE_DBM_AI 为 true 时，才显示 AI 日志分析和原始日志选择 -->
+    <!-- 两个都存在时才显示 tab 切换，所以 ENABLE_DBM_AI 为 false 时，不显示 BkRadioGroup -->
+    <BkRadioGroup
+      v-if="ENABLE_DBM_AI"
+      v-model="errorType"
+      class="ml-16 mb-5"
+      type="capsule">
+      <BkRadioButton label="ai">
+        <img
+          :src="AiBluekingImage"
+          style="width: 12px" />
+        {{ t('AI 日志分析') }}
+      </BkRadioButton>
+      <BkRadioButton label="original">
+        <DbIcon type="file" />
+        {{ t('原始日志') }}
+      </BkRadioButton>
+    </BkRadioGroup>
+    <div
+      class="pl-16"
+      :style="{
+        'max-height': `${errMessageMaxHeight}px`,
+        overflow: 'auto',
+        height: logContentHeight,
+        color: '#4D4F56',
+        lineHeight: '20px',
+        fontSize: '12px',
+      }">
+      <component
+        :is="renderContent"
+        :data="data"
+        :ticket-detail="ticketDetail"
+        @element-height-change="handleElementHeightChange" />
+    </div>
+  </FlowCollapse>
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  import FlowMode from '@services/model/ticket/flow';
+  import TicketModel from '@services/model/ticket/ticket';
+
+  import { useSystemEnviron } from '@stores';
+
+  import AiBluekingImage from '@images/ai-blueking.svg';
+
+  import FlowCollapse from '../../components/FlowCollapse.vue';
+
+  import LogAnnlysis from './components/log-annlysis.vue';
+  import LogOriginal from './components/log-original.vue';
+
+  interface Props {
+    data: FlowMode<unknown, any>;
+    ticketDetail: TicketModel<unknown>;
+  }
+  defineProps<Props>();
+  const { t } = useI18n();
+
+  const { ENABLE_DBM_AI } = useSystemEnviron().urls;
+
+  const errorType = ref<'ai' | 'original'>(ENABLE_DBM_AI ? 'ai' : 'original');
+
+  const errMessageMaxHeight = window.innerHeight * 0.4;
+  const logContentHeight = ref('auto');
+
+  const renderContent = computed(() => {
+    if (errorType.value === 'ai') {
+      return LogAnnlysis;
+    }
+    return LogOriginal;
+  });
+
+  const handleElementHeightChange = (height: number) => {
+    if (height >= errMessageMaxHeight) {
+      logContentHeight.value = `${height}px`;
+    }
+  };
+</script>
