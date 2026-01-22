9f2bce89c6730fbbb075a8b4b5f9c01c6e027275
huangwentai-wendel <huangwentai96@163.com>
2026-01-22 16:09:49 +0800

    feat(backend): mongo数据导出 #15656

--- dbm-ui/backend/ticket/builders/mongodb/mongo_data_export.py
+++ dbm-ui/backend/ticket/builders/mongodb/mongo_data_export.py
@@ -14,6 +14,7 @@ import time
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
+from backend.dbm_init.medium.handlers import MediumHandler
 from backend.flow.consts import MONGODB_DATA_EXPORT_PATH
 from backend.flow.engine.controller.mongodb import MongoDBController
 from backend.ticket import builders
@@ -24,7 +25,12 @@ from backend.ticket.builders.mongodb.base import (
     Cluster,
     DBTableSerializer,
 )
-from backend.ticket.constants import TicketFlowStatus, TicketType
+from backend.ticket.builders.mysql.mysql_dump_data import (
+    MySQLDumpDataItsmMaintainerFlowParamsBuilder,
+    MySQLDumpDataItsmProductorFlowParamsBuilder,
+)
+from backend.ticket.constants import FlowType, TicketFlowStatus, TicketType
+from backend.ticket.models import Flow
 
 
 class MongoDBDataExportDetailSerializer(BaseMongoDBOperateDetailSerializer):
@@ -55,11 +61,13 @@ class MongoDBDataExportFlowParamBuilder(BaseMongoOperateFlowParamBuilder):
             if key in export_options and len(export_options[key]) == 0:
                 export_options.pop(key)
 
+        cluster_ids = [info["cluster_id"] for info in self.ticket_data["infos"]]
+        cluster_id_map = {cluster.id: cluster for cluster in Cluster.objects.filter(id__in=cluster_ids)}
         for info in self.ticket_data["infos"]:
             pop_if_empty(info["export_options"], "query")
             pop_if_empty(info["export_options"], "fields")
 
-            cluster = Cluster.objects.get(id=info["cluster_id"])
+            cluster = cluster_id_map[info["cluster_id"]]
             info["filename"] = f"{cluster.immute_domain}_{int(time.time())}"
 
     def post_callback(self):
@@ -67,13 +75,19 @@ class MongoDBDataExportFlowParamBuilder(BaseMongoOperateFlowParamBuilder):
         if not flow or flow.status != TicketFlowStatus.SUCCEEDED:
             return
 
+        _, files = MediumHandler().storage.listdir(MONGODB_DATA_EXPORT_PATH.format(biz=self.ticket.bk_biz_id))
+        file_name_size_map = {file["name"]: file["size"] for file in files}
         cluster_results = {}
         for info in flow.details["ticket_data"]["infos"]:
             cluster_id = info["cluster_id"]
             result_file_path = "{}/{}.tar".format(
                 MONGODB_DATA_EXPORT_PATH.format(biz=self.ticket.bk_biz_id), info["filename"]
             )
-            cluster_results[cluster_id] = result_file_path
+            cluster_results[cluster_id] = {
+                "file_path": result_file_path,
+                "file_name": info["filename"],
+                "size": file_name_size_map.get(info["filename"]),
+            }
         self.ticket.update_details(exported_files=cluster_results)
 
 
@@ -81,4 +95,48 @@ class MongoDBDataExportFlowParamBuilder(BaseMongoOperateFlowParamBuilder):
 class MongoDBDataExportApplyFlowBuilder(BaseMongoDBTicketFlowBuilder):
     serializer = MongoDBDataExportDetailSerializer
     inner_flow_builder = MongoDBDataExportFlowParamBuilder
+    itsm_flow_maintainer_builder = MySQLDumpDataItsmMaintainerFlowParamsBuilder
+    itsm_flow_productor_builder = MySQLDumpDataItsmProductorFlowParamsBuilder
     inner_flow_name = _("MongoDB 数据导出执行")
+
+    def init_ticket_flows(self):
+        flows = []
+        # 二级审批
+        flows.append(
+            Flow(
+                ticket=self.ticket,
+                flow_type=FlowType.BK_ITSM.value,
+                details=self.itsm_flow_maintainer_builder(self.ticket).get_params(),
+                flow_alias=_("运维人员审批"),
+            )
+        )
+        flows.append(
+            Flow(
+                ticket=self.ticket,
+                flow_type=FlowType.BK_ITSM.value,
+                details=self.itsm_flow_productor_builder(self.ticket).get_params(),
+                flow_alias=_("产品人员审批"),
+            )
+        )
+        # 人工确认
+        flows.append(
+            Flow(
+                ticket=self.ticket,
+                flow_type=FlowType.PAUSE.value,
+                details=self.pause_node_builder(self.ticket).get_params(),
+                flow_alias=_("人工确认"),
+            ),
+        )
+        # 数据导出
+        flows.append(
+            Flow(
+                ticket=self.ticket,
+                flow_type=FlowType.INNER_FLOW.value,
+                details=self.inner_flow_builder(self.ticket).get_params(),
+                flow_alias=self.inner_flow_name,
+                retry_type=self.retry_type,
+            )
+        )
+
+        Flow.objects.bulk_create(flows)
+        return list(Flow.objects.filter(ticket=self.ticket))
