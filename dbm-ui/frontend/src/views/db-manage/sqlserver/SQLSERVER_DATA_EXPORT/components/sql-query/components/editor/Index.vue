8c49d4fb9617181e707bc9bf611246acb2b3234e
chenwenchang <479999519@qq.com>
2026-01-26 16:57:22 +0800

    feat(frontend): sqlserver数据导出_验收问题 #15138

--- dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_DATA_EXPORT/components/sql-query/components/editor/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_DATA_EXPORT/components/sql-query/components/editor/Index.vue
@@ -18,19 +18,6 @@
     :class="{ 'is-full-screen': isFullscreen }">
     <div class="editor-layout-header">
       <div>{{ t('SQL 查询') }}</div>
-      <div class="query-operation-main">
-        <div
-          v-for="item in operationList"
-          :key="item.value"
-          class="operation-item"
-          :class="{ 'operation-item-active': currentOperation === item.value }"
-          @click="() => handleChangeOperation(item.value)">
-          <DbIcon
-            style="font-size: 14px"
-            :type="item.icon" />
-          <span class="ml-4">{{ item.name }}</span>
-        </div>
-      </div>
       <div class="editro-action-box">
         <CodeFormat
           :data="modelValue"
@@ -39,59 +26,13 @@
         <FullScreen @change="handleChangeFullScreen" />
       </div>
     </div>
-    <BkResizeLayout
-      :border="false"
+    <div
       class="editor-main-resizer"
-      :initial-divide="divideMin"
-      :max="800"
-      :min="divideMin"
-      placement="right"
-      style="height: 100%"
-      @after-resize="handleAfterResize">
-      <template #aside>
-        <FrequentQuery
-          v-if="currentOperation === 'query'"
-          @choose-sql="handleChooseSql" />
-        <MyCollection
-          v-if="currentOperation === 'collect'"
-          :key="collectionRenderKey"
-          :sql-profile="sqlProfile"
-          @change="fetchProfileSql"
-          @choose-sql="handleChooseSql" />
-      </template>
-      <template #main>
-        <div
-          ref="editorRef"
-          style="height: calc(100% - 95px)" />
-        <div class="editor-operate-main">
-          <BkPopConfirm
-            :title="t('收藏')"
-            trigger="click"
-            width="340"
-            @confirm="handleClickCollect">
-            <template #content>
-              <div class="editor-collect-main">
-                <div class="collect-title">
-                  {{ t('名称') }}
-                </div>
-                <AutoFocusInput
-                  v-model="collectName"
-                  class="mt-6 mb-18" />
-              </div>
-            </template>
-            <BkButton
-              class="collect-btn"
-              :disabled="!modelValue"
-              outline>
-              <DbIcon
-                class="mr-5"
-                type="star" />
-              {{ t('收藏') }}
-            </BkButton>
-          </BkPopConfirm>
-        </div>
-      </template>
-    </BkResizeLayout>
+      style="height: 100%">
+      <div
+        ref="editorRef"
+        style="height: calc(100% - 88px)" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -99,36 +40,12 @@
   import screenfull from 'screenfull';
   import { format } from 'sql-formatter';
   import { useI18n } from 'vue-i18n';
-  import { useRequest } from 'vue-request';
 
-  import { getProfileSql, upsertProfile } from '@services/source/profile';
-
-  import { DBTypes } from '@common/const';
-
-  import { messageError } from '@utils';
-
-  import AutoFocusInput from './components/AutoFocusInput.vue';
   import CodeFormat from './components/CodeFormat.vue';
   import FontSetting from './components/FontSetting.vue';
-  import FrequentQuery from './components/FrequentQuery.vue';
   import FullScreen from './components/FullScreen.vue';
-  import MyCollection from './components/MyCollection.vue';
-
-  export type SqlProfile = ServiceReturnType<typeof getProfileSql>;
-
-  interface Props {
-    dbType?: DBTypes;
-  }
 
   type Emits = (e: 'change', value: string) => void;
-
-  interface Exposes {
-    updateCollectPanel: () => void;
-  }
-
-  const props = withDefaults(defineProps<Props>(), {
-    dbType: DBTypes.SQLSERVER,
-  });
 
   const emits = defineEmits<Emits>();
 
@@ -143,45 +60,6 @@
   const rootRef = ref();
   const editorRef = ref();
   const isFullscreen = ref(false);
-  const currentOperation = ref('query');
-  const collectionRenderKey = ref(0);
-  const collectName = ref('');
-  const divideMin = ref(290);
-  const sqlProfile = ref<SqlProfile>();
-
-  const operationList = [
-    {
-      icon: 'dingwei',
-      name: t('常用查询'),
-      value: 'query',
-    },
-    {
-      icon: 'wenjian',
-      name: t('我的收藏'),
-      value: 'collect',
-    },
-  ];
-
-  const { run: fetchProfileSql } = useRequest(getProfileSql, {
-    manual: true,
-    onSuccess(data) {
-      // 临时处理
-      if (Array.isArray(data)) {
-        sqlProfile.value = {};
-      } else {
-        sqlProfile.value = data;
-      }
-    },
-  });
-
-  watch(currentOperation, () => {
-    if (currentOperation.value !== '') {
-      divideMin.value = currentOperation.value === 'query' ? 290 : 450;
-      return;
-    }
-
-    divideMin.value = 0;
-  });
 
   watch(
     modelValue,
@@ -197,57 +75,12 @@
     },
   );
 
-  const handleChooseSql = (sql: string) => {
-    modelValue.value = sql;
-  };
-
-  const handleClickCollect = async () => {
-    if (!collectName.value) {
-      messageError(t('收藏名称不能为空'));
-      return;
-    }
-
-    if (!sqlProfile.value![props.dbType]) {
-      sqlProfile.value![props.dbType] = [];
-    }
-    const nameList = sqlProfile.value![props.dbType].map((item) => item.name);
-    if (nameList.includes(collectName.value)) {
-      collectName.value = '';
-      messageError(t('收藏名称已存在'));
-      return;
-    }
-
-    sqlProfile.value![props.dbType].push({
-      is_top: false,
-      name: collectName.value,
-      sql: modelValue.value,
-    });
-    await upsertProfile({
-      label: 'SQL',
-      values: sqlProfile.value,
-    });
-    collectName.value = '';
-    fetchProfileSql();
-  };
-
-  const handleChangeOperation = (operation: string) => {
-    if (currentOperation.value === operation) {
-      currentOperation.value = '';
-      return;
-    }
-    currentOperation.value = operation;
-  };
-
   const handleToggleScreenfull = () => {
     if (screenfull.isFullscreen) {
       isFullscreen.value = true;
     } else {
       isFullscreen.value = false;
     }
-    editor.layout();
-  };
-
-  const handleReize = () => {
     editor.layout();
   };
 
@@ -262,12 +95,6 @@
   const handleCodeFormat = () => {
     modelValue.value = format(modelValue.value);
   };
-
-  const handleAfterResize = () => {
-    collectionRenderKey.value += 1;
-  };
-
-  fetchProfileSql();
 
   onMounted(() => {
     nextTick(() => {
@@ -297,19 +124,11 @@
     });
 
     screenfull.on('change', handleToggleScreenfull);
-    window.addEventListener('resize', handleReize);
   });
 
   onBeforeUnmount(() => {
     editor.dispose();
     screenfull.off('change', handleToggleScreenfull);
-    window.removeEventListener('resize', handleReize);
-  });
-
-  defineExpose<Exposes>({
-    updateCollectPanel() {
-      handleAfterResize();
-    },
   });
 </script>
 <style lang="less" scoped>
@@ -324,10 +143,6 @@
       flex-direction: column;
     }
 
-    :deep(.bk-resize-layout-aside-content) {
-      height: calc(100% - 40px) !important;
-    }
-
     .editor-layout-header {
       display: flex;
       align-items: center;
@@ -337,25 +152,7 @@
       font-size: 12px;
       color: #c4c6cc;
       background: #2e2e2e;
-
-      .query-operation-main {
-        display: flex;
-        margin-left: auto;
-        font-size: 12px;
-        cursor: pointer;
-
-        .operation-item {
-          display: flex;
-          width: 90px;
-          align-items: center;
-          justify-content: center;
-          color: #c4c6cc;
-
-          &.operation-item-active {
-            color: #699df4;
-          }
-        }
-      }
+      justify-content: space-between;
 
       .editro-action-box {
         display: flex;
@@ -365,48 +162,6 @@
         & > * {
           cursor: pointer;
         }
-      }
-    }
-
-    .editor-main-resizer {
-      :deep(.bk-resize-trigger) {
-        background-color: #1a1a1a;
-        height: calc(100% - 40px);
-
-        &:hover {
-          border-left: 1px solid #699df4;
-        }
-      }
-    }
-  }
-</style>
-<style lang="less">
-  .editor-operate-main {
-    display: flex;
-    height: 56px;
-    padding-left: 24px;
-    background-color: #1a1a1a;
-    align-items: center;
-
-    .collect-btn {
-      width: 64px;
-      height: 28px;
-      background: #313238;
-      border: 1px solid #63656e;
-      border-radius: 2px;
-    }
-  }
-
-  .editor-collect-main {
-    .collect-title {
-      position: relative;
-
-      &::after {
-        position: absolute;
-        top: 2px;
-        left: 28px;
-        color: #ea3636;
-        content: '*';
       }
     }
   }
