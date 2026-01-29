803a50562733294fc22bbd144b17869f5f30eba8
xfan0805 <xfan1006@163.com>
2026-01-29 14:54:22 +0800

    fix: fix_partition_block_issue #16043

--- dbm-services/mysql/db-partition/service/check_partition_base_func.go
+++ dbm-services/mysql/db-partition/service/check_partition_base_func.go
@@ -49,9 +49,10 @@ func (config *PartitionConfig) GetPartitionDbLikeTbLike(dbtype string, splitCnt 
 					Addresses: []string{fmt.Sprintf("%s:%d", host.Ip, host.Port)},
 					Cmds: []string{
 						"set lock_wait_timeout=10",
-						fmt.Sprintf("FLUSH TABLE `%s`.`%s` with read lock", tb.DbName, tb.TbName),
+						// fmt.Sprintf("FLUSH TABLE `%s`.`%s` with read lock", tb.DbName, tb.TbName),
+						fmt.Sprintf("FLUSH TABLE `%s`.`%s`", tb.DbName, tb.TbName),
 					},
-					Force:        true,
+					Force:        false,
 					QueryTimeout: 10,
 					BkCloudId:    host.BkCloudId,
 				})
@@ -115,9 +116,10 @@ func (config *PartitionConfig) getOneTableInfo(address string, bkCloudId int, ro
 			// 转换成锁等待, 能正常退出
 			Cmds: []string{
 				"set lock_wait_timeout=10",
-				fmt.Sprintf("FLUSH TABLES `%s`.`%s` with read lock", db, tb),
+				// fmt.Sprintf("FLUSH TABLES `%s`.`%s` with read lock", db, tb),
+				fmt.Sprintf("FLUSH TABLES `%s`.`%s`", db, tb),
 			},
-			Force:        true,
+			Force:        false,
 			QueryTimeout: 10,
 			BkCloudId:    bkCloudId,
 		})
