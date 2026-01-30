0a9dde520865c217b5a7df2ea7d9124ab6d53096
kevn <100595273+Kev1n8@users.noreply.github.com>
2026-01-30 12:05:29 +0800

    feat(mongodb): Change the suffix of exported data file to readable string #16074

--- dbm-ui/backend/ticket/builders/mongodb/mongo_data_export.py
+++ dbm-ui/backend/ticket/builders/mongodb/mongo_data_export.py
@@ -60,7 +60,7 @@ class MongoDBDataExportFlowParamBuilder(BaseMongoOperateFlowParamBuilder):
             pop_if_empty(info["export_options"], "fields")
 
             cluster = Cluster.objects.get(id=info["cluster_id"])
-            info["filename"] = f"{cluster.immute_domain}_{int(time.time())}"
+            info["filename"] = f"{cluster.immute_domain}_{time.strftime('%Y%m%d%H%M%S')}"
 
     def post_callback(self):
         flow = self.ticket.current_flow()
