bbd38682c4f39ea9ec93bb92495a81097e9d572a
vlhwnliang <137610334@qq.com>
2026-02-04 10:35:34 +0800

    feat(backend): mongodb标准化 #16199 # Reviewed, transaction id: 73577

--- dbm-ui/backend/ticket/constants.py
+++ dbm-ui/backend/ticket/constants.py
@@ -646,6 +646,7 @@ class TicketType(StrStructuredEnum):
     MONGODB_AUTOFIX = TicketEnumField("MONGODB_AUTOFIX", _("MongoDB 故障自愈"), _("集群维护"))
     MONGODB_INSTANCE_DEINSTALL = TicketEnumField("MONGODB_INSTANCE_DEINSTALL", _("MongoDB 实例下架"), _("集群维护"))
     MONGODB_INSTANCE_FIX_STATUS = TicketEnumField("MONGODB_INSTANCE_FIX_STATUS", _("MongoDB 节点状态修复"), _("集群维护"))
+    MONGODB_CLUSTER_STANDARDIZE = TicketEnumField("MONGODB_CLUSTER_STANDARDIZE", _("MongoDB 集群标准化"), _("集群维护"))
 
     # 云区域组件
     CLOUD_SERVICE_APPLY = EnumField("CLOUD_SERVICE_APPLY", _("云区域服务部署"))
