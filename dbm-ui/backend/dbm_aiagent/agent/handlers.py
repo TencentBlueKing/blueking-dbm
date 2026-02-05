e921541419a8e023b915f829291dcb3b8c330256
iSecloud <869820505@qq.com>
2026-02-05 19:49:45 +0800

    feat(backend): 查询日志节点的AI分析 #16219

--- dbm-ui/backend/dbm_aiagent/agent/handlers.py
+++ dbm-ui/backend/dbm_aiagent/agent/handlers.py
@@ -60,7 +60,12 @@ class AgentHandler:
 
     @classmethod
     def ask_agent_with_content(
-        cls, agent_code: DBMAgentCode, content: str, username=DEFAULT_USERNAME, session_code=None, stream: bool = False
+        cls,
+        agent_code: DBMAgentCode,
+        content: str,
+        username=DEFAULT_USERNAME,
+        session_code=None,
+        stream: bool = False
     ):
         """根据agent直接内容询问agent"""
         # 创建临时会话
@@ -85,7 +90,11 @@ class AgentHandler:
 
     @classmethod
     def ask_agent_with_content_in_session(
-        cls, agent_code: DBMAgentCode, content: str, username=DEFAULT_USERNAME, session_code=None
+        cls,
+        agent_code: DBMAgentCode,
+        content: str,
+        username=DEFAULT_USERNAME,
+        session_code=None
     ):
         """根据agent直接内容询问agent, 连续对话"""
         session_code = session_code or cls.create_temporary_session(username)
@@ -94,7 +103,11 @@ class AgentHandler:
 
     @classmethod
     def ask_agent_with_command(
-        cls, command: str, command_params: dict, username=DEFAULT_USERNAME, stream: bool = False
+        cls,
+        command: str,
+        command_params: dict,
+        username=DEFAULT_USERNAME,
+        stream: bool = False
     ):
         """根据快捷指令询问agent"""
         if command not in CommandProcessor._handlers:
