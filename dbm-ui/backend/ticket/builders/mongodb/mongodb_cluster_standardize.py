3496ba262d2fea2f681c654ff928debbe1a67c28
vlhwnliang <137610334@qq.com>
2026-02-05 16:46:44 +0800

    feat(backend): mongodb标准化 #16237 # Reviewed, transaction id: 73774

---
+++ dbm-ui/backend/ticket/builders/mongodb/mongodb_cluster_standardize.py
@@ -0,0 +1,26 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from backend.flow.engine.controller.mongodb import MongoDBController
+from backend.ticket import builders
+from backend.ticket.builders.mongodb.base import BaseMongoDBOperateDetailSerializer, BaseMongoDBTicketFlowBuilder
+from backend.ticket.constants import FlowRetryType, TicketType
+
+
+class MongodbClusterStandardizeDetailSerializer(BaseMongoDBOperateDetailSerializer):
+    bk_biz_id = serializers.IntegerField(help_text=_("业务ID"))
+    bk_cloud_id = serializers.IntegerField(help_text=_("云区域ID"))
+    cluster_ids = serializers.ListField(child=serializers.IntegerField())
+    restart_exporter = serializers.BooleanField()
+
+
+class MongodbClusterStandardizeFlowParamBuilder(builders.FlowParamBuilder):
+    controller = MongoDBController.cluster_standardization
+
+
+@builders.BuilderFactory.register(TicketType.MONGODB_CLUSTER_STANDARDIZE)
+class MongodbClusterStandardizeFlowBuilder(BaseMongoDBTicketFlowBuilder):
+    serializer = MongodbClusterStandardizeDetailSerializer
+    inner_flow_builder = MongodbClusterStandardizeFlowParamBuilder
+    inner_flow_name = _("MongoDB集群标准化")
+    retry_type = FlowRetryType.MANUAL_RETRY
