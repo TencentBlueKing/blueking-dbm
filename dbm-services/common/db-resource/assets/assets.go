a7db9464a2e1e27d9591b33e84c85030a4ea2a97
ymakedaq <ymakedaq@163.com>
2026-02-05 13:04:42 +0800

    refactor(dbm-services): 优化语法检查的提示 #16227

--- dbm-services/common/db-resource/assets/assets.go
+++ dbm-services/common/db-resource/assets/assets.go
@@ -12,51 +12,42 @@
 package assets
 
 import (
-	"embed"
-	"fmt"
-
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mysql" // mysql TODO
-	"github.com/golang-migrate/migrate/v4/source"
-	"github.com/golang-migrate/migrate/v4/source/iofs"
-	"github.com/pkg/errors"
-
-	"dbm-services/common/go-pubpkg/logger"
 )
 
 // Migrations embed migrations sqlfile
 
-//go:embed migrations/*.sql
-var fs embed.FS
+// go embed migrations/*.sql //
+// var fs embed.FS
 
-// DoMigrateFromEmbed  do migrate from embed
-func DoMigrateFromEmbed(user, addr, password, dbname string) (err error) {
-	var mig *migrate.Migrate
-	var d source.Driver
-	if d, err = iofs.New(fs, "migrations"); err != nil {
-		return err
-	}
-	dbURL := fmt.Sprintf(
-		"mysql://%s:%s@tcp(%s)/%s?charset=%s&parseTime=true&loc=Local&multiStatements=true&interpolateParams=true",
-		user,
-		password,
-		addr,
-		dbname,
-		"utf8",
-	)
-	mig, err = migrate.NewWithSourceInstance("iofs", d, dbURL)
-	if err != nil {
-		return errors.WithMessage(err, "migrate from embed")
-	}
-	defer mig.Close()
-	if err = mig.Up(); err != nil {
-		if err == migrate.ErrNoChange {
-			logger.Info("migrate source from embed success with", "msg", err.Error())
-			return nil
-		}
-		logger.Error("migrate source from embed failed", err)
-		return err
-	}
-	logger.Info("migrate source from embed success")
-	return nil
-}
+// // DoMigrateFromEmbed  do migrate from embed
+// func DoMigrateFromEmbed(user, addr, password, dbname string) (err error) {
+// 	var mig *migrate.Migrate
+// 	var d source.Driver
+// 	if d, err = iofs.New(fs, "migrations"); err != nil {
+// 		return err
+// 	}
+// 	dbURL := fmt.Sprintf(
+// 		"mysql://%s:%s@tcp(%s)/%s?charset=%s&parseTime=true&loc=Local&multiStatements=true&interpolateParams=true",
+// 		user,
+// 		password,
+// 		addr,
+// 		dbname,
+// 		"utf8",
+// 	)
+// 	mig, err = migrate.NewWithSourceInstance("iofs", d, dbURL)
+// 	if err != nil {
+// 		return errors.WithMessage(err, "migrate from embed")
+// 	}
+// 	defer mig.Close()
+// 	if err = mig.Up(); err != nil {
+// 		if err == migrate.ErrNoChange {
+// 			logger.Info("migrate source from embed success with", "msg", err.Error())
+// 			return nil
+// 		}
+// 		logger.Error("migrate source from embed failed", err)
+// 		return err
+// 	}
+// 	logger.Info("migrate source from embed success")
+// 	return nil
+// }
