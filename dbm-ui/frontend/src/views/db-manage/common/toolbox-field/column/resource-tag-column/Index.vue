30c4e30262a31d2884986ea14ea025051e6cd678
chenwenchang <479999519@qq.com>
2026-02-05 11:56:41 +0800

    fix(frontend): redis容量变更方案确定按钮loading #16225

--- dbm-ui/frontend/src/views/db-manage/common/toolbox-field/column/resource-tag-column/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/common/toolbox-field/column/resource-tag-column/Index.vue
@@ -217,6 +217,10 @@
     tagTheme.value = ids.value[0] === DEFAULT_TAG_ID ? 'success' : '';
   };
 
+  watch(modelValue, () => {
+    updateModel(modelValue.value);
+  });
+
   watch(
     tagList,
     () => {
