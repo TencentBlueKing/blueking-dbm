382e378aaf37275c53e7ad131d3dc60761071696
chenwenchang <479999519@qq.com>
2026-02-06 10:46:46 +0800

    fix(frontend): sqlserver数据导出批量下载修复 #15138 # Reviewed, transaction id: 73841

--- dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/flow-info/components/flow-type-inner-flow/StatusSucceeded.vue
+++ dbm-ui/frontend/src/views/ticket-center/common/ticket-detail/components/flow-info/components/flow-type-inner-flow/StatusSucceeded.vue
@@ -26,7 +26,8 @@
       </template>
       <template v-if="ticketDetail.ticket_type === TicketTypes.SQLSERVER_DATA_EXPORT">
         <span> ，</span>
-        <SqlserverDataExportFile :details="data.details as ComponentProps<typeof SqlserverDataExportFile>['details']" />
+        <SqlserverExportDataDownload
+          :details="data.details as ComponentProps<typeof SqlserverExportDataDownload>['details']" />
       </template>
       <template
         v-if="[TicketTypes.MYSQL_DUMP_DATA, TicketTypes.TENDBCLUSTER_DUMP_DATA].includes(ticketDetail.ticket_type)">
@@ -72,7 +73,7 @@
   import MysqlDumpDataDownload from './components/MysqlDumpDataDownload.vue';
   import RedisAnalysisToList from './components/RedisAnalysisToList.vue';
   import RedisKeysExtractFile from './components/RedisKeysExtractFile.vue';
-  import SqlserverDataExportFile from './components/SqlserverDataExportFile.vue';
+  import SqlserverExportDataDownload from './components/SqlserverExportDataDownload.vue';
 
   interface Props {
     data: FlowMode<unknown>;
