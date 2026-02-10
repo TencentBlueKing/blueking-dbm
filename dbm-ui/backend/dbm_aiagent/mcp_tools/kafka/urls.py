052a4dc7d03b8d655c30dfcd8dbe556c8564e5c6
zvictorino <zvictorino@gmail.com>
2026-02-10 11:53:59 +0800

    fix(backend): kafka mcp tool代码回退 close #16301

--- dbm-ui/backend/dbm_aiagent/mcp_tools/kafka/urls.py
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/kafka/urls.py
@@ -11,7 +11,6 @@ specific language governing permissions and limitations under the License.
 from rest_framework.routers import DefaultRouter
 
 from backend.dbm_aiagent.mcp_tools.kafka.views.kafka_bill_mcp import KafkaBillMcpToolsViewSet
-from backend.dbm_aiagent.mcp_tools.kafka.views.kafka_metrics_mcp import KafkaMetricsMcpToolsViewSet
 from backend.dbm_aiagent.mcp_tools.kafka.views.query_meta import KafkaQueryMetaMcpToolsViewSet
 
 routers = DefaultRouter(trailing_slash=True)
@@ -20,7 +19,5 @@ routers = DefaultRouter(trailing_slash=True)
 routers.register(r"", KafkaQueryMetaMcpToolsViewSet, basename="mcp-kafka-query-meta")
 # 与 dbm 交互 创建单据类的 操作
 routers.register(r"", KafkaBillMcpToolsViewSet, basename="mcp-kafka-bill")
-# 监控指标查询
-routers.register(r"", KafkaMetricsMcpToolsViewSet, basename="mcp-kafka-metrics")
 
 urlpatterns = routers.urls
