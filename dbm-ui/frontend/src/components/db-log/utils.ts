d002be62c218139c1148d2f0a41f19f460b3edf9
austinqli <1344583166@qq.com>
2026-02-24 09:19:30 +0800

    feat(frontend): ai日志分析排除掉无用的过程日志 #16297 # Reviewed, transaction id: 74273

---
+++ dbm-ui/frontend/src/components/db-log/utils.ts
@@ -0,0 +1,40 @@
+import { format } from 'date-fns';
+
+export interface NodeLog {
+  levelname: string;
+  message: string;
+  timestamp: number;
+}
+
+export const formatLogData = (data: NodeLog[] = [], isSetColor = true) => {
+  const regex = /^##\[[a-z]+]/;
+  return data.map((item) => {
+    const { levelname, message, timestamp } = item;
+    const time = timestamp ? format(new Date(Number(timestamp)), 'yyyy-MM-dd HH:mm:ss') : '';
+    if (!time && !levelname) {
+      return message;
+    }
+
+    let rowText = regex.test(message)
+      ? message.replace(regex, (match: string) => `${match}[${time} ${levelname}]`)
+      : `[${time} ${levelname}] ${message}`;
+    rowText = rowText.replace(/\n/g, '\r\n');
+    if (!isSetColor) {
+      return rowText;
+    }
+
+    // if (/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} info\]/.test(rowText)) {
+    //   return `\x1b[32m${rowText}\x1b[0m`;
+    // }
+
+    if (/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} warn\]/i.test(rowText)) {
+      return `\x1b[33m${rowText}\x1b[0m`;
+    }
+
+    if (/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} error\]/i.test(rowText)) {
+      return `\x1b[31m${rowText}\x1b[0m`;
+    }
+
+    return rowText;
+  });
+};
