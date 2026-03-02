3909484cee7db80943f243381a4db734bf53a84a
chenwenchang <479999519@qq.com>
2026-03-02 17:20:29 +0800

    fix(frontend): sqlserver库表备份查询集群配置忽略表优化 #16318 # Reviewed, transaction id: 74670

--- dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_BACKUP_DBS/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/sqlserver/SQLSERVER_BACKUP_DBS/Index.vue
@@ -122,7 +122,6 @@
 <script setup lang="ts">
   import type { ComponentProps } from 'vue-component-type-helpers';
   import { useI18n } from 'vue-i18n';
-  import { useRequest } from 'vue-request';
 
   import SqlserverHaModel from '@services/model/sqlserver/sqlserver-ha';
   import { type Sqlserver } from '@services/model/ticket/ticket';
@@ -212,8 +211,6 @@
     }[];
   }>(TicketTypes.SQLSERVER_BACKUP_DBS);
 
-  const { data: ingoreDbsMap } = useRequest(getIgnoreDbs);
-
   const formRef = useTemplateRef('form');
   const editableTableRef = useTemplateRef('editableTable');
   const tableKey = ref(random());
@@ -270,9 +267,12 @@
 
   const isBackupTypeFull = computed(() => formData.backup_type === 'full_backup');
 
-  const handleClusterRequestSuccess = (rowData: IDataRow) => {
+  const handleClusterRequestSuccess = async (rowData: IDataRow) => {
+    const ingoreDbsMap = await getIgnoreDbs({
+      cluster_ids: [rowData.cluster.id],
+    });
     Object.assign(rowData, {
-      ignore_db_list: ingoreDbsMap.value?.[rowData.cluster.id] || [],
+      ignore_db_list: ingoreDbsMap?.[rowData.cluster.id] || [],
     });
   };
 
@@ -294,7 +294,10 @@
     },
   ];
 
-  const handleClusterBatchEdit = (data: SqlserverHaModel[]) => {
+  const handleClusterBatchEdit = async (data: SqlserverHaModel[]) => {
+    const ingoreDbsMap = await getIgnoreDbs({
+      cluster_ids: data.map((item) => item.id),
+    });
     const dataList = data.reduce<IDataRow[]>((acc, item) => {
       if (!clusterMemo.value[item.master_domain]) {
         acc.push(
@@ -304,7 +307,7 @@
               id: item.id,
               master_domain: item.master_domain,
             },
-            ignore_db_list: ingoreDbsMap.value?.[item.id] || [],
+            ignore_db_list: ingoreDbsMap?.[item.id] || [],
           }),
         );
       }
