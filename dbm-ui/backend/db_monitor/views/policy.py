958842ba070643065861414846ce4b2d78c2a63f
huangwentai-wendel <huangwentai96@163.com>
2026-03-16 17:16:42 +0800

    fix(backend): 告警策略二期review问题修改 #16611

--- dbm-ui/backend/db_monitor/views/policy.py
+++ dbm-ui/backend/db_monitor/views/policy.py
@@ -9,7 +9,6 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 import json
-from collections import defaultdict
 
 from django.core.cache import cache
 from django.db.models import CharField, Q, Value
@@ -32,7 +31,6 @@ from backend.db_meta.enums import ClusterType
 from backend.db_meta.models import Cluster, DBModule, ProxyInstance, StorageInstance
 from backend.db_monitor import constants, serializers
 from backend.db_monitor.models import MonitorPolicy
-from backend.db_monitor.utils import flatten_policy_results
 from backend.iam_app.dataclass import ResourceEnum
 from backend.iam_app.dataclass.actions import ActionEnum
 from backend.iam_app.handlers.drf_perm.base import (
@@ -52,17 +50,15 @@ class MonitorPolicyListFilter(filters.FilterSet):
     updater = filters.CharFilter(lookup_expr="exact", label=_("更新人"))
     creator = filters.CharFilter(lookup_expr="creator", label=_("创建人"))
     db_type = filters.CharFilter(lookup_expr="exact", label=_("db类型"))
-    policy_type = filters.CharFilter(lookup_expr="exact", label=_("策略类型"))
     target_keyword = filters.CharFilter(lookup_expr="icontains", label=_("目标关键字检索"))
     target_level = filters.CharFilter(method="filter_target_level", label=_("策略来源"))
     is_enabled = filters.BooleanFilter(label=_("是否启用"))
-    is_cover = filters.BooleanFilter(method="filter_is_cover", label=_("是否覆盖"))
     monitor_policy_ids = filters.CharFilter(method="filter_monitor_policy_id", label=_("监控策略ID列表"))
     bk_biz_id = filters.NumberFilter(method="filter_bk_biz_id", label=_("业务ID"))
 
     # 如果只需要开区间，可以简化配置，这里的注释留作学习示例
     # (create_at_after, create_at_before): create_at_after=2023-09-05 14:29:00&create_at_before=2023-09-05 14:30:05
-    # create_at = filters.DateTimeFromToRangeFilter("create_at")
+    create_at = filters.DateTimeFromToRangeFilter("create_at")
     # 拆分rangeFilter，支持两端闭区间
     create_at_before = filters.DateTimeFilter(field_name="create_at", lookup_expr="lte")
     create_at_after = filters.DateTimeFilter(field_name="create_at", lookup_expr="gte")
@@ -87,13 +83,6 @@ class MonitorPolicyListFilter(filters.FilterSet):
         """默认包含平台告警策略"""
         return queryset.filter(bk_biz_id__in=[PLAT_BIZ_ID, value])
 
-    def filter_is_cover(self, queryset, name, value):
-        """过滤已有业务策略的全局策略"""
-        if value:
-            parent_ids = queryset.filter(target_level="appid").values_list("parent_id", flat=True)
-            return queryset.exclude(id__in=list(set(parent_ids)), parent_id=0)
-        return queryset
-
     def filter_target_level(self, queryset, name, value):
         """策略来源"""
         return queryset.filter(target_level__in=value.split(","))
@@ -112,8 +101,6 @@ class MonitorPolicyListFilter(filters.FilterSet):
             "is_enabled",
             "target_keyword",
             "notify_groups",
-            "policy_type",
-            "is_cover",
             "target_level",
         ]
 
@@ -209,96 +196,12 @@ class MonitorPolicyViewSet(AuditedModelViewSet):
     )
     @Permission.decorator_permission_field(
         id_field=lambda d: d["id"],
-        data_field=flatten_policy_results,
+        data_field=lambda d: d["results"],
         actions=ActionEnum.get_actions_by_resource(ResourceEnum.MONITOR_POLICY.id),
         resource_meta=ResourceEnum.MONITOR_POLICY,
     )
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        if request.query_params.get("bk_biz_id") != "0":
-            # 父类id既全局策略id
-            parent_ids = set()
-            # 业务策略id
-            app_ids = set()
-            # 除业务和全局策略之外的id
-            child_ids = []
-            # 全局策略和业务策略的映射
-            parent_app_map = defaultdict(list)
-            # 全局策略和子策略的映射
-            parent_child_map = defaultdict(list)
-            # 最后的父策略和子策略的映射
-            last_parent_child_map = defaultdict(list)
-
-            result = queryset.values("id", "parent_id", "target_level")
-            for res in result:
-                id, parent_id, target_level = res["id"], res["parent_id"], res["target_level"]
-                parent_ids.add(id if parent_id == 0 else parent_id)
-                # 全局策略可以跳过，不需要记录父策略和子策略的关系
-                if parent_id == 0:
-                    continue
-                if target_level == constants.TargetLevel.APP:
-                    app_ids.add(id)
-                    parent_app_map[parent_id].append(id)
-                elif target_level not in [constants.TargetLevel.APP, constants.TargetLevel.PLATFORM]:
-                    child_ids.append(id)
-                    parent_child_map[parent_id].append(id)
-
-            # 拿到未查到的业务策略，用来覆盖全局策略
-            missing_parent_ids = [pid for pid in parent_child_map if pid not in parent_app_map]
-            biz_policies = MonitorPolicy.objects.filter(
-                parent_id__in=missing_parent_ids,
-                target_level=constants.TargetLevel.APP,
-                bk_biz_id=request.query_params["bk_biz_id"],
-            ).values("id", "parent_id")
-            biz_policy_map = {p["parent_id"]: p["id"] for p in biz_policies}
-
-            for parent_id in parent_child_map:
-                # 当又有全局策略又有业务策略时， 不返回全局策略
-                if parent_id in parent_app_map:
-                    # 可能会存在一个全局策略有多个业务策略的非标行为， 所以取第一个就行
-                    last_parent_id = parent_app_map[parent_id][0]
-                    last_parent_child_map[last_parent_id] = parent_child_map[parent_id]
-                    app_ids.remove(last_parent_id)
-                # 如果没拿到对应的业务策略则需要获取全局策略对应的业务策略
-                elif parent_id in biz_policy_map:
-                    last_parent_child_map[biz_policy_map[parent_id]] = parent_child_map[parent_id]
-                else:
-                    last_parent_child_map[parent_id] = parent_child_map[parent_id]
-                parent_ids.remove(parent_id)
-
-            # 最后再把存在业务策略的全局策略去掉
-            for parent_id in parent_app_map:
-                if parent_id in parent_ids:
-                    parent_ids.remove(parent_id)
-
-            first_level_ids = list(parent_ids) + list(app_ids) + list(last_parent_child_map.keys())
-            # 拿到所有的需要查询的策略的id
-            need_ids = first_level_ids + child_ids
-            queryset = MonitorPolicy.objects.filter(id__in=need_ids)
-            results = []
-            res_data = self.get_serializer(queryset, many=True).data
-
-            def _get_child_data(all_data, ids):
-                child_data = []
-                for data in all_data:
-                    if data["id"] in ids:
-                        child_data.append(data)
-                return child_data
-
-            for res in res_data:
-                if res["id"] in first_level_ids:
-                    res["child"] = (
-                        _get_child_data(res_data, last_parent_child_map[res["id"]])
-                        if last_parent_child_map[res["id"]]
-                        else []
-                    )
-                    results.append(res)
-
-            return Response({"results": results, "count": len(res_data)})
-
-        page = self.paginate_queryset(queryset)
-        serializer = self.get_serializer(page, many=True)
-        return self.get_paginated_response(serializer.data)
+        return super().list(request, *args, **kwargs)
 
     @common_swagger_auto_schema(
         operation_summary=_("启用策略"),
