ffca34261d682821e0b89a8396028278f15b8428
chenwenchang <479999519@qq.com>
2026-03-19 18:24:15 +0800

    fix(frontend): mysql回档_单据克隆拉取最新备份记录 #16680

--- dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_FIXPOINT_EXIST_CLUSTER/components/time-backup-record-column/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/mysql/MYSQL_FIXPOINT_EXIST_CLUSTER/components/time-backup-record-column/Index.vue
@@ -167,6 +167,10 @@
 
   type Emits = (e: 'change') => void;
 
+  interface Exposes {
+    flush: () => void;
+  }
+
   const props = defineProps<Props>();
 
   const emits = defineEmits<Emits>();
@@ -272,6 +276,37 @@
 
   watch(backupTime, () => {
     emits('change');
+  });
+
+  defineExpose<Exposes>({
+    flush() {
+      setTimeout(() => {
+        // 需要更新的行索引
+        const targetRow: number[] = [];
+        const taskList: Promise<BackupLogRecordModel>[] = [];
+        tableData.value.forEach((rowData, rowIndex) => {
+          if (rowData.cluster.id) {
+            targetRow.push(rowIndex);
+            taskList.push(
+              queryLatestTimeBackupLog({
+                backup_source: props.backupSource,
+                bk_biz_id: window.PROJECT_CONFIG.BIZ_ID,
+                cluster_id: rowData.cluster.id,
+                is_full_backup: true,
+                latest_time: formatDateToUTC(rowData.backupTime),
+              }),
+            );
+          }
+        });
+
+        Promise.all(taskList).then((res) => {
+          res.forEach((data, index) => {
+            tableData.value[targetRow[index]].backupRecord = data;
+            tableData.value[targetRow[index]].backupTime = data.backup_time;
+          });
+        });
+      }, 1000);
+    },
   });
 </script>
 <style lang="less" scoped>
