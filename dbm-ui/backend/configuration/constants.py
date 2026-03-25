d6bc54d3c9c29aa508a411da6d43652e1b5c54c5
huangwentai-wendel <huangwentai96@163.com>
2026-03-25 11:31:48 +0800

    feat(backend): 每日个人代办提醒 #16780

--- dbm-ui/backend/configuration/constants.py
+++ dbm-ui/backend/configuration/constants.py
@@ -189,6 +189,10 @@ class SystemSettingsEnum(StrStructuredEnum):
     WINDOWS_HOST_TO_RECYCLE_SWITCH = EnumField("WINDOWS_HOST_TO_RECYCLE_SWITCH", _("判断windows主机开关"))
     # AIDEV相关配置
     AI_CODE_SCENE_MAP = EnumField("AI_CODE_SCENE_MAP", _("智能体code场景映射关系表"))
+    # 每日代办提醒配置
+    DBM_DAILY_TODO_REMIND = EnumField("DBM_DAILY_TODO_REMIND", _("每日代办提醒配置"))
+    # 代办类型和用户映射信息
+    DBM_USER_TODO_TYPE_MAP = EnumField("DBM_USER_TODO_TYPE_MAP", _("代办类型和用户映射信息"))
 
 
 class BizSettingsEnum(StrStructuredEnum):
@@ -395,4 +399,26 @@ ACCOUNT_RULES_MAP = {
         "gcs_spider",
         "sync",
     ],
+}
+
+DAILY_TODO_REMIND_DEFAULT = {
+    "is_enable": False,
+    "remind_time": {
+        "minute": "0",
+        "hour": "9",
+    },
+    "notice": [],
+}
+
+DBM_USER_TODO_TYPE_MAP_DEFAULT = {
+    "types": {
+        "ticket_todo": _("单据代办"),
+        "inspect_todo": _("巡检代办"),
+        "cluster_disable_todo": _("集群下架代办"),
+        "host_todo": _("主机处理待办"),
+        "alarm_todo": _("告警事件待办"),
+        "risk_memo_todo": _("风险备忘录"),
+    },
+    "ordinary": ["ticket_todo", "cluster_disable_todo"],
+    "dba": ["ticket_todo", "inspect_todo", "cluster_disable_todo", "host_todo", "alarm_todo", "risk_memo_todo"],
 }
---
+++ dbm-ui/backend/configuration/views/todo_remind.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from celery.schedules import crontab
+from django.utils.translation import gettext_lazy as _
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from backend.bk_web import viewsets
+from backend.bk_web.swagger import common_swagger_auto_schema
+from backend.configuration.constants import DAILY_TODO_REMIND_DEFAULT, SystemSettingsEnum
+from backend.configuration.models.system import SystemSettings
+from backend.configuration.serializers import TodoRemindSerializer
+from backend.configuration.tasks.todo_remind_tasks import send_todo_remind
+from backend.db_periodic_task.constants import PeriodicTaskType
+from backend.db_periodic_task.models import DBPeriodicTask
+
+SWAGGER_TAG = _("每日代办提醒")
+
+
+class TodoRemindViewSet(viewsets.SystemViewSet):
+    serializer_class = TodoRemindSerializer
+    default_permission_class = []
+
+    @common_swagger_auto_schema(operation_summary=_("查询代办提醒配置"), tags=[SWAGGER_TAG])
+    @action(methods=["GET"], detail=False)
+    def get_todo_remind_conf(self, request, *args, **kwargs):
+        todo_remind_conf = SystemSettings.get_setting_value(
+            SystemSettingsEnum.DBM_DAILY_TODO_REMIND, default=DAILY_TODO_REMIND_DEFAULT
+        )
+        return Response(todo_remind_conf)
+
+    @common_swagger_auto_schema(operation_summary=_("新增代办提醒配置"), tags=[SWAGGER_TAG])
+    @action(methods=["POST"], detail=False, serializer_class=TodoRemindSerializer)
+    def update_todo_remind_conf(self, request, *args, **kwargs):
+        username = request.user.username
+
+        validated_data = self.params_validate(self.get_serializer_class())
+        SystemSettings.objects.update_or_create(
+            defaults={
+                "value": validated_data,
+                "type": "dict",
+                "creator": username,
+                "updater": username,
+            },
+            key=SystemSettingsEnum.DBM_DAILY_TODO_REMIND.value,
+        )
+        # 获取轮值通知定时任务
+        notice_task = DBPeriodicTask.create_or_update_periodic_task(
+            name=f"dbm_periodic_{send_todo_remind.__name__}",
+            task=f"{send_todo_remind.__module__}.{send_todo_remind.__name__}",
+            run_every=crontab(**validated_data["remind_time"]),
+            task_type=PeriodicTaskType.LOCAL.value,
+        )
+        # 打开/关闭 定时任务
+        notice_task.task.enabled = validated_data["is_enable"]
+        notice_task.task.save(update_fields=["enabled"])
+        return Response()
