597412d58b43f9a0b713e5ba55d7c03a8058d9a1
3octaves <873551943@qq.com>
2026-04-02 11:38:23 +0800

    feat(frontend): 告警策略管理体验优化 #16442

--- dbm-ui/frontend/src/views/monitor-alarm/alarm-events/Index.vue
+++ dbm-ui/frontend/src/views/monitor-alarm/alarm-events/Index.vue
@@ -130,21 +130,11 @@
         show-overflow="tooltip"
         :width="240">
         <template #default="{ data }: { data: RowData }">
-          <TextOverflowLayout>
-            <RouterLink
-              v-if="data.dbm_policy.name"
-              target="_blank"
-              :to="{
-                name: isGlobalPage || isTodoPage ? 'PlatGlobalStrategy' : 'monitorStrategy',
-                query: {
-                  id: data.dbm_policy.id,
-                  db_type: data.dbm_policy.db_type,
-                },
-              }">
-              {{ data.dbm_policy.name }}
-            </RouterLink>
-            <span v-else>--</span>
-          </TextOverflowLayout>
+          <ToAlarmPolicy
+            v-if="data.dbm_policy.id"
+            :data="data.dbm_policy"
+            :is-platform="isTodoPage || isGlobalPage" />
+          <span v-else>--</span>
         </template>
       </BkTableColumn>
       <BkTableColumn
@@ -252,12 +242,12 @@
   import { useGlobalBizs, useSystemEnviron } from '@stores';
 
   import DbTable from '@components/db-table/index.vue';
-  import TextOverflowLayout from '@components/text-overflow-layout/Index.vue';
 
   import { exportExcelFile } from '@utils';
 
   import SearchOperation from './components/SearchOperation.vue';
   import ShieldAlarms from './components/ShieldAlarms.vue';
+  import ToAlarmPolicy from './components/ToAlarmPolicy.vue';
 
   export type RowData = ServiceReturnType<typeof getAlarmEventsList>['results'][number];
 
