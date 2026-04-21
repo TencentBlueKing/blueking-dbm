0c6810c074cc1cad961859ea7b9b1a88f414390b
huangwentai-wendel <huangwentai96@163.com>
2026-04-21 10:51:33 +0800

    feat(backend): mongodb版本升级 #17174

--- dbm-ui/backend/ticket/constants.py
+++ dbm-ui/backend/ticket/constants.py
@@ -654,6 +654,7 @@ class TicketType(StrStructuredEnum):
     MONGODB_INSTANCE_DEINSTALL = TicketEnumField("MONGODB_INSTANCE_DEINSTALL", _("MongoDB 实例下架"), _("集群维护"))
     MONGODB_INSTANCE_FIX_STATUS = TicketEnumField("MONGODB_INSTANCE_FIX_STATUS", _("MongoDB 节点状态修复"), _("集群维护"))
     MONGODB_CLUSTER_STANDARDIZE = TicketEnumField("MONGODB_CLUSTER_STANDARDIZE", _("MongoDB 集群标准化"))
+    MONGODB_UPGRADE_VERSION = TicketEnumField("MONGODB_UPGRADE_VERSION", _("MongoDB 版本升级"))
 
     # 云区域组件
     CLOUD_SERVICE_APPLY = EnumField("CLOUD_SERVICE_APPLY", _("云区域服务部署"))
