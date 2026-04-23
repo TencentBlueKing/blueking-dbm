34e006ce295c5e02b299721feaf0268d5413a52a
iSecloud <869820505@qq.com>
2026-04-23 20:56:39 +0800

    fix(backend): 修复stats路由问题 #17334

--- dbm-ui/backend/db_report/database_router.py
+++ dbm-ui/backend/db_report/database_router.py
@@ -17,12 +17,12 @@ class ReportRouter:
     def db_for_read(self, model: models.Model, **hints):
         if model._meta.app_label in self.route_app_labels:
             return "report_db"
-        return "default"
+        return None
 
     def db_for_write(self, model: models.Model, **hints):
         if model._meta.app_label in self.route_app_labels:
             return "report_db"
-        return "default"
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
@@ -36,12 +36,12 @@ class StatsRouter:
     def db_for_read(self, model: models.Model, **hints):
         if model._meta.app_label in self.route_app_labels:
             return "stats_db"
-        return "default"
+        return None
 
     def db_for_write(self, model: models.Model, **hints):
         if model._meta.app_label in self.route_app_labels:
             return "stats_db"
-        return "default"
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
