ac7b1e99b0adbb9b7a093468ee4df9229bfbe847
austinqli <1344583166@qq.com>
2026-04-24 10:47:20 +0800

    feat(frontend): 新版版本包管理 #14457

---
+++ dbm-ui/frontend/src/views/version-files/v2/components/list/components/sub-version-list/Index.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="sub-version-list-main">
+    <div
+      v-if="isReleaseEmpty"
+      class="release-empty-main">
+      <BkException
+        :description="t('请先在左侧添加发行版')"
+        type="empty" />
+    </div>
+    <template v-else>
+      <div class="title-operate-main">
+        <div class="title-operate-left">
+          <template v-if="dbType === DBTypes.MYSQL">
+            <div class="main-title">{{ releaseVersion?.name }}（{{ dbVersionListCount }}）</div>
+            <BkTag style="margin-right: auto">{{ t('发行版') }}</BkTag>
+          </template>
+          <BkButton
+            theme="primary"
+            @click="handleAddVersion">
+            {{ t('添加版本') }}
+          </BkButton>
+        </div>
+        <DbQuickSearch
+          :key="renderSearchKey"
+          v-model="searchValue"
+          :data="searchSelectData"
+          :placeholder="t('搜索版本号，版本名，操作系统限制，版本文件，是否启用，描述，更新人')"
+          style="width: 670px"
+          unique-select
+          value-split-code=","
+          @change="handleSearchChange" />
+      </div>
+      <div
+        v-if="versionSeriesList && versionSeriesList.length > 0"
+        ref="versionsListRef"
+        class="versions-list-main">
+        <TableList
+          ref="subVersionRefs"
+          :version-series-list="versionSeriesList"
+          @add-new-version="handleAddNewDbVersion"
+          @delete-version-series="handleDeleteVersionSeries"
+          @edit-db-version="(data) => handleEditDbVersion(data)"
+          @filter-value-change="handleFilterValueChange"
+          @list-change="handleTableListChange"
+          @refresh-release-list="() => emits('refreshReleaseList')" />
+      </div>
+      <div v-else>
+        <BkException
+          class="release-empty-main"
+          type="empty">
+          <span>{{ t('暂无版本') }}</span>
+          <span class="ml-4 mr-4">,</span>
+          <BkButton
+            size="small"
+            text
+            theme="primary"
+            @click="handleAddVersion">
+            {{ t('立即添加') }}
+          </BkButton>
+        </BkException>
+      </div>
+    </template>
+  </div>
+  <EditVersion
+    v-model:is-show="isShowEditVersion"
+    :db-type="dbType"
+    :db-version="currentDbVersion"
+    :is-edit="isEditVersion"
+    :pkg-label="pkgLabel"
+    :pkg-type="pkgType"
+    :release-label="releaseLabel"
+    :release-version="releaseVersion"
+    :version-series-id="currentVersionSeriesId"
+    @success="handleEditVersionSuccess" />
+</template>
+<script setup lang="ts">
+  import _ from 'lodash';
+  import { useI18n } from 'vue-i18n';
+  import { useRequest } from 'vue-request';
+
+  import DbVersionModel from '@services/model/version-file/db-version';
+  import ReleaseVersionModel from '@services/model/version-file/release-version';
+  import { getVersionSeriesList } from '@services/source/version';
+
+  import { DBTypes } from '@common/const';
+
+  // import ScrollFaker from '@components/scroll-faker/Index.vue';
+  import EditVersion from './components/edit-version/Index.vue';
+  import TableList from './components/table-list/Index.vue';
+  // import SubVersion from './components/sub-version/Index.vue';
+  import useSearch from './hooks/useSearch';
+
+  interface Props {
+    dbType: string;
+    pkgLabelMap: Record<string, string>;
+    pkgType: string;
+    releaseVersion?: ReleaseVersionModel;
+  }
+
+  type Emits = (e: 'refreshReleaseList') => void;
+
+  interface Exposes {
+    clearFilter: () => void;
+    showReleaseEmpty: (isShow: boolean) => void;
+  }
+
+  type VersionSeriesModel = ServiceReturnType<typeof getVersionSeriesList>[number];
+
+  const props = withDefaults(defineProps<Props>(), {
+    releaseVersion: undefined,
+  });
+  const emits = defineEmits<Emits>();
+
+  const { t } = useI18n();
+  const { searchSelectData, searchValue } = useSearch();
+
+  const subVersionRefs = ref<InstanceType<typeof TableList>>();
+  const renderSearchKey = ref(0);
+  const isShowEditVersion = ref(false);
+  const isEditVersion = ref(false);
+  const isReleaseEmpty = ref(false);
+  const currentVersionSeriesId = ref(0);
+  const dbVersionListCount = ref(0);
+  const currentDbVersion = ref<DbVersionModel>();
+
+  const releaseLabel = computed(() =>
+    props.releaseVersion?.name ? t('发行版：x', { x: props.releaseVersion.name }) : '',
+  );
+  const pkgLabel = computed(() => props.pkgLabelMap[props.releaseVersion?.pkg_type || '']);
+
+  const { data: versionSeriesList, run: runGetVersionSeriesList } = useRequest(getVersionSeriesList, {
+    manual: true,
+  });
+
+  const fetchVersionSeriesList = () => {
+    runGetVersionSeriesList({
+      distribution: props.releaseVersion!.id,
+    });
+  };
+
+  watch(
+    () => [props.dbType, props.pkgType],
+    () => {
+      versionSeriesList.value = [];
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  watch(
+    () => props.releaseVersion,
+    () => {
+      if (props.releaseVersion) {
+        fetchVersionSeriesList();
+      }
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  const handleDeleteVersionSeries = () => {
+    fetchVersionSeriesList();
+    emits('refreshReleaseList');
+  };
+
+  const handleTableListChange = (count: number) => {
+    dbVersionListCount.value = count;
+  };
+
+  const handleSearchChange = (value: Record<string, any>) => {
+    const filterValue = _.cloneDeep(value);
+    Object.keys(value).forEach((key) => {
+      if (key === 'enable') {
+        if (value[key].includes(',')) {
+          filterValue[key] = filterValue[key].split(',').map((item: string) => item === 'true');
+        } else {
+          filterValue[key] = [filterValue[key] === 'true'];
+        }
+      } else {
+        filterValue[key] = filterValue[key].trim();
+      }
+    });
+    subVersionRefs.value?.setFilterValue(value);
+  };
+
+  const handleFilterValueChange = (value: Record<string, any>) => {
+    const filterValue = _.cloneDeep(value);
+
+    Object.keys(filterValue).forEach((key) => {
+      const itemValue = filterValue[key];
+      if (Array.isArray(itemValue)) {
+        filterValue[key] = itemValue.map(String).join(',');
+      } else {
+        filterValue[key] = itemValue.trim();
+      }
+    });
+    searchValue.value = filterValue;
+  };
+
+  const handleEditDbVersion = (data: DbVersionModel) => {
+    currentDbVersion.value = data;
+    currentVersionSeriesId.value = data.version_series;
+    isShowEditVersion.value = true;
+    isEditVersion.value = true;
+  };
+
+  const handleAddNewDbVersion = (versionSeries: VersionSeriesModel) => {
+    currentVersionSeriesId.value = versionSeries.id;
+    isShowEditVersion.value = true;
+    isEditVersion.value = false;
+    currentDbVersion.value = undefined;
+    emits('refreshReleaseList');
+  };
+
+  const handleAddVersion = () => {
+    currentVersionSeriesId.value = 0;
+    isShowEditVersion.value = true;
+    isEditVersion.value = false;
+    currentDbVersion.value = undefined;
+  };
+
+  const handleEditVersionSuccess = (versionSeriesId: number) => {
+    const index = versionSeriesList.value!.findIndex((item) => item.id === versionSeriesId);
+    if (index !== -1) {
+      subVersionRefs.value!.refresh();
+    } else {
+      emits('refreshReleaseList');
+      fetchVersionSeriesList();
+    }
+  };
+
+  defineExpose<Exposes>({
+    clearFilter: () => {
+      subVersionRefs.value?.clearFilter();
+    },
+    showReleaseEmpty: (isShow: boolean) => {
+      isReleaseEmpty.value = isShow;
+    },
+  });
+</script>
+<style lang="less">
+  .sub-version-list-main {
+    display: flex;
+    padding: 0 16px;
+    flex: 1;
+    flex-direction: column;
+    overflow: hidden;
+
+    .release-empty-main {
+      margin-top: 60px;
+
+      .bk-exception-description {
+        margin-top: 0;
+      }
+
+      .bk-exception-footer {
+        margin-top: 0;
+      }
+    }
+
+    .versions-list-main {
+      display: flex;
+      overflow-y: auto;
+      flex: 1;
+      flex-direction: column;
+    }
+
+    .title-operate-main {
+      display: flex;
+      height: 32px;
+      align-items: center;
+      margin-bottom: 20px;
+
+      .title-operate-left {
+        flex: 1;
+        display: flex;
+        margin-right: 8px;
+        align-items: center;
+        justify-content: space-between;
+
+        .main-title {
+          font-size: 16px;
+          font-weight: 700;
+          color: #313238;
+        }
+      }
+    }
+  }
+</style>
