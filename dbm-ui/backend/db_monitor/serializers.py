445160f379cd7dfe278adbe9b43be2656e330335
huangwentai-wendel <huangwentai96@163.com>
2026-04-27 19:16:21 +0800

    fix(backend): 告警策略批量追加告警组添加过滤相同告警组功能 #17395

--- dbm-ui/backend/db_monitor/serializers.py
+++ dbm-ui/backend/db_monitor/serializers.py
@@ -29,6 +29,7 @@ from backend.db_monitor.constants import (
     DetectAlgEnum,
     OperatorEnum,
     TargetLevel,
+    UpdateNotifyGroupType,
 )
 from backend.db_monitor.exceptions import AutofixException
 from backend.db_monitor.mock_data import CALLBACK_REQUEST
@@ -269,6 +270,11 @@ class BatchUpdateMonitorPolicyNotifySerializer(serializers.Serializer):
 
     notify_groups = serializers.ListSerializer(help_text=_("告警组ID列表"), child=NoticeGroupInfoSerializer())
     bk_biz_id = serializers.IntegerField(help_text=_("业务ID"))
+    type = serializers.ChoiceField(
+        help_text=_("修改告警组类型"),
+        default=UpdateNotifyGroupType.APPEND,
+        choices=UpdateNotifyGroupType.get_choices(),
+    )
 
 
 class MonitorPolicyCloneSerializer(MonitorPolicyUpdateSerializer):
