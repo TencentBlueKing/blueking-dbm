ef04ae06b24e59c25dfed00fb1530d9237fd11b9
seanlook <seanlook7@gmail.com>
2026-04-28 12:22:39 +0800

    feat(backend): add report migrations #17411

--- dbm-ui/backend/db_report/models/__init__.py
+++ dbm-ui/backend/db_report/models/__init__.py
@@ -24,6 +24,7 @@ from .monogdb_check_report import MongodbBackupCheckReport
 from .mysql_backup_progress import MysqlBackupProgress
 from .mysql_db_table_size import MysqlDbTableSize
 from .mysql_inspect_ignore import MysqlInspectIgnore
+from .mysql_slowlog_ai_analysis import MysqlSlowlogAiAnalysis
 from .mysql_slowlog_detail import MysqlSlowlogDetail
 from .mysqlbackup_check_report import MysqlBackupCheckReport
 from .redis_check_report import RedisCheckReport
