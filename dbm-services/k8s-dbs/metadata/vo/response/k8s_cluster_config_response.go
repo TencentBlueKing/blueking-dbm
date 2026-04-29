02b23431a9e08ab28bdf4243f90cdb0b4e335ec3
groobyming <lidanzhuang001@gmail.com>
2026-04-29 16:38:20 +0800

    refactor(k8s-dbs): 检索相关接口移除 k8s 敏感信息 #17439

--- dbm-services/k8s-dbs/metadata/vo/response/k8s_cluster_config_response.go
+++ dbm-services/k8s-dbs/metadata/vo/response/k8s_cluster_config_response.go
@@ -19,30 +19,16 @@ limitations under the License.
 
 package response
 
-import commtypes "k8s-dbs/common/types"
-
 // K8sClusterConfigResponse response vo 定义
 type K8sClusterConfigResponse struct {
-	ID           uint64                 `json:"id"`
-	ClusterName  string                 `json:"clusterName"`
-	APIServerURL string                 `json:"apiServerUrl"`
-	CACert       string                 `json:"caCert"`
-	ClientCert   string                 `json:"clientCert"`
-	ClientKey    string                 `json:"clientKey"`
-	Token        string                 `json:"token"`
-	Username     string                 `json:"username"`
-	Password     string                 `json:"password"`
-	IsPublic     bool                   `json:"isPublic"`
-	RegionName   string                 `json:"regionName"`
-	RegionCode   string                 `json:"regionCode"`
-	VpcID        string                 `json:"vpcID"`
-	Provider     string                 `json:"provider"`
-	Active       bool                   `json:"active"`
-	Description  string                 `json:"description"`
-	CreatedBy    string                 `json:"createdBy"`
-	CreatedAt    commtypes.JSONDatetime `json:"createdAt"`
-	UpdatedBy    string                 `json:"updatedBy"`
-	UpdatedAt    commtypes.JSONDatetime `json:"updatedAt"`
+	ID          uint64 `json:"id"`
+	ClusterName string `json:"clusterName"`
+	IsPublic    bool   `json:"isPublic"`
+	RegionName  string `json:"regionName"`
+	RegionCode  string `json:"regionCode"`
+	VpcID       string `json:"vpcID"`
+	Provider    string `json:"provider"`
+	Description string `json:"description"`
 }
 
 // RegionResp 区域信息响应结构体
