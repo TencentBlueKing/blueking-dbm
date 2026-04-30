fd9360a8b30c2d5b77b9c4a25f41c811fe4a559d
lyjpy <jay0613@163.com>
2026-04-30 10:24:59 +0800

    feat(backend): surrealDB多模型图数据库,集群部署 #17505

---
+++ dbm-ui/backend/ticket/builders/surrealdb/k8s_surrealdb_restart.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from backend.flow.engine.controller.surrealdb_temp import SurrealDBController
+from backend.iam_app.dataclass.actions import ActionEnum
+from backend.ticket import builders
+from backend.ticket.builders.common.base import TicketBaseValidateSerializerMixin
+from backend.ticket.builders.surrealdb.base import BaseSurrealDBTicketFlowBuilder
+from backend.ticket.constants import TicketType
+
+
+class K8sSurrealDBRestartDetailSerializer(TicketBaseValidateSerializerMixin, serializers.Serializer):
+    cluster_id = serializers.CharField(help_text=_("集群ID"))
+
+
+class K8sSurrealDBRestartFlowParamBuilder(builders.FlowParamBuilder):
+    controller = SurrealDBController.placeholder
+
+
+@builders.BuilderFactory.register(TicketType.K8S_SURREALDB_RESTART, iam=ActionEnum.K8S_SURREAL_RESTART)
+class K8sSurrealDBRestartFlowBuilder(BaseSurrealDBTicketFlowBuilder):
+    serializer = K8sSurrealDBRestartDetailSerializer
+    inner_flow_builder = K8sSurrealDBRestartFlowParamBuilder
+    inner_flow_name = _("Surrealdb集群重启")
