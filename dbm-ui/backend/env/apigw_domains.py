53972008337d064c12543883592398c328c5b35c
huangwentai-wendel <huangwentai96@163.com>
2026-05-08 10:35:56 +0800

    feat(backend): surrealDB多模型图数据库 #17473

--- dbm-ui/backend/env/apigw_domains.py
+++ dbm-ui/backend/env/apigw_domains.py
@@ -42,6 +42,7 @@ PARTITION_APIGW_DOMAIN = get_type_env(key="PARTITION_APIGW_DOMAIN", _type=str, d
 MYSQL_SIMULATION_DOMAIN = get_type_env(key="MYSQL_SIMULATION_DOMAIN", _type=str, default="http://bk-dbm-dbsimulation")
 HADB_APIGW_DOMAIN = get_type_env(key="HADB_APIGW_DOMAIN", _type=str, default="http://bk-dbm-hadb-api")
 DBRESOURCE_APIGW_DOMAIN = get_type_env(key="DBRESOURCE_APIGW_DOMAIN", _type=str, default="http://bk-dbm-db-resource")
+KUBERNETES_APIGW_DOMAIN = get_type_env(key="KUBERNETES_APIGW_DOMAIN", _type=str, default="http://bk-dbm-kubernetes")
 BACKUP_APIGW_DOMAIN = get_type_env(key="BACKUP_APIGW_DOMAIN", _type=str, default="http://bk-dbm-backup-server:7000")
 SLOW_QUERY_PARSER_DOMAIN = get_type_env(
     key="SLOW_QUERY_PARSER_DOMAIN", _type=str, default="http://bk-dbm-slow-query-parser-service"
