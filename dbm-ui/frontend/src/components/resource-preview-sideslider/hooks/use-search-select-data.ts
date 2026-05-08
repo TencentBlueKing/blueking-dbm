21af3f9dc5cba382d0cb820b8ff716ec2119d74a
chenwenchang <479999519@qq.com>
2026-05-08 15:17:49 +0800

    fix(frontend): 修复资源预览表头筛选 #14958

---
+++ dbm-ui/frontend/src/components/resource-preview-sideslider/hooks/use-search-select-data.ts
@@ -0,0 +1,76 @@
+import { useI18n } from 'vue-i18n';
+
+import { fetchDeviceClass, getResourceOsName } from '@services/source/dbresourceResource';
+import { getCommonCities, getInfrasSubzonesByCity } from '@services/source/infras';
+
+import MultipleSelect from '@components/db-table/components/MultipleSelect.vue';
+
+export default () => {
+  const { t } = useI18n();
+
+  return {
+    city: {
+      component: markRaw(MultipleSelect),
+      name: t('地域'),
+      props: {
+        remoteMethod: () => {
+          return getCommonCities().then((data) =>
+            data.common.map((item) => ({
+              label: item.city_name,
+              value: item.city_code,
+            })),
+          );
+        },
+      },
+      showConfirmAndReset: true,
+    },
+    device_class: {
+      component: markRaw(MultipleSelect),
+      name: t('机型'),
+      props: {
+        remoteMethod: () => {
+          return fetchDeviceClass({
+            limit: -1,
+            offset: 0,
+          }).then((data) =>
+            data.results.map((item) => ({
+              label: item.device_type,
+              value: item.id,
+            })),
+          );
+        },
+      },
+      showConfirmAndReset: true,
+    },
+    os_name: {
+      component: markRaw(MultipleSelect),
+      name: t('操作系统名称'),
+      props: {
+        remoteMethod: () => {
+          return getResourceOsName().then((data) =>
+            data.os_names.map((item) => ({
+              label: item.text,
+              value: item.value,
+            })),
+          );
+        },
+      },
+      showConfirmAndReset: true,
+    },
+    suz_zone: {
+      component: markRaw(MultipleSelect),
+      name: t('园区'),
+      props: {
+        remoteMethod: () => {
+          return getInfrasSubzonesByCity().then((data) =>
+            data.map((item) => ({
+              label: item.bk_sub_zone,
+              value: item.bk_sub_zone_id,
+            })),
+          );
+        },
+      },
+      showConfirmAndReset: true,
+    },
+  };
+};
