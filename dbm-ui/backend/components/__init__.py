a0c78f49fa9efa23a5f333f0249892ccb09704d5
WengChangWei <13106003@qq.com>
2026-05-22 17:18:59 +0800

    feat(backend): qdrant部署协议 #18075

--- dbm-ui/backend/components/__init__.py
+++ dbm-ui/backend/components/__init__.py
@@ -22,6 +22,7 @@ from .dbconfig.client import DBConfigApi
 from .dns.client import DnsApi
 from .itsm.client import ItsmApi
 from .job.client import JobApi
+from .kubernetes.client import KubernetesApi
 from .mysql_priv_manager.client import DBPrivManagerApi
 from .usermanage.client import UserManagerApi
 
@@ -46,6 +47,7 @@ __all__ = [
     "BKMonitorV3Api",
     "NameServiceApi",
     "BKBaseApi",
+    "KubernetesApi",
 ]
 
 
