52bdb686c00f6f754e185c71846cf426613d734a
xfwduke <xfwduke@gmail.com>
2026-05-27 09:30:18 +0800

    feat(mysql): modify-mysql-cluster-spec #17821

--- dbm-ui/backend/dbm_aiagent/mcp_tools/mysql/views/mysql_sensitive_mcp.py
+++ dbm-ui/backend/dbm_aiagent/mcp_tools/mysql/views/mysql_sensitive_mcp.py
@@ -12,11 +12,19 @@ from django.utils.translation import gettext as _
 from rest_framework.response import Response
 
 from backend.components import DRSApi
+from backend.db_meta.enums import ClusterType, InstanceRole, MachineType, TenDBClusterSpiderRole
+from backend.db_meta.models import Cluster, Spec
 from backend.dbm_aiagent.mcp_tools.common.auth_parser.base import auth_parse_instances
 from backend.dbm_aiagent.mcp_tools.constants import DBMMCPTags, DBMMcpTools
 from backend.dbm_aiagent.mcp_tools.decorators import mcp_tools_api_decorator
 from backend.dbm_aiagent.mcp_tools.exceptions import DBMMcpBaseException
 from backend.dbm_aiagent.mcp_tools.mysql.serializers.kill_connection import KillConnectionInputSerializer
+from backend.dbm_aiagent.mcp_tools.mysql.serializers.modify_cluster_spec import (
+    ModifyMySQLClusterSpecOutputSerializer,
+    ModifyTenDBClusterSpecInputSerializer,
+    ModifyTenDBHASpecInputSerializer,
+    ModifyTenDBSingleSpecInputSerializer,
+)
 from backend.dbm_aiagent.mcp_tools.views import McpToolsViewSet
 from backend.iam_app.handlers.drf_perm.mcp import McpClusterDetailPermission, McpIsDbaPermission
 
@@ -60,3 +68,111 @@ class MySQLSensitiveMcpViewSet(McpToolsViewSet):
             raise DBMMcpBaseException(msg=cmd_res["error_msg"])
 
         return Response({"bk_cloud_id": bk_cloud_id, "address": address, "connection_id": connection_id})
+
+    @mcp_tools_api_decorator(
+        description=str(_("""修改 TenDBHA 集群规格, 只能修改 proxy 和 standby backend""")),
+        request_slz=ModifyTenDBHASpecInputSerializer,
+        response_slz=ModifyMySQLClusterSpecOutputSerializer,
+        permission_classes=[McpClusterDetailPermission, McpIsDbaPermission],
+        mcp_auth_parser=auth_parse_instances,
+        tags=[DBMMCPTags.WRITE],
+        mcp=[DBMMcpTools.MYSQL_SENSITIVE],
+        name_prefix="mysql_sensitive",
+        enable_callee_plan=True,
+    )
+    def modify_tendbha_spec(self, request, *args, **kwargs):
+        cluster_domain = self.get_param("cluster_domain")
+        # 对于 TenDBHA, 这个 role 其实是 machine_type [proxy, backend]
+        role = self.get_param("role")
+        spec_id = self.get_param("spec_id")
+
+        new_spec = Spec.objects.get(spec_id=spec_id)
+
+        cluster_obj = Cluster.objects.get(cluster_type=ClusterType.TenDBHA, immute_domain=cluster_domain)
+
+        if role == MachineType.PROXY:
+            for ins in cluster_obj.proxyinstance_set.all():
+                ins.machine.spec_id = new_spec.spec_id
+                ins.machine.spec_config = new_spec.get_spec_info()
+                ins.machine.save()
+        elif role == MachineType.BACKEND:
+            if cluster_obj.storageinstance_set.filter(instance_role=InstanceRole.BACKEND_REPEATER).exists():
+                raise DBMMcpBaseException(msg="can't modify backend spec during migration")
+
+            for ins in cluster_obj.storageinstance_set.filter(
+                is_stand_by=True, instance_role__in=[InstanceRole.BACKEND_MASTER, InstanceRole.BACKEND_SLAVE]
+            ):
+                ins.machine.spec_id = new_spec.spec_id
+                ins.machine.spec_config = new_spec.get_spec_info()
+                ins.machine.save()
+        else:
+            raise DBMMcpBaseException(msg=f"{role} is not valid machine type for tendbha cluster")
+
+        return Response({"spec_id": new_spec.spec_id, "spec_config": new_spec.get_spec_info()})
+
+    @mcp_tools_api_decorator(
+        description=str(_("""修改 TenDBCluster 集群规格, 只能修改 spider_master, spider_slave, remote""")),
+        request_slz=ModifyTenDBClusterSpecInputSerializer,
+        response_slz=ModifyMySQLClusterSpecOutputSerializer,
+        permission_classes=[McpClusterDetailPermission, McpIsDbaPermission],
+        mcp_auth_parser=auth_parse_instances,
+        tags=[DBMMCPTags.WRITE],
+        mcp=[DBMMcpTools.MYSQL_SENSITIVE],
+        name_prefix="mysql_sensitive",
+        enable_callee_plan=True,
+    )
+    def modify_tendbcluster_spec(self, request, *args, **kwargs):
+        cluster_domain = self.get_param("cluster_domain")
+        # 对于 TenDBCluster, 这个 role 是 [spider_master, spider_slave, remote]
+        role = self.get_param("role")
+        spec_id = self.get_param("spec_id")
+
+        new_spec = Spec.objects.get(spec_id=spec_id)
+
+        cluster_obj = Cluster.objects.get(cluster_type=ClusterType.TenDBCluster, immute_domain=cluster_domain)
+
+        if role in [TenDBClusterSpiderRole.SPIDER_MASTER, TenDBClusterSpiderRole.SPIDER_SLAVE]:
+            for ins in cluster_obj.proxyinstance_set.filter(tendbclusterspiderext__spider_role=role):
+                ins.machine.spec_id = new_spec.spec_id
+                ins.machine.spec_config = new_spec.get_spec_info()
+                ins.machine.save()
+        elif role == MachineType.REMOTE:
+            if cluster_obj.storageinstance_set.filter(instance_role=InstanceRole.REMOTE_REPEATER).exists():
+                raise DBMMcpBaseException(msg="can't modify remote spec during migration")
+
+            for ins in cluster_obj.storageinstance_set.filter(
+                is_stand_by=True, instance_role__in=[InstanceRole.REMOTE_MASTER, InstanceRole.REDIS_SLAVE]
+            ):
+                ins.machine.spec_id = new_spec.spec_id
+                ins.machine.spec_config = new_spec.get_spec_info()
+                ins.machine.save()
+        else:
+            raise DBMMcpBaseException(msg=f"not support modify tendbcluster {role} spec")
+
+        return Response({"spec_id": new_spec.spec_id, "spec_config": new_spec.get_spec_info()})
+
+    @mcp_tools_api_decorator(
+        description=str(_("""修改 TenDBSingle 集群规格""")),
+        request_slz=ModifyTenDBSingleSpecInputSerializer,
+        response_slz=ModifyMySQLClusterSpecOutputSerializer,
+        permission_classes=[McpClusterDetailPermission, McpIsDbaPermission],
+        mcp_auth_parser=auth_parse_instances,
+        tags=[DBMMCPTags.WRITE],
+        mcp=[DBMMcpTools.MYSQL_SENSITIVE],
+        name_prefix="mysql_sensitive",
+        enable_callee_plan=True,
+    )
+    def modify_tendbsingle_spec(self, request, *args, **kwargs):
+        cluster_domain = self.get_param("cluster_domain")
+        spec_id = self.get_param("spec_id")
+
+        new_spec = Spec.objects.get(spec_id=spec_id)
+
+        cluster_obj = Cluster.objects.get(cluster_type=ClusterType.TenDBSingle, immute_domain=cluster_domain)
+
+        for ins in cluster_obj.storageinstance_set.all():
+            ins.machine.spec_id = new_spec.spec_id
+            ins.machine.spec_config = new_spec.get_spec_info()
+            ins.machine.save()
+
+        return Response({"spec_id": new_spec.spec_id, "spec_config": new_spec.get_spec_info()})
--- dbm-ui/backend/ticket/constants.py
+++ dbm-ui/backend/ticket/constants.py
@@ -719,7 +719,7 @@ class TicketType(StrStructuredEnum):
     FAKE_TICKET = TicketEnumField("FAKE_TICKET", _("测试专用单据"), register_iam=False)
 
     # 注册MCP callee计划
-    REGISTER_MCP_CALLEE_PLAN = TicketEnumField("REGISTER_MCP_CALLEE_PLAN", _("注册MCP callee计划"), register_iam=False)
+    REGISTER_MCP_CALLEE_PLAN = TicketEnumField("REGISTER_MCP_CALLEE_PLAN", _("注册MCP执行计划"), register_iam=False)
 
 
 class FlowType(StrStructuredEnum):
