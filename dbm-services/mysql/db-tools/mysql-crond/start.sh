88643acedd6e0fbde4f0a8ad5634f9c7d75d2a7e
xfwduke <xfwduke@gmail.com>
2026-06-02 10:53:51 +0800

    fix(mysql): mysql-crond-start-errlog-wrong-path #17949

--- dbm-services/mysql/db-tools/mysql-crond/start.sh
+++ dbm-services/mysql/db-tools/mysql-crond/start.sh
@@ -6,6 +6,7 @@ pgrep -x 'mysql-crond' && echo "mysql-crond process already running" && exit 1
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 START_ERR="$SCRIPT_DIR/start-crond.err"
+PID_FILE="$SCRIPT_DIR/mysql-crond.pid"
 if [ $# -eq 0 ];then
   START_CMD="$SCRIPT_DIR/mysql-crond -c $SCRIPT_DIR/runtime.yaml 1>/dev/null 2>$START_ERR"
 else
@@ -14,16 +15,16 @@ fi
 
 
 if [ $(id -u) -eq 0 ];then
-  cd $SCRIPT_DIR && > start-crond.err && > mysql-crond.pid && chown mysql start-crond.err && chown mysql mysql-crond.pid
+  cd $SCRIPT_DIR && > $START_ERR && > $PID_FILE && chown mysql $START_ERR && chown mysql $PID_FILE
   setsid su - mysql -c "$START_CMD &"
 else
-  cd $SCRIPT_DIR && > start-crond.err && > mysql-crond.pid
+  cd $SCRIPT_DIR && > $START_ERR && > $PID_FILE
   setsid sh -c "exec $START_CMD" < /dev/null &
 fi
 
 sleep 1
-pgrep -x 'mysql-crond' >mysql-crond.pid
+pgrep -x 'mysql-crond' >$PID_FILE
 if [ $? -gt 0 ];then
-  cat start-crond.err >&2
+  cat $START_ERR >&2
   exit 1
 fi
