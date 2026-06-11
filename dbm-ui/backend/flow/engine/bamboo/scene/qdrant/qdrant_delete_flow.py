6d62e275ec739c40a7325ec300084f9a3a768f69
WengChangWei <13106003@qq.com>
2026-06-11 10:43:43 +0800

    feat(backend): qdrant删除集群单据flow #18163

--- dbm-ui/backend/flow/engine/bamboo/scene/qdrant/qdrant_delete_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/qdrant/qdrant_delete_flow.py
@@ -13,13 +13,17 @@ from typing import Dict, Optional
 
 from django.utils.translation import gettext as _
 
+from backend.flow.consts import DnsOpType
 from backend.flow.engine.bamboo.scene.common.builder import Builder
 from backend.flow.engine.bamboo.scene.qdrant.qdrant_base_flow import K8sQdrantBaseFlow
-from backend.flow.plugins.components.collections.qdrant.k8s_qdrant_restart import RestartK8sQdrantComponent
+from backend.flow.plugins.components.collections.qdrant.k8s_qdrant_delete import DeleteK8sQdrantComponent
+from backend.flow.plugins.components.collections.qdrant.qdrant_db_meta import QdrantDBMetaComponent
+from backend.flow.plugins.components.collections.qdrant.qdrant_dns_manage import QdrantDnsManageComponent
+from backend.flow.utils.doris.doris_context_dataclass import DnsKwargs
 from backend.flow.utils.qdrant.qdrant_context_dataclass import K8sQdrantActKwargs, K8sQdrantApplyContext
 
 
-class K8sQdrantRestartFlow(K8sQdrantBaseFlow):
+class K8sQdrantDeleteFlow(K8sQdrantBaseFlow):
     def __init__(self, root_id: str, data: Optional[Dict]):
         """
         @param root_id : 任务流程定义的root_id
@@ -29,7 +33,7 @@ class K8sQdrantRestartFlow(K8sQdrantBaseFlow):
 
     def deploy_qdrant_flow(self):
         """
-        重启qdrant集群
+        删除qdrant集群
         """
         # Builder 传参 为封装好角色IP的数据结构
         qdrant_pipeline = Builder(root_id=self.root_id, data=self.data)
@@ -37,9 +41,21 @@ class K8sQdrantRestartFlow(K8sQdrantBaseFlow):
         act_kwargs = K8sQdrantActKwargs(bk_cloud_id=self.bk_cloud_id)
         act_kwargs.set_trans_data_dataclass = K8sQdrantApplyContext.__name__
 
-        # # 调用dbs禁用接口
+        # 调用dbs删除接口
         qdrant_pipeline.add_act(
-            act_name=_("重启Qdrant集群"), act_component_code=RestartK8sQdrantComponent.code, kwargs=asdict(act_kwargs)
+            act_name=_("删除Qdrant集群"), act_component_code=DeleteK8sQdrantComponent.code, kwargs=asdict(act_kwargs)
+        )
+
+        # 清理域名
+        dns_kwargs = DnsKwargs(bk_cloud_id=self.bk_cloud_id, dns_op_type=DnsOpType.CLUSTER_DELETE)
+        qdrant_pipeline.add_act(
+            act_name=_("删除域名"),
+            act_component_code=QdrantDnsManageComponent.code,
+            kwargs={**asdict(act_kwargs), **asdict(dns_kwargs)},
+        )
+
+        qdrant_pipeline.add_act(
+            act_name=_("修改Meta"), act_component_code=QdrantDBMetaComponent.code, kwargs=asdict(act_kwargs)
         )
 
         qdrant_pipeline.run_pipeline()
