fbb588d2e907d6b3f64740b0488f36d86f4f8681
yksitu <1297650644@qq.com>
2026-06-15 18:27:18 +0800

    fix(mysql): 增加spider原地重建的validator逻辑 #18281

--- dbm-ui/backend/flow/engine/bamboo/scene/spider/validate/spider_rebuild_nodes_validate.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/spider/validate/spider_rebuild_nodes_validate.py
@@ -7,7 +7,15 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from backend.flow.engine.bamboo.scene.spider.validate.exception import SpiderRoleFailedException
+from collections import defaultdict
+
+from django.utils.translation import gettext as _
+
+from backend.db_meta.models import Cluster
+from backend.flow.engine.bamboo.scene.spider.validate.exception import (
+    SpiderCountFailedException,
+    SpiderRoleFailedException,
+)
 from backend.flow.engine.bamboo.scene.spider.validate.spider_switch_nodes_validate import (
     TenDBClusterSwitchNodesFlowValidator,
 )
@@ -28,6 +36,7 @@ class TenDBClusterRebuildNodesFlowValidator(MysqlBaseValidator):
         检验1：同一个flow，同一个集群，传入机器不能有相同
         检验2：同一个flow，同一个集群，不能出现不同待重建的spider角色
         检验3：同一个flow，不能出现重复的cluster
+        检验4：同一个flow，同一个集群同一个spider角色，待重建的节点数量不能大于等于集群中该角色的实例总数
     """
 
     def run_check_for_info(self, info: dict, index: int) -> list:
@@ -60,6 +69,29 @@ class TenDBClusterRebuildNodesFlowValidator(MysqlBaseValidator):
 
         return error_msg_list
 
+    def pre_check_rebuild_spider_count(self):
+        """
+        校验：同一个flow中，同一个集群、同一个spider角色，
+        待重建的spider节点数量不能大于等于集群中该角色的实例总数
+        （否则会导致集群该角色没有可用实例，影响业务连续性）
+        """
+        # 按 (cluster_id, rebuild_spider_role) 维度聚合待重建的ip数量
+        rebuild_count_map = defaultdict(int)
+        for info in self.data["infos"]:
+            key = (int(info["cluster_id"]), info["rebuild_spider_role"])
+            rebuild_count_map[key] += len(info["spider_ip_list"])
+
+        err_msg = ""
+        for (cluster_id, spider_role), rebuild_count in rebuild_count_map.items():
+            cluster = Cluster.objects.get(id=cluster_id)
+            exist_count = cluster.proxyinstance_set.filter(tendbclusterspiderext__spider_role=spider_role).count()
+            if rebuild_count >= exist_count:
+                err_msg += _("【{}】待重建的spider角色[{}]节点数量[{}]大于等于集群中该角色的实例总数[{}]，请检查\n").format(
+                    cluster.immute_domain, spider_role, rebuild_count, exist_count
+                )
+
+        return err_msg
+
     def __call__(self):
         """
         发起校验, 实例函数化
@@ -82,5 +114,10 @@ class TenDBClusterRebuildNodesFlowValidator(MysqlBaseValidator):
         err = self.pre_check_spider_role_for_cluster("cluster_id", "rebuild_spider_role")
         if err:
             raise SpiderRoleFailedException(err)
+
+        # 同一个flow，同一集群同一个spider角色，待重建节点数不能大于等于集群该角色实例总数
+        err = self.pre_check_rebuild_spider_count()
+        if err:
+            raise SpiderCountFailedException(err)
 
         return None
