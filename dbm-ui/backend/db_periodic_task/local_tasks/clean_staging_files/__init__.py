dcbf7af0a74374e53a90923b2f5fcd369919ba43
iSecloud <869820505@qq.com>
2026-06-26 15:24:23 +0800

    feat(backend): 文件暂存上传 & 安全引用删除 #19569

--- dbm-ui/backend/db_periodic_task/local_tasks/clean_staging_files/__init__.py
+++ dbm-ui/backend/db_periodic_task/local_tasks/clean_staging_files/__init__.py
@@ -8,37 +8,4 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
-
-from django.utils.translation import gettext_lazy as _
-
-from ..exceptions import CoreBaseException
-
-
-class FilesBaseException(CoreBaseException):
-    MODULE_CODE = 3001
-
-
-class FilesRegisterCredentialError(CoreBaseException):
-    MESSAGE = _("注册凭证失败")
-    ERROR_CODE = 1
-
-
-class FilesStorageTypeError(CoreBaseException):
-    MESSAGE = _("文件源类型错误")
-    ERROR_CODE = 2
-
-
-class FilesRegisterFileSourceError(CoreBaseException):
-    MESSAGE = _("注册文件失败")
-    ERROR_CODE = 3
-
-
-class FilesTransferError(CoreBaseException):
-    MESSAGE = _("文件传输失败")
-    ERROR_CODE = 4
-
-
-class StagingFileError(CoreBaseException):
-    MESSAGE = _("暂存区文件异常")
-    ERROR_CODE = 5
+# from .task import clean_staging_files_task
