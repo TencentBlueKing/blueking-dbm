0ca3d29ce3e9b863235e67276906916208e1fbcf
groobyming <lidanzhuang001@gmail.com>
2026-06-30 16:28:46 +0800

    feat(backend): surrealdb 单机版部署flow #18610

---
+++ dbm-ui/backend/flow/engine/bamboo/scene/surrealdb/surrealdb_single/surrealdb_single_base_flow.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+import logging.config
+from typing import Dict, Optional
+
+logger = logging.getLogger("flow")
+
+
+class K8sSurrealDBSingleBaseFlow(object):
+    """
+    SurrealDB Flow基类
+    """
+
+    def __init__(self, root_id: str, data: Optional[Dict]):
+        """
+        :param root_id: flow的root_id
+        :param data: flow的data
+        """
+        self.root_id = root_id
+        self.data = data
+        self.bk_cloud_id = data.get("bk_cloud_id")
+        self.cluster = data.get("cluster")
+        self.remark = data.get("remark")
+        self.bk_biz_id = data.get("bk_biz_id")
+        self.ticket_type = data.get("ticket_type")
+        self.db_app_abbr = data.get("db_app_abbr")
+        self.bk_biz_name = data.get("bk_biz_name")
+        self.bk_cloud_region = data.get("bk_cloud_region")
+        self.city_code = data.get("city_code")
+        self.k8s_cluster_name = data.get("k8s_cluster_name")
+        # major_version 用于 dbs 侧的 storageAddonVersion / addonClusterVersion，代表大版本
+        # 具体的小版本 db_version 由前端下发时直接放到 data 里，通过 global_data["db_version"] 访问
+        self.major_version = data.get("major_version")
+        self.cluster_type = data.get("cluster_type")
+        self.cluster_name = data.get("cluster_name")
+        self.cluster_alias = data.get("cluster_alias")
+        self.component_list = data.get("component_list")
---
+++ dbm-ui/backend/flow/utils/surrealdb/consts.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+# SurrealDB 部署相关的常量集中定义，避免多处 magic string / magic number
+
+# 命名空间前缀，最终形如 "{NAMESPACE_PREFIX}-{db_app_abbr}-{bk_biz_id}"
+NAMESPACE_PREFIX = "surreal"
+
+# dbs 侧标识
+STORAGE_ADDON_TYPE = "surrealdb"
+SINGLE_TOPO_NAME = "surreal-rocksdb"
+SINGLE_TAGS = ["dbm", "surrealdb", "single"]
+
+# 组件名（与 dbs 约定的 componentName 一致）
+COMPONENT_SURREAL = "surreal"
+
+# 服务暴露相关
+SERVICE_NAME = "surreal-clb"
+SURREALDB_PORT = 8000
+
+# CLB 名称后缀
+CLB_NAME_SUFFIX = "surrealdb-clb"
+
+# 域名前缀，最终形如 "{DOMAIN_PREFIX}.{cluster_name}.{db_app_abbr}.db"
+DOMAIN_PREFIX = "surrealdb"
+
+# 异步 schedule 轮询相关
+SCHEDULE_INTERVAL_SECONDS = 20
+SCHEDULE_MAX_RETRIES = 15
