505a27a53a3825818ca17eaa01aeac62718ce6cb
v_ttengtian <v_ttengtian@tencent.com>
2026-07-01 07:34:41 +0000

    feat(backend): SQLServer 原地回档与定点构造 V2 #18717

---
+++ dbm-ui/backend/ticket/builders/sqlserver/sqlserver_rollback_local.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from backend.ticket import builders
+from backend.ticket.builders.sqlserver.sqlserver_rollback_base import (
+    SQLServerRollbackBaseDetailSerializer,
+    SQLServerRollbackCommonFlowBuilder,
+)
+from backend.ticket.constants import TicketType
+
+
+class SQLServerLocalDetailSerializer(SQLServerRollbackBaseDetailSerializer):
+    """本地构造(原地回档)：源集群与目标集群为同一集群，无需传目标集群ID"""
+
+    class LocalRollbackInfoSerializer(SQLServerRollbackBaseDetailSerializer.RollbackInfoSerializer):
+        # 原地回档不传目标集群，目标集群与源集群保持一致
+        dst_cluster = serializers.IntegerField(help_text=_("目标集群ID"), required=False)
+
+        def validate(self, attrs):
+            # 原地回档：目标集群ID等于源集群ID
+            attrs["dst_cluster"] = attrs["src_cluster"]
+            return attrs
+
+    infos = serializers.ListSerializer(help_text=_("迁移信息列表"), child=LocalRollbackInfoSerializer())
+    is_local = serializers.BooleanField(help_text=_("是否原地构造"), default=True)
+    is_time_fixed = serializers.BooleanField(help_text=_("是否指定回档时间"))
+
+    def validate(self, attrs):
+        """校验指定回档时间时，必须填写每一条信息的回档时间(指定时间)"""
+        is_time_fixed = attrs.get("is_time_fixed")
+        for index, info in enumerate(attrs["infos"]):
+            if is_time_fixed:
+                # 指定回档时间：必须填写指定时间 restore_time
+                if not info.get("restore_time"):
+                    raise serializers.ValidationError(_("第{}条信息未指定回档时间，请填写指定时间").format(index + 1))
+            else:
+                # 未指定回档时间：清理掉可能存在的回档时间，走最新备份逻辑
+                info.pop("restore_time", None)
+
+        return super().validate(attrs)
+
+
+@builders.BuilderFactory.register(TicketType.SQLSERVER_ROLLBACK_LOCAL)
+class SQLServerRollbackLocalFlowBuilder(SQLServerRollbackCommonFlowBuilder):
+    serializer = SQLServerLocalDetailSerializer
