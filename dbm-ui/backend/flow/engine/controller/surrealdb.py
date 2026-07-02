da1887c0acc0fb54f267a951cb18c2672fe54c6b
groobyming <lidanzhuang001@gmail.com>
2026-07-02 16:32:30 +0800

    feat(backend): surrealdb 集群禁用flow #18661

--- dbm-ui/backend/flow/engine/controller/surrealdb.py
+++ dbm-ui/backend/flow/engine/controller/surrealdb.py
@@ -8,6 +8,8 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from backend.flow.engine.bamboo.scene.surrealdb.surrealdb_disable_flow import K8sSurrealDBDisableFlow
+from backend.flow.engine.bamboo.scene.surrealdb.surrealdb_enable_flow import K8sSurrealDBEnableFlow
 from backend.flow.engine.bamboo.scene.surrealdb.surrealdb_restart_flow import K8sSurrealDBRestartFlow
 from backend.flow.engine.bamboo.scene.surrealdb.surrealdb_single.surrealdb_single_apply_flow import (
     K8sSurrealDBSingleApplyFlow,
@@ -31,13 +33,15 @@ class SurrealDBController(BaseController):
         """
         SurrealDB 启用流程
         """
-        pass
+        flow = K8sSurrealDBEnableFlow(root_id=self.root_id, data=self.ticket_data)
+        flow.enable_surrealdb_flow()
 
     def surrealdb_disable_scene(self):
         """
         SurrealDB 禁用流程
         """
-        pass
+        flow = K8sSurrealDBDisableFlow(root_id=self.root_id, data=self.ticket_data)
+        flow.disable_surrealdb_flow()
 
     def surrealdb_restart_scene(self):
         """
