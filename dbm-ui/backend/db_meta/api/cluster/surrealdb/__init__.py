3e278f99a8a46b65f68c0e4e012b0759aaecde52
groobyming <lidanzhuang001@gmail.com>
2026-07-02 16:32:30 +0800

    feat(backend): surrealdb 集群禁用flow #18661

--- dbm-ui/backend/db_meta/api/cluster/surrealdb/__init__.py
+++ dbm-ui/backend/db_meta/api/cluster/surrealdb/__init__.py
@@ -8,3 +8,7 @@
    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
    specific language governing permissions and limitations under the License.
    """
+from .create import create
+from .delete import delete
+from .disable import disable
+from .enable import enable
