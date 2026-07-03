41cee3926c9085e72c471b29750520a8746e2ef8
groobyming <lidanzhuang001@gmail.com>
2026-07-03 15:21:30 +0800

    feat(backend): surrealdb集群卸载flow #18689
    feat(backend): surrealdb 集群卸载flow #18689

--- dbm-ui/backend/flow/engine/bamboo/scene/surrealdb/surrealdb_destroy_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/surrealdb/surrealdb_destroy_flow.py
@@ -8,9 +8,28 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
+from dataclasses import asdict
 from typing import Dict, Optional
 
+from django.utils.translation import gettext as _
+
+from backend.flow.consts import DnsOpType
+from backend.flow.engine.bamboo.scene.common.builder import Builder
 from backend.flow.engine.bamboo.scene.surrealdb.surrealdb_base_flow import K8sSurrealDBBaseFlow
+from backend.flow.plugins.components.collections.surrealdb.surrealdb_destroy import DestroySurrealDBComponent
+from backend.flow.plugins.components.collections.surrealdb.surrealdb_dns_manage import SurrealDBDnsManageComponent
+from backend.flow.plugins.components.collections.surrealdb.surrealdb_single.surrealdb_meta import (
+    SurrealDBMetaComponent,
+)
+from backend.flow.plugins.components.collections.surrealdb.surrealdb_sync_ticket_id import (
+    SurrealDBSyncTicketIdComponent,
+)
+from backend.flow.utils.surrealdb.surrealdb_context_dataclass import (
+    DnsKwargs,
+    K8sSurrealDBActKwargs,
+    K8sSurrealDBApplyContext,
+)
 
 
 class K8sSurrealDBDestroyFlow(K8sSurrealDBBaseFlow):
@@ -21,9 +40,34 @@ class K8sSurrealDBDestroyFlow(K8sSurrealDBBaseFlow):
         """
         super().__init__(root_id, data)
 
-    def destroy_surrealdb_flow(self):
+    def deploy_surrealdb_flow(self):
         """
-        销毁 surrealdb 集群
+        删除surrealdb集群
         """
-        # TODO: implement in next PR
-        pass
+        surrealdb_pipeline = Builder(root_id=self.root_id, data=self.data)
+        act_kwargs = K8sSurrealDBActKwargs(bk_cloud_id=self.bk_cloud_id)
+        act_kwargs.set_trans_data_dataclass = K8sSurrealDBApplyContext.__name__
+
+        # 调用dbs删除接口
+        surrealdb_pipeline.add_act(
+            act_name=_("删除SurrealDB集群"), act_component_code=DestroySurrealDBComponent.code, kwargs=asdict(act_kwargs)
+        )
+
+        # 清理域名
+        dns_kwargs = DnsKwargs(bk_cloud_id=self.bk_cloud_id, dns_op_type=DnsOpType.CLUSTER_DELETE)
+        surrealdb_pipeline.add_act(
+            act_name=_("删除域名"),
+            act_component_code=SurrealDBDnsManageComponent.code,
+            kwargs={**asdict(act_kwargs), **asdict(dns_kwargs)},
+        )
+
+        surrealdb_pipeline.add_act(
+            act_name=_("修改Meta"), act_component_code=SurrealDBMetaComponent.code, kwargs=asdict(act_kwargs)
+        )
+
+        # 同步ticketId给dbs
+        surrealdb_pipeline.add_act(
+            act_name=_("同步ticketId"), act_component_code=SurrealDBSyncTicketIdComponent.code, kwargs=asdict(act_kwargs)
+        )
+
+        surrealdb_pipeline.run_pipeline()
