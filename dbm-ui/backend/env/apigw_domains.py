0055bd722706095a3c86db7a2710b9ba6066f689
F-cq <2036478450@qq.com>
2026-07-07 15:14:46 +0800

    feat(backend): Add DBHA-v2 switch log API with compatibility handling against DBHA-v1. issue: #18745

--- dbm-ui/backend/env/apigw_domains.py
+++ dbm-ui/backend/env/apigw_domains.py
@@ -41,6 +41,7 @@ MYSQL_PRIV_MANAGER_APIGW_DOMAIN = get_type_env(
 PARTITION_APIGW_DOMAIN = get_type_env(key="PARTITION_APIGW_DOMAIN", _type=str, default="http://bk-dbm-dbpartition")
 MYSQL_SIMULATION_DOMAIN = get_type_env(key="MYSQL_SIMULATION_DOMAIN", _type=str, default="http://bk-dbm-dbsimulation")
 HADB_APIGW_DOMAIN = get_type_env(key="HADB_APIGW_DOMAIN", _type=str, default="http://bk-dbm-hadb-api")
+HADBV2_APIGW_DOMAIN = get_type_env(key="HADBV2_APIGW_DOMAIN", _type=str, default="http://bk-dbm-hadb-api")
 DBRESOURCE_APIGW_DOMAIN = get_type_env(key="DBRESOURCE_APIGW_DOMAIN", _type=str, default="http://bk-dbm-db-resource")
 BACKUP_APIGW_DOMAIN = get_type_env(key="BACKUP_APIGW_DOMAIN", _type=str, default="http://bk-dbm-backup-server:7000")
 SLOW_QUERY_PARSER_DOMAIN = get_type_env(
