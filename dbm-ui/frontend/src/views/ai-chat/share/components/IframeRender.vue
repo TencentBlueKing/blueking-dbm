657cd6687c5e51ca52d15aa2ac8193a62c4a7915
hLinx <327159425@qq.com>
2026-07-15 17:23:49 +0800

    feat(frontend): ai 分享页 markdow 内容新增目录导航 #18909

---
+++ dbm-ui/frontend/src/views/ai-chat/share/components/IframeRender.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe
+    class="report-share-html-frame"
+    :srcdoc="content" />
+</template>
+<script setup lang="ts">
+  interface Props {
+    content: string;
+  }
+
+  defineProps<Props>();
+</script>
+<style lang="postcss">
+  .report-share-html-frame {
+    display: block;
+    width: 100%;
+    height: calc(100vh - 102px - var(--notice-height));
+    border: none;
+  }
+</style>
