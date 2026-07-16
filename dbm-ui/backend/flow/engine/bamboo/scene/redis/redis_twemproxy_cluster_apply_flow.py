288271ac7aa47cf2e57865a6d2e2accd046370cd
OMG-By <504094596@qq.com>
2026-07-16 17:58:57 +0800

    feat(redis): redis_apply_flow_feat #18838

--- dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_twemproxy_cluster_apply_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_twemproxy_cluster_apply_flow.py
@@ -34,7 +34,7 @@ from backend.flow.utils.redis.redis_act_playload import RedisActPayload
 from backend.flow.utils.redis.redis_context_dataclass import ActKwargs, CommonContext, DnsKwargs
 from backend.flow.utils.redis.redis_db_meta import RedisDBMeta
 from backend.flow.utils.redis.redis_proxy_util import get_cache_backup_mode
-from backend.flow.utils.redis.redis_util import check_domain
+from backend.flow.utils.redis.redis_util import add_summary_output_act, build_clb_polaris_apply_subs, check_domain
 
 logger = logging.getLogger("flow")
 
@@ -277,7 +277,18 @@ class RedisClusterApplyFlow(object):
             act_name=_("建立集群 元数据"), act_component_code=RedisDBMetaComponent.code, kwargs=asdict(act_kwargs)
         )
 
-        acts_list = []
+        # 集群部署成功后，根据单据传参决定是否给集群构建创建clb / 北极星的子流程，与后面的注册域名等节点并行执行
+        clb_polaris_subs = build_clb_polaris_apply_subs(
+            root_id=self.root_id,
+            data=self.data,
+            bk_biz_id=self.data["bk_biz_id"],
+            domain_name=self.data["domain_name"],
+            creator=self.data["created_by"],
+            apply_clb=self.data.get("apply_clb", False),
+            apply_polaris=self.data.get("apply_polaris", False),
+        )
+
+        acts_list = list(clb_polaris_subs)
         act_kwargs.cluster = {
             "conf": {
                 "maxmemory": str(self.data["maxmemory"]),
@@ -360,6 +371,17 @@ class RedisClusterApplyFlow(object):
             act_name=_("{}-更新版本").format(self.data["domain_name"]),
             act_component_code=RedisUpdateVersionComponent.code,
             kwargs=asdict(act_kwargs),
+        )
+
+        # 写入集群信息摘要(地区/域名/端口/CLB/北极星)，供前端"执行摘要"展示
+        add_summary_output_act(
+            redis_pipeline=redis_pipeline,
+            bk_biz_id=self.data["bk_biz_id"],
+            domain_name=self.data["domain_name"],
+            region=self.data.get("city_code", ""),
+            proxy_port=self.data["proxy_port"],
+            apply_clb=self.data.get("apply_clb", False),
+            apply_polaris=self.data.get("apply_polaris", False),
         )
 
         redis_pipeline.run_pipeline()
---
+++ dbm-ui/backend/flow/plugins/components/collections/redis/redis_apply_summary.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-DB管理系统(BlueKing-BK-DBM) available.
+Copyright (C) 2017-2023 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+import logging
+from typing import List
+
+from django.utils.translation import gettext as _
+from pipeline.component_framework.component import Component
+from pipeline.core.flow.activity import Service
+from rest_framework import serializers
+
+from backend.db_meta.enums import ClusterEntryType
+from backend.db_meta.models import Cluster, ClusterEntry
+from backend.flow.plugins.components.collections.common.base_service import BaseService
+from backend.flow.utils.base.flow_output import BaseFlowOutputSerializer, FlowOutputHandler
+from backend.ticket.models import Flow
+
+logger = logging.getLogger("flow")
+
+
+class RedisApplySummarySerializer(BaseFlowOutputSerializer):
+    """redis集群部署成功后的摘要信息定义，字段顺序即前端表格展示顺序，第一个字段同时作为表格行唯一标识"""
+
+    table_name = "redis_cluster_info"
+    table_display_name = _("集群信息")
+    table_primary_key = "domain_name"
+
+    domain_name = serializers.CharField(help_text=_("域名"))
+    region = serializers.CharField(help_text=_("地区"), allow_blank=True, default="")
+    proxy_port = serializers.IntegerField(help_text=_("端口"))
+    clb_ip = serializers.CharField(help_text=_("CLB IP"), allow_blank=True, default="")
+    clb_domain = serializers.CharField(help_text=_("CLB域名"), allow_blank=True, default="")
+    polaris_name = serializers.CharField(help_text=_("北极星服务名称"), allow_blank=True, default="")
+    polaris_l5 = serializers.CharField(help_text=_("北极星L5"), allow_blank=True, default="")
+
+
+class RedisApplySummaryService(BaseService):
+    """
+    redis集群部署成功后，将集群关键信息(地区/域名/端口/CLB/北极星)写入FlowSummary，
+    供前端"执行摘要"展示。该节点需要在集群元数据以及CLB/北极星创建节点之后执行，才能查询到完整信息。
+    """
+
+    def _execute(self, data, parent_data) -> bool:
+        kwargs = data.get_one_of_inputs("kwargs")
+        root_id = self.runtime_attrs.get("root_pipeline_id")
+
+        bk_biz_id = kwargs["bk_biz_id"]
+        domain_name = kwargs["domain_name"]
+
+        summary_data = {
+            "region": kwargs.get("region") or "",
+            "domain_name": domain_name,
+            "proxy_port": kwargs["proxy_port"],
+            "clb_ip": "",
+            "clb_domain": "",
+            "polaris_name": "",
+            "polaris_l5": "",
+        }
+
+        try:
+            cluster = Cluster.objects.get(bk_biz_id=bk_biz_id, immute_domain=domain_name)
+        except Cluster.DoesNotExist:
+            self.log_error(_("写入集群信息摘要失败，集群[{}]不存在").format(domain_name))
+            cluster = None
+
+        if cluster:
+            if kwargs.get("apply_clb"):
+                clb_entry = ClusterEntry.objects.filter(
+                    cluster=cluster, cluster_entry_type=ClusterEntryType.CLB.value
+                ).first()
+                if clb_entry:
+                    detail = clb_entry.detail
+                    summary_data["clb_ip"] = detail.get("clb_ip", "") or ""
+                    summary_data["clb_domain"] = detail.get("clb_domain", "") or ""
+                else:
+                    self.log_error(_("集群[{}]未查询到CLB信息").format(domain_name))
+
+            if kwargs.get("apply_polaris"):
+                polaris_entry = ClusterEntry.objects.filter(
+                    cluster=cluster, cluster_entry_type=ClusterEntryType.POLARIS.value
+                ).first()
+                if polaris_entry:
+                    detail = polaris_entry.detail
+                    summary_data["polaris_name"] = detail.get("polaris_name", "") or ""
+                    summary_data["polaris_l5"] = detail.get("polaris_l5", "") or ""
+                else:
+                    self.log_error(_("集群[{}]未查询到北极星信息").format(domain_name))
+
+        # 该flow可能并非由正常单据(ticket)触发，例如DTS内部临时创建目标集群时会用临时root_id直接跑pipeline，
+        # 此时不存在对应的Flow记录(ticket.models.Flow)，无法写入FlowSummary，属于预期情况，跳过即可，不应阻塞流程。
+        if not Flow.objects.filter(flow_obj_id=root_id).exists():
+            self.log_info(_("当前流程[{}]未关联单据Flow记录，跳过写入执行摘要").format(root_id))
+            return True
+
+        FlowOutputHandler(RedisApplySummarySerializer).insert_data(root_id, summary_data)
+        self.log_info(_("集群[{}]信息已写入执行摘要").format(domain_name))
+        return True
+
+    def inputs_format(self) -> List:
+        return [
+            Service.InputItem(name="kwargs", key="kwargs", type="dict", required=True),
+            Service.InputItem(name="global_data", key="global_data", type="dict", required=True),
+        ]
+
+
+class RedisApplySummaryComponent(Component):
+    name = __name__
+    code = "redis_apply_summary"
+    bound_service = RedisApplySummaryService
