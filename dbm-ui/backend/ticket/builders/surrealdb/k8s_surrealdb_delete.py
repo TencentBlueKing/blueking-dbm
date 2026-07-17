2afdc5ccb867b0d4d1d09a981da71e7d5240b3bf
lyjpy <jay0613@163.com>
2026-07-17 10:50:14 +0800

    feat(backend): surrealDB多模型图数据库,集群部署 #17505

--- dbm-ui/backend/ticket/builders/surrealdb/k8s_surrealdb_delete.py
+++ dbm-ui/backend/ticket/builders/surrealdb/k8s_surrealdb_delete.py
@@ -24,6 +24,7 @@ from backend.ticket.constants import TicketType
 
 class K8sSurrealDBDeleteDetailSerializer(TicketBaseValidateSerializerMixin, serializers.Serializer):
     cluster_id = serializers.IntegerField(help_text=_("集群ID"))
+    clusters = serializers.DictField(help_text=_("集群信息"), required=False, default=dict)
 
 
 class K8sSurrealDBDeleteFlowParamBuilder(builders.FlowParamBuilder):
