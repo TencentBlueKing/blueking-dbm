d4a850167437a2d230ebdbd8a27ee5695145d69d
OMG-By <504094596@qq.com>
2026-07-20 11:07:17 +0800

    feat(redis): redis_shutdown_snapshot #18968

--- dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_cluster_shutdown.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/redis/redis_cluster_shutdown.py
@@ -31,6 +31,9 @@ from backend.flow.engine.bamboo.scene.common.get_file_list import GetFileList
 from backend.flow.engine.bamboo.scene.redis.atom_jobs import AccessManagerAtomJob
 from backend.flow.plugins.components.collections.redis.exec_actuator_script import ExecuteDBActuatorScriptComponent
 from backend.flow.plugins.components.collections.redis.get_redis_payload import GetRedisActPayloadComponent
+from backend.flow.plugins.components.collections.redis.redis_cluster_shutdown_snapshot import (
+    RedisClusterShutdownMetaSnapshotComponent,
+)
 from backend.flow.plugins.components.collections.redis.redis_db_meta import RedisDBMetaComponent
 from backend.flow.plugins.components.collections.redis.trans_flies import TransFileComponent
 from backend.flow.utils.redis.redis_act_playload import RedisActPayload
@@ -88,6 +91,14 @@ class RedisClusterShutdownFlow(object):
             7、挪CC
         """
         redis_pipeline = Builder(root_id=self.root_id, data=self.data)
+
+        # 第一个节点：保存集群下架前的元数据快照，用于审计与追溯
+        redis_pipeline.add_act(
+            act_name=_("保存下架集群元数据快照"),
+            act_component_code=RedisClusterShutdownMetaSnapshotComponent.code,
+            kwargs={"bk_biz_id": self.data["bk_biz_id"], "cluster_id": self.data["cluster_id"]},
+        )
+
         trans_files = GetFileList(db_type=DBType.Redis)
         cluster_info = self.__get_cluster_info(self.data["bk_biz_id"], self.data["cluster_id"])
         proxy_ips = list(cluster_info["proxy_map"].keys())
