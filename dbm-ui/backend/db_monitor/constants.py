b9151d7c31445f0eb1f20169357866c9c86ac1c5
iSecloud <869820505@qq.com>
2026-07-21 15:14:11 +0800

    fix(backend): 修改redis负载查询为最近24h #19047

--- dbm-ui/backend/db_monitor/constants.py
+++ dbm-ui/backend/db_monitor/constants.py
@@ -18,6 +18,8 @@ from backend.configuration.constants import DBType
 from backend.db_meta.enums import ClusterType, MachineType
 from blue_krill.data_types.enum import EnumField, IntStructuredEnum, StrStructuredEnum
 
+CLUSTER_LOAD_QUERY_RANGE = 60 * 24
+
 DB_MONITOR_TPLS_DIR = os.path.join(settings.BASE_DIR, "backend/db_monitor/tpls")
 TPLS_COLLECT_DIR = os.path.join(DB_MONITOR_TPLS_DIR, "collect")
 TPLS_ALARM_DIR = os.path.join(DB_MONITOR_TPLS_DIR, "alarm")
