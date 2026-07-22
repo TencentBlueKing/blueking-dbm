252b956426088059b0d899d98fff61f721a90481
iSecloud <869820505@qq.com>
2026-07-22 17:41:50 +0800

    fix(backend): 租户ID默认改为""  #19119

--- dbm-ui/backend/env/__init__.py
+++ dbm-ui/backend/env/__init__.py
@@ -42,7 +42,7 @@ ENABLE_BKBASE_INTERNAL_METRICS = get_type_env(key="ENABLE_BKBASE_INTERNAL_METRIC
 # 租户配置
 ENABLE_MULTI_TENANT_MODE = get_type_env(key="ENABLE_MULTI_TENANT_MODE", _type=bool, default=False)
 ENABLE_BKBASE_METRICS_REPORT = get_type_env(key="ENABLE_BKBASE_METRICS_REPORT", _type=bool, default=False)
-BK_TENANT_ID = get_type_env(key="BK_TENANT_ID", _type=str, default="default")
+BK_TENANT_ID = get_type_env(key="BK_TENANT_ID", _type=str, default="")
 
 # CC业务模型中的英文业务简称
 BK_APP_ABBR = get_type_env(key="BK_APP_ABBR", _type=str, default="")
