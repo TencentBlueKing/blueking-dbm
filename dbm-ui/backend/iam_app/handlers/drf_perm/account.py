e8825d8d5a32a6c2da699c60bb8786f920006cc2
huangwentai-wendel <huangwentai96@163.com>
2026-07-28 15:56:32 +0800

    fix(backend): 修复sqlserver授权规则权限报错问题 #19245

--- dbm-ui/backend/iam_app/handlers/drf_perm/account.py
+++ dbm-ui/backend/iam_app/handlers/drf_perm/account.py
@@ -36,7 +36,10 @@ class AccountPermission(ResourceActionPermission):
 
     def instance_id_getter(self, request, view):
         try:
-            self.actions = [getattr(ActionEnum, f"{self.account_type}_{self.view_action}".upper())]
+            if self.account_type != "sqlserver":
+                self.actions = [getattr(ActionEnum, f"{self.account_type}_{self.view_action}".upper())]
+            else:
+                self.actions = [ActionEnum.SQLSERVER_PRIV_MANAGE]
         except AttributeError:
             ActionNotExistError(_("账号动作:{} 不存在/未实现").format(self.view_action))
         # 只有创建账号的动作是业务的，剩下对账号的操作都是以账号为维度的
@@ -44,5 +47,9 @@ class AccountPermission(ResourceActionPermission):
             self.resource_meta = ResourceEnum.BUSINESS
             return [get_request_key_id(request, "bk_biz_id")]
         else:
-            self.resource_meta = getattr(ResourceEnum, f"{self.account_type.upper()}_ACCOUNT")
-            return [get_request_key_id(request, "account_id")]
+            if self.account_type != "sqlserver":
+                self.resource_meta = getattr(ResourceEnum, f"{self.account_type.upper()}_ACCOUNT")
+                return [get_request_key_id(request, "account_id")]
+            else:
+                self.resource_meta = ResourceEnum.BUSINESS
+                return [get_request_key_id(request, "bizId")]
