1bfecc3ddeaa9ec33d88b119b77d7d1952a02199
chenwenchang <479999519@qq.com>
2026-07-28 11:33:57 +0800

    fix(frontend): 人员选择器兼容dba管理页面 #19234

--- dbm-ui/frontend/src/components/db-member-selector/components/TenantSelector.vue
+++ dbm-ui/frontend/src/components/db-member-selector/components/TenantSelector.vue
@@ -1,17 +1,20 @@
 <template>
   <BkUserSelector
     v-bind="$attrs"
-    v-model="modelValue"
+    v-model="selectedValue"
     :api-base-url="apiBaseUrl"
     draggable
     :multiple="multiple"
-    :tenant-id="tenantId" />
+    :tenant-id="tenantId"
+    @change="handleUsersChange" />
 </template>
 
 <script setup lang="ts">
+  import _ from 'lodash';
   import urlJoin from 'url-join';
 
   import BkUserSelector from '@blueking/bk-user-selector';
+  import type { FormattedUser } from '@blueking/bk-user-selector/typings/types';
 
   import { useSystemEnviron, useUserProfile } from '@stores';
 
@@ -21,7 +24,7 @@
     multiple?: boolean;
   }
 
-  withDefaults(defineProps<Props>(), {
+  const props = withDefaults(defineProps<Props>(), {
     multiple: true,
   });
   const modelValue = defineModel<string[]>({
@@ -32,4 +35,34 @@
   const { tenantId } = useUserProfile();
 
   const apiBaseUrl = urlJoin(systemEnvironStore.urls.USER_MANAGE_FRONTEND_APIGW_DOMAIN);
+
+  const selectedValue = ref<string | string[]>(props.multiple ? [] : '');
+
+  watch(
+    modelValue,
+    (newValue, oldValue) => {
+      if (_.isEqual(newValue, oldValue)) {
+        return;
+      }
+
+      if (props.multiple) {
+        selectedValue.value = newValue;
+      } else {
+        selectedValue.value = newValue[0] || '';
+      }
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  const handleUsersChange = (users: FormattedUser | FormattedUser[] | null) => {
+    if (users) {
+      if (props.multiple) {
+        modelValue.value = (users as FormattedUser[]).map((user) => user.id);
+      } else {
+        modelValue.value = [(users as FormattedUser).id];
+      }
+    }
+  };
 </script>
