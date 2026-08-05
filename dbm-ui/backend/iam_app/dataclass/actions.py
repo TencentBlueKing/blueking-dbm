6e1eef4277c47b98119838cde666c934835bbf55
huangwentai-wendel <huangwentai96@163.com>
2026-08-05 11:03:01 +0800

    fix(backend): 删除组件manage类权限的labels #19461

--- dbm-ui/backend/iam_app/dataclass/actions.py
+++ dbm-ui/backend/iam_app/dataclass/actions.py
@@ -2946,7 +2946,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.MYSQL],
         group=_("MySQL"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     TENDBCLUSTER_MANAGE = ActionMeta(
@@ -2958,7 +2957,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.TENDBCLUSTER],
         group=_("TenDBCluster"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     REDIS_MANAGE = ActionMeta(
@@ -2970,7 +2968,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.REDIS],
         group=_("Redis"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     ES_MANAGE = ActionMeta(
@@ -2982,7 +2979,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.ES],
         group=_("ES"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     DORIS_MANAGE = ActionMeta(
@@ -2994,7 +2990,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.DORIS],
         group=_("Doris"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     KAFKA_MANAGE = ActionMeta(
@@ -3006,7 +3001,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.KAFKA],
         group=_("Kafka"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     HDFS_MANAGE = ActionMeta(
@@ -3018,7 +3012,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.HDFS],
         group=_("HDFS"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     PULSAR_MANAGE = ActionMeta(
@@ -3030,7 +3023,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.PULSAR],
         group=_("Pulsar"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     MONGODB_MANAGE = ActionMeta(
@@ -3042,7 +3034,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.MONGODB],
         group=_("MongoDB"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     SQLSERVER_MANAGE = ActionMeta(
@@ -3055,7 +3046,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.SQLSERVER],
         group=_("SQLServer"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     ORACLE_MANAGE = ActionMeta(
@@ -3067,7 +3057,6 @@ class ActionEnum:
         related_resource_types=[ResourceEnum.ORACLE],
         group=_("Oracle"),
         subgroup=_("集群管理"),
-        common_labels=[CommonActionLabel.BIZ_MAINTAIN],
     )
 
     # -----------------------------------------------------------------------
