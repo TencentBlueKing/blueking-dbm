6997719c532002a9df0ccd1c7a5d538c7aa1df4b
huangwentai-wendel <huangwentai96@163.com>
2026-08-05 09:44:53 +0800

    feat(backend): MongoDB 集群级权限优化 #19435

--- dbm-ui/backend/ticket/builders/mongodb/mongo_replicaset_add_shard_nodes.py
+++ dbm-ui/backend/ticket/builders/mongodb/mongo_replicaset_add_shard_nodes.py
@@ -15,6 +15,7 @@ from backend.db_meta.enums import MachineType
 from backend.db_meta.models import AppCache
 from backend.db_services.dbbase.constants import IpSource
 from backend.flow.engine.controller.mongodb import MongoDBController
+from backend.iam_app.dataclass.actions import ActionEnum
 from backend.ticket import builders
 from backend.ticket.builders.common.base import get_mongodb_cluster_tolerance
 from backend.ticket.builders.mongodb.base import (
@@ -70,7 +71,9 @@ class MongoDBReplicaAddShardNodesResourceParamBuilder(BaseMongoDBOperateResource
                 info["add_shard_nodes"] = info.pop("shard_nodes")
 
 
-@builders.BuilderFactory.register(TicketType.MONGODB_REPLICA_ADD_SHARD_NODES, is_apply=True)
+@builders.BuilderFactory.register(
+    TicketType.MONGODB_REPLICA_ADD_SHARD_NODES, is_apply=True, iam=ActionEnum.MONGODB_MANAGE
+)
 class MongoDBReplicaAddMongosApplyFlowBuilder(BaseMongoDBTicketFlowBuilder):
     serializer = MongoDBReplicaAddShardNodesDetailSerializer
     inner_flow_builder = MongoDBReplicaAddShardNodesFlowParamBuilder
