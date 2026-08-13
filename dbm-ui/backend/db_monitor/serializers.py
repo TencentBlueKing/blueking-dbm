5079d1bf3d9a10240bf6a75ac5a994ba349128aa
huangwentai-wendel <huangwentai96@163.com>
2026-08-13 10:32:32 +0800

    fix(backend): 修复全局策略编辑报错问题 #19684

--- dbm-ui/backend/db_monitor/serializers.py
+++ dbm-ui/backend/db_monitor/serializers.py
@@ -222,7 +222,7 @@ class MonitorPolicyUpdateSerializer(AuditedSerializer, serializers.ModelSerializ
     class NotifyConfigSerializer(serializers.Serializer):
         interval_notify_mode = serializers.CharField(help_text=_("通知间隔类型"))
         notify_interval = serializers.IntegerField(help_text=_("通知间隔时间（秒）"))
-        voice_notice = serializers.CharField(help_text=_("拨打语音方式"))
+        voice_notice = serializers.CharField(help_text=_("拨打语音方式"), required=False)
 
     class AggInfoSerializer(serializers.Serializer):
         metric_id = serializers.CharField(help_text=_("metric id"))
