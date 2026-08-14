8851dcc8be936e9da5042c99dc68087680ef0151
huangwentai-wendel <huangwentai96@163.com>
2026-08-14 14:21:55 +0800

    fix(backend): mysql权限测试问题修复 #19595

--- dbm-ui/backend/iam_app/handlers/drf_perm/monitor.py
+++ dbm-ui/backend/iam_app/handlers/drf_perm/monitor.py
@@ -16,11 +16,11 @@ from django.utils.translation import gettext as _
 from backend import env
 from backend.components import BKMonitorV3Api
 from backend.db_meta.models import Cluster
-from backend.db_monitor.models import MonitorPolicy, NoticeGroup
+from backend.db_monitor.models import NoticeGroup
 from backend.db_monitor.utils import parse_shield_description_biz
 from backend.iam_app.dataclass.actions import ActionEnum, ActionMeta
 from backend.iam_app.dataclass.resources import ResourceEnum, ResourceMeta
-from backend.iam_app.exceptions import ActionNotExistError, ResourceNotExistError
+from backend.iam_app.exceptions import ActionNotExistError
 from backend.iam_app.handlers.drf_perm.base import (
     BizOrGlobalResourceActionPermission,
     ResourceActionPermission,
@@ -63,29 +63,18 @@ class NotifyGroupPermission(ResourceActionPermission):
         return True
 
 
-class MonitorPolicyPermission(ResourceActionPermission):
+class GlobalMonitorPolicyPermission(ResourceActionPermission):
     """
     监控策略相关动作鉴权
     """
 
-    def __init__(self, view_action, actions: List[ActionMeta] = None, resource_meta: ResourceMeta = None):
-        self.view_action = view_action
+    def __init__(self, actions: List[ActionMeta] = None, resource_meta: ResourceMeta = None):
         super().__init__(actions=actions, resource_meta=resource_meta, instance_ids_getter=self.instance_ids_getter)
 
     def instance_ids_getter(self, request, view):
         # 获取策略ID后，决定动作和资源类型
         policy_id = view.kwargs.get("pk") or request.data["parent_id"]
-        bk_biz_id = str(MonitorPolicy.objects.get(id=policy_id).bk_biz_id)
-
-        if bk_biz_id is None:
-            raise ResourceNotExistError(_("未找到策略ID，无法决定告警组相关动作鉴权。请保证参数包含策略ID"))
-
-        if not int(bk_biz_id):
-            self.actions = [getattr(ActionEnum, f"GLOBAL_MONITOR_POLICY_{self.view_action.upper()}")]
-            self.resource_meta = ResourceEnum.MONITOR_POLICY
-        else:
-            self.actions = [getattr(ActionEnum, f"MONITOR_POLICY_{self.view_action.upper()}")]
-            self.resource_meta = ResourceEnum.MONITOR_POLICY
+        self.resource_meta = ResourceEnum.MONITOR_POLICY
 
         return [policy_id]
 
