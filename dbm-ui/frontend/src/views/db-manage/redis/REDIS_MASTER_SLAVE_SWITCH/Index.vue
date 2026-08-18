049fd79c514423246b7738f4c0ae0a0b34527ecd
hLinx <327159425@qq.com>
2026-08-18 17:18:21 +0800

    perf( frontend): 重置按钮体验优化 #19791

--- dbm-ui/frontend/src/views/db-manage/redis/REDIS_MASTER_SLAVE_SWITCH/Index.vue
+++ dbm-ui/frontend/src/views/db-manage/redis/REDIS_MASTER_SLAVE_SWITCH/Index.vue
@@ -86,16 +86,10 @@
         @click="handleSubmit">
         {{ t('提交') }}
       </BkButton>
-      <DbPopconfirm
+      <DbResetButton
+        class="ml-8"
         :confirm-handler="handleReset"
-        :content="t('重置将会清空当前填写的所有内容_请谨慎操作')"
-        :title="t('确认重置页面')">
-        <BkButton
-          class="ml-8 w-88"
-          :disabled="isSubmitting">
-          {{ t('重置') }}
-        </BkButton>
-      </DbPopconfirm>
+        :disabled="isSubmitting" />
     </template>
   </SmartAction>
 </template>
