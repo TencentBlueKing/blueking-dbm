cf10aa5de587ba36c77cad406e4547966f7a02bb
xfwduke <xfwduke@gmail.com>
2026-08-31 14:51:22 +0800

    fix(mysql): add-missing-v2-proxy-view #20125

--- dbm-ui/backend/db_proxy/views/db_remote_service/views.py
+++ dbm-ui/backend/db_proxy/views/db_remote_service/views.py
@@ -49,6 +49,17 @@ class DRSApiProxyPassViewSet(BaseProxyPassViewSet):
         return Response(DRSApi.v2_mysql_rpc(params=validated_data))
 
     @common_swagger_auto_schema(
+        operation_summary=_("[db-remote-service]mysql proxy 远程执行 v2"),
+        request_body=RPCSerializer(),
+        responses={status.HTTP_200_OK: RPCResponseSerializer()},
+        tags=[SWAGGER_TAG],
+    )
+    @action(methods=["POST"], detail=False, serializer_class=RPCSerializer, url_path="drs/v2/proxy-admin/rpc")
+    def v2_proxyrpc(self, request):
+        validated_data = self.params_validate(self.get_serializer_class())
+        return Response(DRSApi.v2_proxyrpc(params=validated_data))
+
+    @common_swagger_auto_schema(
         operation_summary=_("[db-remote-service]Proxy SQL远程执行"),
         request_body=RPCSerializer(),
         responses={status.HTTP_200_OK: RPCResponseSerializer()},
