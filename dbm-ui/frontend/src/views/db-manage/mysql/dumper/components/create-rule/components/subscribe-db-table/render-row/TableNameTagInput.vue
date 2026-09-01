e9b9d7fa097e21e65d3bb6f4e715e47fcc828797
hLinx <327159425@qq.com>
2026-09-01 23:09:36 +0800

    perf(frontend): 删除无效的组件引用 #20188 # Reviewed, transaction id: 85312

--- dbm-ui/frontend/src/views/db-manage/mysql/dumper/components/create-rule/components/subscribe-db-table/render-row/TableNameTagInput.vue
+++ dbm-ui/frontend/src/views/db-manage/mysql/dumper/components/create-rule/components/subscribe-db-table/render-row/TableNameTagInput.vue
@@ -7,27 +7,49 @@
  * You may obtain a copy of the License athttps://opensource.org/licenses/MIT
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+    10| * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
  * the specific language governing permissions and limitations under the License.
 -->
 
 <template>
-  <TableTagInput
-    ref="tagRef"
-    :model-value="localValue"
-    :placeholder="t('请输入表名，支持通配符')"
-    :rules="rules"
-    @change="handleTagValueChange">
-    <template #tip>
-      <p>{{ t('%：匹配任意长度字符串，如 a%') }}</p>
-    </template>
-  </TableTagInput>
+  <div
+    ref="rootRef"
+    class="subscribe-tag-input"
+    :class="{ 'is-error': Boolean(errorMessage) }"
+    @click="handleShowTips">
+    <BkTagInput
+      v-model="localValue"
+      allow-auto-match
+      allow-create
+      clearable
+      has-delete-icon
+      :paste-fn="tagInputPasteFn"
+      :placeholder="t('请输入表名，支持通配符')"
+      @change="handleTagValueChange" />
+    <div
+      v-if="errorMessage"
+      class="input-error">
+      <DbIcon
+        v-bk-tooltips="errorMessage"
+        type="exclamation-fill" />
+    </div>
+    <div style="display: none">
+      <div
+        ref="popRef"
+        style="font-size: 12px; line-height: 24px; color: #63656e">
+        <p>{{ t('%：匹配任意长度字符串，如 a%') }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
   import _ from 'lodash';
+  import tippy, { type Instance, type SingleTarget } from 'tippy.js';
   import { useI18n } from 'vue-i18n';
 
-  import TableTagInput from '@components/render-table/columns/tag-input/index.vue';
+  import { type Rules, useValidtor } from '@hooks';
+
+  import { batchSplitRegex } from '@common/regex';
 
   interface Exposes {
     getValue: () => Promise<string[]>;
@@ -35,33 +57,67 @@
 
   const { t } = useI18n();
 
-  const tagRef = ref();
+  const rootRef = ref();
+  const popRef = ref();
   const localValue = ref<string[]>([]);
 
-  const rules = [
+  let tippyIns: Instance | undefined;
+
+  const rules: Rules = [
     {
       message: t('不能为空'),
       validator: (value: string[]) => value && value.length > 0,
     },
-    // {
-    //   validator: (value: string []) => _.every(value, item => !/^%$/.test(item)),
-    //   message: t('% 不允许单独使用'),
-    //   trigger: 'change',
-    // },
     {
       message: t('不合法的输入'),
-      trigger: 'change',
       validator: (value: string[]) => _.some(value, (item) => /^[a-zA-Z0-9_%]+$/.test(item)),
     },
   ];
 
+  const { message: errorMessage, validator } = useValidtor(rules);
+
+  const tagInputPasteFn = (value: string) => value.split(batchSplitRegex).map((item) => ({ id: item }));
+
   const handleTagValueChange = (value: string[]) => {
-    localValue.value = value;
+    nextTick(() => {
+      validator(value).then(() => {
+        window.changeConfirm = true;
+      });
+    });
   };
+
+  const handleShowTips = () => {
+    tippyIns?.show();
+  };
+
+  onMounted(() => {
+    tippyIns = tippy(rootRef.value as SingleTarget, {
+      appendTo: () => document.body,
+      arrow: true,
+      content: popRef.value,
+      hideOnClick: true,
+      interactive: true,
+      maxWidth: 'none',
+      offset: [0, 18],
+      placement: 'top',
+      theme: 'light',
+      trigger: 'manual',
+      zIndex: 9998,
+    });
+  });
+
+  onBeforeUnmount(() => {
+    if (tippyIns) {
+      tippyIns.hide();
+      tippyIns.unmount();
+      tippyIns.destroy();
+      tippyIns = undefined;
+    }
+  });
 
   defineExpose<Exposes>({
     getValue() {
-      return tagRef.value.getValue().then(() => {
+      return validator(localValue.value).then(() => {
         if (!localValue.value) {
           return Promise.reject();
         }
@@ -70,3 +126,27 @@
     },
   });
 </script>
+<style lang="less" scoped>
+  .subscribe-tag-input {
+    position: relative;
+
+    &.is-error {
+      :deep(.bk-tag-input-trigger) {
+        background: #fff0f1;
+      }
+    }
+
+    .input-error {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 100;
+      display: flex;
+      padding-right: 10px;
+      font-size: 14px;
+      color: #ea3636;
+      align-items: center;
+    }
+  }
+</style>
