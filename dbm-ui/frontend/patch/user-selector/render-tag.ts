4cb83f9e8370af5ae8d893a96539eb250faf43e0
hLinx <327159425@qq.com>
2026-09-04 11:45:48 +0800

    perf(frontend): 人员选择器样式问题 #20277

--- dbm-ui/frontend/patch/user-selector/render-tag.ts
+++ dbm-ui/frontend/patch/user-selector/render-tag.ts
@@ -1,38 +1,8 @@
-// import * as Vue from 'vue';
-// export default {
-//   name: 'render-tag',
-//   props: ['username', 'user', 'index'],
-//   render() {
-//     return this.$parent.renderTag(Vue.h, {
-//       username: this.username,
-//       index: this.index,
-//       user: this.user,
-//     });
-//   },
-// };
-
 import { h, inject } from 'vue';
 
 export default {
   name: 'render-tag',
   props: ['username', 'user', 'index'],
-  // props: {
-  //   selector: {
-  //     type: Object,
-  //   },
-  //   user: {
-  //     type: Object,
-  //   },
-  //   keyword: {
-  //     type: String,
-  //   },
-  //   index: {
-  //     type: Number,
-  //   },
-  //   disabled: {
-  //     type: Boolean,
-  //   },
-  // },
   setup(props: any) {
     const parentSelector = inject('parentSelector');
     return () => (parentSelector as any).renderTag(h, {
