a2f2b27a9c78faf516f11404e5ee52b8610f16b4
hLinx <327159425@qq.com>
2026-09-04 11:45:48 +0800

    perf(frontend): 人员选择器样式问题 #20277

--- dbm-ui/frontend/patch/user-selector/render-avatar.tsx
+++ dbm-ui/frontend/patch/user-selector/render-avatar.tsx
@@ -1,26 +1,3 @@
-// export default {
-//   name: 'render-avatar',
-//   props: ['user', 'urlMethod'],
-//   render() {
-//     return <span class="user-selector-avatar"></span>;
-//   },
-//   async created() {
-//     try {
-//       let avatar = null;
-//       if (typeof this.user === 'string') {
-//         avatar = await this.urlMethod(this.user);
-//       } else if (typeof this.user === 'object') {
-//         avatar = this.user.avatar
-//           || this.user.logo
-//           || (await this.urlMethod(this.user.username));
-//       }
-//       if (avatar) {
-//         this.$el.style.backgroundImage = `url(${avatar})`;
-//       }
-//     } catch (e) {}
-//   },
-// };
-
 import { onMounted, ref, watch } from 'vue';
 
 export default {
@@ -36,7 +13,7 @@ export default {
           avatar.value = props.user.avatar || props.user.logo || (await props.urlMethod(props.user.username));
         }
       } catch (e) {
-        console.log(e)
+        console.error(e);
       }
     });
 
