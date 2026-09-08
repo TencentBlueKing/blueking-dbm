33abac9d2791254bcfd4c453d9ac62c269bb9b85
zfrendo <842557664@qq.com>
2026-09-08 16:27:59 +0800

    fix: 修复slave重建复制账号添加问题 #20368

--- dbm-ui/backend/flow/engine/bamboo/scene/mysql/mysql_migrate_cluster_remote_flow.py
+++ dbm-ui/backend/flow/engine/bamboo/scene/mysql/mysql_migrate_cluster_remote_flow.py
@@ -405,7 +405,7 @@ class MySQLMigrateClusterRemoteFlow(object):
                         act_name=MySQLCheckSumTicketResultComponent.node_name,
                         act_component_code=MySQLCheckSumTicketResultComponent.code,
                         kwargs={
-                            "bk_cloud_id": cluster_class.bk_cloud_id,
+                            "bk_cloud_id": cluster_model.bk_cloud_id,
                             "checksum_pairs": [
                                 {
                                     "master": master_model.ip_port,
@@ -471,7 +471,7 @@ class MySQLMigrateClusterRemoteFlow(object):
                     act_component_code=MysqlCrondMonitorControlComponent.code,
                     kwargs=asdict(
                         CrondMonitorKwargs(
-                            bk_cloud_id=cluster_class.bk_cloud_id,
+                            bk_cloud_id=cluster_model.bk_cloud_id,
                             exec_ips=[self.data["master_ip"], self.data["old_slave_ip"]],
                             name="dbbackup",
                             port=master_model.port,
