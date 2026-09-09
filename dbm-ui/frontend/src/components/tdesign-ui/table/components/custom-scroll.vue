cc34822a87094bbd8daaafb5f5c6d56e5ccb086b
hLinx <327159425@qq.com>
2026-09-09 15:12:43 +0800

    feat(frontend): 子路径部署 #19922

--- dbm-ui/frontend/src/components/tdesign-ui/table/components/custom-scroll.vue
+++ dbm-ui/frontend/src/components/tdesign-ui/table/components/custom-scroll.vue
@@ -31,7 +31,7 @@
   </Teleport>
 </template>
 <script setup lang="ts">
-  import { throttle } from 'lodash-es';
+  import { throttle } from 'lodash';
   import { getCurrentInstance, onBeforeUnmount, onMounted, ref } from 'vue';
 
   const tableEl = ref<HTMLElement>();
