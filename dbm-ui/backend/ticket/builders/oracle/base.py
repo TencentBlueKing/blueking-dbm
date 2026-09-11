0ef75853b5526f7af3b84b1db7b96f40a8df36a4
huangwentai-wendel <huangwentai96@163.com>
2026-09-11 15:07:07 +0800

    feat(backend): oracle添加从库/整机替换 #20390

--- dbm-ui/backend/ticket/builders/oracle/base.py
+++ dbm-ui/backend/ticket/builders/oracle/base.py
@@ -8,12 +8,44 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from django.utils.translation import gettext as _
+from rest_framework import serializers
+
 from backend.configuration.constants import DBType
 from backend.db_meta.enums import ClusterType
 from backend.ticket.builders import TicketFlowBuilder
-from backend.ticket.builders.common.base import BaseTicketFlowBuilderPatchMixin
+from backend.ticket.builders.common.base import (
+    BaseOperateResourceParamBuilder,
+    BaseTicketFlowBuilderPatchMixin,
+    ParamValidateSerializerMixin,
+    SkipToRepresentationMixin,
+    TicketBaseValidateSerializerMixin,
+)
 
 
 class BaseOracleTicketFlowBuilder(BaseTicketFlowBuilderPatchMixin, TicketFlowBuilder):
     group = DBType.Oracle.value
     cluster_types = [ClusterType.OraclePrimaryStandby.value]
+
+
+class OracleOpsBaseDetailSerializer(
+    TicketBaseValidateSerializerMixin, SkipToRepresentationMixin, ParamValidateSerializerMixin, serializers.Serializer
+):
+    # TODO: rules内部校验
+    rules = serializers.JSONField(help_text=_("提取/删除/备份规则列表"))
+
+    def validate(self, attrs):
+        """
+        公共校验：集群操作互斥校验
+        """
+        attrs = super().validate(attrs)
+        attrs = super().validated_params(attrs=attrs)
+        return attrs
+
+
+class OracleOperateResourceParamBuilder(BaseOperateResourceParamBuilder):
+    def format(self):
+        super().format()
+
+    def post_callback(self):
+        super().post_callback()
