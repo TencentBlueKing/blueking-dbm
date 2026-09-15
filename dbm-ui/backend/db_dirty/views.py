ee48968043ff8c4fa112d2ebe48997d46ff84924
huangwentai-wendel <huangwentai96@163.com>
2026-09-15 17:50:10 +0800

    fix(backend): 修复主机代办弹出权限问题 #20548

--- dbm-ui/backend/db_dirty/views.py
+++ dbm-ui/backend/db_dirty/views.py
@@ -43,12 +43,18 @@ class DBDirtyMachineViewSet(viewsets.SystemViewSet):
         (
             "list_machine_events",
             "get_host_current_events",
-            "query_machine_pool",
             "check_host_is_dissolved",
         ): [ResourceActionPermission([ActionEnum.RESOURCE_MANAGE])],
         ("transfer_hosts_to_pool",): [ResourceActionPermission([ActionEnum.RESOURCE_POLL_MANAGE])],
     }
     default_permission_class = [ResourceActionPermission([ActionEnum.RESOURCE_POLL_MANAGE])]
+
+    def _get_custom_permissions(self):
+        if self.action in ["query_machine_pool"]:
+            if self.request.query_params.get("is_todo"):
+                return []
+            else:
+                return [ResourceActionPermission([ActionEnum.RESOURCE_MANAGE])]
 
     @common_swagger_auto_schema(
         operation_summary=_("将主机转移至待回收/故障池模块"),
