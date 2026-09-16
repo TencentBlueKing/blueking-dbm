1dc3da2414f562a0416738e16d9a1d2a990e9361
hLinx <327159425@qq.com>
2026-09-16 18:22:09 +0800

    fix(frontend): 分片选择器 #20587

--- dbm-ui/frontend/src/components/machine-resource-selector/components/RenderTable.vue
+++ dbm-ui/frontend/src/components/machine-resource-selector/components/RenderTable.vue
@@ -15,14 +15,14 @@
   <div class="machine-resource-selector-render-table">
     <DbQuickSearch
       v-model="searchSelectValue"
-      class="mb-12"
+      class="mt-16 mb-16"
       :data="searchSelectData"
       parse-url />
     <DbTable
       ref="table"
+      :container-height="containerHeight"
       :data-source="dataSource"
-      fixed-pagination
-      :height="540"
+      row-click-selectable
       row-key="ip"
       selectable
       :selected="selected"
@@ -120,6 +120,8 @@
     },
   ];
 
+  const containerHeight = 570 - 32 - 16; // 去除搜索框的高度和margin bottom
+
   const searchSelectValue = ref<Record<string, string>>({});
   const dbTableRef = useTemplateRef('table');
 
@@ -146,7 +148,8 @@
 
 <style lang="less">
   .machine-resource-selector-render-table {
-    padding: 12px 24px;
+    height: 570px;
+    padding: 0 24px;
 
     .t-table__body {
       tr {
