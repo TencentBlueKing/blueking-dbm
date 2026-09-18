96f998a94334755a8f7698b4140e7b27f8f39cd6
lyjpy <jay0613@163.com>
2026-09-18 19:39:05 +0800

    feat(backend): 变更 SQL 执行展示默认存储引擎 #20683

--- dbm-ui/backend/db_services/mysql/resources/tendbsingle/query.py
+++ dbm-ui/backend/db_services/mysql/resources/tendbsingle/query.py
@@ -17,9 +17,9 @@ from backend.db_meta.enums import InstanceInnerRole, InstanceRole
 from backend.db_meta.enums.cluster_type import ClusterType
 from backend.db_meta.models import AppCache
 from backend.db_meta.models.cluster import Cluster
-from backend.db_services.dbbase.resources import query
 from backend.db_services.dbbase.resources.query import CommonExportQueryResourceMixin
 from backend.db_services.dbbase.resources.register import register_resource_decorator
+from backend.db_services.mysql.resources.query import MysqlListRetrieveResource
 
 
 class TenDBSingleExportQueryResourceMixin(CommonExportQueryResourceMixin):
@@ -45,7 +45,7 @@ class TenDBSingleExportQueryResourceMixin(CommonExportQueryResourceMixin):
 
 
 @register_resource_decorator()
-class ListRetrieveResource(query.ListRetrieveResource, TenDBSingleExportQueryResourceMixin):
+class ListRetrieveResource(MysqlListRetrieveResource, TenDBSingleExportQueryResourceMixin):
     """查看 mysql 单点部署的资源"""
 
     cluster_types = [ClusterType.TenDBSingle]
